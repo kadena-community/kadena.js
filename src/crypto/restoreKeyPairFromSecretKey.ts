7d1cac7705a9ab0383d0c98726e0f137b62b1053
ggobugi27 <ggobugi27@github.com>
2022-05-10 02:47:06 +0900

    rename, remove unnecessary codes

--- src/crypto/restoreKeyPairFromSecretKey.ts
+++ src/crypto/restoreKeyPairFromSecretKey.ts
@@ -7,12 +7,12 @@ import { KeyPair } from '../util';
  * Generate a deterministic ED25519 keypair from a given Kadena secretKey
  */
 export default function restoreKeyPairFromSecretKey(seed: string): KeyPair {
-  if (!seed) throw new Error(`seed for KeyPair generation not provided`);
-  if (seed.length !== 64)
+  if (seed.length !== 64) {
     throw new Error('Seed for KeyPair generation has bad size');
-  var seedForNacl = hexToBin(seed);
-  var kp = nacl.sign.keyPair.fromSeed(seedForNacl);
-  var pubKey = binToHex(kp.publicKey);
-  var secKey = binToHex(kp.secretKey).slice(0, 64);
-  return { publicKey: pubKey, secretKey: secKey };
+  }
+  const seedForNacl = hexToBin(seed);
+  const keyPair = nacl.sign.keyPair.fromSeed(seedForNacl);
+  const publicKey = binToHex(keyPair.publicKey);
+  const secretKey = binToHex(keyPair.secretKey).slice(0, 64);
+  return { publicKey, secretKey };
 }
