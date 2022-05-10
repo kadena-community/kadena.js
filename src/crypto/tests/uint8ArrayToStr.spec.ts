684c54eb22bfb4679c357aed4bbb45e581db562a
ggobugi27 <ggobugi27@github.com>
2022-05-11 04:28:12 +0900

    add functions for base64 encoding and decoding

---
+++ src/crypto/tests/uint8ArrayToStr.spec.ts
@@ -0,0 +1,12 @@
+import uint8ArrayToStr from '../uint8ArrayToStr';
+
+test('should convert Uint8 array to a string', () => {
+  const arr = new Uint8Array([
+    205, 170, 167, 69, 13, 17, 99, 60, 83, 113, 200, 237, 98, 128, 111, 66, 192,
+    232, 228, 175, 102, 198, 190, 19, 16, 95, 135, 33, 132, 226, 228, 154,
+  ]);
+  const actual = uint8ArrayToStr(arr);
+  const expected = '';
+
+  // expect(expected).toEqual(actual);
+});
