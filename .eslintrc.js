279ae241ee41b96b2392d727b38133d9898297f1
Randynamic <randy@randynamic.com>
2022-05-16 11:44:56 -0400

    reverted the json eslint rename needed the path

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+
+const config = {
+  root: true,
+  extends: 'airbnb-typescript/base',
+  plugins: ['import', 'prettier', 'jest'],
+  parserOptions: {
+    project: resolve(__dirname, './tsconfig.eslint.json'),
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  env: {
+    'jest/globals': true,
+  },
+};
+
+module.exports = config;
