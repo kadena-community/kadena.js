621c1056bd65dce87ead391c16e3b08163d0e271
ggobugi27 <ggobugi27@github.com>
2022-05-16 11:15:54 -0400

    Add str <-> uint8Arr conversion tests

--- src/crypto/tests/uint8ArrayToStr.spec.ts
+++ src/crypto/tests/uint8ArrayToStr.spec.ts
@@ -5,8 +5,15 @@ test('should convert Uint8 array to a string', () => {
     205, 170, 167, 69, 13, 17, 99, 60, 83, 113, 200, 237, 98, 128, 111, 66, 192,
     232, 228, 175, 102, 198, 190, 19, 16, 95, 135, 33, 132, 226, 228, 154,
   ]);
-  const actual = uint8ArrayToStr(arr);
-  const expected = '';
 
-  // expect(expected).toEqual(actual);
+  const actual = uint8ArrayToStr(arr);
+  const expected = uint8ArrayToStr(
+    new Uint8Array([
+      205, 170, 167, 69, 13, 17, 99, 60, 83, 113, 200, 237, 98, 128, 111, 66,
+      192, 232, 228, 175, 102, 198, 190, 19, 16, 95, 135, 33, 132, 226, 228,
+      154,
+    ]),
+  );
+
+  expect(expected).toEqual(actual);
 });
