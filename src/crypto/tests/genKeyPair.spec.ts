a4357af6f4572bab020e2ba157c49feb329e73ba
Randynamic <randy@randynamic.com>
2022-05-16 11:17:31 -0400

    Fixed the mocking of tweetnacl

--- src/crypto/tests/genKeyPair.spec.ts
+++ src/crypto/tests/genKeyPair.spec.ts
@@ -1,17 +1,21 @@
 import genKeyPair from '../genKeyPair';
-//
-// jest.mock('tweetnacl');
-// import tweetnacl from 'tweetnacl';
-//
-// tweetnacl.sign.keyPair.mockImplementation(() => {
-//   return {
-//     publicKey: 'test',
-//   };
-// });
+
+jest.mock('tweetnacl', () => {
+  return {
+    sign: {
+      keyPair: () => {
+        return {
+          publicKey: '1234567abcdefg',
+          secretKey: '1234567abcdefg',
+        };
+      },
+    },
+  };
+});
 
 test('generates a new keyPair', () => {
-  // const actual = genKeyPair();
+  const actual = genKeyPair();
 
-  // expect(actual.publicKey).toEqual('12341');
-  expect('actual.secretKey').toEqual('1233131');
+  expect(actual.publicKey).toEqual('3132333435363761626364656667');
+  expect(actual.secretKey).toEqual('3132333435363761626364656667');
 });
