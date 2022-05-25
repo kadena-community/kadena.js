207879677dade23f63213e81c2d7dbb47a66bb59
Linda Ortega <linda.ortega2013@gmail.com>
2022-05-25 14:58:30 -0400

    fix lint error

--- src/SigData/tests/mkCap.spec.ts
+++ src/SigData/tests/mkCap.spec.ts
@@ -8,8 +8,8 @@ test('should create a baseline cap', () => {
 });
 
 test('should create a TRANSFER cap with arguments', () => {
-  const actual = mkCap('coin.TRANSFER', ["fromAcctName", "toAcctName", 0.1]);
-  const expected = { name: 'coin.TRANSFER', args: ["fromAcctName", "toAcctName", 0.1] };
+  const actual = mkCap('coin.TRANSFER', ['fromAcctName', 'toAcctName', 0.1]);
+  const expected = { name: 'coin.TRANSFER', args: ['fromAcctName', 'toAcctName', 0.1] };
 
   expect(expected).toEqual(actual);
 });
