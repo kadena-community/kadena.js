c6712b0ad459f54f485d57465e96ec47ef9b02a5
Linda Ortega <linda.ortega2013@gmail.com>
2022-05-26 11:05:29 -0400

    expand util Cap def to use new PactValue sumtype, refactor mkCap to use util def

--- src/SigData/mkCap.ts
+++ src/SigData/mkCap.ts
@@ -1,24 +1,14 @@
-/**
- * @typedef {object} Cap - A capability
- * @property {string} name of pact capability to be signed
- * @property {array} args - array of arguments used in pact capability, default to empty array.
- */
-type Cap = {
-  name: string;
-  args: Array<any>;
-};
+import { PactValue } from '../util/PactValue';
+import { Cap } from '../util/PactCommand';
 
 /**
- * TODO: define the set of all possible `args` that are allowed, I believe it's just string/number/object but we need to verify that objects are allowed as args.
- * /
-
-/**
- * Prepares a capability object for use in mkSignerCList.
- * @param {string} name of pact capability to be signed
- * @param {array} args - array of arguments used in pact capability, default to empty array.
- * @returns {Cap} A properly formatted cap object required in SigBuilder
+ * Helper function for creating a pact capability object.
+ * Output can be used with the `mkSignerCList` function.
+ * @param {string} name - Qualified name of the capability.
+ * @param {array} args - Array of PactValue arguments the capability expects (default: empty array).
+ * @returns {Cap} - A properly formatted Cap object required in SigBuilder.
  */
-export default function mkCap(name: string, args: Array<any> = []): Cap {
+export default function mkCap(name: string, args: Array<PactValue> = []): Cap {
   return {
     name,
     args,
---
+++ src/util/PactValue.ts
@@ -0,0 +1,27 @@
+/**
+ * @typedef {sumtype} PactLiteral - A sum type representing a `pact` literal value.
+ * Should have parity with the JSON serialization of the Haskell type `Literal` as defined in `pact`:
+ * https://github.com/kadena-io/pact/blob/master/src/Pact/Types/Exp.hs#L95
+ * @property {string}
+ * @property {number} - JavaScript integer and decimal values.
+ *                      Max `number` value is 9007199254740991.
+ *                      Min `number` value is -9007199254740991.
+ * @property {boolean}
+ * TODO: Should `isSafeInteger` be used?
+ * TODO: add `UTCTime` literal.
+ * TODO: add object representation of Integer.
+ * TODO: add object representation of Decimal.
+ */
+ export type PactLiteral = string | number | boolean;
+
+ /**
+  * @typedef {sumtype} PactValue - A sum type representing a `pact` value.
+  * Should have parity with the JSON serialization of the Haskell type `PactValue` as defined in `pact`:
+  * https://github.com/kadena-io/pact/blob/master/src/Pact/Types/PactValue.hs#L109
+  * @property {PactLiteral}
+  * @property {Array<PactValue>} - Array of pact values (recursive type)
+  * TODO: add object map of pact values type.
+  * TODO: add guard type of pact values type.
+  * TODO: add module reference type type.
+  */
+ export type PactValue = PactLiteral | Array<PactValue>;
