b3e07222559d41b60c3aee603b7bb643127d4ecc
Linda Ortega <linda.ortega2013@gmail.com>
2022-06-06 22:14:21 -0400

    clean up sign functions, add docs for pact api types

--- src/crypto/base64UrlEncodeArr.ts
+++ src/crypto/base64UrlEncodeArr.ts
@@ -1,10 +1,11 @@
 import base64UrlEncode from './base64UrlEncode';
+import { Base64Url } from '../util/Base64Url';
 import uint8ArrayToStr from './uint8ArrayToStr';
 
 /**
  * Takes in Uint8Array binary object and outputs hex string.
  */
 
-export default function base64UrlEncodeArr(input: Uint8Array): string {
+export default function base64UrlEncodeArr(input: Uint8Array): Base64Url {
   return base64UrlEncode(uint8ArrayToStr(input));
 }
---
+++ src/fetch/stringifyAndMakePOSTRequest.ts
@@ -0,0 +1,16 @@
+/**
+ * Formats API request body to use with `fetch` function.
+ *
+ * Corresponds to `mkReq` function:
+ * https://github.com/kadena-io/pact-lang-api/blob/master/pact-lang-api.js#L533
+ *
+ */
+export function stringifyAndMakePOSTRequest<T>(body: T): RequestInit {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(body),
+    };
+  };
