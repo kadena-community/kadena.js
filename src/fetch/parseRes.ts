982914a165fb3b2b11fb7255a3a4ce0fa5c3d4ba
Linda Ortega <linda.ortega2013@gmail.com>
2022-06-06 22:19:13 -0400

    lint fixes

--- src/fetch/parseRes.ts
+++ src/fetch/parseRes.ts
@@ -1,7 +1,7 @@
 type JSONResponse<T> = {
   data?: T,
-  errors?: Array<{message: string}>
-}
+  errors?: Array<{ message: string }>
+};
 
 /**
  * Parses raw `fetch` response into a typed JSON value.
@@ -12,20 +12,20 @@ type JSONResponse<T> = {
  */
 export async function parseRes<T>(raw: Promise<Response>):Promise<T> {
   const res = await raw;
-  const {data, errors}: JSONResponse<T> = await res.json()
+  const { data, errors }: JSONResponse<T> = await res.json();
   if (res.ok) {
     if (typeof data !== 'undefined') {
       return data;
     } else {
       return Promise.reject(new Error('Response omitted expected `data` field.'));
-    };
+    }
   } else {
     // Concatenate and emit API errors
     if (typeof errors !== 'undefined') {
-        const errorMsg = errors.map(e => e.message).join('\n');
-        return Promise.reject(new Error (errorMsg));
+      const errorMsg = errors.map(e => e.message).join('\n');
+      return Promise.reject(new Error(errorMsg));
     } else {
-        return Promise.reject(new Error('Response omitted expected `errors` field.'));
-    };
-  };
-};
+      return Promise.reject(new Error('Response omitted expected `errors` field.'));
+    }
+  }
+}
