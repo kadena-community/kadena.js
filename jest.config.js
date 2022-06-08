87700827deb74cce49857ddab88778dd7e1fbf55
Randynamic <randy@randynamic.com>
2022-06-08 11:47:12 +0200

    no message

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['package.json'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
