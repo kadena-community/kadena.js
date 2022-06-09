ee5e3b71fdb3567741535d499d0eee47ab420b84
Linda Ortega <linda.ortega2013@gmail.com>
2022-06-09 11:02:52 -0400

    Fix what Chainweb API actually returns as a Response

--- src/fetch/parseResponse.ts
+++ src/fetch/parseResponse.ts
@@ -1,8 +1,3 @@
-type JSONResponse<T> = {
-  data?: T,
-  errors?: Array<{ message: string }>
-};
-
 /**
  * Parses raw `fetch` response into a typed JSON value.
  *
@@ -11,21 +6,13 @@ type JSONResponse<T> = {
  *
  */
 export async function parseResponse<T>(raw: Promise<Response>):Promise<T> {
-  const response = await raw;
-  const jsonResponse: JSONResponse<T> = await response.json();
-  if (response.ok) {
-    if (typeof jsonResponse.data !== 'undefined') {
-      return jsonResponse.data;
-    } else {
-      return Promise.reject(new Error('Response omitted expected `data` field.'));
-    }
-  } else {
-    // Concatenate and emit API errors
-    if (typeof jsonResponse.errors !== 'undefined') {
-      const errorMsg:string = jsonResponse.errors.map(e => e.message).join('\n');
-      return Promise.reject(new Error(errorMsg));
-    } else {
-      return Promise.reject(new Error('Response omitted expected `errors` field.'));
-    }
-  }
+  const response:Response = await raw;
+  if (response.ok){
+     const JSONResponse:T = await response.json();
+     return JSONResponse;
+   } else {
+     // Handle API errors
+     const TEXTResponse:string = await response.text();
+     return Promise.reject(new Error(TEXTResponse));
+   }
 }
