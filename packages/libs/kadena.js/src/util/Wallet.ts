aab71a2004c6177ef5f5d47f8d879320614dc48d
ggobugi27 <ggobugi27@github.com>
2022-06-25 03:12:13 +0900

    add description of util/SigningCap

--- packages/libs/kadena.js/src/util/Wallet.ts
+++ packages/libs/kadena.js/src/util/Wallet.ts
@@ -1,5 +1,11 @@
 import { Cap } from './PactCommand';
 
+/**
+ * Pact capability object with role and description to be consumed in Signing API
+ * @param role - role of the capability.
+ * @param description - description of the capability.
+ * @param Cap - name and arguments of the capability
+ */
 export interface SigningCap {
   role: string;
   description: string;
