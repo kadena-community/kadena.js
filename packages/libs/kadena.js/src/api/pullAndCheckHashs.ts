f61d0c07db033fe6bcc77b2abda9990eb38f411c
Linda Ortega <linda.ortega2013@gmail.com>
2022-07-08 13:24:56 -0400

    PR Review: smaller cleanup

--- packages/libs/kadena.js/src/api/pullAndCheckHashs.ts
+++ packages/libs/kadena.js/src/api/pullAndCheckHashs.ts
@@ -4,7 +4,6 @@ export function pullAndCheckHashs(sigs: Array<SignatureWithHash>): string {
   const { hash } = sigs[0];
   sigs.forEach((sig) => {
     if (sig.hash !== hash) {
-      // eslint-disable-next-line no-console
       throw new Error(
         'Sigs for different hashes found: ' + JSON.stringify(sigs),
       );
