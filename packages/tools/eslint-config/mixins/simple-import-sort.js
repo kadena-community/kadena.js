5e909b674f1995916c7671cf35438b6f990cdc27
Albert G <516972+alber70g@users.noreply.github.com>
2022-07-13 16:11:07 +0200

    chore(monorepo): remove redundant import group rules

--- packages/tools/eslint-config/mixins/simple-import-sort.js
+++ packages/tools/eslint-config/mixins/simple-import-sort.js
@@ -12,16 +12,12 @@ module.exports = {
             groups: [
               // Side effect imports.
               ['^\\u0000'],
-              // Packages. `react` related packages come first.
-              ['^(react|styled-components)', '^@?\\w'],
               // Internal packages.
               ['^@kadena(/.*|$)'],
               // Parent imports. Put `..` last.
               ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
               // Other relative imports. Put same-folder imports and `.` last.
               ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-              // Style imports.
-              ['^.+\\.s?css$'],
             ],
           },
         ],
