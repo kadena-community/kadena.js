67de9a1291c97322354b98f61d4acfcf73450e9b
kimjohn <john.kim@architech.nyc>
2022-08-23 16:28:52 +0300

    update for rush

---
+++ packages/apps/explorer/components/common/Hint/Hint.tsx
@@ -0,0 +1,47 @@
+import React, { FC, memo, useEffect, useState } from 'react';
+import ReactTooltipLib from 'react-tooltip';
+import { tooltipTexts } from './Tooltip/tooltipTexts';
+import s from './Hint.module.css';
+
+interface IProps {
+  messageKey: string;
+  id: string;
+}
+
+const ReactTooltip: any = ReactTooltipLib;
+
+const Hint: FC<IProps> = ({ messageKey, id }) => {
+  const {
+    [messageKey]: { head, text },
+  } = tooltipTexts;
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <div className={s.hintContainer}>
+      <span data-tip data-for={id}>
+        ?
+      </span>
+      {isMounted && (
+        <ReactTooltip
+          id={id}
+          place="top"
+          effect="solid"
+          backgroundColor="#fff"
+          textColor="#000"
+          className={s.tooltip}>
+          <div className={s.tooltipContainer}>
+            <p className={s.tooltipHead}>{head}</p>
+            <span className={s.tooltipText}>{text}</span>
+          </div>
+        </ReactTooltip>
+      )}
+    </div>
+  );
+};
+
+export default memo(Hint);
---
+++ packages/apps/explorer/pages/testnet/chain/[chainId]/block/[hash]/txs.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import TransactionBlock from '../../../../../../components/common/Transaction/TransactionBlock';
+import Layout from '../../../../../../components/common/Layout/Layout';
+import s from '../../../../../../components/common/Transaction/TransactionDetails.module.css';
+import { useBlockData } from '../../../../../../services/block';
+import HistoryPage from '../../../../../../components/common/Transaction/components/HistoryPage/HistoryPage';
+import { Loader } from '../../../../../../components/common/Loader/Loader';
+import { NetworkName } from '../../../../../../utils/api';
+import { hasWindow } from '../../../../../../utils/window';
+
+const TestBlockDetailsComponent = () => {
+  const { headerData, parentLink, payloadData, blockInfo, error } =
+    useBlockData(NetworkName.TEST_NETWORK);
+
+  useEffect(() => {
+    if (blockInfo && hasWindow) {
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  }, [blockInfo]);
+
+  return (
+    <Layout>
+      <div className={s.transactionDetails}>
+        <p className={s.transactionHeader}>Block Details</p>
+        {blockInfo ? (
+          <div
+            className={`${s.transactionBlocks} ${s.transactionBlockDetails}`}>
+            <div className={s.blocks}>
+              <TransactionBlock
+                blockLink={parentLink}
+                title="Block Header"
+                data={headerData}
+              />
+            </div>
+            <HistoryPage data={payloadData} block="block" />
+          </div>
+        ) : error ? (
+          <div className={`${s.transactionBlocks} ${s.error}`}>
+            Requested Object not found
+          </div>
+        ) : (
+          <Loader size={64} />
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+const TestBlockDetailsDynamic: any = dynamic<any>(
+  // @ts-ignore
+  () => Promise.resolve(TestBlockDetailsComponent),
+  {
+    ssr: false,
+  },
+);
+
+const TestBlockDetails: NextPage = () => {
+  return <TestBlockDetailsDynamic />;
+};
+
+export default TestBlockDetails;
