fd6e3882bb2a0ff443dd1db805deb1b3c02b9cc2
Albert G <516972+alber70g@users.noreply.github.com>
2022-10-21 12:38:31 +0200

    fix(github): add ci.yml similar to rushstack

--- packages/libs/bootstrap-lib/src/index.ts
+++ packages/libs/bootstrap-lib/src/index.ts
@@ -3,7 +3,7 @@ import { fetch, Response } from 'cross-fetch';
 /**
  * @internal
  */
-export const x = () => (): Promise<Response> =>
+export const x = (): (() => Promise<Response>) => (): Promise<Response> =>
   fetch('http://somethod.com', {
     method: 'POST',
     body: JSON.stringify({ test: 'hahaha' }),
