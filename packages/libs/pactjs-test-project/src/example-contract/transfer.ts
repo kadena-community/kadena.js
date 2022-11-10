884cb3a8224cfc93ed20266cce77439c7d71daf2
Albert G <516972+alber70g@users.noreply.github.com>
2022-11-10 15:34:00 +0100

    feat(pactjs-test-project): update example to reflect new @kadena/client interface

--- packages/libs/pactjs-test-project/src/example-contract/transfer.ts
+++ packages/libs/pactjs-test-project/src/example-contract/transfer.ts
@@ -1,46 +1,98 @@
-import {
-  IUnsignedTransaction,
-  Pact,
-  signAndSubmitWithChainweaver,
-  signWithChainweaver,
-} from '@kadena/client';
+import { ISendResponse, poll } from '@kadena/chainweb-node-client';
+import { Pact, signWithChainweaver } from '@kadena/client';
 
-// send some kda
-const senderAccount: string = 'your-account';
-const receiverAccount: string =
-  'k:554754f48b16df24b552f6832dda090642ed9658559fef9f3ee1bb4637ea7c94';
-const senderPubkey: string =
-  '554754f48b16df24b552f6832dda090642ed9658559fef9f3ee1bb4637ea7c94';
-const amount: number = 1000.0;
-const data: { ks: { keys: string[]; pred: string } } = {
-  ks: {
-    keys: [senderPubkey],
-    pred: 'keys-all',
-  },
+const apiHost = (
+  chainId: string,
+  network: string = '',
+  networkId: string = 'mainnet01',
+  apiVersion: string = '0.0',
+): string => {
+  return `https://api.${network}chainweb.com/chainweb/${apiVersion}/${networkId}/chain/${chainId}/pact`;
 };
 
-const unsignedTransaction = Pact.modules.coin['transfer-create'](
-  senderAccount,
-  receiverAccount,
-  () => "(read-keyset 'ks)",
-  amount,
-)
-  .addData(data)
-  .addCap('coin.GAS', senderPubkey)
-  .addCap('coin.TRANSFER', senderPubkey, senderAccount, receiverAccount, amount)
-  .setMeta(
+const testnetChain1ApiHost: string = apiHost('1', 'testnet.', 'testnet04');
+
+async function transactionMain(): Promise<void> {
+  const senderAccount: string =
+    'k:554754f48b16df24b552f6832dda090642ed9658559fef9f3ee1bb4637ea7c94';
+  const receiverAccount: string =
+    'k:e34b62cb48526f89e419dae4e918996d66582b5951361c98ee387665a94b7ad8';
+  const onlyKey = (s: string): string => s.split(':')[1];
+
+  const amount: number = 0.1337;
+
+  const unsignedTransaction = Pact.modules.coin
+    .transfer(
+      senderAccount,
+      receiverAccount,
+      // () => '(read-keyset "ks")',
+      amount,
+    )
+    // .addData({
+    //   ks: {
+    //     keys: [onlyKey(receiverAccount)],
+    //     pred: 'keys-all',
+    //   },
+    // })
+    .addCap(
+      'coin.TRANSFER',
+      onlyKey(senderAccount),
+      senderAccount,
+      receiverAccount,
+      amount,
+    )
+    .addCap('coin.GAS', onlyKey(senderAccount))
+    .setMeta({ sender: senderAccount }, 'testnet04');
+
+  const res = await signWithChainweaver(unsignedTransaction);
+
+  console.log('signed transactions', JSON.stringify(res, null, 2));
+
+  const sendRequests = res.map((t) => {
+    console.log('sending transaction', t.code);
+    return t.send(testnetChain1ApiHost);
+  });
+
+  const sendResponses = await Promise.all(sendRequests);
+  sendResponses.map(async function startPolling(
+    sendResponse: ISendResponse,
+  ): Promise<void> {
+    console.log('sendResponse', sendResponse);
+    const requestKey = (await sendRequests[0]).requestKeys[0];
+    await pollMain(requestKey);
+  });
+}
+
+async function pollMain(...requestKeys: string[]): Promise<void> {
+  if (requestKeys.length === 0) {
+    console.log('function called without arguments');
+    return;
+  }
+
+  console.log(`polling for ${requestKeys.join(',')}`);
+  const pollResponse = await poll(
     {
-      sender: senderPubkey,
+      requestKeys,
     },
-    'testnet04',
-  )
-  .createTransaction();
+    testnetChain1ApiHost,
+  );
 
-// write numbers as decimal
-// console.log(JSON.stringify(unsignedTransaction));
+  if (Object.keys(pollResponse).length === 0) {
+    console.log('no transaction found yet');
+    setTimeout(() => pollMain(...requestKeys), 5000);
+  } else {
+    console.log('found transaction');
+    const foundRequestKeys: string[] = [];
+    Object.keys(pollResponse).forEach((requestKey) => {
+      console.log(`${requestKey} poll response`, pollResponse[requestKey]);
+      foundRequestKeys.push(requestKey);
+    });
 
-signWithChainweaver(unsignedTransaction)
-  .then((res: any) =>
-    console.log('signed transaction: \n  ', JSON.stringify(res, null, 2)),
-  )
-  .catch(console.error);
+    await pollMain(
+      ...requestKeys.filter((k) => !foundRequestKeys.some((fk) => fk === k)),
+    );
+  }
+}
+
+transactionMain().catch(console.error);
+// pollMain('').catch(console.error);
