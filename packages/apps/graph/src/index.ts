dc5735d05630a4b00a336258dd4c80d6e8f066e3
Albert G <516972+alber70g@users.noreply.github.com>
2022-12-09 16:21:44 +0100

    chore(graph): fix linting issues

--- packages/apps/graph/src/index.ts
+++ packages/apps/graph/src/index.ts
@@ -2,24 +2,23 @@ import 'json-bigint-patch';
 
 import { getBlocks } from './lastBlock/Blocks';
 
-import { blocks, PrismaClient } from '@prisma/client';
+import { blocks } from '@prisma/client';
 import {
   BigIntTypeDefinition,
   DateTypeDefinition,
   PositiveFloatTypeDefinition,
 } from 'graphql-scalars';
-import { createPubSub, createSchema, createYoga } from 'graphql-yoga';
+import { createPubSub, createSchema, createYoga, PubSub } from 'graphql-yoga';
 import { createServer } from 'node:http';
 
-const pubsub = createPubSub<{ NEW_BLOCKS: [NEW_BLOCKS: blocks[]] }>();
+const pubsub: PubSub<{ NEW_BLOCKS: [NEW_BLOCKS: blocks[]] }> = createPubSub<{
+  NEW_BLOCKS: [NEW_BLOCKS: blocks[]];
+}>();
 const blocksProvider: ReturnType<typeof getBlocks> = getBlocks(pubsub);
 blocksProvider.start();
 
-// Provide your schema
+// eslint-disable-next-line @rushstack/typedef-var
 const yoga = createYoga({
-  context: () => ({
-    prisma: new PrismaClient(),
-  }),
   schema: createSchema({
     typeDefs: [
       BigIntTypeDefinition,
@@ -53,17 +52,6 @@ const yoga = createYoga({
       `,
     ],
     resolvers: {
-      Query: {
-        lastBlockHeight: async (_parent, _args, ctx, _info) => {
-          const prisma = (ctx as any).prisma as PrismaClient;
-          const lastBlock = await prisma.blocks.findFirst({
-            orderBy: {
-              height: 'desc',
-            },
-          });
-          return lastBlock?.height.toString();
-        },
-      },
       Block: {
         chainid: (parent) => BigInt(parent.chainid),
         height: (parent) => BigInt(parent.height),
@@ -78,6 +66,7 @@ const yoga = createYoga({
   }),
 });
 
+// eslint-disable-next-line @rushstack/typedef-var
 const server = createServer(yoga);
 server.listen(4000, () => {
   console.info('Server is running on http://localhost:4000/graphql');
