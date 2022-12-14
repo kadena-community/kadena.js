9d619fee2d8e24f978453900318e54114fe063e0
Albert G <516972+alber70g@users.noreply.github.com>
2022-12-14 17:34:17 +0100

    test(pactjs-generator): adds test case for namespace with double quotes

--- packages/libs/pactjs-generator/src/contract/parsing/tests/createAst.test.ts
+++ packages/libs/pactjs-generator/src/contract/parsing/tests/createAst.test.ts
@@ -61,6 +61,17 @@ describe('createAst', () => {
     expect(ast[0].children![0].text).toEqual('namespace');
     expect(ast[0].children![1].text).toContain('free');
   });
+
+  it('provides an ast with children, the first being the namespace using double quotes', () => {
+    const contract = `(namespace "free")
+    (module coin GOVERNANCE
+      (defun transfer (sender:string receiver:string))
+    )`;
+    const ast = createAst(contract, console.log);
+    expect(ast).toHaveLength(4);
+    expect(ast[0].children![0].text).toEqual('namespace');
+    expect(ast[0].children![1].text).toContain('free');
+  });
 });
 
 type SimpleAst = ISimpleAstNode[];
