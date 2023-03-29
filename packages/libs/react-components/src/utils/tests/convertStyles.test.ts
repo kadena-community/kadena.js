927008f11cbe90507a83606e602d138edbcd7b37
Steven Straatemans <steven@straatemans.nl>
2023-03-29 17:46:49 +0200

    write test

--- packages/libs/react-components/src/utils/tests/convertStyles.test.ts
+++ packages/libs/react-components/src/utils/tests/convertStyles.test.ts
@@ -1,5 +1,32 @@
+import { convertStyles } from './../convertStyles';
 describe('convertStyles', () => {
   it('should return the converted style', () => {
-    expect(1).toBe(2);
+    const data = { test: { md: 1 } };
+    const result = convertStyles(data);
+    const expectedResult = { '@md': { test: 1 } };
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return the converted style, where md is filled with multiple values', () => {
+    const data = { test: { md: 1 }, 'he-man': { md: 2, lg: 3 } };
+    const result = convertStyles(data);
+    const expectedResult = {
+      '@md': { test: 1, 'he-man': 2 },
+      '@lg': { 'he-man': 3 },
+    };
+    console.log(result);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return the converted style, where primitives are returned as is', () => {
+    const data = { test: { md: 1 }, test2: 3 };
+    const result = convertStyles(data);
+    const expectedResult = {
+      '@md': { test: 1 },
+      test2: 3,
+    };
+    console.log(result);
+    expect(result).toEqual(expectedResult);
   });
 });
