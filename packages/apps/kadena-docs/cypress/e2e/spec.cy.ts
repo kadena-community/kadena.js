c6c9a89c3018e4c98d7561c16082be553c08aab4
Steven Straatemans <steven@straatemans.nl>
2023-03-29 09:37:29 +0200

    adding cypress test functionality

--- packages/apps/kadena-docs/cypress/e2e/spec.cy.ts
+++ packages/apps/kadena-docs/cypress/e2e/spec.cy.ts
@@ -3,3 +3,5 @@ describe('start', () => {
     cy.visit('/');
   });
 });
+
+export {};
--- packages/apps/kadena-docs/src/pages/_app.tsx
+++ packages/apps/kadena-docs/src/pages/_app.tsx
@@ -2,14 +2,14 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const AnyComponent = Component as any;
   return (
     <>
       <Head>
         <title>Kadena Docs</title>
       </Head>
-      <Component {...pageProps} />
+      <AnyComponent {...pageProps} />
     </>
   );
 }
