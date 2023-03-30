cb0aa6ef1683a9c89b5a86b456e4de14f67937b0
Eileen Guo <eileen.m.guo@gmail.com>
2023-03-30 10:14:16 +0200

    fixing lint warning

--- packages/libs/react-components/.eslintrc.js
+++ packages/libs/react-components/.eslintrc.js
@@ -4,4 +4,9 @@ require('@rushstack/eslint-config/patch/modern-module-resolution');
 module.exports = {
   extends: ['@kadena-dev/eslint-config/profile/react'],
   parserOptions: { tsconfigRootDir: __dirname },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
