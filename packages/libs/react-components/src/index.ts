c7378da565ffc05e0e282915d0b96de01e37dc6c
Eileen Guo <eileen.m.guo@gmail.com>
2023-03-31 09:47:34 +0200

    add export

--- packages/libs/react-components/src/index.ts
+++ packages/libs/react-components/src/index.ts
@@ -1,5 +1,6 @@
 export { Button, IButtonProps } from './components/Button/Button';
 export { Switch } from './components/Switch/Switch';
+export { Stack, IStackProps } from './components/Stack/Stack';
 export {
   styled,
   css,
