c30887a9eb897205df17b81579115753a4514457
Ashwin van Dijk <contact@ashwinvandijk.nl>
2023-04-03 15:32:04 +0200

    remove types from git

--- packages/libs/react-components/types/styles/colors/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const colors: Record<string, string>;
-export declare const colorsDark: Record<string, string>;
--- packages/libs/react-components/types/styles/stitches.config.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CSS as StitchesCSS } from '@stitches/react';
-export declare const styled: any, css: any, config: any, theme: any, getCssText: any, createTheme: any, globalCss: any;
-export declare const darkTheme: any;
-export declare type ThemeCSS = StitchesCSS<typeof config>;
