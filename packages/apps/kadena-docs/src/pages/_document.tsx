5ec3248256a01a4cc978990cac69c691881b2c55
Steven Straatemans <steven@straatemans.nl>
2023-04-11 10:42:09 +0200

    overwrite types of next

--- packages/apps/kadena-docs/src/pages/_document.tsx
+++ packages/apps/kadena-docs/src/pages/_document.tsx
@@ -1,20 +1,23 @@
 import { getCssText } from '@kadena/react-components';
 
 import { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
+import React, { ComponentType, FC, ReactNode } from 'react';
 
 export default function Document(): JSX.Element {
+  // Fixes "Component' cannot be used as a JSX component."
+  const TypedHead = Head as unknown as FC<{ children?: ReactNode }>;
+  const TypedNextScript = NextScript as ComponentType;
   return (
     <Html>
-      <Head>
+      <TypedHead>
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         ></style>
-      </Head>
+      </TypedHead>
       <body>
         <Main />
-        <NextScript />
+        <TypedNextScript />
       </body>
     </Html>
   );
