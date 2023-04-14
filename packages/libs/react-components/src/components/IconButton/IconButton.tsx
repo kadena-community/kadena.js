6654ecb1871fe403424b65d7f748471efe37b974
Steven <steven@straatemans.nl>
2023-04-14 11:22:39 +0200

    [UI] add iconbutton component (#227)

---
+++ packages/libs/react-components/src/components/IconButton/IconButton.tsx
@@ -0,0 +1,42 @@
+import { styled } from '../../styles';
+import { SystemIcons } from '../Icons';
+
+import React, { FC } from 'react';
+
+export interface IIConButtonProps
+  extends React.HTMLAttributes<HTMLButtonElement> {
+  icon: typeof SystemIcons[keyof typeof SystemIcons];
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  title: string;
+}
+
+const Button = styled('button', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: 'transparent',
+  width: '$11',
+  height: '$11',
+  border: 0,
+  color: 'inherit',
+  cursor: 'pointer',
+  transition: 'opacity .2s ease',
+  '&:hover': {
+    opacity: '.6',
+  },
+});
+
+export const IconButton: FC<IIConButtonProps> = ({
+  icon,
+  onClick,
+  ...props
+}) => {
+  const Icon = icon;
+  const ariaLabel = props['aria-label'] ?? props.title;
+
+  return (
+    <Button onClick={onClick} {...props} aria-label={ariaLabel}>
+      <Icon size="md" />
+    </Button>
+  );
+};
