198f427ea02fae9db2a69474be5174bb0bc71afc
Steven Straatemans <steven@straatemans.nl>
2023-04-18 13:13:40 +0200

    fix test

--- packages/apps/kadena-docs/src/utils/tests/getLayout.test.ts
+++ packages/apps/kadena-docs/src/utils/tests/getLayout.test.ts
@@ -7,9 +7,9 @@ describe('utils getLayout', () => {
   });
   test('should return layout Full-Component when arg is anything', () => {
     expect(getLayout('full').displayName).toEqual('Full');
-    expect(getLayout('he-man').displayName).toEqual('Full');
   });
   test('should return layout Landing-Component when arg is "landing"', () => {
     expect(getLayout('landing').displayName).toEqual('Landing');
+    expect(getLayout('he-man').displayName).toEqual('Landing');
   });
 });
