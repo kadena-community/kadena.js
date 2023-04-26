04a9fb0b0ebf3b0eb459abd368bae60b3e702380
Steven Straatemans <steven@straatemans.nl>
2023-04-26 14:06:39 +0200

    add code back png

--- packages/apps/kadena-docs/src/components/Layout/Code/styles.ts
+++ packages/apps/kadena-docs/src/components/Layout/Code/styles.ts
@@ -1,53 +1,76 @@
 import { styled, StyledComponent } from '@kadena/react-components';
 
-export const CodeBackground: StyledComponent<'div', { layout?: 'code' }> =
-  styled('div', {
-    position: 'absolute',
-    zIndex: 2,
-    pointerEvents: 'none',
-    gridColumn: '1 / span 5',
-    gridRow: '2 / span 2',
+export const CodeBackground: StyledComponent<
+  'div',
+  { layout?: 'code' | 'default' }
+> = styled('div', {
+  position: 'absolute',
+  zIndex: 2,
+  pointerEvents: 'none',
+  gridColumn: '1 / span 5',
+  gridRow: '2 / span 2',
+  display: 'block',
+  transform: 'translateX(100%)',
+  transition: 'transform .3s ease',
+
+  '@md': {
+    position: 'relative',
     display: 'block',
-    transform: 'translateX(100%)',
-    transition: 'transform .3s ease',
-    '@md': {
-      position: 'relative',
-      display: 'block',
-      transform: 'translateX(0)',
-    },
+    transform: 'translateX(0)',
+  },
 
-    variants: {
-      layout: {
-        code: {},
+  defaultVariants: {
+    layout: 'default',
+  },
+  variants: {
+    layout: {
+      default: {
+        '&::before': {
+          content: '',
+          position: 'absolute',
+          pointerEvents: 'none',
+          inset: 0,
+          backgroundImage: 'url("/assets/bg-code.png")',
+          backgroundRepeat: 'no-repeat',
+          backgroundPositionY: '-100px',
+          backgroundPositionX: 'calc(100vw  - (200px + 120px))',
+          '@2xl': {
+            backgroundPositionX:
+              'calc($sizes$pageWidth + ((100vw - $sizes$pageWidth) /2 ) - (300px +  120px))',
+          },
+        },
+      },
+      code: {
+        '&::before': {
+          content: '',
+          position: 'absolute',
+          pointerEvents: 'none',
+          inset: 0,
+          backgroundImage: 'url("/assets/bg-vertical.png")',
+          backgroundRepeat: 'no-repeat',
+          backgroundPositionY: '-100px',
+          backgroundPositionX: 'calc(100vw  - (200px + 100px))',
+          '@2xl': {
+            backgroundPositionX:
+              'calc($sizes$pageWidth + ((100vw - $sizes$pageWidth) /2 ) - (300px +  100px))',
+          },
+        },
       },
     },
+  },
 
-    '&::before': {
-      content: '',
-      position: 'absolute',
-      pointerEvents: 'none',
-      inset: 0,
-      backgroundImage: 'url("/assets/bg-code.png")',
-      backgroundRepeat: 'no-repeat',
-      backgroundPositionY: '-100px',
-      backgroundPositionX: 'calc(100vw  - (200px + 120px))',
-      '@2xl': {
-        backgroundPositionX:
-          'calc($sizes$pageWidth + ((100vw - $sizes$pageWidth) /2 ) - (300px +  120px))',
-      },
+  '&::after': {
+    content: '',
+    position: 'absolute',
+    inset: 0,
+    left: 'calc(100vw  - (200px + 15px))',
+    backgroundColor: '$backgroundOverlayColor',
+    zIndex: 1,
+    '@2xl': {
+      left: 'calc($sizes$pageWidth + ((100vw - $sizes$pageWidth) /2) - 300px)',
     },
-    '&::after': {
-      content: '',
-      position: 'absolute',
-      inset: 0,
-      left: 'calc(100vw  - (200px + 15px))',
-      backgroundColor: '$backgroundOverlayColor',
-      zIndex: 1,
-      '@2xl': {
-        left: 'calc($sizes$pageWidth + ((100vw - $sizes$pageWidth) /2) - 300px)',
-      },
-    },
-  });
+  },
+});
 
 export const Article: StyledComponent<'article'> = styled('article', {
   width: '100%',
@@ -81,7 +104,6 @@ export const Content: StyledComponent<'div'> = styled('div', {
   gridRow: '3 / span 1',
   width: '100%',
   height: '100%',
-  background: 'green',
 
   '@md': {
     gridColumn: '3 / span 1',
