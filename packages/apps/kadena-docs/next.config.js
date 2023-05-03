9c50a24ad5063289fdf569ff30356f98fc5435d5
Steven Straatemans <steven@straatemans.nl>
2023-05-03 12:36:25 +0200

    try build

--- packages/apps/kadena-docs/next.config.js
+++ packages/apps/kadena-docs/next.config.js
@@ -1,6 +1,4 @@
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import mdx from '@next/mdx';
+const mdx = require('@next/mdx');
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
@@ -8,7 +6,7 @@ const withMDX = mdx({
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    remarkPlugins: [],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     providerImportSource: '@mdx-js/react',
@@ -21,4 +19,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withMDX(nextConfig);
+module.exports = withMDX(nextConfig);
