8911ca105da056cbf51e450f3f02421251d6106f
Steven Straatemans <steven@straatemans.nl>
2023-05-08 16:11:16 +0200

    copy content

--- packages/apps/kadena-docs/src/components/Layout/components/ArticleStyles.ts
+++ packages/apps/kadena-docs/src/components/Layout/components/ArticleStyles.ts
@@ -11,6 +11,7 @@ export const Content: StyledComponent<'div'> = styled('div', {
   position: 'relative',
   gridColumn: '1 / span 1',
   gridRow: '3 / span 1',
+  overflow: 'hidden',
   width: '100%',
   height: '100%',
 
