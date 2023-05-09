58a060b32044b99031319f7e5a3a0287d4129003
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-05-09 15:43:32 +0200

    added list-module pact script

--- packages/apps/kadena-transfer/scripts/list-module.js
+++ packages/apps/kadena-transfer/scripts/list-module.js
@@ -1,33 +1,32 @@
-// import { PactCommand } from '@kadena/client';
-// import { createExp } from '@kadena/pactjs';
+import { PactCommand } from '@kadena/client';
+import { createExp } from '@kadena/pactjs';
 
-// const NETWORK_ID = 'testnet04';
-// const chainId = '1';
-// const gasLimit = 6000;
-// const gasPrice = 0.001;
-// const ttl = 600;
-// export const API_HOST = `https://api.testnet.chainweb.com/chainweb/0.0/${NETWORK_ID}/chain/${chainId}/pact`;
+//List modules PACT code example
 
-// // Tips: we provided some tips in the README file
-// // Don't forget to explore the documentation for more details
-// // ==>
-// export async function listModules() {
-//   // 1 - Create a new PactCommand
-//   const cmd = new PactCommand();
+const NETWORK_ID = 'testnet04';
+const chainId = '1';
+const gasLimit = 6000;
+const gasPrice = 0.001;
+const ttl = 600;
+export const API_HOST = `https://api.testnet.chainweb.com/chainweb/0.0/${NETWORK_ID}/chain/${chainId}/pact`;
 
-//   // 2 - Bind to the Pact code
-//   cmd.code = createExp('list-modules');
+export async function listModules() {
+  // 1 - Create a new PactCommand
+  const cmd = new PactCommand();
 
-//   // 3 - Set the meta data
-//   cmd.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
+  // 2 - Bind to the Pact code
+  cmd.code = createExp('list-modules');
 
-//   // 4 - Call the Pact local endpoint to retrieve the result
-//   return await cmd.local(API_HOST);
-// }
+  // 3 - Set the meta data
+  cmd.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
 
-// export async function freeClock() {
-//   const pactCommand = new PactCommand();
-//   pactCommand.code = createExp('free.clock.get-line');
-//   pactCommand.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
-//   return await pactCommand.local(API_HOST);
-// }
+  // 4 - Call the Pact local endpoint to retrieve the result
+  return await cmd.local(API_HOST);
+}
+
+export async function freeClock() {
+  const pactCommand = new PactCommand();
+  pactCommand.code = createExp('free.clock.get-line');
+  pactCommand.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
+  return await pactCommand.local(API_HOST);
+}
