7d9761d8b83827662ef268e1fd913c25c3e978d5
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-05-09 16:13:04 +0200

    restructured modules folder and fix script

--- packages/apps/kadena-transfer/src/services/modules/list-module.js
+++ packages/apps/kadena-transfer/src/services/modules/list-module.js
@@ -12,21 +12,14 @@ export const API_HOST = `https://api.testnet.chainweb.com/chainweb/0.0/${NETWORK
 
 export async function listModules() {
   // 1 - Create a new PactCommand
-  const cmd = new PactCommand();
+  const pactCommand = new PactCommand();
 
   // 2 - Bind to the Pact code
-  cmd.code = createExp('list-modules');
+  pactCommand.code = createExp('list-modules');
 
   // 3 - Set the meta data
-  cmd.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
+  pactCommand.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
 
   // 4 - Call the Pact local endpoint to retrieve the result
-  return await cmd.local(API_HOST);
-}
-
-export async function freeClock() {
-  const pactCommand = new PactCommand();
-  pactCommand.code = createExp('free.clock.get-line');
-  pactCommand.setMeta({ gasLimit, gasPrice, ttl }, NETWORK_ID);
   return await pactCommand.local(API_HOST);
 }
