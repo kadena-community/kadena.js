312bd2aad92fd0cfff37992d8fe5c4c767bb47bd
Steven Straatemans <steven@straatemans.nl>
2023-05-09 14:01:38 +0200

    style the searchbutton

---
+++ packages/apps/kadena-docs/src/components/Layout/components/Header/SearchButton.tsx
@@ -0,0 +1,18 @@
+import { SystemIcons } from '@kadena/react-components';
+
+import { StyledSearchButton, StyledSearchButtonSlash } from './styles';
+
+import { useOpenSearch } from '@/hooks';
+import React, { FC } from 'react';
+
+export const SearchButton: FC = () => {
+  const { handleOpenSearch } = useOpenSearch();
+  return (
+    <StyledSearchButton onClick={handleOpenSearch} color="positive">
+      <SystemIcons.Magnify />
+      <StyledSearchButtonSlash>
+        <SystemIcons.SlashForward />
+      </StyledSearchButtonSlash>
+    </StyledSearchButton>
+  );
+};
