57a40defc53614aa5eafb47fea3f88745990b920
Javad Khalilian <javad@passionatepeople.io>
2023-05-15 23:05:14 +0200

    chore(cka): delete generated projects after running test:ci

--- packages/tools/create-kadena-app/scripts/generate-test.mjs
+++ packages/tools/create-kadena-app/scripts/generate-test.mjs
@@ -1,16 +1,40 @@
 #!/usr/bin/env zx
+// This script is used to generate projects for every supported template and test them.
 
 import 'zx/globals';
 
-// This script is used to generate projects for every supported template and test them.
+import { rm } from 'fs/promises';
+
+// the default log uses stderr for write output of commands, even for commands that do not generate errors or warnings.
+// This can cause Rush to treat everything as a warning message. This function changes this behavior.
+$.log = (entry) => {
+  switch (entry.kind) {
+    case 'stderr':
+      log(entry);
+      break;
+    case 'cmd':
+      console.log(entry.cmd);
+      break;
+    default:
+      if (entry.data) {
+        process.stdout.write(entry.data);
+      }
+  }
+};
+
 const generateAndTest = (template) =>
   within(async () => {
     const directory = `test-${template}-project`;
-    // echo "Generating the project"
-    await $`node lib/index.js generate-project --name ${directory} --template ${template}`;
-    await cd(directory);
-    // echo "Running tests in the project"
-    await $`npm run test:ci`;
+    const script = path.join('lib', 'index.js');
+
+    await echo(`Generating ${template} project`);
+    await $`node ${script} generate-project --name ${directory} --template ${template}`;
+
+    await echo(`Running tests in ${template} project`);
+    await $`npm run --prefix ${directory} test:ci`;
+
+    await echo(`deleting ${template}`);
+    await rm(directory, { recursive: true, force: true });
   });
 
 await generateAndTest('nextjs');
