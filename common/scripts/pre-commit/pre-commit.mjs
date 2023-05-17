cca76c84d6ef5cd6e35b96e31237ac5964522f00
Takadenoshi <tasos@kadena.io>
2023-05-17 14:59:39 +0200

    rewrote pre-commit hook in zx

---
+++ common/scripts/pre-commit/pre-commit.mjs
@@ -0,0 +1,8 @@
+import { $, echo } from 'zx';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const targetDir = join(__dirname, '..');
+
+await $`cd ${targetDir} && node install-run-rush.js lint-staged`
