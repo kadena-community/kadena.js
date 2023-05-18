1673866302ab60b64e57f1e6fc84c0587696f22f
Maarten van den Hoven <maarten.vandenhoven@deptagency.com>
2023-05-18 14:08:49 +0200

    Reviewed code suggetions

--- packages/apps/transfer/src/index.tsx
+++ packages/apps/transfer/src/index.tsx
@@ -1,7 +1,9 @@
+import './index.css';
+
+import App from './App';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
