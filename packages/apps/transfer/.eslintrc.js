0a371b8e951259576ce734f04df36dfa4a97b853
Maarten van den Hoven <maarten.vandenhoven@deptagency.com>
2023-05-19 16:53:23 +0200

    fixed project build

--- packages/apps/transfer/.eslintrc.js
+++ packages/apps/transfer/.eslintrc.js
@@ -1,6 +1,7 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@kadena-dev/eslint-config/profile/next'],
+  // TODO: Disabled until we use Next.js.
+  // extends: ['@kadena-dev/eslint-config/profile/next'],
   parserOptions: { tsconfigRootDir: __dirname },
 };
