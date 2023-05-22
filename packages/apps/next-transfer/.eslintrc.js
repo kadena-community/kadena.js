82cf7bd03a706e38b5f360ab91eeb01b04436075
Reza Erami <reza.eramikalleh@deptagency.com>
2023-05-22 16:30:49 +0200

    fix kadena transfer
    - remove old transfer module
    - rename next-transfer to transfer

--- packages/apps/next-transfer/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-require('@rushstack/eslint-config/patch/modern-module-resolution');
-
-module.exports = {
-  extends: ['next/core-web-vitals', '@kadena-dev/eslint-config/profile/react'],
-  parserOptions: { tsconfigRootDir: __dirname },
-};
