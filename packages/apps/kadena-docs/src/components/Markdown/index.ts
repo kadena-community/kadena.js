6ef51475110c0ed733564ef91ebb89b805008b5f
Steven <steven@straatemans.nl>
2023-05-23 10:05:27 +0200

    [Docs] Table component for MarkDown (#300)

--- packages/apps/kadena-docs/src/components/Markdown/index.ts
+++ packages/apps/kadena-docs/src/components/Markdown/index.ts
@@ -1,3 +1,5 @@
+import { Table } from '@kadena/react-components';
+
 import { Code, TitleWrapper } from './Code';
 import { Heading1, Heading2, Heading3, Heading4 } from './Heading';
 import { Paragraph } from './Paragraph';
@@ -12,4 +14,10 @@ export const markDownComponents: Record<string, FC> = {
   p: Paragraph as FC<JSX.IntrinsicElements['p']>,
   code: Code as FC<JSX.IntrinsicElements['code']>,
   div: TitleWrapper as FC<JSX.IntrinsicElements['div']>,
+  table: Table as FC<JSX.IntrinsicElements['table']>,
+  tbody: Table.Body as FC<JSX.IntrinsicElements['tbody']>,
+  thead: Table.Head as FC<JSX.IntrinsicElements['thead']>,
+  tr: Table.Tr as FC<JSX.IntrinsicElements['tr']>,
+  td: Table.Tr.Td as FC<JSX.IntrinsicElements['td']>,
+  th: Table.Tr.Th as FC<JSX.IntrinsicElements['th']>,
 };
