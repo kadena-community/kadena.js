170938ec67d1d6de299fc0fea09c1e4229205a99
Steven Straatemans <steven@straatemans.nl>
2023-05-23 16:44:39 +0200

    clean up

--- packages/apps/kadena-docs/environment.d.ts
+++ packages/apps/kadena-docs/environment.d.ts
@@ -8,7 +8,3 @@ declare namespace NodeJS {
     NEXT_PUBLIC_GA_PRIVATE_KEY: string;
   }
 }
-
-// If this file has no import/export statements (i.e. is a script)
-// convert it into a module by adding an empty export statement.
-// export {};
