7a27ad1878fbdd58b533ee2539c5b5e23e8b1ccd
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-05-25 00:32:05 +0100

    fixed utils method to return decimal

--- packages/apps/transfer/src/services/utils/utils.ts
+++ packages/apps/transfer/src/services/utils/utils.ts
@@ -1,17 +1,14 @@
-export const convertDecimal = (decimal: number): number => {
-  if (decimal.toString().includes('.')) {
-    return decimal;
-  }
-  if (decimal / Math.floor(decimal) === 1) {
-    decimal = Number(`${decimal.toString}.0`);
-  }
-  return decimal;
-};
-
 export const onlyKey = (account: string): string => {
   return account.split(':')[1];
 };
 
 export const generateApiHost = (networkId: string, chainId: string): string => {
   return `https://api.testnet.chainweb.com/chainweb/0.0/${networkId}/chain/${chainId}/pact`;
+};
+
+export const convertDecimal = (decimal: string): string => {
+  if (decimal.includes('.')) {
+    return decimal;
+  }
+  return Number(decimal).toFixed(1);
 };
