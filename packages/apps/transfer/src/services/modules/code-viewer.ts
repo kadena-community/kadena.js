bb35799d28b2202f1fa77bbee47937faf71e3bb4
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-05-30 21:12:12 +0100

    code viewer working service

--- packages/apps/transfer/src/services/modules/code-viewer.ts
+++ packages/apps/transfer/src/services/modules/code-viewer.ts
@@ -1,7 +1,9 @@
 import { PactCommand } from '@kadena/client';
+import { createExp } from '@kadena/pactjs';
+import { ChainId } from '@kadena/types';
 
 const NETWORK_ID: any = 'mainnet01';
-const chainId: string = '1';
+const chainId: ChainId = '1';
 const gasLimit: number = 60000;
 const gasPrice: number = 0.00000001;
 const ttl: number = 60000;
@@ -9,14 +11,16 @@ const sender = 'not-real';
 export const API_HOST: string = `https://api.chainweb.com/chainweb/0.0/${NETWORK_ID}/chain/${chainId}/pact`;
 
 export const codeViewer = async (): Promise<void> => {
-  //module: string = 'coin'
   const pactCommand = new PactCommand();
-  pactCommand.code = `(describe-module "coin")`;
+  pactCommand.code = createExp(`describe-module "coin"`);
 
-  pactCommand.setMeta({ gasLimit, gasPrice, ttl, sender });
+  pactCommand.setMeta({ gasLimit, gasPrice, ttl, sender, chainId });
 
   try {
-    const response = await pactCommand.send(API_HOST);
+    const response = await pactCommand.local(API_HOST, {
+      signatureVerification: false,
+      preflight: false,
+    });
     console.log(response);
   } catch (error) {
     console.log(error);
