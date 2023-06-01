791b1f0926a19d1f99d914ba983676c21b73bfff
eileenmguo <eileen.m.guo@gmail.com>
2023-06-01 06:55:03 +0200

    [@kadena/react-ui]: Adding new component library using vanilla extract  (#291)

---
+++ packages/libs/react-ui/src/components/Button/Button.css.ts
@@ -0,0 +1,67 @@
+import { ColorType, sprinkles, vars } from '../../styles';
+
+import { createVar, style, styleVariants } from '@vanilla-extract/css';
+
+const bgHoverColor = createVar(),
+  bgActiveColor = createVar(),
+  focusOutlineColor = createVar();
+
+export const container = style([
+  sprinkles({
+    display: 'flex',
+    placeItems: 'center',
+    gap: 1,
+    borderRadius: 'sm',
+    cursor: 'pointer',
+    paddingX: 4,
+    paddingY: 3,
+    border: 'none',
+    fontSize: 'base',
+  }),
+  {
+    transition: 'background-color 0.4s ease',
+    ':hover': {
+      background: bgHoverColor,
+    },
+    ':active': {
+      background: bgActiveColor,
+    },
+    ':focus-visible': {
+      outlineOffset: '2px',
+      outlineWidth: vars.borderWidths.md,
+      outlineStyle: 'solid',
+      outlineColor: focusOutlineColor,
+    },
+    ':disabled': {
+      opacity: 0.7,
+      background: vars.colors.neutral3,
+      color: vars.colors.neutral1,
+      cursor: 'not-allowed',
+    },
+  },
+]);
+
+const colors: Record<ColorType, ColorType> = {
+  primary: 'primary',
+  secondary: 'secondary',
+  positive: 'positive',
+  warning: 'warning',
+  negative: 'negative',
+};
+
+export const colorVariants = styleVariants(colors, (color) => {
+  return [
+    container,
+    sprinkles({
+      color: `${color}Surface`,
+      bg: `${color}Contrast`,
+    }),
+    {
+      vars: {
+        [bgHoverColor]: vars.colors[`${color}HighContrast`],
+        [bgActiveColor]: vars.colors[`${color}Accent`],
+        [focusOutlineColor]: vars.colors[`${color}Accent`],
+      },
+    },
+  ];
+});
