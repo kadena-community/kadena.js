d5b4b73a4499c62b8a49c744a661c580e4a87bf0
Sander Looijenga <slooijenga@anwb.nl>
2023-06-01 11:57:20 +0200

    Add initial Notification component

---
+++ packages/apps/transfer/src/components/Global/Notification/styles.tsx
@@ -0,0 +1,19 @@
+import { styled } from '@kadena/react-components';
+
+export const StyledContainer = styled('div', {
+  borderLeftWidth: '4px',
+  borderColor: '#FF8A00',
+  padding: '1rem', // 16px
+  borderRadius: '0.25rem', // 4px
+  backgroundColor: 'rgb(112 67 0 / 0.5)',
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '0.625rem', //10px
+  marginBottom: '1.5rem', // 24px
+});
+
+export const StyledBody = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+});
