a2d08683704c59c3296af2de02f555442f492995
Andy Tang <andy.tang@action.nl>
2023-06-05 16:54:04 +0200

    Update to use eslint and tsconfig from mono repo
     - fixed all issues regarding eslint

---
+++ packages/tools/kda-cli/src/app.tsx
@@ -0,0 +1,53 @@
+import { OnAnswer, QuestionWrapper } from './components/question.js';
+import { Summary } from './components/summary.js';
+import { questions } from './questions/init.js';
+import {
+  getNextQuestion,
+  IAnswers,
+  IQuestionAnswer,
+} from './questions/questions.js';
+
+import React, { useCallback, useState } from 'react';
+
+interface IProps {
+  task?: string;
+}
+export default function App({ task = '' }: IProps): JSX.Element {
+  const [qna, setQNA] = useState<IQuestionAnswer>({
+    current: questions[0],
+    answers: {},
+    questions,
+    answeredQuestions: [],
+  });
+  const onAnswer = useCallback<OnAnswer>(
+    (answer: IAnswers) => {
+      if (!qna.current) throw new Error('No current question');
+      const mergedAnswers = { ...qna.answers, ...answer };
+      const nextQNA = getNextQuestion({
+        ...qna,
+        answers: mergedAnswers,
+        answeredQuestions: [
+          ...qna.answeredQuestions,
+          { question: qna.current, answer },
+        ],
+      });
+      setQNA(nextQNA);
+      return mergedAnswers;
+    },
+    [qna, setQNA],
+  );
+  if (!qna.current || task)
+    return (
+      <Summary
+        answers={qna.answers}
+        answeredQuestions={qna.answeredQuestions}
+      />
+    );
+  return (
+    <QuestionWrapper
+      {...qna.current}
+      onAnswer={onAnswer}
+      answers={qna.answers}
+    />
+  );
+}
