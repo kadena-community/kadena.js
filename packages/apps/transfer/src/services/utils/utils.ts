f96e84aba7e74f0acb9134e4b3892523c0447c60
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-06-05 17:46:47 +0100

    adapt transfer with pact number formatting

--- packages/apps/transfer/src/services/utils/utils.ts
+++ packages/apps/transfer/src/services/utils/utils.ts
@@ -8,13 +8,6 @@ export const generateApiHost = (networkId: string, chainId: string): string => {
   return `https://api.testnet.chainweb.com/chainweb/0.0/${networkId}/chain/${chainId}/pact`;
 };
 
-export const convertDecimal = (decimal: string): string => {
-  if (decimal.includes('.')) {
-    return decimal;
-  }
-  return Number(decimal).toFixed(1);
-};
-
 export const convertIntToChainId = (value: number): ChainId => {
   return value.toString() as ChainId;
 };
