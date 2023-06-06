2bf503b4d367486184e3680a07d6f3103f2a38ac
Steven <steven@straatemans.nl>
2023-06-06 09:00:15 +0200

    [docs] checkdeadlinks (#356)

--- packages/apps/kadena-docs/src/utils/hasSameBasePath.ts
+++ packages/apps/kadena-docs/src/utils/hasSameBasePath.ts
@@ -2,11 +2,7 @@ export const hasSameBasePath = (
   pathname1?: string,
   pathname2?: string,
 ): boolean => {
-  let basePathLength = 3;
-  if (process.env.NEXT_PUBLIC_APP_DEV === 'test') {
-    basePathLength = 4;
-  }
-
+  const basePathLength = 3;
   return (
     pathname1?.split('/').slice(0, basePathLength).join('/').toLowerCase() ===
     pathname2?.split('/').slice(0, basePathLength).join('/').toLowerCase()
