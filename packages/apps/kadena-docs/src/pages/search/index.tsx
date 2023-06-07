b24e419a107e993544a1a5247266046233a514c0
Steven Straatemans <steven@straatemans.nl>
2023-06-07 14:41:41 +0200

    add useRouter

---
+++ packages/apps/kadena-docs/src/pages/search/index.tsx
@@ -0,0 +1,48 @@
+import { TextField } from '@kadena/react-components';
+
+import {
+  checkSubTreeForActive,
+  getPathName,
+} from '@/utils/staticGeneration/checkSubTreeForActive';
+import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import React, { ChangeEvent, FC, useState } from 'react';
+
+const Search: FC = () => {
+  const {
+    query: { q },
+  } = useRouter();
+
+  const [query, setQuery] = useState<string | undefined>(q);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { currentTarget } = event;
+    const value = currentTarget.value;
+    setQuery(value);
+  };
+  return (
+    <section>
+      <TextField inputProps={{ onChange }} />
+      <div>{query}</div>
+    </section>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context, ...args) => {
+  return {
+    props: {
+      leftMenuTree: checkSubTreeForActive(getPathName(__filename)),
+      frontmatter: {
+        title: 'Search',
+        menu: 'Search',
+        label: 'Search',
+        order: 0,
+        description: 'We will find stuff for u',
+        layout: 'landing',
+        icon: 'KadenaOverview',
+      },
+    },
+  };
+};
+
+export default Search;
