d4992ae70bac35d74952e0b449a133ad3b9f8dce
Steven Straatemans <steven@straatemans.nl>
2023-06-08 15:06:28 +0200

    fix the button to also use it as anchor

--- packages/apps/kadena-docs/src/components/BottomPageSection/components/EditPage.tsx
+++ packages/apps/kadena-docs/src/components/BottomPageSection/components/EditPage.tsx
@@ -11,8 +11,14 @@ const editRoot = process.env.NEXT_PUBLIC_GIT_EDIT_ROOT;
 export const EditPage: FC<IProps> = ({ filename }) => {
   if (editRoot === null || filename === null) return null;
   return (
-    <a href={`${editRoot}${filename}`} target="_blank" rel="noreferrer">
-      <Button title="Edit this page">Edit this page</Button>
-    </a>
+    <Button
+      as="a"
+      href={`${editRoot}${filename}`}
+      target="_blank"
+      rel="noreferrer"
+      title="Edit this page"
+    >
+      Edit this page
+    </Button>
   );
 };
