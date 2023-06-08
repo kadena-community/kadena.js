a2f54adb80a877acd6f5982a3142181a0f33ce95
bearni95 <bernatcanal@gmail.com>
2023-06-08 15:33:21 -0400

    added corresponding exports onto the index.ts files

--- packages/libs/react-ui/src/components/index.ts
+++ packages/libs/react-ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Button, IButtonProps } from './Button/Button';
 export { Card, ICardProps } from './Card/Card';
 export { SystemIcon, ProductIcon, IIconProps } from './Icons';
+export { IconButton, IIconButtonProps } from './IconButton/IconButton';
