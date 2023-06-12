8af4db6e29ef13f76cf7a7859811031eca79ca15
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-06-12 16:37:10 +0200

    pactCreator update

--- packages/libs/client/src/pact.ts
+++ packages/libs/client/src/pact.ts
@@ -22,6 +22,7 @@ import {
 
 import { IPactCommand } from './interfaces/IPactCommand';
 import { parseType } from './utils/parseType';
+import { ContCommand } from '.';
 
 import debug, { Debugger } from 'debug';
 
@@ -494,6 +495,41 @@ const pactCreator = (): IPact => {
       // e.g.: `Pact.modules.coin.transfer(...someArgs)`
       log('apply', args);
       transaction.code = createExp(transaction.code, ...args.map(parseType));
+      return transaction;
+    },
+  }) as IPact;
+  return ThePact;
+};
+
+const pactCreator1 = (): IPact => {
+  let code = '';
+  const ThePact: IPact = new Proxy(function () {} as unknown as IPact, {
+    get(target: unknown, p: string): IPact {
+      log('get', p);
+      if (typeof p === 'string')
+        if (code.length !== 0) {
+          code += '.' + p;
+        } else {
+          code += p;
+        }
+      return ThePact;
+    },
+    apply(
+      target: unknown,
+      that: unknown,
+      args: Array<string | number | boolean>,
+    ) {
+      if (code.endsWith('.cont')) {
+        const transaction: ContCommand = new ContCommand(
+          ...(args as [string, number, string, boolean]),
+        );
+      }
+      // when the expression is called, finalize the call
+      // e.g.: `Pact.modules.coin.transfer(...someArgs)`
+      const transaction: PactCommand = new PactCommand();
+      code = code.replace('.exec', '');
+      log('apply', args);
+      transaction.code = createExp(code, ...args.map(parseType));
       return transaction;
     },
   }) as IPact;
