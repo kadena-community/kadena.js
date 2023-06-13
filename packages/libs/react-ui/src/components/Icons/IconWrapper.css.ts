8ac6510676d7cf6ac2f0a2632abef30d1d53431d
Eileen Guo <eileen.m.guo@gmail.com>
2023-06-13 10:44:39 +0200

    pr cleanup

--- packages/libs/react-ui/src/components/Icons/IconWrapper.css.ts
+++ packages/libs/react-ui/src/components/Icons/IconWrapper.css.ts
@@ -10,8 +10,8 @@ export const iconContainer = style([
 
 export const sizeVariants = styleVariants({
   sm: [iconContainer, sprinkles({ size: '$4' })],
-  md: [iconContainer, sprinkles({ size: 6 })],
-  lg: [iconContainer, sprinkles({ size: 8 })],
-  xl: [iconContainer, sprinkles({ size: 10 })],
-  heroHeader: [iconContainer, sprinkles({ size: 24 })],
+  md: [iconContainer, sprinkles({ size: '$6' })],
+  lg: [iconContainer, sprinkles({ size: '$8' })],
+  xl: [iconContainer, sprinkles({ size: '$10' })],
+  heroHeader: [iconContainer, sprinkles({ size: '$24' })],
 });
--- packages/libs/react-ui/src/components/Typography/typography.css.ts
+++ packages/libs/react-ui/src/components/Typography/typography.css.ts
@@ -2,19 +2,19 @@ import { sprinkles } from '../../styles';
 
 import { ComplexStyleRule } from '@vanilla-extract/css';
 
-export const fontVariants: Record<string | number, ComplexStyleRule> = {
+export const fontVariants: Record<string, ComplexStyleRule> = {
   main: [sprinkles({ fontFamily: '$main' })],
   mono: [sprinkles({ fontFamily: '$mono' })],
 };
 
-export const transformVariants: Record<string | number, ComplexStyleRule> = {
+export const transformVariants: Record<string, ComplexStyleRule> = {
   uppercase: [sprinkles({ textTransform: 'uppercase' })],
   lowercase: [sprinkles({ textTransform: 'lowercase' })],
   capitalize: [sprinkles({ textTransform: 'capitalize' })],
   none: [sprinkles({ textTransform: 'none' })],
 };
 
-export const colorVariants: Record<string | number, ComplexStyleRule> = {
+export const colorVariants: Record<string, ComplexStyleRule> = {
   default: [sprinkles({ color: '$neutral4' })],
   emphasize: [sprinkles({ color: '$neutral6' })],
 };
