c0d3d513ef21e17354aa14d42261de831863dc95
Steven Straatemans <steven@straatemans.nl>
2023-06-13 12:36:59 +0200

    create search index

--- packages/apps/kadena-docs/src/scripts/createSearchIndex.mjs
+++ packages/apps/kadena-docs/src/scripts/createSearchIndex.mjs
@@ -1,8 +1,42 @@
 import * as fs from 'fs';
+import yaml from 'js-yaml';
+import { frontmatter } from 'micromark-extension-frontmatter';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { frontmatterFromMarkdown } from 'mdast-util-frontmatter';
+import MiniSearch from 'minisearch';
 
 const INITIALPATH = './src/pages/docs';
 const INDEX = [];
 const INDEXFILE = './src/data/searchIndex.json';
+
+const isMarkDownFile = (name) => {
+  const extension = name.split('.').at(-1);
+  return extension.toLowerCase() === 'md' || extension.toLowerCase() === 'mdx';
+};
+
+const getFrontMatter = (doc) => {
+  const tree = fromMarkdown(doc.toString(), {
+    extensions: [frontmatter()],
+    mdastExtensions: [frontmatterFromMarkdown()],
+  });
+
+  return yaml.load(tree.children[0].value);
+};
+
+const convertFile = (file) => {
+  const doc = fs.readFileSync(`${file}`, 'utf-8');
+  let data;
+
+  data = getFrontMatter(doc);
+
+  return {
+    id: file,
+    title: data.title,
+    description: data.description,
+    filename: file,
+    content: doc,
+  };
+};
 
 const createIndex = (rootDir, parent = []) => {
   const files = fs.readdirSync(rootDir);
@@ -15,7 +49,10 @@ const createIndex = (rootDir, parent = []) => {
     if (fs.statSync(currentFile).isDirectory()) {
       return createIndex(currentFile, parent);
     } else {
-      parent.push(currentFile);
+      if (isMarkDownFile(currentFile)) {
+        const data = convertFile(currentFile);
+        parent.push(data);
+      }
     }
   });
 
@@ -23,5 +60,13 @@ const createIndex = (rootDir, parent = []) => {
 };
 
 const result = createIndex(INITIALPATH, INDEX);
-console.log(result);
-//fs.writeFileSync(INDEXFILE, result);
+const fileStr = JSON.stringify(result, null, 2);
+
+const miniSearch = new MiniSearch({
+  fields: ['title', 'description', 'content'],
+  storeFields: ['title', 'filename'],
+});
+
+await miniSearch.addAllAsync(result);
+
+fs.writeFileSync(INDEXFILE, JSON.stringify(miniSearch.toJSON()));
