ef43b6490b2c611d79edf28d577f793f1d6d2f97
Steven Straatemans <steven@straatemans.nl>
2023-06-13 12:04:42 +0200

    rebuild

--- packages/apps/kadena-docs/src/scripts/createSearchIndex.mjs
+++ packages/apps/kadena-docs/src/scripts/createSearchIndex.mjs
@@ -4,6 +4,31 @@ const INITIALPATH = './src/pages/docs';
 const INDEX = [];
 const INDEXFILE = './src/data/searchIndex.json';
 
+const createIndex = (rootDir, parent = []) => {
+  const files = fs.readdirSync(rootDir);
+
+  files.forEach((file) => {
+    const currentFile = `${rootDir}/${file}`;
+
+    const arr = [];
+    let child = {
+      children: arr,
+    };
+
+    console.log(currentFile);
+
+    // parent = pushToParent(parent, child);
+
+    // if (fs.statSync(currentFile).isDirectory()) {
+    //   child.children = createTree(currentFile, child.children);
+
+    //   return child.children;
+    // }
+  });
+
+  return parent;
+};
+
 const result = createIndex(INITIALPATH, INDEX);
 
 fs.writeFileSync(INDEXFILE, result);
