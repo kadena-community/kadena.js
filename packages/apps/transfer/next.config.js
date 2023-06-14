b65aa1ed6a7a42aca8d6d642052a5c68be653223
Reza Erami <reza.eramikalleh@deptagency.com>
2023-06-14 15:54:33 +0200

    fix build
    - ignore tests for getting build or transpiled

--- packages/apps/transfer/next.config.js
+++ packages/apps/transfer/next.config.js
@@ -3,5 +3,14 @@ const nextTranslate = require('next-translate-plugin');
 
 module.exports = nextTranslate({
   reactStrictMode: true,
-  pageExtensions: ['tsx'],
+  pageExtensions: ['^((?!(test|spec)).)*.tsx$'],
+  webpack: (config, { dev }) => {
+    config.module.rules.push(
+      {
+        test: /\.(spec|test).*$/,
+        loader: 'ignore-loader'
+      }
+    );
+    return config;
+  }
 });
