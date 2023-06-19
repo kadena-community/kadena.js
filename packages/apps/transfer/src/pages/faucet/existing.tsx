b54d4b6f192a8e18543568e29c536b0dac6bd8eb
Sander Looijenga <slooijenga@anwb.nl>
2023-06-19 13:25:45 +0200

    Reuse layout components from transfer pages

--- packages/apps/transfer/src/pages/faucet/existing.tsx
+++ packages/apps/transfer/src/pages/faucet/existing.tsx
@@ -1,33 +1,89 @@
-import { NextPageWithLayout } from '../_app';
+import { Button, TextField } from '@kadena/react-components';
 
-import { StyledContainer, StyledHeader, StyledSection } from './styles';
+import {
+  StyledAccountForm,
+  StyledForm,
+  StyledFormButton,
+  StyledMainContent,
+} from '../transfer/cross-chain-transfer-finisher/styles';
 
-import { Layout, NestedLayout } from '@/components/Common';
-import { GetStaticProps } from 'next';
-import React from 'react';
+import { type Chain, CHAINS } from './new';
 
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: { title: 'Add Funds to Existing Account' } };
-};
+import MainLayout from '@/components/Common/Layout/MainLayout';
+import { Select, SidebarMenu } from '@/components/Global';
+import { StyledOption } from '@/components/Global/Select/styles';
+import { fundExistingAccount } from '@/services/faucet';
+import useTranslation from 'next-translate/useTranslation';
+import React, {
+  FC,
+  FormEvent,
+  FormEventHandler,
+  useCallback,
+  useState,
+} from 'react';
 
-const ExistingFaucetPage: NextPageWithLayout = (props) => {
-  return (
-    <StyledContainer className="grid grid-cols-1 divide-y">
-      <StyledSection>
-        <StyledHeader>
-          <h2>Account</h2>
-          <a href="#">Fill in public key as your account name +</a>
-        </StyledHeader>
-      </StyledSection>
-    </StyledContainer>
+const ExistingFaucetPage: FC = () => {
+  const { t } = useTranslation('common');
+
+  const [accountName, setAccountName] = useState('');
+  const [chainID, setChainID] = useState<Chain>('0');
+
+  const onFormSubmit = useCallback(async (e: FormEvent) => {
+    e.preventDefault();
+    console.log('onFormSubmit', { e, accountName });
+    // isExistingAccount();
+    await fundExistingAccount(accountName, chainID, 100);
+  }, []);
+
+  const onAccountNameChange = useCallback<FormEventHandler<HTMLInputElement>>(
+    (e) => {
+      setAccountName(e.currentTarget.value);
+    },
+    [],
   );
-};
 
-ExistingFaucetPage.getLayout = function getLayout(page) {
+  const onChainSelectChange = useCallback<FormEventHandler<HTMLSelectElement>>(
+    (e) => {
+      console.log('onChainSelectChange', { value: e.currentTarget.value });
+      setChainID(e.currentTarget.value as Chain);
+    },
+    [],
+  );
+
   return (
-    <Layout>
-      <NestedLayout>{page}</NestedLayout>
-    </Layout>
+    <MainLayout title={t('Add Funds to Existing Account')}>
+      <StyledMainContent>
+        <SidebarMenu />
+        <StyledForm onSubmit={onFormSubmit}>
+          <StyledAccountForm>
+            <TextField
+              label={t(
+                'Associate your public key with a unique account name of your choosing',
+              )}
+              status="error"
+              inputProps={{
+                onChange: onAccountNameChange,
+                leadingText: 'K:',
+              }}
+            />
+            <Select
+              label={t('Chain ID')}
+              onChange={onChainSelectChange}
+              value={chainID}
+              status="error"
+              leadingText="C:"
+            >
+              {CHAINS.map((x) => {
+                return <StyledOption key={`chain-${x}`}>{x}</StyledOption>;
+              })}
+            </Select>
+          </StyledAccountForm>
+          <StyledFormButton>
+            <Button title={t('Fund 100 Coins')}>{t('Fund 100 Coins')}</Button>
+          </StyledFormButton>
+        </StyledForm>
+      </StyledMainContent>
+    </MainLayout>
   );
 };
 
--- packages/apps/transfer/src/pages/faucet/new.tsx
+++ packages/apps/transfer/src/pages/faucet/new.tsx
@@ -1,45 +1,122 @@
-import { NextPageWithLayout } from '../_app';
+import { Button, SystemIcons, TextField } from '@kadena/react-components';
 
-import { StyledContainer, StyledHeader, StyledSection } from './styles';
+import {
+  StyledAccountForm,
+  StyledForm,
+  StyledFormButton,
+  StyledMainContent,
+} from '../transfer/cross-chain-transfer-finisher/styles';
 
-import { Layout, NestedLayout } from '@/components/Common';
-import Notification from '@/components/Global/Notification';
-import { GetStaticProps } from 'next';
-import React from 'react';
+import MainLayout from '@/components/Common/Layout/MainLayout';
+import { Notification, Select, SidebarMenu } from '@/components/Global';
+import { StyledOption } from '@/components/Global/Select/styles';
+import { isExistingAccount } from '@/services/accounts/is-existing';
+import { fundNewAccount } from '@/services/faucet';
+import useTranslation from 'next-translate/useTranslation';
+import React, {
+  FC,
+  FormEvent,
+  FormEventHandler,
+  useCallback,
+  useState,
+} from 'react';
 
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: { title: 'Create and Fund New Account' } };
-};
+export const CHAINS = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+] as const;
 
-const NewFaucetPage: NextPageWithLayout = (props) => {
-  return (
-    <>
-      <Notification
-        title="Before you start"
-        body='Generate a key pair by visiting this <a href="#"><strong>webpage</strong></a> or by downloading a <a href="#"><strong>wallet</strong></a>'
-      />
-      <StyledContainer className="grid grid-cols-1 divide-y">
-        <StyledSection>
-          <StyledHeader>
-            <h2>Account</h2>
-            <a href="#">Fill in public key as your account name +</a>
-          </StyledHeader>
-        </StyledSection>
-        <StyledSection>
-          <StyledHeader>
-            <h2>Public Key</h2>
-          </StyledHeader>
-        </StyledSection>
-      </StyledContainer>
-    </>
+export type ChainTuple = typeof CHAINS;
+export type Chain = ChainTuple[number];
+
+const NewFaucetPage: FC = () => {
+  const { t } = useTranslation('common');
+
+  const [accountName, setAccountName] = useState('');
+  const [chainID, setChainID] = useState<Chain>('0');
+
+  const onFormSubmit = useCallback(async (e: FormEvent) => {
+    e.preventDefault();
+    console.log('onFormSubmit', { e, accountName });
+    // isExistingAccount();
+    await fundNewAccount(accountName, chainID, [
+      '426623443b003a765092ed0896de0ca3aaebf4fdda9e45940e6e9cf36801dada',
+    ]);
+  }, []);
+
+  const onAccountNameChange = useCallback<FormEventHandler<HTMLInputElement>>(
+    (e) => {
+      setAccountName(e.currentTarget.value);
+    },
+    [],
   );
-};
 
-NewFaucetPage.getLayout = function getLayout(page) {
+  const onChainSelectChange = useCallback<FormEventHandler<HTMLSelectElement>>(
+    (e) => {
+      console.log('onChainSelectChange', { value: e.currentTarget.value });
+      setChainID(e.currentTarget.value as Chain);
+    },
+    [],
+  );
+
   return (
-    <Layout>
-      <NestedLayout>{page}</NestedLayout>
-    </Layout>
+    <MainLayout title={t('Create and Fund New Account')}>
+      <StyledMainContent>
+        <SidebarMenu />
+        <StyledForm onSubmit={onFormSubmit}>
+          <Notification
+            title="Before you start"
+            body='Generate a key pair by visiting this <a href="#"><strong>webpage</strong></a> or by downloading a <a href="#"><strong>wallet</strong></a>'
+          />
+          <StyledAccountForm>
+            <TextField
+              label={t(
+                'Associate your public key with a unique account name of your choosing',
+              )}
+              status="error"
+              inputProps={{
+                onChange: onAccountNameChange,
+                leadingText: 'K:',
+              }}
+            />
+            <Select
+              label={t('Chain ID')}
+              onChange={onChainSelectChange}
+              value={chainID}
+              status="error"
+              leadingText="C:"
+            >
+              {CHAINS.map((x) => {
+                return <StyledOption key={`chain-${x}`}>{x}</StyledOption>;
+              })}
+            </Select>
+          </StyledAccountForm>
+          <StyledFormButton>
+            <Button title={t('Create and Fund Account')}>
+              {t('Create and Fund Account')}
+            </Button>
+          </StyledFormButton>
+        </StyledForm>
+      </StyledMainContent>
+    </MainLayout>
   );
 };
 
