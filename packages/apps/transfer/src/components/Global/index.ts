d22203245d933d725e4faab6cab7b5b5fba4d635
Sander Looijenga <slooijenga@anwb.nl>
2023-06-19 13:27:23 +0200

    Re-export Notification component

--- packages/apps/transfer/src/components/Global/index.ts
+++ packages/apps/transfer/src/components/Global/index.ts
@@ -1,3 +1,4 @@
 export { Select } from '@/components/Global/Select';
 export { Option } from '@/components/Global/Select/Option';
 export { SidebarMenu } from '@/components/Global/SidebarMenu';
+export { default as Notification } from '@/components/Global/Notification';
