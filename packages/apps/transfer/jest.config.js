db22c6b27c6a7e00bb9b71caa221a87a09b76eeb
Lars Kappert <lars@webpro.nl>
2023-06-20 14:53:02 +0200

    Revert Jest reporter

--- packages/apps/transfer/jest.config.js
+++ packages/apps/transfer/jest.config.js
@@ -3,12 +3,12 @@ const createJestConfig = nextJest({
   dir: './',
 });
 const customJestConfig = {
-  reporters: ["default", "jest-junit"],
+  reporters: ['jest-standard-reporter'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
   },
   moduleDirectories: ['node_modules', 'src'],
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
 module.exports = createJestConfig(customJestConfig);
