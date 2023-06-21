2cf3f8cf2ccb2e7cf95bde8c9c2e125fb2d2a099
Steven Straatemans <steven@straatemans.nl>
2023-06-21 11:11:25 +0200

    add sprinkles

--- packages/libs/react-ui/src/components/Modal/Modal.css.ts
+++ packages/libs/react-ui/src/components/Modal/Modal.css.ts
@@ -1,45 +1,53 @@
-import { vars } from '../../styles';
-import { breakpoints } from '../../styles/sprinkles.css';
+import { sprinkles } from '../../styles';
 
 import { style } from '@vanilla-extract/css';
 
-export const background = style({
-  position: 'fixed',
-  backgroundColor: vars.colors.$neutral6,
-  opacity: '.8',
-  inset: 0,
-  padding: 0,
-  cursor: 'pointer',
-});
-
-export const wrapper = style({
-  position: 'fixed',
-  inset: 0,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'stretch',
-  pointerEvents: 'none',
-
-  '@media': {
-    [`screen and ${breakpoints.sm}`]: {
-      margin: `0 ${vars.sizes.$4}`,
-    },
-    [`screen and ${breakpoints.md}`]: {
-      margin: `0 auto`,
-      width: '75vw',
-    },
-    [`screen and ${breakpoints.lg}`]: {
-      margin: `0 auto`,
-      width: '50vw',
-      maxWidth: '700px',
-    },
+export const background = style([
+  sprinkles({
+    position: 'fixed',
+    backgroundColor: '$neutral6',
+    padding: 0,
+    cursor: 'pointer',
+  }),
+  {
+    inset: 0,
+    opacity: '.8',
   },
-});
+]);
 
-export const modal = style({
-  display: 'flex',
-  flexDirection: 'column',
-  pointerEvents: 'initial',
-  width: '100%',
-});
+export const wrapper = style([
+  sprinkles({
+    position: 'fixed',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    pointerEvents: 'none',
+    width: {
+      xs: '100%',
+      md: '$screen75',
+    },
+    marginX: {
+      xs: 0,
+      sm: '$4',
+      md: '$auto',
+    },
+  }),
+  {
+    maxWidth: '700px',
+    inset: 0,
+  },
+]);
+
+export const modal = style([
+  sprinkles({
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  }),
+  {
+    maxHeight: '50vh',
+    overflowY: 'scroll',
+    pointerEvents: 'initial',
+  },
+]);
