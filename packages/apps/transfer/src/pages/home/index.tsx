f8422ac9744328d03340744d331c8b7e97b3323b
Sander Looijenga <slooijenga@anwb.nl>
2023-06-23 13:03:52 +0200

    Faucet account creation will be disabled for now

--- packages/apps/transfer/src/pages/home/index.tsx
+++ packages/apps/transfer/src/pages/home/index.tsx
@@ -46,11 +46,11 @@ const Home: FC = () => {
       title: t('Existing account'),
       href: routes.FAUCET_EXISTING,
     },
-    {
-      icon: Chain,
-      title: t('New account'),
-      href: routes.FAUCET_NEW,
-    },
+    // {
+    //   icon: Chain,
+    //   title: t('New account'),
+    //   href: routes.FAUCET_NEW,
+    // },
   ];
 
   return (
