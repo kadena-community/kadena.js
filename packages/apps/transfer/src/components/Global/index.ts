c958e78ab5a356922006763df1f2c2b391201805
Sander Looijenga <slooijenga@anwb.nl>
2023-06-26 15:53:59 +0200

    Remove redundant Notification

--- packages/apps/transfer/src/components/Global/index.ts
+++ packages/apps/transfer/src/components/Global/index.ts
@@ -1,4 +1,3 @@
 export { Select } from '@/components/Global/Select';
 export { Option } from '@/components/Global/Select/Option';
 export { SidebarMenu } from '@/components/Global/SidebarMenu';
-export { default as Notification } from '@/components/Global/Notification';
