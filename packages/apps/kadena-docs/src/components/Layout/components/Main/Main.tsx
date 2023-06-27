12f6861023d036028255e97259e5a9708d80f69d
Steven Straatemans <steven@straatemans.nl>
2023-06-27 14:56:27 +0200

    start style of searchpage

--- packages/apps/kadena-docs/src/components/Layout/components/Main/Main.tsx
+++ packages/apps/kadena-docs/src/components/Layout/components/Main/Main.tsx
@@ -20,7 +20,6 @@ export const Main: FC<IPageProps> = ({
     editLink,
     navigation,
   },
-  topDocs,
   aSideMenuTree,
   leftMenuTree,
 }) => {
--- packages/apps/kadena-docs/src/components/Search/ResultSection.tsx
+++
@@ -1,5 +0,0 @@
-import { styled, StyledComponent } from '@kadena/react-components';
-
-export const ResultSection: StyledComponent<'section'> = styled('section', {
-  width: '50%',
-});
