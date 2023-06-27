bd7087de7bf5caa4217b2cc67389885b32bf6863
Steven Straatemans <steven@straatemans.nl>
2023-06-27 15:09:23 +0200

    fix the build

--- packages/apps/kadena-docs/src/components/Layout/Home/Home.tsx
+++ packages/apps/kadena-docs/src/components/Layout/Home/Home.tsx
@@ -1,8 +1,8 @@
 import { ILayout } from '@/types/Layout';
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 export const Home: FC<ILayout> = ({ children }) => {
-  return children;
+  return <>{children}</>;
 };
 
 Home.displayName = 'Home';
