c673a659d6f54a087bf6068bd8fdea5487b039b9
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-06-27 12:26:51 +0200

    fix sidebar

--- packages/apps/transfer/src/components/Global/SidebarMenu/styles.ts
+++ packages/apps/transfer/src/components/Global/SidebarMenu/styles.ts
@@ -3,6 +3,4 @@ import { styled } from '@kadena/react-components';
 export const StyledSidebar = styled('div', {
   display: 'flex',
   flexDirection: 'column',
-  width: '30%',
-  height: '100%',
 });
