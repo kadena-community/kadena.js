a2f3fdd0f53d69a41ed479df143e5963f5f489f8
Timo Heddes <timo.heddes@gmail.com>
2023-06-28 11:09:12 +0200

    feat: add some basic tests

---
+++ packages/libs/react-ui/src/components/Input/Input.test.tsx
@@ -0,0 +1,13 @@
+import { Input } from './Input';
+
+import { render } from '@testing-library/react';
+import React from 'react';
+
+describe('Input', () => {
+  test('renders correctly', () => {
+    const { getByTestId } = render(<Input id="test-input" />);
+
+    const inputContainer = getByTestId('kda-input');
+    expect(inputContainer).toBeInTheDocument();
+  });
+});
