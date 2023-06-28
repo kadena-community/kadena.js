c5f782a4b288dffda0ba5ab500fd04b06f5bc7ab
Timo Heddes <timo.heddes@gmail.com>
2023-06-28 10:24:31 +0200

    chore: remove unnecessary async

--- packages/libs/react-ui/src/components/Tag/Tag.test.tsx
+++ packages/libs/react-ui/src/components/Tag/Tag.test.tsx
@@ -4,10 +4,10 @@ import { render } from '@testing-library/react';
 import React from 'react';
 
 describe('Tag', () => {
-  test('renders correctly', async () => {
+  test('renders correctly', () => {
     const { getByTestId } = render(<Tag>Hello, Tag!</Tag>);
 
-    const tagContainer = await getByTestId('kda-tag');
+    const tagContainer = getByTestId('kda-tag');
     expect(tagContainer).toBeInTheDocument();
   });
 });
