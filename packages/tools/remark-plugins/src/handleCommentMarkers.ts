d9a863d677d8e3541198dfb077818fd6460b3bf0
Lars Kappert <lars@webpro.nl>
2023-06-28 15:40:16 +0200

    Add vFile argument to comment marker functions

--- packages/tools/remark-plugins/src/handleCommentMarkers.ts
+++ packages/tools/remark-plugins/src/handleCommentMarkers.ts
@@ -2,12 +2,13 @@ import type { Content, Root } from 'mdast';
 import { commentMarker } from 'mdast-comment-marker';
 import { zone } from 'mdast-zone';
 import { visit } from 'unist-util-visit';
+import type { VFile } from 'vfile';
 
 type Node = Root | Content;
-type CommentMarkers = Record<string, () => Node | undefined>;
+type CommentMarkers = Record<string, (vFile: VFile) => Node | undefined>;
 
 export const handleCommentMarkers =
-  (commentMarkers: CommentMarkers) => (tree: Root) => {
+  (commentMarkers: CommentMarkers) => (tree: Root, vFile: VFile) => {
     visit(tree, (node) => {
       const info = commentMarker(node);
       if (!info) return;
@@ -15,7 +16,7 @@ export const handleCommentMarkers =
       const name = info.name;
       if (typeof commentMarkers[name] !== 'function') return;
 
-      const nodes = commentMarkers[name]();
+      const nodes = commentMarkers[name](vFile);
 
       zone(tree, name, (start, _nodes, end) => [start, nodes, end]);
     });
