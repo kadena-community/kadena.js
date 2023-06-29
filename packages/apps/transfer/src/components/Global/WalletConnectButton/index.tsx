8fac1b9457dee00e811a682addfce5a0c0bc7ebf
Steven Straatemans <steven@straatemans.nl>
2023-06-29 15:07:35 +0200

    fix the button component

--- packages/apps/transfer/src/components/Global/WalletConnectButton/index.tsx
+++ packages/apps/transfer/src/components/Global/WalletConnectButton/index.tsx
@@ -1,5 +1,4 @@
-import { SystemIcons } from '@kadena/react-components';
-import { Button } from '@kadena/react-ui';
+import { Button, SystemIcon } from '@kadena/react-ui';
 
 import useTranslation from 'next-translate/useTranslation';
 import React, { FC } from 'react';
@@ -7,9 +6,9 @@ import React, { FC } from 'react';
 export const WalletConnectButton: FC = () => {
   const { t } = useTranslation();
   return (
-    <Button title={t('Connect your wallet')} color="positive">
+    <Button.Root title={t('Connect your wallet')} color="positive">
       {t('Connect your wallet')}
-      <SystemIcons.Link />
-    </Button>
+      <Button.Icon icon={SystemIcon.Link} />
+    </Button.Root>
   );
 };
