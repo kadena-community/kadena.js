5407ec54c871ad7bdd5c4c77949e6d971612df5c
Timo Heddes <timo.heddes@gmail.com>
2023-06-29 09:57:58 +0200

    chore: fix breadcrumbs test and import

--- packages/libs/react-ui/src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ packages/libs/react-ui/src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -1,12 +1,12 @@
-import { BreadcrumbsContainer } from '@components/Breadcrumbs/Breadcrumbs';
+import { Breadcrumbs } from '@components/Breadcrumbs';
 import { render } from '@testing-library/react';
 import React from 'react';
 
 describe('Breadcrumps', () => {
   test('renders correctly', () => {
-    const { getByTestId } = render(<BreadcrumbsContainer />);
+    const { getByTestId } = render(<Breadcrumbs.Root />);
 
-    const boxContainer = getByTestId('kda-breadcrumps');
+    const boxContainer = getByTestId('kda-breadcrumbs');
     expect(boxContainer).toBeInTheDocument();
   });
 });
