687c6f0043a6f3af62266c61fbcbe789e495d5b0
Reza Erami <reza.eramikalleh@deptagency.com>
2023-06-29 13:21:58 +0200

    fix eslint
    - move global rules into the lib
    - move frontend rules in react
    - add necessary packages to eslint-config module
    - remove packages from transfer

--- packages/tools/eslint-config/profile/lib.js
+++ packages/tools/eslint-config/profile/lib.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../../../../.prettierrc'), 'utf8'),
+);
+
 module.exports = {
   root: true,
   extends: [
@@ -10,8 +17,22 @@ module.exports = {
     '../mixins/simple-import-sort.js',
     '../mixins/typedef-allow-implicitly-typed-parameters.js',
   ],
-  plugins: ['@kadena-dev/eslint-plugin', 'import', 'simple-import-sort'],
+  plugins: [
+    '@kadena-dev/eslint-plugin',
+    'import',
+    'simple-import-sort',
+    'prettier',
+  ],
   rules: {
     '@kadena-dev/no-eslint-disable': 'error',
+    'prettier/prettier': ['warn', prettierOptions],
+    'prefer-template': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-unresolved': [
+      'warn',
+      {
+        ignore: ['^@/'], // Ignore custom paths starting with '@/'
+      },
+    ],
   },
 };
--- packages/tools/eslint-config/profile/react.js
+++ packages/tools/eslint-config/profile/react.js
@@ -1,19 +1,41 @@
 module.exports = {
   root: true,
   extends: ['./lib', 'plugin:react/recommended'],
-  plugins: ['import', 'simple-import-sort', 'react'],
+  plugins: ['import', 'simple-import-sort', 'react', 'jsx-a11y'],
   rules: {
     '@rushstack/typedef-var': 'off',
     // @kadena-dev/typedef-var allows for inferred types in exported constants
     //  when they are created by a function call.
     //  e.g. This is allowed: `export const StyledButton = styled('button', {})`
     '@kadena-dev/typedef-var': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/mouse-events-have-key-events': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
+  // turned off boolean expressions for jsx and tsx files, so we can have non-boolean statements in react components
+  // this rule should be enabled for js and ts files even in a ReactJS or NextJS setup
+  overrides: [
+    {
+      files: ['*.jsx', '*.tsx'],
+      rules: {
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+      },
+    },
+  ],
 
   // rules: {
   //   '@typescript-eslint/explicit-function-return-type': {
