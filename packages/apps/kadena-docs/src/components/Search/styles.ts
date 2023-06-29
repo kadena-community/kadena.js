90bc2c256fb16060e7025217afdcabf17c660e79
Steven <steven@straatemans.nl>
2023-06-29 16:10:12 +0200

    feat(docs): style search results (#513)

--- packages/apps/kadena-docs/src/components/Search/styles.ts
+++ packages/apps/kadena-docs/src/components/Search/styles.ts
@@ -8,3 +8,23 @@ export const StaticResultsList: StyledComponent<'ul'> = styled('ul', {
   listStyle: 'none',
   padding: 0,
 });
+
+export const ScrollBox: StyledComponent<'div'> = styled('div', {
+  height: '55vh',
+  overflowY: 'scroll',
+  margin: '$2 0',
+});
+
+export const StyledItem: StyledComponent<'li'> = styled('li', {
+  marginBottom: '$4',
+  '& a': {
+    textDecoration: 'none',
+
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  '& > span': {
+    color: '$neutral4',
+  },
+});
