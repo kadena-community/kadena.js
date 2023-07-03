e7ac8612fc5ce21f587f265f461b924452b107ff
Reza Erami <reza.eramikalleh@deptagency.com>
2023-07-03 09:11:58 +0200

    fix use-did-update-effect args

--- packages/apps/transfer/src/hooks/use-did-update-effect.ts
+++ packages/apps/transfer/src/hooks/use-did-update-effect.ts
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export default (fn: Function, inputs: unknown[]): boolean => {
+export default (fn: () => void, inputs: unknown[]): boolean => {
   const didMountRef = useRef(false);
 
   useEffect(() => {
