c379862c525204603c0210312b19105e56b8c806
Timo Heddes <timo.heddes@gmail.com>
2023-07-11 14:41:42 +0200

    test: add some more tests

--- packages/libs/react-ui/src/components/Button/Button.test.tsx
+++ packages/libs/react-ui/src/components/Button/Button.test.tsx
@@ -11,4 +11,42 @@ describe('Button', () => {
     const buttonContainer = getByTestId('kda-button');
     expect(buttonContainer).toBeInTheDocument();
   });
+
+  it('disables the button when `disabled` prop is true', () => {
+    const { getByTestId } = render(
+      <Button.Root title="Button" disabled>
+        Hello, Button!
+      </Button.Root>,
+    );
+
+    const buttonContainer = getByTestId('kda-button') as HTMLButtonElement;
+
+    expect(buttonContainer.disabled).toBe(true);
+  });
+
+  it('renders as an anchor element when `as` prop = "a"', () => {
+    const { getByTestId } = render(
+      <Button.Root title="Button" as="a" href="https://kadena.io/">
+        Hello, Button!
+      </Button.Root>,
+    );
+
+    const buttonContainer = getByTestId('kda-button') as HTMLAnchorElement;
+
+    console.log(buttonContainer.nodeName);
+
+    expect(buttonContainer.nodeName === 'A').toBe(true);
+  });
+
+  it('requires the `href` prop to be set when rendered as anchor', () => {
+    const { getByTestId } = render(
+      <Button.Root title="Button" as="a">
+        Hello, Button!
+      </Button.Root>,
+    );
+
+    const buttonContainer = getByTestId('kda-button') as HTMLAnchorElement;
+
+    expect(buttonContainer.nodeName === 'BUTTON').toBe(true);
+  });
 });
