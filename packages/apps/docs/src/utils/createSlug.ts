77a4934afe6b853401101782d00f6b9e4097d48f
Mohamed Ismail <ismailreality1@gmail.com>
2023-07-11 11:30:37 +0200

    fix(kadena-docs): update slug method for operators doc links

--- packages/apps/docs/src/utils/createSlug.ts
+++ packages/apps/docs/src/utils/createSlug.ts
@@ -1,10 +1,30 @@
+import slugify from 'slugify';
+
+slugify.extend({
+  '!': 'isNot',
+  '&': 'bitwiseAnd',
+  '<=': 'lessThanOrEqual',
+  '>=': 'greaterThanOrEqual',
+  '==': 'equal',
+  '!=': 'notEqual',
+  '===': 'strictEqual',
+  '=': 'equal',
+  '+': 'add',
+  '-': 'subtract',
+  '*': 'multiply',
+  '/': 'divide',
+  '^': 'power',
+  '~': 'reverse',
+  '|': 'bitwiseOr',
+  '?': 'logical',
+});
+
 export const createSlug = (str?: string): string => {
   if (str === undefined) return '';
-  return str
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/[^\w\s]+/g, '')
-    .replace(/ /g, '-')
-    .toLowerCase()
-    .replace(/^-+|-+$/g, '');
+
+  return slugify(str, {
+    lower: true,
+    replacement: '-', // replace spaces with replacement character, defaults to `-`
+    remove: /[^\w\s]+/g, // remove characters that match regex, defaults to `undefined`
+  });
 };
