40f970387dc767a33c418af458fa413edc26df52
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-07-11 13:32:07 +0200

    Initial Implementation of storybook tests

--- packages/libs/react-ui/.storybook/main.ts
+++ packages/libs/react-ui/.storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-coverage',
     'storybook-dark-mode',
   ],
   framework: {
