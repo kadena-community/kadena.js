6232046b2d6126f9b084718965cd4bf91d8009a9
Mohamed Ismail <ismailreality1@gmail.com>
2023-07-12 13:19:56 +0200

    fix(kadena-docs): fix slug to concatenate index

--- packages/apps/docs/src/utils/createSlug.ts
+++ packages/apps/docs/src/utils/createSlug.ts
@@ -1,10 +1,16 @@
-export const createSlug = (str?: string): string => {
+export const createSlug = (str?: string, index?: number): string => {
   if (str === undefined) return '';
-  return str
+  const normalizedSlug = str
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
     .replace(/[^\w\s]+/g, '')
     .replace(/ /g, '-')
     .toLowerCase()
     .replace(/^-+|-+$/g, '');
+
+  if (normalizedSlug === '' && index !== undefined) return `operator-${index}`;
+
+  if (index === undefined) return normalizedSlug;
+
+  return `${normalizedSlug}-${index}`;
 };
--- packages/apps/docs/src/utils/tests/createSlug.test.ts
+++ packages/apps/docs/src/utils/tests/createSlug.test.ts
@@ -1,12 +1,29 @@
 import { createSlug } from '..';
 
 describe('utils createSlug', () => {
-  test('should return a slug from given string"', () => {
+  it('should return a slug from given string"', () => {
     expect(createSlug('By the power of grayskull!')).toEqual(
       'by-the-power-of-grayskull',
     );
     expect(createSlug('here comes spëcíāl characters')).toEqual(
       'here-comes-special-characters',
     );
+  });
+
+  it('should return a slug from given string with index', () => {
+    expect(createSlug('By the power of grayskull!', 1)).toEqual(
+      'by-the-power-of-grayskull-1',
+    );
+    expect(createSlug('here comes spëcíāl characters', 2)).toEqual(
+      'here-comes-special-characters-2',
+    );
+  });
+
+  it('should return operator-{index} if slug is empty', () => {
+    expect(createSlug('', 1)).toEqual('operator-1');
+  });
+
+  it('should return empty string if slug is empty and no index is given', () => {
+    expect(createSlug('')).toEqual('');
   });
 });
