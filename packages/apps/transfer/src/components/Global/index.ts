8cb9913b3047d2908e4019fe1a43ca4ec9be550b
Sander Looijenga <slooijenga@anwb.nl>
2023-07-13 17:34:51 +0200

    Export chain select from global components

--- packages/apps/transfer/src/components/Global/index.ts
+++ packages/apps/transfer/src/components/Global/index.ts
@@ -4,3 +4,4 @@ export { SidebarMenu } from '@/components/Global/SidebarMenu';
 export { Container } from '@/components/Global/Container';
 export { GridCol, GridRow } from '@/components/Global/Grid';
 export { WalletConnectButton } from '@/components/Global/WalletConnectButton';
+export { ChainSelect } from '@/components/Global/ChainSelect';
