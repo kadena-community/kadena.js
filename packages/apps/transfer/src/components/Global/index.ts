743eb84dcab91c3325090e426fecd8acb106ff6a
Sander Looijenga <slooijenga@anwb.nl>
2023-07-13 17:42:02 +0200

    re-export OnChainSelectChange type

--- packages/apps/transfer/src/components/Global/index.ts
+++ packages/apps/transfer/src/components/Global/index.ts
@@ -4,4 +4,7 @@ export { SidebarMenu } from '@/components/Global/SidebarMenu';
 export { Container } from '@/components/Global/Container';
 export { GridCol, GridRow } from '@/components/Global/Grid';
 export { WalletConnectButton } from '@/components/Global/WalletConnectButton';
-export { ChainSelect } from '@/components/Global/ChainSelect';
+export {
+  ChainSelect,
+  type OnChainSelectChange,
+} from '@/components/Global/ChainSelect';
