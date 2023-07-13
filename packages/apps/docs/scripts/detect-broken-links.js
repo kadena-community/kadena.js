17d69f8b828e92f696bfaa7f46febcbe280838ff
bearni95 <bernatcanal@gmail.com>
2023-07-13 11:36:43 -0400

    added check for tsx files; added link check for external urls

--- packages/apps/docs/scripts/detect-broken-links.js
+++ packages/apps/docs/scripts/detect-broken-links.js
@@ -3,42 +3,66 @@ const path = require('path');
 const http = require('http');
 const https = require('https');
 
+const externalLinks = {}
 
-function extractBrokenLinksFromMdFile(filePath) {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const linkRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
-    const links = [];
-    let match;
+function getBrokenLinks(filePath, links) {
+  const brokenLinks = [];
+  const directory = path.dirname(filePath);
+  links.forEach((link, index) => {
+    // clean the link of hash fragments
+    link = link.split('#')[0];
+    if (link.length === 0) {
+      return
+    }
+    if (link.startsWith('http')) {
+      if (!externalLinks[filePath]) {
+        externalLinks[filePath] = []
+      }
 
-    while (match = linkRegex.exec(fileContent)) {
-        links.push(match[2]);
+      externalLinks[filePath].push(link)
+      return;
     }
 
-    const brokenLinks = [];
-    const directory = path.dirname(filePath);
-    links.forEach((link, index) => {
-        // clean the link of hash fragments
-        link = link.split('#')[0];
-        if (link.length === 0){
-            return
-        }
-        // ignore external links, for now
-        if (link.startsWith('http')) {
-            return;
-        }
+    if (link.startsWith('/assets')) {
+      links[index] = path.join('../public/', link)
+    } else {
+      links[index] = path.join(directory, link);
+    }
 
-        if (link.startsWith('/assets')) {
-            links[index] = path.join('../public/', link)
-        } else {
-            links[index] = path.join(directory, link);
-        }
+    if (!fs.existsSync(links[index])) {
+      brokenLinks.push(links[index]);
+    }
+  });
 
-        if (!fs.existsSync(links[index])) {
-            brokenLinks.push(links[index]);
-        }
-    });
+  return brokenLinks
+}
 
-    return brokenLinks;
+function extractBrokenLinksFromTsFile(filePath) {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const linkRegex = /<a href="([^"]+)">/g;
+  const links = [];
+  let match;
+
+  while (match = linkRegex.exec(fileContent)) {
+    links.push(match[1]);
+  }
+
+  const broken = getBrokenLinks(filePath, links)
+  return broken
+}
+function extractBrokenLinksFromMdFile(filePath) {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const linkRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
+  const links = [];
+  let match;
+
+  while (match = linkRegex.exec(fileContent)) {
+    links.push(match[2]);
+  }
+
+  const brokenLinks = getBrokenLinks(filePath, links)
+
+  return brokenLinks;
 }
 
 const filesWithBrokenLinks = {}
@@ -56,18 +80,84 @@ function processFiles(directory) {
 
       if (fileExtension === '.md' || fileExtension === '.mdx') {
         const brokenLinks = extractBrokenLinksFromMdFile(filePath);
-        if (brokenLinks.length > 0){
-            filesWithBrokenLinks[filePath] = brokenLinks;
+        if (brokenLinks.length > 0) {
+          filesWithBrokenLinks[filePath] = brokenLinks;
+        }
+      }
+
+      if (fileExtension === '.tsx') {
+        const brokenLinks = extractBrokenLinksFromTsFile(filePath);
+        if (brokenLinks.length > 0) {
+          filesWithBrokenLinks[filePath] = brokenLinks;
         }
       }
     }
   });
 }
 
-const main = () => {
-    const directoryPath = path.join(__dirname, '../src');
-    processFiles(directoryPath);
-    console.log(filesWithBrokenLinks)
+async function wait(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+}
+
+async function checkExternalLink(link) {
+  await wait(500)
+  return new Promise((resolve, reject) => {
+    // establish a timeout to fail
+    setTimeout(() => {
+      resolve(false)
+    }, 5000)
+
+    const method = link.startsWith('https') ? https.get : http.get
+    method(link, res => {
+      console.log('checking external link', link)
+      if (res.statusCode !== 200) {
+        resolve(false)
+      }
+      resolve(true)
+    }).on('error', e => {
+      resolve(false)
+    })
+  })
+}
+
+async function processExternalLinks(externalLinks) {
+  for (const filePath in externalLinks) {
+    const links = externalLinks[filePath]
+    for (const link of links) {
+      const isValid = await checkExternalLink(link)
+      if (!isValid) {
+        if (!filesWithBrokenLinks[filePath]) {
+          filesWithBrokenLinks[filePath] = []
+        }
+        filesWithBrokenLinks[filePath].push(link)
+      }
+    }
+  }
+
+  console.log('finished processing external links')
+}
+
+const main = async () => {
+  const directoryPath = path.join(__dirname, '../src');
+  processFiles(directoryPath);
+
+  await processExternalLinks(externalLinks)
+  
+  // write filesWithBrokenLinks as a markdown file
+  const reportMd = ['# Broken links']
+  for (const filePath in filesWithBrokenLinks) {
+    const links = filesWithBrokenLinks[filePath]
+    reportMd.push(`## ${filePath}`)
+    for (const link of links) {
+      reportMd.push(`- ${link}`)
+    }
+  }
+
+  fs.writeFileSync(path.join(__dirname, '../broken-links.md'), reportMd.join('\n'))
 }
 
 main()
