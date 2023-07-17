c9bfb54a4c154e4808a16f48f2c9a97ebe9a2e72
Javad Khalilian <khalilian.javad@gmail.com>
2023-07-17 14:36:29 +0200

    fix(pactjs-generator): typo

--- packages/libs/pactjs-generator/src/contract/parsing/utils/parser-utilities/str.ts
+++ packages/libs/pactjs-generator/src/contract/parsing/utils/parser-utilities/str.ts
@@ -6,7 +6,7 @@ export const str: IParser<string> = rule((pointer) => {
   const token = pointer.next();
   if (token !== undefined && token.type === 'string')
     return trim(token.value, '"');
-  // for now we only consider symbols as string, we can split this two different parser if there is separate scenario
+  // For now, we are treating symbols as strings. If there is a separate scenario, we can split this parser into two different parsers
   if (token?.type === 'symbol') return trim(token.value, "'");
   return FAILED;
 });
