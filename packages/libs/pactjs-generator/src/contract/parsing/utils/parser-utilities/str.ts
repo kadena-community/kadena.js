e288e7023f50b10ecd407722c228f85fc67539e9
Javad Khalilian <khalilian.javad@gmail.com>
2023-07-17 14:28:27 +0200

    fix(pactjs-generator): parse symbols as string

--- packages/libs/pactjs-generator/src/contract/parsing/utils/parser-utilities/str.ts
+++ packages/libs/pactjs-generator/src/contract/parsing/utils/parser-utilities/str.ts
@@ -4,6 +4,9 @@ import { FAILED, IParser, rule } from './rule';
 
 export const str: IParser<string> = rule((pointer) => {
   const token = pointer.next();
-  if (!token || token.type !== 'string') return FAILED;
-  return trim(token.value, '"');
+  if (token !== undefined && token.type === 'string')
+    return trim(token.value, '"');
+  // for now we only consider symbols as string, we can split this two different parser if there is separate scenario
+  if (token?.type === 'symbol') return trim(token.value, "'");
+  return FAILED;
 });
