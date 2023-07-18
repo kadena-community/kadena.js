08d7874d6e4e513fb156d5c7ca07cae50b922e50
Sander Looijenga <slooijenga@anwb.nl>
2023-07-18 14:04:32 +0200

    Fix lint warnings in useDidUpdateEffect hook

--- packages/apps/transfer/src/hooks/use-did-update-effect.ts
+++ packages/apps/transfer/src/hooks/use-did-update-effect.ts
@@ -1,12 +1,15 @@
 import { useEffect, useRef } from 'react';
 
-export default (fn: () => void, inputs: unknown[]): boolean => {
+const useDidUpdateEffect = (fn: () => void, inputs: unknown[]): boolean => {
   const didMountRef = useRef(false);
 
   useEffect(() => {
     if (didMountRef.current) fn();
     else didMountRef.current = true;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, inputs);
 
   return true;
 };
+
+export default useDidUpdateEffect;
