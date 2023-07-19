f6dfe54f00d523470ca83b6d0dd9d187fa72f045
Javad Khalilian <khalilian.javad@gmail.com>
2023-07-19 10:28:26 +0200

    test(client): unit test for pact-values

--- packages/libs/client/src/utils/tests/pact-values.test.ts
+++ packages/libs/client/src/utils/tests/pact-values.test.ts
@@ -1,7 +1,25 @@
-import { readKeyset } from '../pact-values';
+import { decimal, integer, readKeyset, reference } from '../pact-values';
 
 describe('readKeyset', () => {
   it('returns read-keyset string', () => {
     expect(readKeyset('ks')()).toBe('(read-keyset "ks")');
+  });
+});
+
+describe('reference', () => {
+  it('returns a function that returns the string = always in FP', () => {
+    expect(reference('free.contract')()).toBe('free.contract');
+  });
+});
+
+describe('decimal', () => {
+  it('returns pact decimal object', () => {
+    expect(decimal('12')).toEqual({ decimal: '12.0' });
+  });
+});
+
+describe('integer', () => {
+  it('returns pact integer object', () => {
+    expect(integer('12')).toEqual({ int: '12' });
   });
 });
