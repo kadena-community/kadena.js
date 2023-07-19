9b9cb8221aa8f2145d5b8a0b39ee5a2ec3781acc
bearni95 <bernatcanal@gmail.com>
2023-07-19 10:33:19 -0400

    split stories for GridRoot and GridItem column controls

--- packages/libs/react-ui/src/components/Grid/Grid.stories.tsx
+++ packages/libs/react-ui/src/components/Grid/Grid.stories.tsx
@@ -1,11 +1,16 @@
-import { gapVariants } from './Grid.css';
+import { gapVariants, ResponsiveInputType } from './Grid.css';
+import { IGridRootProps } from './GridRoot';
 import { ContentClass } from './stories.css';
 
 import { Grid } from '@components/Grid';
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 
-const meta: Meta<typeof Grid.Root> = {
+const meta: Meta<
+  {
+    columnSpan: ResponsiveInputType;
+  } & IGridRootProps
+> = {
   title: 'Layout/Grid',
   component: Grid.Root,
   argTypes: {
@@ -23,11 +28,25 @@ const meta: Meta<typeof Grid.Root> = {
         xxl: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       },
     },
+    columnSpan: {
+      control: { type: 'object' },
+      options: {
+        sm: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        md: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        lg: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        xl: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        xxl: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      },
+    },
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof Grid.Root>;
+type Story = StoryObj<
+  {
+    columnSpan: ResponsiveInputType;
+  } & IGridRootProps
+>;
 
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
@@ -35,15 +54,15 @@ type Story = StoryObj<typeof Grid.Root>;
  * to learn how to use render functions.
  */
 
-export const Primary: Story = {
+export const GridRoot: Story = {
   name: 'Grid',
   args: {
     spacing: 'xl',
     columns: {
-      sm: 6,
-      md: 12,
-      lg: 12,
-      xl: 12,
+      sm: 2,
+      md: 4,
+      lg: 6,
+      xl: 10,
       xxl: 12,
     },
   },
@@ -56,41 +75,16 @@ export const Primary: Story = {
           </Grid.Item>
         ))}
 
-        <Grid.Item
-          columnSpan={{
-            sm: 6,
-            md: 4,
-            lg: 3,
-          }}
-        >
+        <Grid.Item>
           <div className={ContentClass}>2</div>
         </Grid.Item>
-        <Grid.Item
-          columnSpan={{
-            sm: 6,
-            md: 4,
-            lg: 3,
-          }}
-        >
+        <Grid.Item>
           <div className={ContentClass}>3</div>
         </Grid.Item>
-        <Grid.Item
-          columnSpan={{
-            sm: 6,
-            md: 4,
-            lg: 3,
-          }}
-        >
+        <Grid.Item>
           <div className={ContentClass}>4</div>
         </Grid.Item>
-        <Grid.Item
-          columnSpan={{
-            sm: 6,
-            md: 4,
-            lg: 3,
-          }}
-          rowSpan={3}
-        >
+        <Grid.Item>
           <div className={ContentClass}>5</div>
         </Grid.Item>
         <Grid.Item>
@@ -100,9 +94,34 @@ export const Primary: Story = {
           <div className={ContentClass}>7</div>
         </Grid.Item>
 
-        <Grid.Item columnSpan={12}>
+        <Grid.Item>
           <div className={ContentClass}>8</div>
         </Grid.Item>
+      </Grid.Root>
+    </>
+  ),
+};
+
+export const GridItem: Story = {
+  args: {
+    spacing: 'xl',
+    columns: 12,
+    columnSpan: {
+      sm: 2,
+      md: 4,
+      lg: 6,
+      xl: 10,
+      xxl: 12,
+    },
+  },
+  render: ({ spacing, columns, columnSpan }) => (
+    <>
+      <Grid.Root spacing={spacing} columns={columns}>
+        {Array.from(new Array(12)).map((empty, i) => (
+          <Grid.Item key={i} columnSpan={columnSpan}>
+            <div className={ContentClass}>{i}</div>
+          </Grid.Item>
+        ))}
       </Grid.Root>
     </>
   ),
