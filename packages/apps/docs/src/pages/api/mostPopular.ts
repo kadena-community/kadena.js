f9cb96dd771de010293126ed57a229e087a6c260
Mohamed Ismail <ismailreality1@gmail.com>
2023-07-20 11:30:50 +0200

    feat(docs-most-popular): add cache validation check for most popular pages

--- packages/apps/docs/src/pages/api/mostPopular.ts
+++ packages/apps/docs/src/pages/api/mostPopular.ts
@@ -1,6 +1,8 @@
 import analyticsDataClient from '@/utils/analyticsDataClient';
 import storeAnalyticsData from '@/utils/storeAnalyticsData';
+import fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
 
 const lastMonths = 3;
 
@@ -18,15 +20,46 @@ const day = startDate.getDate();
 // Put it in Google's date format
 const dayFormat = `${year}-${month - lastMonths}-${day}`;
 
+function getModifiedTimeInSeconds(file: string): number | undefined {
+  if (!fs.existsSync(file)) return;
+
+  const stats = fs.statSync(file, { bigint: false });
+  if (!stats.isFile() || stats.mtime === undefined) return;
+
+  const seconds =
+    (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000;
+  return seconds;
+}
+
+function validateCache(): boolean {
+  const dayInSecs = 86400;
+  const dataFilePath = path.join(process.cwd(), 'src/data/mostPopular.json');
+
+  const fileLastModifiedTime: number | undefined =
+    getModifiedTimeInSeconds(dataFilePath) ?? 0;
+
+  if (!fileLastModifiedTime) return true;
+
+  if (fileLastModifiedTime > dayInSecs) return true;
+
+  return false;
+}
+
 const mostPopular = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
+  if (!validateCache()) {
+    const dataFilePath = path.join(process.cwd(), 'src/data/mostPopular.json');
+    const data = fs.readFileSync(dataFilePath, 'utf8');
+    res.status(200).json(JSON.parse(data));
+    return;
+  }
+
   const [response] = await analyticsDataClient.runReport({
     property: `properties/${process.env.GOOGLE_ANALYTICS_PROPERTY_ID}`,
     dateRanges: [
       {
-        // Run from today to 31 days ago
         startDate: dayFormat,
         endDate: 'today',
       },
