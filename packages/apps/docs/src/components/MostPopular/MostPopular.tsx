425574081571298c3e35d1e9d02f2106be15172d
Mohamed Ismail <ismailreality1@gmail.com>
2023-07-21 16:11:42 +0200

    feat(docs-most-popular): move data fetch to parent component and make as a props

--- packages/apps/docs/src/components/MostPopular/MostPopular.tsx
+++ packages/apps/docs/src/components/MostPopular/MostPopular.tsx
@@ -1,17 +1,19 @@
 import { BrowseSection } from '../BrowseSection';
 
-import { useFetchMostPopularPages } from './useFetchMostPopularPages';
-
+import { IMostPopularPage } from '@/types/MostPopularData';
 import Link from 'next/link';
 import React, { FC } from 'react';
 
-const MostPopular: FC = () => {
-  const { data } = useFetchMostPopularPages();
+interface IProps {
+  pages: IMostPopularPage[];
+  title: string;
+}
 
+const MostPopular: FC<IProps> = ({ pages = [], title }) => {
   return (
     <BrowseSection>
-      <BrowseSection.LinkList title="Most Popular">
-        {data.map((page, index) => {
+      <BrowseSection.LinkList title={title}>
+        {pages.map((page, index) => {
           return (
             <Link href={page.path} key={`most-popular-${index}`}>
               {page.title}
