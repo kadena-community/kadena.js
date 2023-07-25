3cefb43a2a2b9fa4b10fa3f11671994010d6703e
Timo Heddes <timo.heddes@gmail.com>
2023-07-25 14:02:58 +0200

    feat: refactor button

--- packages/libs/react-ui/src/components/Button/index.tsx
+++ packages/libs/react-ui/src/components/Button/index.tsx
@@ -1,17 +1,4 @@
-import type { IButtonProps } from './Button';
-import { Button as ButtonContainer } from './Button';
-import type { IButtonIconProps } from './ButtonIcon';
-import { ButtonIcon } from './ButtonIcon';
+import { Button, IButtonProps } from './Button';
 
-import { FC } from 'react';
-
-interface IButton {
-  Root: FC<IButtonProps>;
-  Icon: FC<IButtonIconProps>;
-}
-
-export type { IButtonProps };
-export const Button: IButton = {
-  Root: ButtonContainer,
-  Icon: ButtonIcon,
-};
+export { IButtonProps };
+export { Button };
