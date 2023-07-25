368f43ef883fac975fad08b868df0894b6b0741f
Steven <steven@straatemans.nl>
2023-07-25 12:40:36 +0200

    chore(docs): refactor templates (#651)

---
+++ packages/apps/docs/src/components/Layout/Blog/styles.ts
@@ -0,0 +1,32 @@
+import { styled, StyledComponent } from '@kadena/react-components';
+
+import { BasePageGrid } from '../components';
+
+export const PageGrid: StyledComponent<typeof BasePageGrid> = styled(
+  BasePageGrid,
+  {
+    gridTemplateColumns: 'auto auto',
+    gridTemplateAreas: `
+            "header header"
+            "pageheader pageheader"
+            "content content"
+            "footer footer"
+          `,
+
+    '@md': {
+      gridTemplateColumns:
+        '1% $leftSideWidth minmax(auto, calc($pageWidth - $leftSideWidth)) 1%',
+
+      gridTemplateAreas: `
+              "header header header header"
+              "pageheader pageheader pageheader pageheader"
+              ". menu content ."
+              "footer footer footer footer"
+            `,
+    },
+    '@2xl': {
+      gridTemplateColumns:
+        'minmax(1%, auto) $leftSideWidth minmax(auto, calc($pageWidth - $leftSideWidth)) minmax(1%, auto)',
+    },
+  },
+);
