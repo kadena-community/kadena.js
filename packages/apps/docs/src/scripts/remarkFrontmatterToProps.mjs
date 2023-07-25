583c6236fbe0e9483fc02a157eb2990856690044
Steven <steven@straatemans.nl>
2023-07-25 15:32:51 +0200

    chore(docs): add readtime to metadata (#657)

--- packages/apps/docs/src/scripts/remarkFrontmatterToProps.mjs
+++ packages/apps/docs/src/scripts/remarkFrontmatterToProps.mjs
@@ -64,6 +64,19 @@ const createNavigation = (file) => {
   };
 };
 
+const getReadTime = (content) => {
+  const WORDS_PER_MINUTE = 200;
+  let result = {};
+  //Matches words
+  //See
+  //https://regex101.com/r/q2Kqjg/6
+  const regex = /\w+/g;
+  result.wordCount = (content || '').match(regex).length;
+  result.readingTimeInMinutes = Math.ceil(result.wordCount / WORDS_PER_MINUTE);
+
+  return result;
+};
+
 const remarkFrontmatterToProps = () => {
   return async (tree, file) => {
     tree.children = tree.children.map((node) => {
@@ -74,6 +87,7 @@ const remarkFrontmatterToProps = () => {
         type: 'props',
         data: {
           frontmatter: {
+            ...getReadTime(file.value),
             editLink:
               process.env.NEXT_PUBLIC_GIT_EDIT_ROOT +
               getFileNameInPackage(file),
