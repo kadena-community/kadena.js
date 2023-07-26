77b8eb88df4504510afaa6863688fa3ed5c3d72f
Bart Huijgen <bart@codecapi.nl>
2023-07-26 13:10:36 +0200

    feat: immutable records app boilerplate

---
+++ packages/apps/immutable-records/next.config.mjs
@@ -0,0 +1,7 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withVanillaExtract(nextConfig);
