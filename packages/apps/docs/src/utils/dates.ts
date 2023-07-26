20736ee33e5b3c5d146dc6c07a58256f79389f2f
Steven Straatemans <steven@straatemans.nl>
2023-07-26 13:33:55 +0200

    create formatDate component

--- packages/apps/docs/src/utils/dates.ts
+++ packages/apps/docs/src/utils/dates.ts
@@ -1,7 +1,29 @@
-import { addMonths, format } from 'date-fns';
+import {
+  addMonths,
+  format,
+  formatDistance,
+  isFuture,
+  isToday,
+  isYesterday,
+} from 'date-fns';
 
 export const getOneMonthAgo = (date: Date): Date => {
   return addMonths(date, -1);
 };
 
 export const formatISODate = (date: Date): string => format(date, 'yyyy-MM-dd');
+
+export const formatDateDistance = (date: Date): string => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  date.setHours(0, 0, 0, 0);
+
+  if (isToday(date) || isFuture(date)) {
+    return 'today';
+  }
+  if (isYesterday(date)) {
+    return 'yesterday';
+  }
+
+  return `${formatDistance(date, today)} ago`;
+};
