4852152195d90f42402f32024ad685e543223c48
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-07-26 10:36:32 +0200

    run integration tests in CI

---
+++ packages/tools/integration-tests/src/test/util/client.ts
@@ -0,0 +1,29 @@
+import { getClient } from '@kadena/client';
+import { ChainId, NetworkId } from '@kadena/types';
+
+// you can edit this function if you want to use different network like dev-net or a private net
+export const apiHostGenerator = ({
+  networkId,
+  chainId,
+}: {
+  networkId: NetworkId;
+  chainId: ChainId;
+}): string => {
+  switch (networkId) {
+    default:
+      return `https://localhost:8080/chainweb/0.0/${networkId}/chain/${
+        chainId ?? '1'
+      }/pact`;
+  }
+};
+
+// configure the client and export the functions
+export const {
+  submit,
+  preflight,
+  dirtyRead,
+  pollCreateSpv,
+  pollStatus,
+  getStatus,
+  createSpv,
+} = getClient(apiHostGenerator);
