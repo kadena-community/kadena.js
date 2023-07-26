7ab3b86082725c67d857a5d92a11e0936c5cac65
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-07-26 16:25:11 +0200

    Change describe

--- packages/libs/client/src/integration-tests/create-account.int.test.ts
+++ packages/libs/client/src/integration-tests/create-account.int.test.ts
@@ -2,7 +2,7 @@ import { fund } from './util/simple-transfer';
 
 import before, { describe } from 'node:test';
 
-describe('Fund account and perform transfer.', async () => {
+describe('Fund account', async () => {
   await before(async () => {
     await fund(
       'k:2f48080efe54e6eb670487f664bcaac7684b4ebfcfc8a3330ef080c9c97f7e11',
