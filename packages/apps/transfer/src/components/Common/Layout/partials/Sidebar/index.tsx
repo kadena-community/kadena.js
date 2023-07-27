a26b0d5a57d4f3b6ff3a24dce33a1284554b5257
Reza Erami <reza.eramikalleh@deptagency.com>
2023-07-27 12:44:22 +0200

    fix layout
    - remove grid
    - use fixed position for layout partials

--- packages/apps/transfer/src/components/Common/Layout/partials/Sidebar/index.tsx
+++ packages/apps/transfer/src/components/Common/Layout/partials/Sidebar/index.tsx
@@ -1,13 +1,14 @@
 import { Menu } from './Menu';
+import { sidebarClass } from './styles.css';
 import { Toolbar } from './Toolbar';
 
 import React, { FC } from 'react';
 
 export const Sidebar: FC = () => {
   return (
-    <>
+    <aside className={sidebarClass}>
       <Toolbar />
       <Menu />
-    </>
+    </aside>
   );
 };
