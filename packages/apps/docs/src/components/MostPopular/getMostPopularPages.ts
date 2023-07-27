711098c88e843aa6129f21c7e38e3c645c7cff8c
Mohamed Ismail <ismailreality1@gmail.com>
2023-07-27 15:16:36 +0200

    refactor(docs): remove most popular pages docs file base url

--- packages/apps/docs/src/components/MostPopular/getMostPopularPages.ts
+++ packages/apps/docs/src/components/MostPopular/getMostPopularPages.ts
@@ -8,8 +8,7 @@ export const getMostPopularPages = async (
   slug = '/',
 ): Promise<IGetMostPopularPagesResult> => {
   async function fetchMostPopularPages(): Promise<IMostPopularPage[]> {
-    const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
-    const res = await fetch(`${BASE_URL}/mostpopular?slug=${slug}`);
+    const res = await fetch(`api/mostpopular?slug=${slug}`);
     const data = await res.json();
     return data;
   }
