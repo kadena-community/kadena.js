6eac9d14b43463dcc73f5e3adf030d0d288e92e9
Bart Huijgen <bart@codecapi.nl>
2023-07-27 09:27:22 +0200

    chore: clean up boilerplate code to only keep what is going to be used

---
+++ packages/apps/immutable-records/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { container } from './index.css';
+
+const Index: FC = () => {
+  return <div className={container}>hello world</div>;
+};
+
+export default Index;
