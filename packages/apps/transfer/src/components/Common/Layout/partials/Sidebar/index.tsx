9e3d177925544a5d42bb80abd87bd8a052978366
Reza Erami <reza.eramikalleh@deptagency.com>
2023-07-28 12:57:50 +0200

    fix layout
    - use vanilla extract for header to sync with theme
    - fix colors of sidebar and toolbar
    - force sidebar to get dark theme
    - change width of content when menu is expanded

--- packages/apps/transfer/src/components/Common/Layout/partials/Sidebar/index.tsx
+++ packages/apps/transfer/src/components/Common/Layout/partials/Sidebar/index.tsx
@@ -1,12 +1,15 @@
+import { darkThemeClass } from '@kadena/react-ui/theme';
+
 import { Menu } from './Menu';
 import { sidebarClass } from './styles.css';
 import { Toolbar } from './Toolbar';
 
+import classNames from 'classnames';
 import React, { FC } from 'react';
 
 export const Sidebar: FC = () => {
   return (
-    <aside className={sidebarClass}>
+    <aside className={classNames(sidebarClass, darkThemeClass)}>
       <Toolbar />
       <Menu />
     </aside>
