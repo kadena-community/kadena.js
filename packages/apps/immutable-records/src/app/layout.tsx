990507e3d9997d88ac6efbb77d7c40221dc99a03
Bart <45095973+barthuijgen@users.noreply.github.com>
2023-07-28 14:59:52 +0200

    feat: immutable records app boilerplate (#660)
    feat: immutable records app boilerplate
    
    ---------
    
    Co-authored-by: Bart Huijgen <bart@codecapi.nl>

---
+++ packages/apps/immutable-records/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { FC, PropsWithChildren } from 'react';
+
+export const metadata = {
+  title: 'Immutable Records',
+  description: 'Immutable Records',
+};
+
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
