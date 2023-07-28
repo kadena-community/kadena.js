0a72dc15162c7e7ec0cc3e5608308f4118c8f497
Bart Huijgen <bart@codecapi.nl>
2023-07-28 12:07:34 +0200

    chore: add explenation to custom build and missing image from readme

--- packages/apps/immutable-records/README.md
+++ packages/apps/immutable-records/README.md
@@ -4,6 +4,11 @@
 
 A auctioning dApp for NFTs.
 
+<picture>
+  <source srcset="https://raw.githubusercontent.com/kadena-community/kadena.js/main/common/images/Kadena.JS_logo-white.png" media="(prefers-color-scheme: dark)"/>
+  <img src="https://raw.githubusercontent.com/kadena-community/kadena.js/main/common/images/Kadena.JS_logo-black.png" width="200" alt="kadena.js logo" />
+</picture>
+
 <!-- genericHeader end -->
 
 ## Getting Started
--- packages/apps/immutable-records/build.mjs
+++ packages/apps/immutable-records/build.mjs
@@ -1,5 +1,17 @@
 import { spawn } from 'child_process';
 
+/*
+ * NextJS build outputs a depredaction warning related to
+ * usage of @kadena/react-ui/theme. It originated from webpack5
+ * with the mini-css-extract-plugin.
+ *
+ * We have investiated the issue and even when upgrading packages
+ * to the latest versions the warning remains.
+ *
+ * This file is a workaround to mute the warning until newer versions
+ * are available that fix the issue.
+ */
+
 const stderrFilter = [
   '[DEP_WEBPACK_MODULE_UPDATE_HASH] DeprecationWarning',
   'node --trace-deprecation',
