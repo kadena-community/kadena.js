a9dcd0339fd6341828814f2122389ae0ee9eda83
bearni95 <bernatcanal@gmail.com>
2023-07-31 13:30:49 -0400

    refactored Layout/Header/ThemeToggle

--- packages/apps/docs/src/components/Layout/components/Header/ThemeToggle.tsx
+++ packages/apps/docs/src/components/Layout/components/Header/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import { IconButton, SystemIcons } from '@kadena/react-components';
+import { IconButton, SystemIcon } from '@kadena/react-ui';
 
 import { useTheme } from '@/hooks';
 import { analyticsEvent, EVENT_NAMES } from '@/utils/analytics';
@@ -26,7 +26,7 @@ export const ThemeToggle: FC = () => {
     <IconButton
       title="Toggle theme"
       onClick={toggleTheme}
-      icon={SystemIcons.ThemeLightDark}
+      icon={SystemIcon.ThemeLightDark}
       color="inverted"
     />
   );
