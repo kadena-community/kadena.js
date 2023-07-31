6241b7e17497089a59cb8848e704c018db55547e
Javad Khalilian <khalilian.javad@gmail.com>
2023-07-31 10:17:56 +0200

    chore: format the file

--- packages/libs/client/src/signing/ISignFunction.ts
+++ packages/libs/client/src/signing/ISignFunction.ts
@@ -2,9 +2,9 @@ import { ICommand, IUnsignedCommand } from '@kadena/types';
 
 export interface ISignFunction {
   (transaction: IUnsignedCommand): Promise<ICommand | IUnsignedCommand>;
-  (transactionList: IUnsignedCommand[]): Promise<
-    (ICommand | IUnsignedCommand)[]
-  >;
+  (
+    transactionList: IUnsignedCommand[],
+  ): Promise<(ICommand | IUnsignedCommand)[]>;
 }
 
 export interface ISignSingleFunction {
