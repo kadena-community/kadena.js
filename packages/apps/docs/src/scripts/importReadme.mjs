e7a9c83535d6acb9291fe6363a13f744036e1260
Lars Kappert <lars@webpro.nl>
2023-08-01 12:02:12 +0200

    Remove create-kadena-app from docs readme importer

--- packages/apps/docs/src/scripts/importReadme.mjs
+++ packages/apps/docs/src/scripts/importReadme.mjs
@@ -383,24 +383,6 @@ const imports = [
       RootOrder: 2,
     },
   },
-  /** /tools/create-kadena-app */
-  {
-    file: 'tools/create-kadena-app/README.md',
-    destination: 'build/tools/create-kadena-app',
-    title: 'Create Kadena App',
-    options: {
-      RootOrder: 2,
-    },
-  },
-  {
-    file: 'tools/create-kadena-app/etc/create-kadena-app.api.md',
-    destination: 'build/tools/create-kadena-app/api',
-    title: 'Create Kadena App',
-    options: {
-      RootOrder: 99,
-      hideEditLink: true,
-    },
-  },
   /** /tools/kda-cli */
   {
     file: 'tools/kda-cli/README.md',
