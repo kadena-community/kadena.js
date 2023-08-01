f23421af9fa68f0a61280e8ae05b99c48fc05e9e
Mohamed Ismail <ismailreality1@gmail.com>
2023-08-01 11:37:07 +0200

    feat(docs): add most popular page for pact docs

---
+++ packages/apps/docs/src/pages/docs/pact/index.tsx
@@ -0,0 +1,147 @@
+import { Button, Card, Grid, Heading, Stack, Text } from '@kadena/react-ui';
+
+import { BrowseSection } from '@/components';
+import {
+  checkSubTreeForActive,
+  getPathName,
+} from '@/utils/staticGeneration/checkSubTreeForActive.mjs';
+import getMostPopularPages from '@/utils/getMostPopularPages';
+import { GetStaticProps } from 'next';
+import React, { FC } from 'react';
+import { MostPopular } from '@/components/MostPopular';
+import { IMostPopularPage } from '@/types/MostPopularData';
+
+interface IProps {
+  popularPages: IMostPopularPage[];
+}
+
+const Home: FC<IProps> = ({ popularPages = [] }) => {
+  return (
+    <Stack direction="column" spacing="$2xs">
+      <Stack direction="column">
+        <Heading as="h4">Smart-contract language</Heading>
+        <Text as="p">
+          designed for correct, transactional execution on a
+          high-performance blockchain.
+        </Text>
+        <Grid.Root columns={10} spacing="md">
+          <Grid.Item
+            columnSpan={{
+              sm: 10,
+              md: 7,
+            }}
+          >
+            <Card>
+              <Stack direction="column" spacing="$sm">
+                <Heading as="h6">Getting Started is Simple</Heading>
+                <Text as="p">
+                  Learn Kadena's core concepts & tools for development in 15
+                  minutes.
+                </Text>
+                <Stack direction="row" spacing="$sm">
+                  <Button
+                    title="Get Started"
+                    iconAlign="right"
+                    icon="TrailingIcon"
+                  >
+                    Get Started
+                  </Button>
+                  <Button title="Hello world tutorial" as="a" href="/">
+                    Hello world tutorial
+                  </Button>
+                </Stack>
+              </Stack>
+            </Card>
+          </Grid.Item>
+          {popularPages.length > 0 && (
+            <Grid.Item
+              columnSpan={{
+                sm: 10,
+                md: 3,
+              }}
+            >
+              <MostPopular pages={popularPages} title="Most viewed pact docs" />
+            </Grid.Item>
+          )}
+        </Grid.Root>
+      </Stack>
+      <Heading as="h6">Browse by Resources</Heading>
+      <Stack direction="column" spacing="$sm">
+        <BrowseSection title="General" titleAs="h6" direction="row">
+          <BrowseSection.LinkBlock
+            title="Overview of Pact"
+            subtitle="Explore all products"
+            icon="Overview"
+            href="/docs/pact"
+          />
+          <BrowseSection.LinkBlock
+            title="Chainweb"
+            subtitle="Explore all products"
+            icon="SmartContract"
+            href="/docs/chainweb"
+          />
+          <BrowseSection.LinkBlock
+            title="Marmalade"
+            subtitle="Explore all products"
+            icon="Marmalade"
+            href="/docs/marmalade"
+          />
+        </BrowseSection>
+        <BrowseSection title="Pact" titleAs="h6" direction="row">
+          <BrowseSection.LinkBlock
+            title="Pact Language"
+            subtitle="Explore all products"
+            icon="PactLanguage"
+            href="/docs/pact"
+          />
+          <BrowseSection.LinkBlock
+            title="Useful Tools"
+            subtitle="Explore all products"
+            icon="UsefulTools"
+            href="/docs/build/tools"
+          />
+          <BrowseSection.LinkBlock
+            title="Beginner Tutorials"
+            subtitle="For starters"
+            icon="PactDeveloper"
+            href="/docs/pact/beginner"
+          />
+          <BrowseSection.LinkBlock
+            title="Intermediate Tutorials"
+            subtitle="get some more experience"
+            icon="PactDeveloper"
+            href="docs/pact/intermediate"
+          />
+          <BrowseSection.LinkBlock
+            title="Quickstart"
+            subtitle="Explore all products"
+            icon="QuickStart"
+            href="/docs/chainweb"
+          />
+        </BrowseSection>
+      </Stack>
+    </Stack>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const mostPopularPages = await getMostPopularPages('/docs/pact');
+  return {
+    props: {
+      popularPages: mostPopularPages,
+      leftMenuTree: checkSubTreeForActive(getPathName(__filename)),
+      frontmatter: {
+        title: 'Pact',
+        menu: 'Pact',
+        subTitle: 'The safest, most user-friendly language for smart contracts',
+        label: 'Pact',
+        order: 0,
+        description: 'Kadena makes blockchain work for everyone.',
+        layout: 'landing',
+        icon: 'PactDeveloper',
+      },
+    },
+  };
+};
+
+export default Home;
