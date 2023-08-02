90cc97f0b8890251b3cc78813e9811268a755095
MRVDH <info@maartenvandenhoven.com>
2023-08-02 17:19:23 +0200

    hotfix/react-ui/table

--- packages/libs/react-ui/src/components/Table/Table.css.ts
+++ packages/libs/react-ui/src/components/Table/Table.css.ts
@@ -13,7 +13,7 @@ export const tdClass = style([
 export const trClass = style([
   sprinkles({
     backgroundColor: '$neutral1',
-    height: '$10',
+    height: '$12',
   }),
   {
     selectors: {
