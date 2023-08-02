290a3c24154a30440bc3cf5fc4453f2db2dfecef
MRVDH <info@maartenvandenhoven.com>
2023-08-02 14:36:37 +0200

    [@kadena/react-ui] Table Component: Striped option and styling fixes

--- packages/libs/react-ui/src/components/Table/Table.tsx
+++ packages/libs/react-ui/src/components/Table/Table.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { tableClass } from './Table.css';
 import { TBody } from './TBody';
 import { THead } from './THead';
@@ -7,11 +8,12 @@ import React, { FC } from 'react';
 
 export interface ITableProps {
   children?: CompoundType<typeof TBody> | CompoundType<typeof THead>;
+  striped?: boolean;
 }
 
-export const Table: FC<ITableProps> = ({ children }) => {
+export const Table: FC<ITableProps> = ({ children, striped }) => {
   return (
-    <table className={tableClass}>
+    <table className={striped ? classNames(tableClass, 'stripedClass') : tableClass}>
       {React.Children.map(children, (child) => {
         if (
           !React.isValidElement(child) ||
