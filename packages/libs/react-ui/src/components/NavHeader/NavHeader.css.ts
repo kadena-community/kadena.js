513a9d6a73e8e9b280f77de0789bbf1c60f7b673
Timo Heddes <timo.heddes@gmail.com>
2023-08-02 11:29:00 +0200

    feat: add glow effect

--- packages/libs/react-ui/src/components/NavHeader/NavHeader.css.ts
+++ packages/libs/react-ui/src/components/NavHeader/NavHeader.css.ts
@@ -12,6 +12,8 @@ export const containerClass = style([
     flexWrap: 'nowrap',
     height: '$16',
     justifyContent: 'flex-start',
+    position: 'relative',
+    overflow: 'hidden',
   }),
   {
     selectors: {
@@ -33,15 +35,29 @@ export const logoClass = style([
   }),
   {
     alignSelf: 'center',
+    zIndex: 1,
   },
 ]);
 
-export const navClass = style([
+export const navWrapperClass = style([
   sprinkles({
     alignItems: 'stretch',
     display: 'flex',
     justifyContent: 'center',
   }),
+]);
+
+export const navListClass = style([
+  sprinkles({
+    alignItems: 'stretch',
+    display: 'flex',
+    justifyContent: 'center',
+  }),
+  {
+    listStyle: 'none',
+    alignSelf: 'center',
+    zIndex: 1,
+  },
 ]);
 
 export const linkClass = style([
@@ -57,20 +73,36 @@ export const linkClass = style([
   }),
   {
     alignSelf: 'center',
-    padding: '4px 8px 4px 4px',
+    padding: '4px 8px 4px 8px',
+    transition: 'background 0.1s ease',
+    fontWeight: vars.fontWeights.$semiBold,
   },
   {
     selectors: {
+      '&:active': {
+        color: vars.colors.$gray90,
+        textDecoration: 'none',
+      },
       '&:hover': {
         color: vars.colors.$white,
         textDecoration: 'none',
       },
-      '&:focus': {
+      '&:focus-visible': {
         color: vars.colors.$blue40,
         textDecoration: 'none',
       },
-      '&:active': {
-        backgroundColor: vars.colors.$white,
+    },
+  },
+]);
+
+export const activeLinkClass = style([
+  {
+    backgroundColor: vars.colors.$white,
+    color: vars.colors.$gray90,
+  },
+  {
+    selectors: {
+      '&:hover': {
         color: vars.colors.$gray90,
         textDecoration: 'none',
       },
@@ -86,5 +118,16 @@ export const childrenClass = style([
   }),
   {
     alignSelf: 'center',
+  },
+]);
+
+export const glowClass = style([
+  {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 0,
+    pointerEvents: 'none',
+    transition: 'all 0.3s ease',
   },
 ]);
--- packages/libs/react-ui/src/components/NavHeader/NavHeader.tsx
+++ packages/libs/react-ui/src/components/NavHeader/NavHeader.tsx
@@ -3,12 +3,18 @@ import {
   containerClass,
   childrenClass,
   linkClass,
+  activeLinkClass,
   logoClass,
-  navClass,
+  navWrapperClass,
+  navListClass,
+  glowClass,
 } from './NavHeader.css';
 
-import React, { FC } from 'react';
+import classNames from 'classnames';
+
+import React, { FC, useState, useRef, useEffect } from 'react';
 import Logo, { LogoVariant, logoVariants } from '@components/Logo';
+import { NavGlow } from './assets/glow';
 
 export type INavItemTarget = '_self' | '_blank';
 export type INavItem = { title: string; href: string; target?: INavItemTarget };
@@ -25,29 +31,76 @@ export const NavHeader: FC<INavHeaderProps> = ({
   children,
   items,
 }) => {
+  const headerRef = useRef<HTMLInputElement>(null);
+  const logoRef = useRef<HTMLInputElement>(null);
+  const navRef = useRef<HTMLInputElement>(null);
+  const glowRef = useRef<HTMLInputElement>(null);
+
+  const [glowX, setGlowX] = useState(0);
+  const [activeNav, setActiveNav] = useState(0);
+
+  useEffect(() => {
+    const activeNavElement = navRef.current?.querySelector(
+      `li:nth-child(${activeNav}) a`,
+    );
+    const headerBounds = headerRef.current?.getBoundingClientRect();
+    const logoBounds = logoRef.current?.getBoundingClientRect();
+    const activeNavBounds = activeNavElement?.getBoundingClientRect();
+    const glowBounds = glowRef.current?.getBoundingClientRect();
+
+    console.log(activeNavElement);
+
+    setGlowX(
+      activeNav === 0
+        ? -logoBounds!.x + headerBounds!.x / 2 + logoBounds!.width / 2 - 20
+        : activeNavBounds!.x -
+            headerBounds!.x -
+            glowBounds!.width / 2 +
+            activeNavBounds!.width / 2,
+    );
+  }, [logoRef, glowX, activeNav]);
   return (
-    <header className={containerClass} data-testid="kda-navheader">
-      <div className={logoClass}>
+    <header
+      className={containerClass}
+      ref={headerRef}
+      data-testid="kda-navheader"
+    >
+      <div
+        className={glowClass}
+        style={{ transform: `translateX(${glowX}px)` }}
+        ref={glowRef}
+      >
+        <NavGlow />
+      </div>
+      <div className={logoClass} ref={logoRef}>
         {logoVariants.includes(brand) && (
           <Link.Root href="/" target="_self">
             <Logo variant={brand} />
           </Link.Root>
         )}
       </div>
-      <nav className={navClass}>
-        {items &&
-          items.map((item, index) => {
-            return (
-              <a
-                className={linkClass}
-                href={item.href}
-                target={item.target}
-                key={`navItem-${index}`}
-              >
-                {item.title}
-              </a>
-            );
-          })}
+      <nav className={navWrapperClass} ref={navRef}>
+        <ul className={navListClass}>
+          {items &&
+            items.map((item, index) => {
+              return (
+                <li
+                  key={`navItem-${index}`}
+                  onClick={() => setActiveNav(index + 1)}
+                >
+                  <a
+                    className={classNames(linkClass, {
+                      [activeLinkClass]: activeNav === index + 1,
+                    })}
+                    href={item.href}
+                    target={item.target}
+                  >
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+        </ul>
       </nav>
       <div className={childrenClass}>{children}</div>
     </header>
