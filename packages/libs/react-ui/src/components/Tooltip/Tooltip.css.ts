10462b6cfc4fa822de2dc1159d79677e1701b781
Eileen Guo <eileen.m.guo@gmail.com>
2023-08-02 09:38:45 +0200

    removing unrelated changes

--- packages/libs/react-ui/src/components/Tooltip/Tooltip.css.ts
+++ packages/libs/react-ui/src/components/Tooltip/Tooltip.css.ts
@@ -16,7 +16,6 @@ export const container = style([
     width: 'max-content',
     position: 'fixed',
     display: 'none',
-    pointerEvents: 'none',
   }),
   {
     top: '50%',
@@ -31,7 +30,6 @@ export const baseArrow = style([
     width: '$4',
     height: '$4',
     backgroundColor: '$neutral1',
-    pointerEvents: 'none',
   }),
   {
     rotate: '45deg',
