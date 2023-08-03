39704a986ab8015eab85cd55b02788034e9a951a
MRVDH <info@maartenvandenhoven.com>
2023-08-03 12:52:13 +0200

    Fix icon colours

--- packages/libs/react-ui/src/components/Icon/System/svgs/ArrowExpandUp.tsx
+++ packages/libs/react-ui/src/components/Icon/System/svgs/ArrowExpandUp.tsx
@@ -12,8 +12,8 @@ const ArrowExpandUp: React.FC<SVGProps<SVGSVGElement>> = (
     {...props}
   >
     <path
+      fill="currentColor"
       d="M2 20V22H22V20H13V5.83L18.5 11.33L19.92 9.92L12 2L4.08 9.92L5.5 11.33L11 5.83V20H2Z"
-      fill="black"
     />
   </svg>
 );
