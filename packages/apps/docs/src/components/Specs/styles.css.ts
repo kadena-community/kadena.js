9224ae68333d480503e394a35517d1247d8f1ede
Mohamed Ismail <ismailreality1@gmail.com>
2023-08-06 12:27:36 +0200

    refactor(docs): remove style and use string class name

--- packages/apps/docs/src/components/Specs/styles.css.ts
+++ packages/apps/docs/src/components/Specs/styles.css.ts
@@ -1,68 +1,68 @@
 import { vars } from '@kadena/react-ui/theme';
 
-import { globalStyle, style } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 
-export const specsWrapper = style({});
+export const specsWrapper = 'specs-wrapper';
 
-globalStyle(`${specsWrapper} .menu-content li span`, {
+globalStyle(`.${specsWrapper} .menu-content li span`, {
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .menu-content li polygon`, {
+globalStyle(`.${specsWrapper} .menu-content li polygon`, {
   fill: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content`, {
+globalStyle(`.${specsWrapper} .api-content`, {
   width: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content polygon`, {
+globalStyle(`.${specsWrapper} .api-content polygon`, {
   fill: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content h1`, {
+globalStyle(`.${specsWrapper} .api-content h1`, {
   lineHeight: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content h2`, {
+globalStyle(`.${specsWrapper} .api-content h2`, {
   lineHeight: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content h3`, {
+globalStyle(`.${specsWrapper} .api-content h3`, {
   lineHeight: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content h4`, {
+globalStyle(`.${specsWrapper} .api-content h4`, {
   lineHeight: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content h5`, {
+globalStyle(`.${specsWrapper} .api-content h5`, {
   lineHeight: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} .api-content h6`, {
+globalStyle(`.${specsWrapper} .api-content h6`, {
   lineHeight: '100%',
   color: vars.colors.$foreground,
 });
 
-globalStyle(`${specsWrapper} img[alt="logo"]`, {
+globalStyle(`.${specsWrapper} img[alt="logo"]`, {
   display: 'none',
 });
 
-globalStyle(`${specsWrapper} .api-content ul[role="tablist"]`, {
+globalStyle(`.${specsWrapper} .api-content ul[role="tablist"]`, {
   padding: `${vars.sizes.$sm} 0`,
 });
 
-globalStyle(`${specsWrapper} .api-content select + label`, {
+globalStyle(`.${specsWrapper} .api-content select + label`, {
   width: '100%',
 });
 
-globalStyle(`${specsWrapper} .api-content div[role="tabpanel"]`, {
+globalStyle(`.${specsWrapper} .api-content div[role="tabpanel"]`, {
   marginBottom: vars.sizes.$md,
 });
