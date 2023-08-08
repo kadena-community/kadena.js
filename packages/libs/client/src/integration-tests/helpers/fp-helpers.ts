b6d59c36a05f0024905a95b648098cdfdbf470d0
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-08-08 10:53:45 +0200

    add disable eslint for specific lines

--- packages/libs/client/src/integration-tests/helpers/fp-helpers.ts
+++ packages/libs/client/src/integration-tests/helpers/fp-helpers.ts
@@ -7,10 +7,12 @@ export const asyncPipe =
   (init: all): Promise<all> =>
     args.reduce((chain, fn) => chain.then(fn), Promise.resolve(init));
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const head = (args: all[]): any => args[0];
 
 export const inspect =
   (tag: string): (<T extends unknown>(data: T) => T) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   <T extends any>(data: T): T => {
     console.log(tag, data);
     return data;
