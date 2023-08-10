aee7fb592a0e0d550def8a7dcadf5cdd3f95d92a
bearni95 <bernatcanal@gmail.com>
2023-08-10 16:53:14 +0200

    recreated button->icon api into link->icon

--- packages/libs/react-ui/src/components/Link/Link.stories.tsx
+++ packages/libs/react-ui/src/components/Link/Link.stories.tsx
@@ -4,7 +4,9 @@ import { ILinkProps, Link } from '@components/Link';
 import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 
-const meta: Meta<ILinkProps> = {
+const meta: Meta<{
+  selectIcon: keyof typeof SystemIcon;
+} & ILinkProps> = {
   title: 'Components/Link',
   component: Link,
   argTypes: {
@@ -19,32 +21,51 @@ const meta: Meta<ILinkProps> = {
         options: ['_blank', '_self', '_parent', '_top'],
       },
     },
+    icon: {
+      options: [
+        ...['-'],
+        ...Object.keys(SystemIcon),
+      ] as (keyof typeof SystemIcon)[],
+      control: {
+        type: 'select',
+      },
+    },
+    iconAlign: {
+      description: 'align icon to left or right',
+      options: ['left', 'right'] as ILinkProps['iconAlign'][],
+      control: {
+        type: 'radio',
+      },
+      if: { arg: 'selectIcon', neq: '-' },
+    },
   },
 };
 
 export default meta;
 
-type Story = StoryObj<ILinkProps>;
+type Story = StoryObj<{
+  selectIcon: keyof typeof SystemIcon;
+} & ILinkProps>;
 
 export const Primary: Story = {
   name: 'Link',
   args: {
     href: 'https://kadena.io',
     target: '_blank',
+    icon: 'Link',
+    iconAlign: 'left',
   },
-  render: ({ href, target }) => {
+  render: ({ href, target, icon, iconAlign }) => {
     return (
       <>
         <Link href={href} target={target}>
           Link without icon
         </Link>
-        <Link href={`${href}?${Date.now()}`} target={target}>
-          <SystemIcon.Account />
+        <Link href={`${href}?${Date.now()}`} target={target} iconAlign={iconAlign} icon={icon}>
           Non-visited
         </Link>
-        <Link href={href} target={target}>
+        <Link href={href} target={target} icon={icon}>
           Kadena.io
-          <SystemIcon.Link />
         </Link>
       </>
     );
--- packages/libs/react-ui/src/components/Link/Link.tsx
+++ packages/libs/react-ui/src/components/Link/Link.tsx
@@ -1,26 +1,42 @@
 import { SystemIcon } from '..';
-import { linkContainerClass } from './Link.css';
+
+import { colorVariants, linkContainerClass } from './Link.css';
+import { LinkIcon } from './LinkIcon';
 
 import React, { FC, ReactNode } from 'react';
 
 export interface ILinkProps {
   href: string;
   target?: '_blank' | '_self' | '_parent' | '_top';
-  //variant?: keyof typeof colorVariants;
   children: ReactNode;
   icon?: keyof typeof SystemIcon;
   iconAlign?: 'left' | 'right';
-  loading?: boolean;
 }
+
 
 export const Link: FC<ILinkProps> = ({
   href, target = '_blank', children,
-  icon, iconAlign, loading
+  icon, iconAlign
 
 }) => {
+  let Icon = icon && SystemIcon[icon];
+
+
+  const buttonChildren = (
+    <>
+      {Icon && iconAlign === 'left' && (
+        <LinkIcon icon={Icon} />
+      )}
+      {children}
+      {Icon && iconAlign === 'right' && (
+        <LinkIcon icon={Icon} />
+      )}
+    </>
+  );
+
   return (
     <a href={href} target={target} className={linkContainerClass}>
-      {children}
+      {buttonChildren}
     </a>
   );
 };
