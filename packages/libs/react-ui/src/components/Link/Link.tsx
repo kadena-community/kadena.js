b1cdab183e8294c7f98510b477577c5bd624a36a
bearni95 <bernatcanal@gmail.com>
2023-08-10 16:27:32 +0200

    urgent task change commit

--- packages/libs/react-ui/src/components/Link/Link.tsx
+++ packages/libs/react-ui/src/components/Link/Link.tsx
@@ -1,3 +1,4 @@
+import { SystemIcon } from '..';
 import { linkContainerClass } from './Link.css';
 
 import React, { FC, ReactNode } from 'react';
@@ -5,10 +6,18 @@ import React, { FC, ReactNode } from 'react';
 export interface ILinkProps {
   href: string;
   target?: '_blank' | '_self' | '_parent' | '_top';
+  //variant?: keyof typeof colorVariants;
   children: ReactNode;
+  icon?: keyof typeof SystemIcon;
+  iconAlign?: 'left' | 'right';
+  loading?: boolean;
 }
 
-export const Link: FC<ILinkProps> = ({ href, target = '_blank', children }) => {
+export const Link: FC<ILinkProps> = ({
+  href, target = '_blank', children,
+  icon, iconAlign, loading
+
+}) => {
   return (
     <a href={href} target={target} className={linkContainerClass}>
       {children}
