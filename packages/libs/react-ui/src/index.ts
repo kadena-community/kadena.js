5dd4bf06655a908cc58a859f2dcee8f6e1f45e61
Timo Heddes <timo.heddes@gmail.com>
2023-08-11 08:38:34 +0200

    chore: type export/import rename

--- packages/libs/react-ui/src/index.ts
+++ packages/libs/react-ui/src/index.ts
@@ -24,7 +24,7 @@ export type {
   INavFooterIconButtonProps,
   INavFooterLinkProps,
   INavFooterPanelProps,
-  INavFooterRootProps,
+  INavFooterContainerProps,
   INotificationActionsProps,
   INotificationProps,
   InputWrapperStatus,
