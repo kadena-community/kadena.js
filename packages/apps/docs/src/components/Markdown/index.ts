51a74d949c020fe4a1274ba4e7d793f8263e813f
Mohamed Ismail <ismailreality1@gmail.com>
2023-08-16 16:36:56 +0200

    feat(embed-tweets): implement tweet component

--- packages/apps/docs/src/components/Markdown/index.ts
+++ packages/apps/docs/src/components/Markdown/index.ts
@@ -9,6 +9,7 @@ import { Link } from './Link';
 import { MDNotification } from './MDNotification';
 import { Paragraph } from './Paragraph';
 import { Table } from './Table';
+import { Tweet } from './Tweet';
 import { UnorderedList } from './UnorderedList';
 import { Youtube } from './Youtube';
 
@@ -18,6 +19,10 @@ export type ExtendedIntrinsicElements = JSX.IntrinsicElements & {
     HTMLDivElement
   >;
   'kda-youtube': React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >;
+  'kda-tweet': React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLDivElement>,
     HTMLDivElement
   >;
@@ -60,6 +65,9 @@ export const markDownComponents: MDXComponents = {
   >,
   'kda-youtube': Youtube as FunctionComponent<
     ExtendedIntrinsicElements['kda-youtube']
+  >,
+  'kda-tweet': Tweet as FunctionComponent<
+    ExtendedIntrinsicElements['kda-tweet']
   >,
   a: Link as FunctionComponent<ExtendedIntrinsicElements['a']>,
 };
