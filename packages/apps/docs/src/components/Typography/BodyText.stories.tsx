1657d23065746b2da19c00224bde0ea514e62841
Mohamed Ismail <ismailreality1@gmail.com>
2023-08-22 14:53:12 +0200

    chore(docs): add BodyText component stories

---
+++ packages/apps/docs/src/components/Typography/BodyText.stories.tsx
@@ -0,0 +1,41 @@
+import { BodyText, IBodyTextProps } from './BodyText';
+
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+
+const selectOptions: string[] = ['p', 'span'];
+
+const meta: Meta<IBodyTextProps> = {
+  title: 'Typography/BodyText',
+  argTypes: {
+    as: {
+      options: selectOptions,
+      control: {
+        type: 'select',
+      },
+    },
+    bold: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<IBodyTextProps>;
+
+export const Default: Story = {
+  name: 'Default',
+  args: {
+    as: 'p',
+    bold: false,
+  },
+  render: ({ as, bold }) => (
+    <div>
+      <BodyText as={as} bold={bold}>
+        Hello! I&apos;m there...!
+      </BodyText>
+    </div>
+  ),
+};
