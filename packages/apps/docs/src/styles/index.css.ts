9f1ac7323b3107de444c90510308804a655ae547
Mohamed Ismail <realdreamer@users.noreply.github.com>
2023-08-22 13:07:15 +0200

    fix(docs): ui alignment fixes (#803)
    * fix(docs): browse section ui fixes
    
    * fix(docs): add an additional check for active element + href
    
    * chore(docs): remove unused component
    
    * Revert "fix(docs): add an additional check for active element + href"
    
    This reverts commit 5e83fa0e9aae68bc31dfd007bfe16ee11043daae.

--- packages/apps/docs/src/styles/index.css.ts
+++ packages/apps/docs/src/styles/index.css.ts
@@ -1,18 +1,14 @@
-import { breakpoints, sprinkles } from '@kadena/react-ui/theme';
+import { breakpoints, vars } from '@kadena/react-ui/theme';
 
 import { style } from '@vanilla-extract/css';
 
 const browseSectionWrapper = style([
-  sprinkles({
-    gap: '$4',
-    paddingY: 0,
-    paddingX: '$4',
-  }),
   {
-    flexBasis: '48%',
+    flexBasis: '50%',
+    rowGap: vars.sizes.$4,
     '@media': {
       [breakpoints.md]: {
-        flexBasis: '30%',
+        flexBasis: '33%',
       },
     },
   },
