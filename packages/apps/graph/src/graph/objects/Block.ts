890103ccfe957aff7a22058ff697436b7cef4e34
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-08-30 05:14:45 +0100

    implement various ways of getting ancestors

--- packages/apps/graph/src/graph/objects/Block.ts
+++ packages/apps/graph/src/graph/objects/Block.ts
@@ -15,6 +15,7 @@ export default builder.prismaNode('Block', {
     epoch: t.expose('epoch', { type: 'DateTime' }),
     height: t.expose('height', { type: 'BigInt' }),
     powhash: t.exposeString('powhash'),
+    parent: t.exposeString('parent'),
 
     // computed fields
 
@@ -47,6 +48,30 @@ export default builder.prismaNode('Block', {
         });
       },
     }),
+
+    // Add the confirmationDepth field
+    confirmationDepth1: t.int({
+      resolve: async (parent, args, context, info) => {
+        // Implement the logic to calculate confirmation depth here
+        return getConfirmationDepth1(parent.hash);
+      },
+    }),
+
+    // Add the confirmationDepth field
+    confirmationDepth2: t.int({
+      resolve: async (parent, args, context, info) => {
+        // Implement the logic to calculate confirmation depth here
+        return getConfirmationDepth2(parent.hash);
+      },
+    }),
+
+    // Add the confirmationDepth field
+    confirmationDepth3: t.int({
+      resolve: async (parent, args, context, info) => {
+        // Implement the logic to calculate confirmation depth here
+        return getConfirmationDepth3(parent.hash);
+      },
+    }),
   }),
 });
 
@@ -65,4 +90,93 @@ async function getTransactionsRequestkeyByEvent(
       WHERE e.qualname IN (${Prisma.join(events as string[])})
       AND t.block = ${parent.hash}`
   ).map((r) => r.requestkey);
+}
+
+async function getConfirmationDepth(blockHash: string): Promise<number> {
+  const results = await prismaClient.$queryRaw`
+      SELECT b.payload
+      FROM blocks b
+      INNER JOIN blocks b1 ON (b.chainid = b1.chainid AND b.hash = b1.parent)
+      INNER JOIN blocks b2 ON (b1.chainid = b2.chainid AND b1.hash = b2.parent)
+      INNER JOIN blocks b3 ON (b2.chainid = b3.chainid AND b2.hash = b3.parent)
+      INNER JOIN blocks b4 ON (b3.chainid = b4.chainid AND b3.hash = b4.parent)
+      INNER JOIN blocks b5 ON (b4.chainid = b5.chainid AND b4.hash = b5.parent)
+      `;
+
+  const table = await prismaClient.block.findMany();
+  console.log(results);
+
+  // return results.length;
+
+  return 1;
+}
+
+async function getConfirmationDepth1(blockHash: string): Promise<number> {
+  let depth = 0;
+  let currentHash = blockHash;
+
+  while (currentHash) {
+    const result = await prismaClient.$queryRaw<{ parent: string }[]>`
+      SELECT parent
+      FROM blocks
+      WHERE hash = ${currentHash}
+    `;
+
+    if (result && result[0] && result[0].parent) {
+      depth++;
+      currentHash = result[0].parent;
+    } else {
+      break;
+    }
+  }
+
+  return depth;
+}
+async function getConfirmationDepth2(blockHash: string): Promise<number> {
+  const result = await prismaClient.$queryRaw<{ depth: number }[]>`
+    WITH RECURSIVE BlockAncestors AS (
+      SELECT hash, parent, 0 AS depth
+      FROM blocks
+      WHERE hash = ${blockHash}
+      UNION ALL
+      SELECT b.hash, b.parent, a.depth + 1 AS depth
+      FROM BlockAncestors a
+      JOIN blocks b ON a.parent = b.hash
+    )
+    SELECT MAX(depth) AS depth
+    FROM BlockAncestors;
+  `;
+
+  if (result && result[0] && result[0].depth) {
+    console.log(result[0].depth);
+    return Number(result[0].depth);
+  } else {
+    return 0;
+  }
+}
+
+async function getConfirmationDepth3(blockHash: string): Promise<number> {
+  const blocksTable = await prismaClient.block.findMany();
+
+  const block = blocksTable.find((b) => b.hash === blockHash);
+
+  if (!block) {
+    return 0;
+  }
+
+  let parent = block.parent;
+  let depth = 0;
+
+  while (parent) {
+    const parentBlock = blocksTable.find((b) => b.hash === parent);
+
+    if (!parentBlock) {
+      break;
+    }
+
+    parent = parentBlock.parent;
+    depth++;
+  }
+
+  return depth;
 }
