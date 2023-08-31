481b7839da8e2898fea8c266e433dc28da9b0af3
Lars Kappert <lars@webpro.nl>
2023-08-31 18:04:22 +0200

    Add @kadena-dev/lint-package

---
+++ packages/tools/lint-package/src/rules/eslint/config.ts
@@ -0,0 +1,19 @@
+import type { Issues, Rule } from '../../types.js';
+
+// ESLint configs can only be loaded by ESLint itself because of this line:
+// require('@rushstack/eslint-config/patch/modern-module-resolution');
+// So we can only load it as a string. Not worth it to strip this line and eval the rest.
+
+const extendMatch = /extends:\s+\[\s*'@kadena-dev\/eslint-config\/profile\//;
+
+const rule: Rule = ({ eslintConfig }) => {
+  const issues: Issues = [];
+
+  if (!extendMatch.test(eslintConfig)) {
+    issues.push(['warn', 'Config extends from incorrect config']);
+  }
+
+  return issues;
+};
+
+export default rule;
