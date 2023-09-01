cc353e619f8799766b338044dfffdde561250fbe
Steven Straatemans <steven@straatemans.nl>
2023-09-01 08:24:19 +0200

    reactor the home layout

--- packages/apps/docs/src/components/Layout/Home/Home.tsx
+++ packages/apps/docs/src/components/Layout/Home/Home.tsx
@@ -1,18 +1,20 @@
-import { BasePageGrid } from '../components';
+import { baseGridClass } from '../basestyles.css';
 import { Template } from '../components/Template';
 
 import { globalClass } from './../global.css';
 
 import { IPageProps } from '@/types/Layout';
+import classnames from 'classnames';
 import React, { FC } from 'react';
 
 export const Home: FC<IPageProps> = ({ children, leftMenuTree }) => {
+  const gridClassNames = classnames(globalClass, baseGridClass);
   return (
-    <BasePageGrid className={globalClass}>
+    <div className={gridClassNames}>
       <Template menuItems={leftMenuTree} hideSideMenu>
         {children}
       </Template>
-    </BasePageGrid>
+    </div>
   );
 };
 
