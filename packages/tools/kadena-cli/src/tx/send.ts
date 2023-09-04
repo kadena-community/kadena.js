b5634ac8579a41f5a4e4602f34d03dbc30dbb3d7
Albert G <516972+alber70g@users.noreply.github.com>
2023-09-04 17:21:05 +0200

    feat(kadena-cli): adds `kda tx send`

---
+++ packages/tools/kadena-cli/src/tx/send.ts
@@ -0,0 +1,23 @@
+import { processZodErrors } from '../utils/process-zod-errors';
+
+import { Command } from 'commander';
+
+export interface ISendOptions {}
+
+export function sendCommand(program: Command, version: string): void {
+  program
+    .command('send')
+    .description('send a transaction to the blockchain')
+    .action((args: ISendOptions) => {
+      try {
+        // TODO: use @inquirer/prompts to interactively get missing flags
+        // TODO: create zod validator
+        // Options.parse(args);
+      } catch (e) {
+        processZodErrors(program, e, args);
+      }
+
+      // TODO: implement
+      throw new Error('Not Implemented Yet');
+    });
+}
