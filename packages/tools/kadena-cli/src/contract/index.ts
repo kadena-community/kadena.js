c884117e2f2c6cec1555a24ca20e08c688a0f4e8
Albert G <516972+alber70g@users.noreply.github.com>
2023-09-05 09:34:48 +0200

    chore(kadena-cli): fix `kadena contract` structure. Serves as example

--- packages/tools/kadena-cli/src/contract/index.ts
+++ packages/tools/kadena-cli/src/contract/index.ts
@@ -1,15 +1,25 @@
-import { retrieveCommand } from './retrieve';
+import { deployCommand } from './deployCommand';
+import { retrieveCommand } from './retrieveCommand';
 
 import { Command } from 'commander';
 
 const SUBCOMMAND_ROOT: 'contract' = 'contract';
 
-export default [
-  (program: Command, version: string) =>
-    retrieveCommand(
-      program
-        .command(SUBCOMMAND_ROOT)
-        .description(`Tool for managing smart-contracts`),
-      version,
-    ),
-];
+/**
+ * Create subcommand `kadena contract`
+ * `kadena contract retrieve`
+ * `kadena contract deploy`
+ */
+export function contractCommandFactory(
+  program: Command,
+  version: string,
+): void {
+  const contractProgram = program
+    .command(SUBCOMMAND_ROOT)
+    .description(`Tool for managing smart-contracts`);
+
+  retrieveCommand(contractProgram, version);
+  deployCommand(contractProgram, version);
+}
+
+
