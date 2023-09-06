bfc0dea810c16e25efcc7985d7c11e0cc5b23374
Mohamed Ismail <ismailreality1@gmail.com>
2023-09-06 13:24:17 +0200

    feat(docs): add storybook for page helpful component

---
+++ packages/apps/docs/src/components/PageHelpful/PageHelpful.stories.tsx
@@ -0,0 +1,37 @@
+import { ModalProvider } from '@kadena/react-ui';
+
+import { PageHelpful } from './PageHelpful';
+
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+
+const selectOptions: string[] = ['p', 'span'];
+
+const meta: Meta = {
+  title: 'Components/PageHelpful',
+  argTypes: {
+    as: {
+      options: selectOptions,
+      control: {
+        type: 'select',
+      },
+    },
+    bold: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj = {
+  name: 'Default',
+  render: () => (
+    <ModalProvider>
+      <div id="modalportal"></div>
+      <PageHelpful />
+    </ModalProvider>
+  ),
+};
