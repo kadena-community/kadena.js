778ce151750be4d29cf8244c145eb2c6c6edc437
Eileen Guo <eileen.m.guo@gmail.com>
2023-09-07 13:57:29 +0200

    fix table break behavior

--- packages/libs/react-ui/src/components/Table/Table.stories.tsx
+++ packages/libs/react-ui/src/components/Table/Table.stories.tsx
@@ -33,12 +33,6 @@ type Story = StoryObj<
   } & ITableProps
 >;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/7.0/react/api/csf
- * to learn how to use render functions.
- */
-
 export const Primary: Story = {
   name: 'Table',
   args: {
