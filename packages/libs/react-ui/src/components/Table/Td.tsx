25be1682c5021e67c49e17075dfaaf6ecfcaecb7
Eileen Guo <eileen.m.guo@gmail.com>
2023-09-08 10:24:16 +0200

    add sizing options to td classes

--- packages/libs/react-ui/src/components/Table/Td.tsx
+++ packages/libs/react-ui/src/components/Table/Td.tsx
@@ -1,12 +1,22 @@
+import type { Sprinkles } from '@theme/sprinkles.css';
 import { tdClass } from './Table.css';
 
+import { sprinkles } from '@theme/sprinkles.css';
+import classNames from 'classnames';
 import type { FC } from 'react';
 import React from 'react';
 
-export interface ITdProps {
+export interface ITdProps
+  extends Pick<Sprinkles, 'width' | 'minWidth' | 'maxWidth'> {
   children?: React.ReactNode;
 }
 
-export const Td: FC<ITdProps> = ({ children }) => {
-  return <td className={tdClass}>{children}</td>;
+export const Td: FC<ITdProps> = ({ children, width, minWidth, maxWidth }) => {
+  return (
+    <td
+      className={classNames(tdClass, sprinkles({ width, minWidth, maxWidth }))}
+    >
+      {children}
+    </td>
+  );
 };
