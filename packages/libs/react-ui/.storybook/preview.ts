55672993b7b704294c13adaa75bc53e8462b6fb6
Timo Heddes <timo.heddes@gmail.com>
2023-09-11 12:59:10 +0200

    feat: enhance storybook

--- packages/libs/react-ui/.storybook/preview.ts
+++ packages/libs/react-ui/.storybook/preview.ts
@@ -13,13 +13,18 @@ const preview: Preview = {
       },
     },
     darkMode: {
+      current: 'light',
       stylePreview: true,
       darkClass: darkThemeClass,
       // Override the default dark theme
       dark: { ...themes.dark, appBg: 'black' },
       // Override the default light theme
       light: { ...themes.normal, appBg: 'white' },
+      theme: themes.dark,
     },
+    // docs: {
+    //   theme: themes.dark,
+    // },
   },
 };
 
