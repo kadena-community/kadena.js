3bbb02bf334c676aea2a590e5eb0017fd3e57ccd
Timo Heddes <timo.heddes@gmail.com>
2023-09-12 11:06:59 +0200

    feat: fix dark/light mode for docs

--- packages/libs/react-ui/.storybook/preview.ts
+++ packages/libs/react-ui/.storybook/preview.ts
@@ -1,7 +1,10 @@
 import { darkThemeClass } from '../src/styles';
 import { type Preview } from '@storybook/react';
 import { themes } from '@storybook/theming';
+import { DocsContainer } from '@storybook/addon-docs';
+import { useDarkMode } from 'storybook-dark-mode';
 import '../src/styles/global.css';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -15,16 +18,27 @@ const preview: Preview = {
     darkMode: {
       current: 'light',
       stylePreview: true,
-      darkClass: darkThemeClass,
+      classTarget: 'html',
+      darkClass: [darkThemeClass, 'dark-mode'],
+      lightClass: ['light-mode'],
       // Override the default dark theme
       dark: { ...themes.dark, appBg: 'black' },
       // Override the default light theme
       light: { ...themes.normal, appBg: 'white' },
       theme: themes.dark,
     },
-    // docs: {
-    //   theme: themes.dark,
-    // },
+    docs: {
+			container: (context: any) => {
+				const isDark = useDarkMode();
+
+				const props = {
+					...context,
+					theme: isDark ? themes.dark : themes.light,
+				};
+
+				return React.createElement(DocsContainer, props);
+			},
+		},
   },
 };
 
