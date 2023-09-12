1979a3777deb5a4a6145e0036428ce92af76970c
Steven Straatemans <steven@straatemans.nl>
2023-09-12 09:01:30 +0200

    add content to contribute landing page

--- packages/apps/docs/src/pages/docs/contribute/index.tsx
+++ packages/apps/docs/src/pages/docs/contribute/index.tsx
@@ -1,38 +1,101 @@
-import { Stack } from '@kadena/react-ui';
+import { Box, Button, Card, Heading, Stack, Text } from '@kadena/react-ui';
 
 import { BrowseSection } from '@/components';
+import {
+  cardClass,
+  cardSectionClass,
+} from '@/components/Layout/Home/styles.css';
 import {
   checkSubTreeForActive,
   getPathName,
 } from '@/utils/staticGeneration/checkSubTreeForActive.mjs';
 import type { GetStaticProps } from 'next';
+import Link from 'next/link';
 import type { FC } from 'react';
 import React from 'react';
 
 const Home: FC = () => {
   return (
-    <Stack direction="column" gap="$2xl">
-      <BrowseSection>
-        <BrowseSection.LinkBlock
-          title="Run a Node"
-          subtitle=" "
-          icon="RestApi"
-          href="node/overview"
-        />
-        <BrowseSection.LinkBlock
-          title="Ambassadors"
-          subtitle=" "
-          icon="Contribute"
-          href="ambassadors/overview"
-        />
-        <BrowseSection.LinkBlock
-          title="Kadena DAO"
-          subtitle=" "
-          icon="Concepts"
-          href="kadena-dao"
-        />
-      </BrowseSection>
-    </Stack>
+    <>
+      <div className={cardSectionClass}>
+        <section className={cardClass}>
+          <Heading as="h4">Kadena DAO</Heading>
+          <Box marginY="$4">
+            <Text>
+              Kadena has built the framework for the first DAO, Decentralized
+              Autonomous Organization, on its public blockchain called dao.init.
+              As Kadena’s ecosystem continues to grow, the creation of a DAO
+              will allow for the broader community to provide their input in a
+              decentralized manner.
+            </Text>
+          </Box>
+          <Button as="a" asChild>
+            <Link href={'/docs/contribute/kadena-dao'}>Start contributing</Link>
+          </Button>
+        </section>
+        <section className={cardClass}>
+          <Heading as="h4">We are looking for Ambassadors</Heading>
+          <Box marginY="$4">
+            <Text>
+              Since the launch of Kadena&apos;s public blockchain, we have had
+              active individuals in our community’s social channels who drive
+              adoption. Whether it is keeping the community up to date, writing
+              blog posts, or educating new members about the Kadena project,
+              they have help promote Kadena and spread awareness.
+            </Text>
+          </Box>
+          <Button as="a" asChild>
+            <Link href={'/docs/contribute/ambassadors'}>
+              Become an ambassador
+            </Link>
+          </Button>
+        </section>
+
+        <section className={cardClass}>
+          <Heading as="h4">Run a Node</Heading>
+          <Box marginY="$4">
+            <Text>
+              <ul>
+                <li>
+                  <Link href="/docs/contribute/node">Run a Node</Link>
+                </li>
+                <li>
+                  <Link href="/docs/contribute/node/start-mining">
+                    Start mining
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/docs/contribute/node/interact-with-nodes">
+                    Interact with Nodes
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/docs/contribute/node/troubleshooting-chainweb">
+                    Troubleshooting Chainweb
+                  </Link>
+                </li>
+              </ul>
+            </Text>
+          </Box>
+        </section>
+
+        <section className={cardClass}>
+          <Heading as="h4">Contribute to the docs</Heading>
+          <Box marginY="$4">
+            <Text>
+              The Kadena documentation is open source and hosted on GitHub.
+              Using our public-facing Docs repo in the Kadena Community GitHub,
+              you can make suggested changes using pull requests. This allows
+              community members to improve the documentation and helps improve
+              the Kadena developer experience.
+            </Text>
+          </Box>
+          <Button as="a" asChild>
+            <Link href={'/docs/contribute/contribute'}>Fix our docs</Link>
+          </Button>
+        </section>
+      </div>
+    </>
   );
 };
 
