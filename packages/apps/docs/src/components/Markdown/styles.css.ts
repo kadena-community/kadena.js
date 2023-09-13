202e37f6657bce312b3126dfaff76bed40c0ec99
Steven Straatemans <steven@straatemans.nl>
2023-09-13 15:30:09 +0200

    cleanup

--- packages/apps/docs/src/components/Markdown/styles.css.ts
+++ packages/apps/docs/src/components/Markdown/styles.css.ts
@@ -2,11 +2,8 @@ import { sprinkles } from '@kadena/react-ui/theme';
 
 import { paragraphWrapperClass } from './Paragraph/styles.css';
 
+import { getClassName } from '@/utils/getClassName';
 import { globalStyle, style } from '@vanilla-extract/css';
-
-const getClassName = (str: string): string => {
-  return `.${str.split(' ').splice(0, 1)}`;
-};
 
 export const wrapperClass = style([
   sprinkles({
