17c1d2aefe371fb0ecfc8979b5cd8c5f03b22dc3
Sander Looijenga <slooijenga@anwb.nl>
2023-09-18 10:09:57 +0200

    install TanStack Query

--- packages/apps/tools/.eslintrc.js
+++ packages/apps/tools/.eslintrc.js
@@ -2,7 +2,10 @@ require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
   plugins: ['jest'],
-  extends: ['@kadena-dev/eslint-config/profile/next'],
+  extends: [
+    '@kadena-dev/eslint-config/profile/next',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
   parserOptions: { tsconfigRootDir: __dirname },
   ignorePatterns: ['**/generated/**'],
 };
