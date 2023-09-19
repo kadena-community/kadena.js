fb1f268571b2cf8e79f21594fb541484289ffb82
Mohamed Ismail <ismailreality1@gmail.com>
2023-09-19 11:23:55 +0200

    feat(docs): poc of algolia search

---
+++ packages/apps/docs/src/components/Search/useAlgoliaSearch.ts
@@ -0,0 +1,59 @@
+import type { StreamMetaData } from '@7-docs/edge';
+import algoliasearch from 'algoliasearch';
+import { useState } from 'react';
+
+interface IHookResult {
+  handleSubmit: (query: string) => void;
+  metadata: StreamMetaData[];
+  isLoading: boolean;
+  error: string | undefined;
+}
+
+export default function useAlgoliaSearch(limitResults = 25): IHookResult {
+  const client = algoliasearch(
+    'BD67NIA9JD',
+    '0e7307de57fb813aab55f7429257aa61',
+  );
+  const index = client.initIndex('docs_website_dev');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<undefined | string>(undefined);
+  const [metaData, setMetadata] = useState<StreamMetaData[]>([]);
+
+  function handleSubmit(query: string): void {
+    console.log('handleSubmit', query);
+    setIsLoading(true);
+    console.log('handleSubmit');
+    index
+      .search(query, { hitsPerPage: limitResults })
+      .then(({ hits }) => {
+        console.log({
+          hits,
+        });
+        setIsLoading(false);
+        const mappedData = hits.map((hit) => {
+          // @ts-ignore
+          const { filePath, title, content, url, header } = hit || {};
+          return {
+            filePath,
+            title,
+            content,
+            url,
+            header,
+          };
+        });
+        setMetadata(mappedData);
+        console.log('handleSubmit', mappedData);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }
+
+  return {
+    handleSubmit,
+    metadata: metaData,
+    isLoading,
+    error,
+  };
+}
