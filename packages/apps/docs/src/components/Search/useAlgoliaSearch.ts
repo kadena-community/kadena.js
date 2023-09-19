fbe0bb545437920a920abf122fe0ae0875762410
Mohamed Ismail <ismailreality1@gmail.com>
2023-09-19 14:59:55 +0200

    feat(docs): update route query params with search string

--- packages/apps/docs/src/components/Search/useAlgoliaSearch.ts
+++ packages/apps/docs/src/components/Search/useAlgoliaSearch.ts
@@ -1,5 +1,6 @@
 import type { StreamMetaData } from '@7-docs/edge';
 import algoliasearch from 'algoliasearch';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 interface IHookResult {
@@ -12,23 +13,19 @@ interface IHookResult {
 export default function useAlgoliaSearch(limitResults = 25): IHookResult {
   const client = algoliasearch(
     'BD67NIA9JD',
-    '0e7307de57fb813aab55f7429257aa61',
+    '2a38c9156d62b12da7a1dfa759a7ae39',
   );
   const index = client.initIndex('docs_website_dev');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<undefined | string>(undefined);
   const [metaData, setMetadata] = useState<StreamMetaData[]>([]);
+  const router = useRouter();
 
   function handleSubmit(query: string): void {
-    console.log('handleSubmit', query);
     setIsLoading(true);
-    console.log('handleSubmit');
     index
       .search(query, { hitsPerPage: limitResults })
       .then(({ hits }) => {
-        console.log({
-          hits,
-        });
         setIsLoading(false);
         const mappedData = hits.map((hit) => {
           // @ts-ignore
@@ -42,7 +39,8 @@ export default function useAlgoliaSearch(limitResults = 25): IHookResult {
           };
         });
         setMetadata(mappedData);
-        console.log('handleSubmit', mappedData);
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        router.push(`${router.route}?q=${query}`);
       })
       .catch((err) => {
         setIsLoading(false);
