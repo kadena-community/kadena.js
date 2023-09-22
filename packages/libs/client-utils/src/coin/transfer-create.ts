364caa926e2ebd373fc3c86c90ac98ba3d399466
Albert G <516972+alber70g@users.noreply.github.com>
2023-09-22 17:29:36 +0200

    feat(client-utils): wip, adds @kadena/client-utils as shared utility wrapper around @kadena/client
    feat(client-utils): wip, javad brainstorm

---
+++ packages/libs/client-utils/src/coin/transfer-create.ts
@@ -0,0 +1,64 @@
+import {
+  isSignedTransaction,
+  Pact,
+  readKeyset,
+  signWithChainweaver,
+} from '@kadena/client';
+import type { IPactDecimal } from '@kadena/types';
+
+import { listen, preflight, submit } from './util/client';
+
+const NETWORK_ID: string = 'testnet04';
+
+async function main(): Promise<void> {
+  const senderAccount =
+    'k:dc20ab800b0420be9b1075c97e80b104b073b0405b5e2b78afd29dd74aaf5e46';
+  const receiverAccount =
+    'k:1a98599ff4677a119565d852b29b0d447c5051cb2c49673c32cba3fae096e209';
+  const amount: IPactDecimal = { decimal: '0.23' };
+  const signerKey = senderAccount.split('k:')[1];
+  const receiverKey = receiverAccount.split('k:')[1];
+
+  const transaction = Pact.builder
+    .execution(
+      Pact.modules.coin['transfer-create'](
+        senderAccount,
+        receiverAccount,
+        readKeyset('ks'),
+        amount,
+      ),
+    )
+    .addSigner(signerKey, (withCapability) => [
+      withCapability('coin.GAS'),
+      withCapability('coin.TRANSFER', senderAccount, receiverAccount, amount),
+    ])
+    .addKeyset('ks', 'keys-all', receiverKey)
+    .setMeta({
+      chainId: '1',
+      gasLimit: 1000,
+      gasPrice: 1.0e-6,
+      senderAccount,
+      ttl: 10 * 60, // 10 minutes
+    })
+    .setNetworkId(NETWORK_ID)
+    .createTransaction();
+
+  const signedTx = await signWithChainweaver(transaction);
+
+  const preflightResult = await preflight(signedTx);
+
+  if (preflightResult.result.status === 'failure') {
+    console.error(preflightResult.result.status);
+    throw new Error('failure');
+  }
+
+  console.log('preflight successful');
+
+  if (isSignedTransaction(signedTx)) {
+    const transactionDescriptor = await submit(signedTx);
+    const result = await listen(transactionDescriptor);
+    console.log(result);
+  }
+}
+
+main().catch(console.error);
---
+++ packages/libs/client-utils/src/example-contract/poll-status.ts
@@ -0,0 +1,37 @@
+import { createClient } from '@kadena/client';
+import type { ChainId } from '@kadena/types';
+
+import { apiHostGenerator } from '../utils/apiHostGenerator';
+
+const { pollStatus } = createClient(apiHostGenerator);
+
+export async function pollRequestsAndWaitForEachPromiseExample(): Promise<void> {
+  const someRequestKeys = ['key1', 'key2'];
+  const transactionDescriptors = someRequestKeys.map((requestKey) => ({
+    requestKey,
+    networkId: 'testnet04',
+    chainId: '1' as ChainId,
+  }));
+  // You can await this promise, but you can also await the result of each individual request
+  const results = pollStatus(transactionDescriptors, {
+    onPoll: (requestKey) => {
+      console.log('polling status of', requestKey);
+    },
+  });
+
+  // await for each individual request
+  Object.entries(results.requests).map(([requestKey, promise]) =>
+    promise
+      .then((data) => {
+        console.log('the request ', requestKey, 'result:', data);
+      })
+      .catch((error) => {
+        console.log(
+          'error while getting the status of ',
+          requestKey,
+          'error:',
+          error,
+        );
+      }),
+  );
+}
