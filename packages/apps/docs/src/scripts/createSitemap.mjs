c444881601769cd252099385776b039c8a149c0d
Steven <steven@straatemans.nl>
2023-09-25 13:28:21 +0200

    [DOCS] feat: add sitemap (#955)

---
+++ packages/apps/docs/src/scripts/createSitemap.mjs
@@ -0,0 +1,113 @@
+import * as fs from 'fs';
+import chalk from 'chalk';
+import {
+  getAuthorData,
+  getTagsData,
+} from './../utils/staticGeneration/getData.mjs';
+import { getFlatData } from './../utils/staticGeneration/flatPosts.mjs';
+import { format, isValid } from 'date-fns';
+
+const MENUFILE = './public/sitemap.xml';
+const URL = 'https://docs.kadena.io';
+
+const errors = [];
+const posts = getFlatData();
+const authors = getAuthorData();
+const tags = getTagsData();
+
+const setPrio = (root) => {
+  if (root.includes('/blogchain')) return '0.5';
+
+  return '1';
+};
+
+const formatDate = (dateStr) => {
+  const date = new Date(dateStr);
+  if (!isValid(date)) return '';
+
+  return format(date, 'yyyy-MM-dd');
+};
+
+const getPosts = (root) => {
+  return posts
+    .map(
+      (post) => `
+    <url>
+      <loc>${root}${post.root}}</loc>
+      ${
+        post.lastModifiedDate &&
+        `<lastmod>${formatDate(post.lastModifiedDate)}</lastmod>`
+      }
+      <changefreq>monthly</changefreq>
+      <priority>${setPrio(post.root)}</priority>
+    </url>`,
+    )
+    .join('');
+};
+
+const getTags = (root) => {
+  return tags
+    .map(
+      (tag) => `
+    <url>
+      <loc>${root}/tags/${encodeURIComponent(tag.tag)}</loc>
+    </url>`,
+    )
+    .join('');
+};
+
+const getAuthors = (root) => {
+  return authors
+    .map(
+      (author) => `
+    <url>
+      <loc>${root}/authors/${encodeURIComponent(author.id)}</loc>
+    </url>`,
+    )
+    .join('');
+};
+
+const init = () => {
+  console.log(
+    '========================================== START CREATE SITEMAP ==\n\n',
+  );
+
+  const fileStr = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${URL}/</loc>
+    </url>  
+    <url>
+      <loc>${URL}/search</loc>
+    </url>  
+    <url>  
+      <loc>${URL}/tags</loc>
+    </url>  
+      ${getTags(URL)}
+    <url>  
+      <loc>${URL}/authors</loc>
+    </url>  
+    ${getAuthors(URL)}
+    <url>
+      <loc>${URL}/help</loc>
+    </url>
+    ${getPosts(URL)}
+    </urlset>`;
+
+  if (errors.length) {
+    errors.map((error) => {
+      console.warn(chalk.red('⨯'), error);
+    });
+    process.exitCode = 1;
+  } else {
+    fs.writeFileSync(MENUFILE, fileStr);
+
+    console.log(chalk.green('✓'), 'SITEMAP CREATED');
+  }
+
+  console.log(
+    '========================================== END CREATE SITEMAP ====\n\n',
+  );
+};
+
+init();
