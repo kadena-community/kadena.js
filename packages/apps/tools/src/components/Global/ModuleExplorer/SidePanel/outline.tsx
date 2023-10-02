d45452633f0fe8514f47aecf6430e22d1b656a8f
Sander Looijenga <slooijenga@anwb.nl>
2023-10-02 13:27:47 +0200

    Initial outline implementation

--- packages/apps/tools/src/components/Global/ModuleExplorer/SidePanel/outline.tsx
+++ packages/apps/tools/src/components/Global/ModuleExplorer/SidePanel/outline.tsx
@@ -1,14 +1,80 @@
-import { Heading, Text } from '@kadena/react-ui';
+import { contractParser } from '@kadena/pactjs-generator';
+import type { ITreeProps } from '@kadena/react-ui';
+import { Button, Heading, Text, Tree } from '@kadena/react-ui';
+
+import type { IModule } from '..';
 
 import React from 'react';
 
-interface IOutlineProps extends React.HTMLAttributes<HTMLDivElement> {}
+export interface IOutlineProps extends React.HTMLAttributes<HTMLDivElement> {
+  selectedModule?: IModule & { code: string };
+}
+
+type Contract = ReturnType<typeof contractParser>[0][0]; // TODO: fix this because it's ugly/nasty
+
+const contractToTreeItems = (contract: Contract): ITreeProps['items'] => {
+  const { usedInterface: interfaces, capabilities, functions } = contract;
+  const items: ITreeProps['items'] = [];
+
+  if (interfaces?.length) {
+    items.push({
+      title: 'Interfaces',
+      items: interfaces.map((i) => ({
+        title: (
+          <Button variant="compact" icon="ExitToApp">
+            {i.name}
+          </Button>
+        ),
+      })),
+    });
+  }
+
+  if (capabilities?.length) {
+    items.push({
+      title: 'Capabilities',
+      items: capabilities.map((c) => ({
+        title: c.name,
+      })),
+    });
+  }
+
+  if (functions?.length) {
+    items.push({
+      title: 'Functions',
+      items: functions.map((f) => ({
+        title: f.name,
+      })),
+    });
+  }
+
+  return items;
+};
 
 const Outline = (props: IOutlineProps): React.JSX.Element => {
+  const { selectedModule } = props;
+
+  let parsedContract: Contract;
+  if (selectedModule) {
+    const [, namespace] = selectedModule.moduleName.split('.');
+    const [parsedModules] = contractParser(selectedModule.code, namespace);
+    parsedContract = parsedModules[0]; // TODO: improve this
+  }
+
   return (
     <div {...props}>
-      <Heading as="h4">Outline</Heading>
-      <Text>To be implemented…</Text>
+      <Heading as="h4">
+        Outline
+        {selectedModule
+          ? ` ${selectedModule.moduleName} @ ${selectedModule.chainId}`
+          : null}
+      </Heading>
+      {selectedModule ? (
+        <Tree items={contractToTreeItems(parsedContract!)} isOpen />
+      ) : (
+        <Text>
+          Select/open a module to see the outline of the contract/module
+        </Text>
+      )}
     </div>
   );
 };
