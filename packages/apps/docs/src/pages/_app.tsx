073d00c9f481ce11041c8339bbb259d33e62bddd
Mohamed Ismail <ismailreality1@gmail.com>
2023-10-06 15:55:25 +0200

    feat(docs): add favicons for multiple sizes

--- packages/apps/docs/src/pages/_app.tsx
+++ packages/apps/docs/src/pages/_app.tsx
@@ -65,27 +65,52 @@ export const MyApp = ({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="icon"
-          href="/assets/light/icon@32.png"
+          href="/assets/favicons/light/icon@32.png"
+          sizes="32x32"
           media="(prefers-color-scheme: light)"
         />
         <link
           rel="icon"
-          href="/assets/dark/icon@32.png"
+          href="/assets/favicons/dark/icon@32.png"
+          sizes="32x32"
           media="(prefers-color-scheme: dark)"
         />
         <link
           rel="icon"
-          sizes="any"
-          href="/assets/light/icon@192.png"
+          href="/assets/favicons/light/icon@128.png"
+          sizes="128x128"
           media="(prefers-color-scheme: light)"
         />
         <link
           rel="icon"
-          sizes="any"
-          href="/assets/dark/icon@192.png"
+          href="/assets/favicons/dark/icon@128.png"
+          sizes="128x128"
           media="(prefers-color-scheme: dark)"
         />
-        <link rel="apple-touch-icon" href="/assets/dark/icon@512.png" />
+        <link
+          rel="shortcut icon"
+          sizes="192x192"
+          href="/assets/favicons/dark/icon@192.png"
+        />
+        <link
+          rel="icon"
+          href="/assets/favicons/light/icon@192.png"
+          sizes="192x192"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          href="/assets/favicons/dark/icon@192.png"
+          sizes="192x192"
+          media="(prefers-color-scheme: dark)"
+        />
+        {/* Android Shortcut icon */}
+        <link rel="shortcut icon" href="/assets/favicons/dark/icon@192.png" />
+        {/* Apple touch icon */}
+        <link
+          rel="apple-touch-icon"
+          href="/assets/favicons/dark/icon@192.png"
+        />
       </Head>
       <MDXProvider components={markDownComponents}>
         <ThemeProvider
