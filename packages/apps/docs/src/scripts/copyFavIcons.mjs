d223eac96868cd38683672e60dd5c56563267f5a
Mohamed Ismail <realdreamer@users.noreply.github.com>
2023-10-09 12:57:49 +0200

    chore: bump docs deployment (#1010)

--- packages/apps/docs/src/scripts/copyFavIcons.mjs
+++ packages/apps/docs/src/scripts/copyFavIcons.mjs
@@ -5,7 +5,7 @@ const COMMON_FAV_ICONS_PATH = '../../../common/images/icons/internal';
 (async function () {
   try {
     const results = await copy(COMMON_FAV_ICONS_PATH, 'public/assets/favicons');
-    console.info(`Copy fav icons${results.length} files succeeded`);
+    console.info(`Copy fav icons ${results.length} files succeeded`);
   } catch (error) {
     console.error(`Copy fav icons failed: ${error}`);
   }
