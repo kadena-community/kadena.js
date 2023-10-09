33d7c10dba0065d8abaaf0b0e6eb11bb563c49be
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-09 16:19:11 +0200

    update config files

--- packages/tools/heft-rig/playwright.config.base.ts
+++ packages/tools/heft-rig/playwright.config.base.ts
@@ -15,7 +15,7 @@ export const  defaultConfig = {
   workers: process.env.CI ? 4 : 1,
   reporter: process.env.CI ? 'github' : 'list',
   use: {
-    headless: process.env.CI,
+    headless: !!process.env.CI,
     baseURL: process.env.PLAYWRIGHT_BASE_URL
       ? process.env.PLAYWRIGHT_BASE_URL
       : 'http://127.0.0.1:3000',
