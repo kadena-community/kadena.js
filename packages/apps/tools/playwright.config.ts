c318f1868db54ec3cf405b3c0fab3e8e71f44c8d
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-09 19:18:35 +0200

    update workflow and config

--- packages/apps/tools/playwright.config.ts
+++ packages/apps/tools/playwright.config.ts
@@ -1,16 +1,5 @@
 import { defaultConfig } from '@kadena-dev/heft-rig/playwright.config.base';
 import { defineConfig } from "@playwright/test";
 
-export default defineConfig({
-  ...defaultConfig,
-  use: {
-    ...defaultConfig.use,
-    trace: 'retain-on-failure'
-  },
-  webServer: {
-    ...defaultConfig.webServer
-  },
-  expect: {
-    ...defaultConfig.expect
-  }
-});
+export default defineConfig({...defaultConfig}
+);
