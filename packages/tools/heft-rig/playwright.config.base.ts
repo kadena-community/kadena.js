91b8e03dd4117b478eab26f8d6989efe4788ae8f
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-09 19:25:10 +0200

    update path to report

--- packages/tools/heft-rig/playwright.config.base.ts
+++ packages/tools/heft-rig/playwright.config.base.ts
@@ -15,12 +15,7 @@ export const  defaultConfig: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 4 : 1,
-  reporter: process.env.CI
-    ? [
-      ['github'],
-      ['html', { open: 'never' }],
-    ]
-    : [['list'], ['html', { open: 'never' }]],
+  reporter: [['github'],['list'], ['html', { open: 'never' }]],
   use: {
     headless: !!process.env.CI,
     baseURL: process.env.PLAYWRIGHT_BASE_URL
