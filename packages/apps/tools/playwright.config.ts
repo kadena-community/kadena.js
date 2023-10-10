3951b28d5f36dfeff6fb19c5639da25be686b9ce
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-10 12:33:49 +0200

    try to create env file dynamically

--- packages/apps/tools/playwright.config.ts
+++ packages/apps/tools/playwright.config.ts
@@ -8,16 +8,11 @@ process.env.FAUCET_PRIVATE_KEY = "93fd78bd1e79a3b593d4732b76050b418aeefa5e4e6ea8
 export default defineConfig({
     ...defaultConfig,
     webServer: {
-      command: 'pnpm run dev',
+      command: 'pnpm run start',
       url: 'http://127.0.0.1:3000',
       reuseExistingServer: false,
-      stdout: 'pipe',
-      stderr: 'pipe',
-      timeout: 120 * 1000,
-      env: {
-        FAUCET_PUBLIC_KEY: "782127638ab9cc8fa8598ff0120464ceef6f367ddcd616b47afffbdb175dcc5e",
-        FAUCET_PRIVATE_KEY: "93fd78bd1e79a3b593d4732b76050b418aeefa5e4e6ea80f351b4c3a5b0dd19f"
-      }
+      stdout: 'ignore',
+      stderr: 'ignore',
     }
   }
 );
