75d92663e2f12040ec9d4800b475f7f71e25719d
Mohamed Ismail <ismailreality1@gmail.com>
2023-10-10 12:30:04 +0200

    fix(docs): update cookie consent ui changes

---
+++ packages/apps/docs/src/components/CookieConsent/styles.css.ts
@@ -0,0 +1,10 @@
+import { sprinkles } from '@kadena/react-ui/theme';
+
+import { style } from '@vanilla-extract/css';
+
+export const notificationClass = style([
+  sprinkles({
+    position: 'relative',
+    zIndex: 5,
+  }),
+]);
