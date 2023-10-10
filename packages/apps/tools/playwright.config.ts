cc8ede213a12ff99d6711ff9bf3d52b9ff92b5c3
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-10 13:23:32 +0200

    try to set faucet keys at build time

--- packages/apps/tools/playwright.config.ts
+++ packages/apps/tools/playwright.config.ts
@@ -1,10 +1,6 @@
 import { defaultConfig } from '@kadena-dev/heft-rig/playwright.config.base';
 import { defineConfig } from "@playwright/test";
 
-process.env.FAUCET_PUBLIC_KEY = "782127638ab9cc8fa8598ff0120464ceef6f367ddcd616b47afffbdb175dcc5e";
-process.env.FAUCET_PRIVATE_KEY = "93fd78bd1e79a3b593d4732b76050b418aeefa5e4e6ea80f351b4c3a5b0dd19f";
-
-
 export default defineConfig({
     ...defaultConfig,
     webServer: {
