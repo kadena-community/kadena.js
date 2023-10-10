6aa0c49c1b19610c89efe5c819b506d47716d544
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-10 15:31:35 +0200

    Formatting & linting

--- packages/apps/tools/tests/fixtures/data/accounts.ts
+++ packages/apps/tools/tests/fixtures/data/accounts.ts
@@ -1,11 +1,8 @@
-
-interface accounts  {
-  publicKey: string,
-  secretkey: string
+interface accounts {
+  publicKey: string;
+  secretkey: string;
 }
 export const accountsData: accounts = {
-  publicKey: "938c8ec66283493844c205481f220cfbd7024bda5859c7897141f6a5c6fc52c1",
-  secretkey: "be6442bd49bb7c1c48413596da6e879f85c047bbe8236abe9f9afb63eb32705d"
-}
-
-
+  publicKey: '938c8ec66283493844c205481f220cfbd7024bda5859c7897141f6a5c6fc52c1',
+  secretkey: 'be6442bd49bb7c1c48413596da6e879f85c047bbe8236abe9f9afb63eb32705d',
+};
