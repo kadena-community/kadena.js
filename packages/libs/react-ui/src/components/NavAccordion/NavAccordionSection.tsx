f240568c6877253f28bbb58e73f59a19a132f186
Timo Heddes <timo.heddes@gmail.com>
2023-10-11 17:33:26 +0200

    feat: style refactor

--- packages/libs/react-ui/src/components/NavAccordion/NavAccordionSection.tsx
+++ packages/libs/react-ui/src/components/NavAccordion/NavAccordionSection.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { NavAccordionContext } from './NavAccordion.context';
+import {
+  navAccordionListClass,
+  navAccordionListItemClass,
+} from './NavAccordion.css';
 import type { INavAccordionGroupProps } from './NavAccordionGroup';
 import { NavAccordionGroup } from './NavAccordionGroup';
 import type { INavAccordionLinkProps } from './NavAccordionLink';
@@ -12,13 +16,15 @@ import {
 } from '@components/Accordion/Accordion.css';
 import { AccordionHeading } from '@components/Accordion/AccordionHeading';
 import classNames from 'classnames';
-import type { FC, FunctionComponentElement } from 'react';
+import type { FC, FunctionComponentElement as FCElement } from 'react';
 import React, { Children, useContext } from 'react';
 
 export interface INavAccordionSectionProps {
-  children?: FunctionComponentElement<
-    INavAccordionGroupProps | INavAccordionLinkProps
-  >[];
+  children?:
+    | FCElement<INavAccordionGroupProps>[]
+    | FCElement<INavAccordionLinkProps>[]
+    | FCElement<INavAccordionGroupProps>
+    | FCElement<INavAccordionLinkProps>;
   onClick?: () => void;
   onClose?: () => void;
   onOpen?: () => void;
@@ -34,8 +40,10 @@ export const NavAccordionSection: FC<INavAccordionSectionProps> = ({
 }) => {
   const { openSections, setOpenSections, linked } =
     useContext(NavAccordionContext);
-  const sectionId = title.replace(/\s+/g, '-').toLowerCase();
+  const sectionId = title.replace(/\s+/g, '').toLowerCase();
   const isOpen = openSections.includes(sectionId);
+
+  console.log(openSections);
 
   const handleClick = (): void => {
     if (isOpen) {
@@ -61,13 +69,15 @@ export const NavAccordionSection: FC<INavAccordionSectionProps> = ({
 
       {children && (
         <ul
-          className={classNames('list', [
+          className={classNames(navAccordionListClass, [
             isOpen ? accordionExpand : accordionCollapse,
           ])}
         >
           {Children.map(children, (child) => {
             const Tag = child.type === NavAccordionGroup ? 'ul' : 'li';
-            return <Tag>{child}</Tag>;
+            const className =
+              Tag === 'ul' ? navAccordionListClass : navAccordionListItemClass;
+            return <Tag className={className}>{child}</Tag>;
           })}
         </ul>
       )}
