8f9725ceddddef2eb2c2af8558a8e94f7b9d7970
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-12 10:42:38 +0200

    update config

--- packages/tools/e2e-tests/.eslintrc.js
+++ packages/tools/e2e-tests/.eslintrc.js
@@ -2,9 +2,12 @@ require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
   extends: [
-    '@kadena-dev/eslint-config/profile/next',
+    '@kadena-dev/eslint-config/profile/lib',
     'plugin:playwright/recommended',
   ],
   parserOptions: { tsconfigRootDir: __dirname },
   ignorePatterns: ['playwright.config.ts'],
+  rules: {
+    '@kadena-dev/typedef-var': 'off',
+  },
 };
