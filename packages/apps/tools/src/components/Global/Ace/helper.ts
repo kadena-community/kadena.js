e2536087ff460a9cf017156942076b7143bb1252
Sander Looijenga <slooijenga@anwb.nl>
2023-10-16 15:43:46 +0200

    theme & mode editor settings

--- packages/apps/tools/src/components/Global/Ace/helper.ts
+++ packages/apps/tools/src/components/Global/Ace/helper.ts
@@ -6,3 +6,9 @@ export const keyboards = [
   'ace',
 ] as const;
 export type KeyboardHandler = (typeof keyboards)[number];
+
+export const themes = ['monokai', 'one_dark'] as const;
+export type Theme = (typeof themes)[number];
+
+export const modes = ['lisp', 'clojure'] as const;
+export type Mode = (typeof modes)[number];
