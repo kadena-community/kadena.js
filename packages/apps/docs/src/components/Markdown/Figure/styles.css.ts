b92022b28baadb2c4dea55532b2dd79564cc2d01
Isa Ozler <isaozler@gmail.com>
2023-10-16 13:11:31 +0200

    fixed figure element spacing issues on breakpoints

--- packages/apps/docs/src/components/Markdown/Figure/styles.css.ts
+++ packages/apps/docs/src/components/Markdown/Figure/styles.css.ts
@@ -1,4 +1,4 @@
-import { sprinkles, vars } from '@kadena/react-ui/theme';
+import { responsiveStyle, sprinkles, vars } from '@kadena/react-ui/theme';
 
 import { paragraphWrapperClass } from '../Paragraph/styles.css';
 
@@ -10,24 +10,53 @@ export const figure = style([
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    marginX: '$5',
+    marginX: 0,
     marginY: 0,
     width: '100%',
     position: 'relative',
   }),
+  {
+    ...responsiveStyle({
+      md: {
+        boxSizing: 'border-box',
+        marginLeft: vars.sizes.$5,
+        marginRight: vars.sizes.$5,
+        marginBottom: vars.sizes.$8,
+      },
+    }),
+  },
 ]);
 
 export const figureImg = style([
   sprinkles({
     height: '100%',
+    width: '100%',
   }),
+  {
+    ...responsiveStyle({
+      md: {
+        width: 'unset',
+      },
+    }),
+  },
 ]);
 
 export const figCaption = style([
   sprinkles({
     textAlign: 'center',
     marginBottom: '$8',
+    marginX: '$4',
   }),
+  {
+    ...responsiveStyle({
+      md: {
+        maxWidth: '50%',
+        marginTop: vars.sizes.$2,
+        marginLeft: 0,
+        marginRight: 0,
+      },
+    }),
+  },
 ]);
 
 globalStyle(
@@ -38,3 +67,7 @@ globalStyle(
     marginTop: vars.sizes.$4,
   },
 );
+
+globalStyle(`table figure`, {
+  margin: '0 !important',
+});
