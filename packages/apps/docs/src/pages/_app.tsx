bb0e0cd63214d39a78d686a81423b8ae88a04e9b
Mohamed Ismail <ismailreality1@gmail.com>
2023-10-17 10:35:53 +0200

    feat(docs): add social meta tags fb+twitter

--- packages/apps/docs/src/pages/_app.tsx
+++ packages/apps/docs/src/pages/_app.tsx
@@ -87,6 +87,19 @@ export const MyApp = ({
             <meta name="author" content={authorInfo.name} />
           </>
         )}
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={headerImage} />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={headerImage} />
+
         <link
           rel="icon"
           href="/assets/favicons/light/icon@32.png"
