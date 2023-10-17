b45fdd90dd4a889fa13ea5fe827ca219824dc0ed
Sander Looijenga <slooijenga@anwb.nl>
2023-10-17 12:13:35 +0200

    Add some documentation

--- packages/apps/tools/src/components/Global/Ace/styles.css.ts
+++ packages/apps/tools/src/components/Global/Ace/styles.css.ts
@@ -2,4 +2,8 @@ import { globalStyle, style } from '@vanilla-extract/css';
 
 export const containerStyle = style({});
 
+/*
+ * <span> elements get a global style (which includes font-family) from the `react-ui` library, we
+ * need to reset it here otherwise code in the editor won't be rendered as a monospace font.
+ */
 globalStyle(`${containerStyle} span`, { fontFamily: 'inherit !important' });
