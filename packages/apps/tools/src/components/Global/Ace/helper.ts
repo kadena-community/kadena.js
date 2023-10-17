e30895991e4deee2da9d67f67d79f1becab43375
Sander Looijenga <slooijenga@anwb.nl>
2023-10-17 12:03:06 +0200

    Remove unsupported keyboards

--- packages/apps/tools/src/components/Global/Ace/helper.ts
+++ packages/apps/tools/src/components/Global/Ace/helper.ts
@@ -1,10 +1,4 @@
-export const keyboards = [
-  'vim',
-  'emacs',
-  'sublime', // TODO: Not sure if this works
-  'vscode', // TODO: Not sure if this works
-  'ace',
-] as const;
+export const keyboards = ['vim', 'emacs', 'ace'] as const;
 export type KeyboardHandler = (typeof keyboards)[number];
 
 export const themes = ['monokai', 'one_dark'] as const;
