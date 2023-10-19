e607d4940c7d2b3e5fd369ab6a662a18ad60b1a5
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-10-19 10:01:00 +0200

    initial implementation of transaction query with dynamic params

--- packages/apps/graph/src/graph/Query/transactions.ts
+++ packages/apps/graph/src/graph/Query/transactions.ts
@@ -1,31 +1,74 @@
+import { Prisma } from '@prisma/client';
 import { prismaClient } from '../../db/prismaClient';
 import { builder } from '../builder';
 
 builder.queryField('transactions', (t) => {
   return t.prismaConnection({
     args: {
-      accountName: t.arg.string({ required: true }),
-      moduleName: t.arg.string({ required: true }),
+      accountName: t.arg.string({ required: false }),
+      moduleName: t.arg.string({ required: false }),
       chainId: t.arg.string({ required: false }),
     },
     type: 'Transaction',
     cursor: 'blockHash_requestKey',
     resolve: (query, parent, args) => {
-      return prismaClient.transaction.findMany({
-        ...query,
-        where: {
+      const conditions: Prisma.TransactionWhereInput[] = [];
+      const conditionsObj: Prisma.TransactionWhereInput = {};
+
+      // conditionsObj.events = {
+      //   some: {
+      //     moduleName: args.moduleName,
+      //   },
+      //   }
+      // }
+
+      if (args.accountName) {
+        conditions.push({
           senderAccount: args.accountName,
+        });
+      }
+
+      if (args.moduleName) {
+        conditions.push({
           events: {
             some: {
               moduleName: args.moduleName,
             },
           },
-          ...(args.chainId && { chainId: parseInt(args.chainId) }),
+        });
+      }
+
+      if (args.chainId) {
+        conditions.push({
+          chainId: parseInt(args.chainId),
+        });
+      }
+
+      return prismaClient.transaction.findMany({
+        ...query,
+        where: {
+          AND: conditions,
         },
         orderBy: {
           height: 'desc',
         },
       });
+
+      // return prismaClient.transaction.findMany({
+      //   ...query,
+      //   where: {
+      //     senderAccount: args.accountName,
+      //     events: {
+      //       some: {
+      //         moduleName: args.moduleName,
+      //       },
+      //     },
+      //     ...(args.chainId && { chainId: parseInt(args.chainId) }),
+      //   },
+      //   orderBy: {
+      //     height: 'desc',
+      //   },
+      // });
     },
   });
 });
