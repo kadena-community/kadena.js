cab74a3f0322b50ad922d39bdce0bdaee60fdbf5
Lars Kappert <lars@webpro.nl>
2023-10-21 10:11:20 +0200

    Boyscouting some old code (after refactorings?)

--- packages/apps/tools/src/pages/_document.tsx
+++ packages/apps/tools/src/pages/_document.tsx
@@ -1,13 +1,12 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import type { FC, ReactNode } from 'react';
+import type { FC } from 'react';
 import React from 'react';
 import { bodyStyle } from './styles.css';
 
 const Document: FC = () => {
-  const TypedHead = Head as unknown as FC<{ children?: ReactNode }>;
   return (
     <Html lang="en">
-      <TypedHead></TypedHead>
+      <Head />
       <body className={bodyStyle}>
         <Main />
         <div id="modalportal" />
