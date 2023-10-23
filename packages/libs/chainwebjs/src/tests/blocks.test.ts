5c3e81138126811d8a7a3a67074734ee4ef37926
Lars Kappert <lars@webpro.nl>
2023-10-23 10:33:42 +0200

    Reuse `header` in some specs

--- packages/libs/chainwebjs/src/tests/blocks.test.ts
+++ packages/libs/chainwebjs/src/tests/blocks.test.ts
@@ -57,30 +57,7 @@ describe('chainweb.block', () => {
             ctx.status(200),
             ctx.json({
               limit: 1,
-              items: [
-                {
-                  nonce: '299775665679630368',
-                  creationTime: 1617745627822054,
-                  parent: 'XtgUmsnF20vMX4Dx9kN2W8cIXXiLNtDdFZLugMoDjrY',
-                  adjacents: {
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '15': 'ZoBvuaVZWBOKLaDZfM51A9LaKb5B1f2fW83VLftQa3Y',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '10': 'oT8NLW-IZSziaOgI_1AfCdJ18u3epFpGONrkQ_F6w_Y',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '5': 'SDj4sXByWVqi9epbPAiz1zqhmBGJrzSY2bPk9-IMaA0',
-                  },
-                  target: '9sLMdbnd1x6vtRGpIw5tKMt_1hgprJS0oQkAAAAAAAA',
-                  payloadHash: '2Skc1JkkBdLPkj5ZoV27nzhR3WjGD-tJiztCFGTaKIQ',
-                  chainId: 0,
-                  weight: 'iFU5b59ACHSOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-                  height: 1511601,
-                  chainwebVersion: 'mainnet01',
-                  epochStart: 1617743254198411,
-                  featureFlags: 0,
-                  hash: 'BsxyrIDE0to4Kn9bjdgR_Q7Ha9bYkzd7Yso8r0zrdOc',
-                },
-              ],
+              items: [header],
               next: null,
             }),
           );
@@ -119,30 +96,7 @@ describe('chainweb.block', () => {
             ctx.status(200),
             ctx.json({
               limit: 1,
-              items: [
-                {
-                  nonce: '299775665679630368',
-                  creationTime: 1617745627822054,
-                  parent: 'XtgUmsnF20vMX4Dx9kN2W8cIXXiLNtDdFZLugMoDjrY',
-                  adjacents: {
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '15': 'ZoBvuaVZWBOKLaDZfM51A9LaKb5B1f2fW83VLftQa3Y',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '10': 'oT8NLW-IZSziaOgI_1AfCdJ18u3epFpGONrkQ_F6w_Y',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '5': 'SDj4sXByWVqi9epbPAiz1zqhmBGJrzSY2bPk9-IMaA0',
-                  },
-                  target: '9sLMdbnd1x6vtRGpIw5tKMt_1hgprJS0oQkAAAAAAAA',
-                  payloadHash: '2Skc1JkkBdLPkj5ZoV27nzhR3WjGD-tJiztCFGTaKIQ',
-                  chainId: 0,
-                  weight: 'iFU5b59ACHSOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-                  height: 1511601,
-                  chainwebVersion: 'mainnet01',
-                  epochStart: 1617743254198411,
-                  featureFlags: 0,
-                  hash: 'BsxyrIDE0to4Kn9bjdgR_Q7Ha9bYkzd7Yso8r0zrdOc',
-                },
-              ],
+              items: [header],
               next: null,
             }),
           );
@@ -178,30 +132,7 @@ describe('chainweb.block', () => {
             ctx.status(200),
             ctx.json({
               limit: 1,
-              items: [
-                {
-                  nonce: '299775665679630368',
-                  creationTime: 1617745627822054,
-                  parent: 'XtgUmsnF20vMX4Dx9kN2W8cIXXiLNtDdFZLugMoDjrY',
-                  adjacents: {
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '15': 'ZoBvuaVZWBOKLaDZfM51A9LaKb5B1f2fW83VLftQa3Y',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '10': 'oT8NLW-IZSziaOgI_1AfCdJ18u3epFpGONrkQ_F6w_Y',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    '5': 'SDj4sXByWVqi9epbPAiz1zqhmBGJrzSY2bPk9-IMaA0',
-                  },
-                  target: '9sLMdbnd1x6vtRGpIw5tKMt_1hgprJS0oQkAAAAAAAA',
-                  payloadHash: '2Skc1JkkBdLPkj5ZoV27nzhR3WjGD-tJiztCFGTaKIQ',
-                  chainId: 0,
-                  weight: 'iFU5b59ACHSOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-                  height: 1511601,
-                  chainwebVersion: 'mainnet01',
-                  epochStart: 1617743254198411,
-                  featureFlags: 0,
-                  hash: 'BsxyrIDE0to4Kn9bjdgR_Q7Ha9bYkzd7Yso8r0zrdOc',
-                },
-              ],
+              items: [header],
               next: null,
             }),
           );
