7e2484699d4faa23839f679382fc775b5b89b654
Lars Kappert <lars@webpro.nl>
2023-10-23 09:47:56 +0200

    Clean up

--- packages/libs/react-ui/vitest.config.ts
+++ packages/libs/react-ui/vitest.config.ts
@@ -1,10 +1,6 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
-
-const dir = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
--- packages/libs/react-ui/vitest.setup.ts
+++ packages/libs/react-ui/vitest.setup.ts
@@ -1,6 +1,6 @@
 import { cleanup } from '@testing-library/react';
 import '@vanilla-extract/css/disableRuntimeStyles';
-import { afterEach, beforeAll, vi } from 'vitest';
+import { afterEach } from 'vitest';
 import 'vitest-dom/extend-expect';
 
 afterEach(() => {
