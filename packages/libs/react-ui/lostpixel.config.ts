69e0a4e2549e3ae8bda7e0c51076121ba484a126
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-10-24 15:29:56 +0200

    try out lost pixel

---
+++ packages/libs/react-ui/lostpixel.config.ts
@@ -0,0 +1,14 @@
+import { CustomProjectConfig } from 'lost-pixel';
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: './storybook-static',
+  },
+  // OSS mode
+  generateOnly: true,
+  failOnDifference: true,
+
+  // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
+  // lostPixelProjectId: "xxxx",
+  // process.env.LOST_PIXEL_API_KEY,
+};
