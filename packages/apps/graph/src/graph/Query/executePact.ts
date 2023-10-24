21721ebed597f20b82491c85ac81034bad7a43fd
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-10-24 14:54:16 +0200

    add chain id as an argument and implement array query and changed response type to array

--- packages/apps/graph/src/graph/Query/executePact.ts
+++ packages/apps/graph/src/graph/Query/executePact.ts
@@ -1,33 +1,33 @@
 import { Pact } from '@kadena/client';
-import { resourceLimits } from 'worker_threads';
 import { devnetConfig } from '../../scripts/devnet/config';
 import { dirtyRead } from '../../scripts/devnet/helper';
 import { builder } from '../builder';
 
 builder.queryField('executePact', (t) => {
   return t.field({
-    type: 'String',
+    type: ['String'],
     args: {
-      query: t.arg.string({ required: true }),
+      pactQuery: t.arg.stringList({ required: true }),
+      chainId: t.arg.string({ required: true }),
     },
     resolve: async (parent, args, context, info) => {
-      const query = Pact.builder
-        .execution(args.query)
-        .setMeta({
-          chainId: devnetConfig.CHAIN_ID,
-        })
-        .setNetworkId(devnetConfig.NETWORK_ID)
-        .createTransaction();
+      return args.pactQuery.map(async (query) => {
+        const transaction = Pact.builder
+          .execution(query)
+          .setMeta({
+            chainId: devnetConfig.CHAIN_ID,
+          })
+          .setNetworkId(devnetConfig.NETWORK_ID)
+          .createTransaction();
 
-      const response = await dirtyRead(query);
-      console.log(response);
+        const response = await dirtyRead(transaction);
 
-      if (response.result.status === 'failure') {
-        console.log('entrei');
-        return String(response.result.status);
-      }
+        if (response.result.status === 'failure') {
+          return String(response.result.status);
+        }
 
-      return response.result.data.toString();
+        return response.result.data.toString();
+      });
     },
   });
 });
