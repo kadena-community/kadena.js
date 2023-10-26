f35f3350f51641aa9de6006743d87e7243add57c
Lars Kappert <lars@webpro.nl>
2023-10-26 14:20:36 +0200

    Add coverage + initial thresholds

---
+++ packages/tools/shared-config/vitest.config.js
@@ -0,0 +1,14 @@
+const { default: tsconfigPaths } = require('vite-tsconfig-paths');
+const { defineConfig } = require('vitest/config');
+
+module.exports = defineConfig({
+  plugins: [tsconfigPaths({ projects: ['./tsconfig.json'] })],
+  test: {
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    coverage: {
+      enabled: false, // It's enabled in CI (root package.json)
+      provider: 'v8',
+      thresholdAutoUpdate: false, // Should be enabled? Too precise though, annoying in CI if missed by 0.01%
+    },
+  },
+});
