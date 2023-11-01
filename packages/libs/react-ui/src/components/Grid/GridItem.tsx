26ff38f392f9639001ead82446b1ab90a09023f5
Eileen Guo <eileen.m.guo@gmail.com>
2023-11-01 16:28:51 +0100

    allow classnames on layout components

--- packages/libs/react-ui/src/components/Grid/GridItem.tsx
+++ packages/libs/react-ui/src/components/Grid/GridItem.tsx
@@ -1,10 +1,11 @@
-import classNames from 'classnames';
+import classnames from 'classnames';
 import type { FC, ReactNode } from 'react';
 import React from 'react';
 import type { ResponsiveInputType } from './Grid.css';
 import { gridItemClass, itemColumnVariants, rowSpanVariants } from './Grid.css';
 
 export interface IGridItemProps {
+  className?: string;
   children?: ReactNode;
   columnSpan?: ResponsiveInputType;
   rowSpan?: keyof typeof rowSpanVariants;
@@ -23,14 +24,21 @@ const assembleColumnSpanVariants = (
 };
 
 export const GridItem: FC<IGridItemProps> = ({
+  className,
   children,
   columnSpan,
   rowSpan = 1,
 }) => {
-  const className = classNames(
-    gridItemClass,
-    rowSpanVariants[rowSpan],
-    columnSpan && assembleColumnSpanVariants(columnSpan),
+  return (
+    <div
+      className={classnames(
+        gridItemClass,
+        rowSpanVariants[rowSpan],
+        columnSpan && assembleColumnSpanVariants(columnSpan),
+        className,
+      )}
+    >
+      {children}
+    </div>
   );
-  return <div className={className}>{children}</div>;
 };
