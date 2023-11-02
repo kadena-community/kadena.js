6756b793d9afe1ffb1f4efe9623e271180c06e67
Sander Looijenga <slooijenga@anwb.nl>
2023-11-02 11:35:26 +0100

    Add more specific AccountHoverTag component

---
+++ packages/apps/tools/src/components/Global/AccountHoverTag/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { HoverTag } from '../HoverTag';
+import type { IHoverTagProps } from '../HoverTag/HoverTag';
+
+export interface IAccountHoverTagProps extends Pick<IHoverTagProps, 'value'> {}
+export const AccountHoverTag = ({ value }: IAccountHoverTagProps) => {
+  const [tooltipContent, setTooltipContent] = useState<string>(value);
+  return (
+    <HoverTag
+      icon="ContentCopy"
+      value={value}
+      onIconButtonClick={async () => {
+        await navigator.clipboard.writeText(value);
+        setTooltipContent('Copied!');
+        setTimeout(() => {
+          setTooltipContent(value);
+        }, 1000);
+      }}
+      tooltipValue={tooltipContent}
+    />
+  );
+};
