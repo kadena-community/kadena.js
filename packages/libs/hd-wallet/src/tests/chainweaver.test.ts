90fec0fd0243027ef98a93174c5ad3631ed68f62
Javad Khalilian <khalilian.javad@gmail.com>
2023-11-02 13:37:40 +0100

    test: some unit test

--- packages/libs/hd-wallet/src/tests/chainweaver.test.ts
+++ packages/libs/hd-wallet/src/tests/chainweaver.test.ts
@@ -4,34 +4,71 @@ import {
   kadenaGenMnemonic,
   kadenaMnemonicToRootKeypair,
 } from '../chainweaver';
+import { harden } from '../utils';
+
+const toHexStr = (bytes: Uint8Array) => Buffer.from(bytes).toString('hex');
 
 describe('kadenaGenMnemonic', () => {
   it('should generate a mnemonic', () => {
-    const words: string = kadenaGenMnemonic();
-    expect(words.split(' ')).toHaveLength(12);
+    const mnemonic = kadenaGenMnemonic();
+    const wordsList = mnemonic.split(' ');
+    expect(wordsList).toHaveLength(12);
+    expect(wordsList.every((word) => word.length > 2)).toBe(true);
   });
 });
 
 describe('kadenaMnemonicToRootKeypair', () => {
   it('should generate a Root key from mnemonic', () => {
-    const words: string =
+    const mnemonic =
       'bubble fade wasp analyst then panel desert hold spatial sound lucky weekend';
     const password = 'password';
-    const rootKey = kadenaMnemonicToRootKeypair(password, words);
+    const rootKey = kadenaMnemonicToRootKeypair(password, mnemonic);
     expect(rootKey).toHaveLength(128);
+  });
+
+  it('should generate a the same root key from the same mnemonic', () => {
+    const mnemonic =
+      'bubble fade wasp analyst then panel desert hold spatial sound lucky weekend';
+    const password = 'password';
+    const rootKey1 = kadenaMnemonicToRootKeypair(password, mnemonic);
+    const rootKey2 = kadenaMnemonicToRootKeypair(password, mnemonic);
+
+    expect(toHexStr(rootKey1)).toBe(toHexStr(rootKey2));
+  });
+
+  it('should generate different root key for different mnemonic', () => {
+    const mnemonicFirst =
+      'bubble fade wasp analyst then panel desert hold spatial sound lucky weekend';
+
+    const mnemonicSecond =
+      'tuna nerve predict all catch early oblige inform hamster magnet century goddess';
+    const password = 'password';
+    const rootKey1 = kadenaMnemonicToRootKeypair(password, mnemonicFirst);
+    const rootKey2 = kadenaMnemonicToRootKeypair(password, mnemonicSecond);
+
+    expect(toHexStr(rootKey1)).not.toBe(toHexStr(rootKey2));
+  });
+
+  it('should generate different root key for different passwords same mnemonic', () => {
+    const mnemonic =
+      'bubble fade wasp analyst then panel desert hold spatial sound lucky weekend';
+    const rootKey1 = kadenaMnemonicToRootKeypair('pass-one', mnemonic);
+    const rootKey2 = kadenaMnemonicToRootKeypair('pass-two', mnemonic);
+
+    expect(toHexStr(rootKey1)).not.toBe(toHexStr(rootKey2));
   });
 });
 
 describe('kadenaGenKeypair', () => {
   it('should generate keypair frpm the rootKey', () => {
-    const words: string =
+    const mnemonic: string =
       'bubble fade wasp analyst then panel desert hold spatial sound lucky weekend';
     const password = 'password';
-    const rootKey = kadenaMnemonicToRootKeypair(password, words);
+    const rootKey = kadenaMnemonicToRootKeypair(password, mnemonic);
     const [encryptedSecret, publicKey] = kadenaGenKeypair(
       password,
       rootKey,
-      0x80000000 + 1,
+      harden(1),
     );
     expect(encryptedSecret.byteLength).toEqual(128);
     expect(publicKey.byteLength).toBe(32);
---
+++ packages/libs/hd-wallet/src/utils.ts
@@ -0,0 +1,1 @@
+export const harden = (n: number) => 0x80000000 + n;
