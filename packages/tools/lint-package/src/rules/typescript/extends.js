ed996632d666ef9b19de101f598a788398d9f0c0
Sam Ferrero <ferrerocompanylimited@gmail.com>
2023-11-03 13:00:11 +0000

    chore: align with main

--- packages/tools/lint-package/src/rules/typescript/extends.js
+++
@@ -1,9 +0,0 @@
-const tsConfigBasePath = './node_modules/@kadena-dev/shared-config/tsconfig-base.json';
-const rule = ({ dir, file, tsConfig }) => {
-    const issues = [];
-    if (tsConfig.extends !== tsConfigBasePath) {
-        issues.push(['warn', 'Incorrect "extends" value']);
-    }
-    return issues;
-};
-export default rule;
