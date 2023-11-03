4bba6da8aa73128147aa3acbd88f8838b27f1e43
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-03 11:40:00 +0100

    [test] depend on shared vitest config for pactjs-cli.

--- packages/tools/pactjs-cli/src/utils/tests/retrieveContractFromChain.test.ts
+++ packages/tools/pactjs-cli/src/utils/tests/retrieveContractFromChain.test.ts
@@ -8,6 +8,10 @@ const httpHandlers = [
     'https://api.chainweb.com/chainweb/0.0/mainnet01/chain/8/pact/api/v1/local',
     () => HttpResponse.json({ result: { data: { code: 'some pactCode' } } }),
   ),
+  http.post(
+    'https://api.chainweb.com/chainweb/0.0/mainnet01/chain/1/pact/api/v1/local',
+    () => HttpResponse.text('hello world!'),
+  ),
 ];
 
 const server = setupServer(...httpHandlers);
@@ -25,5 +29,15 @@ describe('retrieveContractFromChain', () => {
     );
 
     expect(result).toBe('some pactCode');
+  });
+  it('Undefined result if unable to return JSON.', async () => {
+    const result = await retrieveContractFromChain(
+      'free.crankk01',
+      'https://api.chainweb.com/chainweb/0.0/mainnet01/chain/1/pact',
+      0,
+      'mainnet',
+    );
+
+    expect(result).toBe(undefined);
   });
 });
