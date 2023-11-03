7b247bc85aa06a0b49a3bd22ffcab4051bda6f8b
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-03 12:17:52 +0100

    [test] Depend on shared vitest config for client-utils

--- packages/libs/client-utils/vitest.config.ts
+++ packages/libs/client-utils/vitest.config.ts
@@ -5,6 +5,7 @@ export default mergeConfig(
   baseConfig,
   defineConfig({
     test: {
+      threads: false, // To prevent error in tests using jsdom environment: Module did not self-register: canvas.node
       exclude: ['src/**/*.int.test.ts'],
       coverage: {
         provider: 'v8',
