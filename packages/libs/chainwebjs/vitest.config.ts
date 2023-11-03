2143611dcec1ad64d6b7250fc1acc8e46a4ae822
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-03 15:34:35 +0100

    [test] exclude tests for coverage calculation

--- packages/libs/chainwebjs/vitest.config.ts
+++ packages/libs/chainwebjs/vitest.config.ts
@@ -1,4 +1,14 @@
 import baseConfig from '@kadena-dev/shared-config/vitest.config';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
-export default defineConfig(baseConfig);
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      coverage: {
+        provider: 'v8',
+        exclude: ['src/tests/**'], // This packages uses a non standard location for tests, explicitly ignoring them for code coverage
+      },
+    },
+  }),
+);
