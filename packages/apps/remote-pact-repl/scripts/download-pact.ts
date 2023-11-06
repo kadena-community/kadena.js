84da2e8a24a5cf68e9c13ec699e78d33b05da317
Albert G <516972+alber70g@users.noreply.github.com>
2023-11-06 15:28:48 +0100

    WIP: more stuff

--- packages/apps/remote-pact-repl/scripts/download-pact.ts
+++ packages/apps/remote-pact-repl/scripts/download-pact.ts
@@ -1,31 +1,45 @@
-import { unlinkSync } from 'node:fs';
+import { spawn } from 'node:child_process';
+import fs, { unlink } from 'node:fs';
+import { mkdir } from 'node:fs/promises';
+import path from 'node:path';
+import { Readable } from 'node:stream';
+import { finished } from 'node:stream/promises';
 
-const response = await fetch(
-  'https://github.com/kadena-io/pact/releases/download/v4.9.0/pact-4.9.0-linux-20.04.zip',
-);
+const downloadFile = async (url, folder = '.') => {
+  const res = await fetch(url);
+  const destination = path.resolve('./', folder);
+  const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+  await finished(Readable.fromWeb(res.body).pipe(fileStream));
+};
+async function main() {
+  await downloadFile(
+    'https://github.com/kadena-io/pact/releases/download/v4.9.0/pact-4.9.0-linux-20.04.zip',
+    'pact.zip',
+  );
 
-await Bun.write('pact.zip', response);
-
-import { spawn } from 'child_process';
-
-function runCommand(command: string, args: string[]): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    const child = spawn(command, args, { stdio: 'inherit' });
-    child.on('exit', (code) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(
-          new Error(
-            `Command "${command} ${args.join(' ')}" exited with code ${code}`,
-          ),
-        );
-      }
+  function runCommand(command: string, args: string[]): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const child = spawn(command, args, { stdio: 'inherit' });
+      child.on('exit', (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(
+            new Error(
+              `Command "${command} ${args.join(' ')}" exited with code ${code}`,
+            ),
+          );
+        }
+      });
     });
-  });
+  }
+
+  await runCommand('unzip', ['-o', 'pact.zip', '-d', 'pact-bin']);
+  await runCommand('chmod', ['+x', 'pact-bin/pact']);
+
+  unlink('pact.zip', () => {});
 }
 
-await runCommand('unzip', ['-o', 'pact.zip', '-d', 'pact-bin']);
-await runCommand('chmod', ['+x', 'pact-bin/pact']);
-
-unlinkSync('pact.zip');
+main()
+  .catch(console.error)
+  .finally(() => {});
