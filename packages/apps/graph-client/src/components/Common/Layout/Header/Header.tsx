74d71809fe85ca92f04936f75ad52b51d8c2b308
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-11-06 14:28:43 +0100

    adjustment of header component to incorportate gas estimation arguments

--- packages/apps/graph-client/src/components/Common/Layout/Header/Header.tsx
+++ packages/apps/graph-client/src/components/Common/Layout/Header/Header.tsx
@@ -17,7 +17,9 @@ const Header: FC<IHeaderProps> = (props) => {
 
   const [searchType, setSearchType] = useState<string>('request-key');
   const [searchField, setSearchField] = useState<string>('');
-  const [moduleField, setModuleField] = useState<string>('coin');
+  const [secondSearchField, setSecondSearchField] = useState<string>('');
+  const [thirdSearchField, setThirdSearchField] = useState<string>('');
+  const [gridColumns, setGridColumns] = useState<number>(3);
   const [defaultHashOption, setDefaultHashOption] =
     useState<string>('request-key');
 
@@ -26,6 +28,7 @@ const Header: FC<IHeaderProps> = (props) => {
     account: 'Account',
     event: 'Event Name',
     block: 'Block Hash',
+    gasEstimation: 'Cmd',
   };
 
   const searchTypePlaceholders: Record<string, string> = {
@@ -33,6 +36,25 @@ const Header: FC<IHeaderProps> = (props) => {
     account: 'k:1234...',
     event: 'coin.TRANSFER',
     block: 'CA9orP2yM...',
+    gasEstimation: 'cmd',
+  };
+
+  const secondSearchTypeLabels: Record<string, string> = {
+    account: 'Module',
+    gasEstimation: 'Hash',
+  };
+
+  const secondSearchFieldPlaceholders: Record<string, string> = {
+    account: 'coin',
+    gasEstimation: 'hash',
+  };
+
+  const thirdSeachTypeLabels: Record<string, string> = {
+    gasEstimation: 'Signatures',
+  };
+
+  const thirdSearchFieldPlaceholders: Record<string, string> = {
+    gasEstimation: 'sigs',
   };
 
   const search = (): void => {
@@ -43,7 +65,7 @@ const Header: FC<IHeaderProps> = (props) => {
         break;
       case 'account':
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        router.push(`${routes.ACCOUNT}/${moduleField}/${searchField}`);
+        router.push(`${routes.ACCOUNT}/${secondSearchField}/${searchField}`);
         break;
       case 'event':
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -53,6 +75,16 @@ const Header: FC<IHeaderProps> = (props) => {
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         router.push(`${routes.BLOCK_OVERVIEW}/${searchField}`);
         break;
+      case 'gasEstimation':
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        router.push({
+          pathname: `${routes.GAS_ESTIMATION}`,
+          query: {
+            cmd: searchField,
+            hash: secondSearchField,
+            sigs: thirdSearchField,
+          },
+        });
     }
   };
 
@@ -67,6 +99,10 @@ const Header: FC<IHeaderProps> = (props) => {
   ) => {
     setSearchField(event.target.value);
     const fieldValue = event.target.value;
+
+    if (searchType === 'gasEstimation') {
+      return;
+    }
 
     if (
       fieldValue.startsWith('k:') ||
@@ -92,9 +128,22 @@ const Header: FC<IHeaderProps> = (props) => {
     setSearchType(event.target.value);
     if (event.target.value === 'request-key') {
       setDefaultHashOption('request-key');
+      setGridColumns(3);
     }
     if (event.target.value === 'block') {
       setDefaultHashOption('block');
+      setGridColumns(3);
+    }
+    if (event.target.value === 'event') {
+      setGridColumns(3);
+    }
+    if (event.target.value === 'account') {
+      setSecondSearchField('coin');
+      setGridColumns(4);
+    }
+    if (event.target.value === 'gasEstimation') {
+      setSecondSearchField('');
+      setGridColumns(5);
     }
   };
 
@@ -115,7 +164,7 @@ const Header: FC<IHeaderProps> = (props) => {
           {title}
         </Text>
 
-        <Grid.Root columns={searchType.startsWith('account') ? 4 : 3}>
+        <Grid.Root columns={gridColumns}>
           <Grid.Item>
             <InputWrapper htmlFor="search-type" label="Search Type">
               <Select
@@ -128,9 +177,11 @@ const Header: FC<IHeaderProps> = (props) => {
                 <option value="account">Account</option>
                 <option value="event">Event</option>
                 <option value="block">Block</option>
+                <option value="gasEstimation">Gas Estimation</option>
               </Select>
             </InputWrapper>
           </Grid.Item>
+
           <Grid.Item>
             <InputWrapper
               htmlFor="search-field"
@@ -145,14 +196,37 @@ const Header: FC<IHeaderProps> = (props) => {
               />
             </InputWrapper>
           </Grid.Item>
-          {searchType.startsWith('account') && (
+
+          {(searchType.startsWith('account') ||
+            searchType.startsWith('gas')) && (
             <Grid.Item>
-              <InputWrapper htmlFor="module" label="Module name">
+              <InputWrapper
+                htmlFor="second-search-field"
+                label={secondSearchTypeLabels[searchType]}
+              >
                 <Input
-                  id="module"
-                  value={moduleField}
-                  placeholder="coin"
-                  onChange={(event) => setModuleField(event.target.value)}
+                  id="second-search-field"
+                  value={secondSearchField}
+                  placeholder={secondSearchFieldPlaceholders[searchType]}
+                  onChange={(event) => setSecondSearchField(event.target.value)}
+                  onKeyDown={handleKeyPress}
+                />
+              </InputWrapper>
+            </Grid.Item>
+          )}
+
+          {searchType.startsWith('gas') && (
+            <Grid.Item>
+              <InputWrapper
+                htmlFor="third-search-field"
+                label={thirdSeachTypeLabels[searchType]}
+              >
+                <Input
+                  id="third-search-field"
+                  value={thirdSearchField}
+                  placeholder={thirdSearchFieldPlaceholders[searchType]}
+                  onChange={(event) => setThirdSearchField(event.target.value)}
+                  onKeyDown={handleKeyPress}
                 />
               </InputWrapper>
             </Grid.Item>
