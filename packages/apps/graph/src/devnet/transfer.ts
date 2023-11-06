3dda3023118e7e69888775b0e51a805e226eb4f8
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-11-06 14:23:55 +0100

    changes in graph, added query and necessary functions

--- packages/apps/graph/src/devnet/transfer.ts
+++ packages/apps/graph/src/devnet/transfer.ts
@@ -1,11 +1,13 @@
-import type { ChainId, ICommandResult } from '@kadena/client';
+import type { ChainId, ICommandResult, IUnsignedCommand } from '@kadena/client';
 import { Pact } from '@kadena/client';
+import { hash as hashFunction } from '@kadena/cryptography-utils';
 import { PactNumber } from '@kadena/pactjs';
 import { devnetConfig } from './config';
 import type { IAccount } from './helper';
 import {
   inspect,
   listen,
+  localReadForGasEstimation,
   logger,
   sender00,
   signAndAssertTransaction,
@@ -67,3 +69,32 @@ export async function transfer({
     return result;
   }
 }
+export const localReadTransfer = async ({
+  cmd,
+  hash = undefined,
+  sigs = [],
+}: {
+  cmd: string;
+  hash?: string | undefined | null;
+  sigs?: string[] | undefined | null;
+}): Promise<ICommandResult> => {
+  if (!hash) {
+    hash = hashFunction(cmd);
+  }
+
+  let existingSigs: IUnsignedCommand['sigs'] = [];
+
+  if (sigs && sigs?.length > 0) {
+    existingSigs = sigs.map((sig) => ({
+      sig: sig,
+    }));
+  }
+
+  const transaction: IUnsignedCommand = {
+    cmd,
+    hash,
+    sigs: existingSigs,
+  };
+
+  return await localReadForGasEstimation(transaction);
+};
---
+++ packages/apps/graph/src/graph/Query/gasLimitEstimate.ts
@@ -0,0 +1,53 @@
+import { localReadTransfer } from '../../devnet/transfer';
+import { builder } from '../builder';
+
+const PactTransaction = builder.inputType('PactTransaction', {
+  fields: (t) => ({
+    cmd: t.field({ type: 'String', required: true }),
+    hash: t.field({ type: 'String' }),
+    sigs: t.field({ type: ['String'] }),
+  }),
+});
+
+builder.queryField('gasLimitEstimate', (t) => {
+  return t.field({
+    type: 'Int',
+    args: {
+      transaction: t.arg({ type: PactTransaction, required: true }),
+    },
+    resolve: async (parent, args, context, info) => {
+      if (args.transaction.cmd.includes(`\\`)) {
+        args.transaction.cmd = args.transaction.cmd.replace(/\\\\/g, '\\');
+      }
+
+      const result = await localReadTransfer({
+        cmd: args.transaction.cmd,
+        hash: args.transaction.hash,
+        sigs: args.transaction.sigs,
+      });
+      return result.gas;
+    },
+  });
+});
+
+builder.queryField('gasLimitEstimates', (t) => {
+  return t.field({
+    type: ['Int'],
+    args: {
+      transactions: t.arg({ type: [PactTransaction], required: true }),
+    },
+    resolve: async (parent, args, context, info) => {
+      return args.transactions.map(async (transaction) => {
+        if (transaction.cmd.includes('//')) {
+          transaction.cmd = transaction.cmd.replace(/\/\//g, '/');
+        }
+        const result = await localReadTransfer({
+          cmd: transaction.cmd,
+          hash: transaction.hash,
+          sigs: transaction.sigs,
+        });
+        return result.gas;
+      });
+    },
+  });
+});
