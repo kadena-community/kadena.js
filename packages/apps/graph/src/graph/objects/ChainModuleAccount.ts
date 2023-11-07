b18195c0f05fcf884a01e4edd530a7850471d399
Maarten van den Hoven <info@maartenvandenhoven.com>
2023-11-07 15:35:14 +0100

    Chain Module Accounts transformed to Nodes, and use of Promise optimizations

--- packages/apps/graph/src/graph/objects/ChainModuleAccount.ts
+++ packages/apps/graph/src/graph/objects/ChainModuleAccount.ts
@@ -1,81 +1,129 @@
 import { prismaClient } from '@db/prismaClient';
+import { getChainModuleAccount } from '@services/account-service';
 import { normalizeError } from '@utils/errors';
 import { builder } from '../builder';
 import { accountDetailsLoader } from '../data-loaders/account-details';
+import { ChainModuleAccount } from '../types/graphql-types';
 
-export default builder.objectType('ChainModuleAccount', {
-  fields: (t) => ({
-    chainId: t.exposeID('chainId'),
-    accountName: t.exposeString('accountName'),
-    moduleName: t.exposeString('moduleName'),
-    guard: t.field({
-      type: 'Guard',
-      async resolve(parent) {
-        const accountDetails = await accountDetailsLoader.load({
-          moduleName: parent.moduleName,
-          accountName: parent.accountName,
-          chainId: parent.chainId,
-        });
-
+export default builder.node(
+  builder.objectRef<ChainModuleAccount>('ChainModuleAccount'),
+  {
+    id: {
+      resolve(parent, __args, __context, info) {
+        return `${info.parentType}/${parent.chainId}/${parent.moduleName}/${parent.accountName}`;
+      },
+      parse(id) {
         return {
-          keys: accountDetails.guard.keys,
-          predicate: accountDetails.guard.pred,
+          chainId: id.split('/')[1],
+          accountName: id.split('/')[3],
+          moduleName: id.split('/')[2],
         };
       },
-    }),
-    balance: t.exposeFloat('balance'),
-    transactions: t.prismaConnection({
-      type: 'Transaction',
-      cursor: 'blockHash_requestKey',
-      async resolve(query, parent) {
-        try {
-          return await prismaClient.transaction.findMany({
-            ...query,
-            where: {
-              senderAccount: parent.accountName,
-              events: {
-                some: {
-                  moduleName: parent.moduleName,
-                },
-              },
-              chainId: parseInt(parent.chainId),
-            },
-            orderBy: {
-              height: 'desc',
-            },
-          });
-        } catch (error) {
-          throw normalizeError(error);
-        }
-      },
-    }),
-    transfers: t.prismaConnection({
-      type: 'Transfer',
-      cursor: 'blockHash_chainId_orderIndex_moduleHash_requestKey',
-      async resolve(query, parent) {
-        try {
-          return await prismaClient.transfer.findMany({
-            ...query,
-            where: {
-              OR: [
-                {
-                  senderAccount: parent.accountName,
-                },
-                {
-                  receiverAccount: parent.accountName,
-                },
-              ],
+    },
+    isTypeOf: () => true,
+    async loadOne(id) {
+      try {
+        return getChainModuleAccount({
+          chainId: id.chainId,
+          moduleName: id.moduleName,
+          accountName: id.accountName,
+        });
+      } catch (error) {
+        throw normalizeError(error);
+      }
+    },
+    async loadMany(ids) {
+      try {
+        return await Promise.all(
+          ids.map(async (id) => {
+            return await getChainModuleAccount({
+              chainId: id.chainId,
+              moduleName: id.moduleName,
+              accountName: id.accountName,
+            });
+          }),
+        );
+      } catch (error) {
+        throw normalizeError(error);
+      }
+    },
+    fields: (t) => ({
+      chainId: t.exposeID('chainId'),
+      accountName: t.exposeString('accountName'),
+      moduleName: t.exposeString('moduleName'),
+      guard: t.field({
+        type: 'Guard',
+        async resolve(parent) {
+          try {
+            const accountDetails = await accountDetailsLoader.load({
               moduleName: parent.moduleName,
-              chainId: parseInt(parent.chainId),
-            },
-            orderBy: {
-              height: 'desc',
-            },
-          });
-        } catch (error) {
-          throw normalizeError(error);
-        }
-      },
+              accountName: parent.accountName,
+              chainId: parent.chainId,
+            });
+
+            return {
+              keys: accountDetails.guard.keys,
+              predicate: accountDetails.guard.pred,
+            };
+          } catch (error) {
+            throw normalizeError(error);
+          }
+        },
+      }),
+      balance: t.exposeFloat('balance'),
+      transactions: t.prismaConnection({
+        type: 'Transaction',
+        cursor: 'blockHash_requestKey',
+        async resolve(query, parent) {
+          try {
+            return await prismaClient.transaction.findMany({
+              ...query,
+              where: {
+                senderAccount: parent.accountName,
+                events: {
+                  some: {
+                    moduleName: parent.moduleName,
+                  },
+                },
+                chainId: parseInt(parent.chainId),
+              },
+              orderBy: {
+                height: 'desc',
+              },
+            });
+          } catch (error) {
+            throw normalizeError(error);
+          }
+        },
+      }),
+      transfers: t.prismaConnection({
+        type: 'Transfer',
+        cursor: 'blockHash_chainId_orderIndex_moduleHash_requestKey',
+        async resolve(query, parent) {
+          try {
+            return await prismaClient.transfer.findMany({
+              ...query,
+              where: {
+                OR: [
+                  {
+                    senderAccount: parent.accountName,
+                  },
+                  {
+                    receiverAccount: parent.accountName,
+                  },
+                ],
+                moduleName: parent.moduleName,
+                chainId: parseInt(parent.chainId),
+              },
+              orderBy: {
+                height: 'desc',
+              },
+            });
+          } catch (error) {
+            throw normalizeError(error);
+          }
+        },
+      }),
     }),
-  }),
-});
+  },
+);
