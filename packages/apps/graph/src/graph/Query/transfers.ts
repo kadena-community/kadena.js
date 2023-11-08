f713f3f67ff969e43276d04da66fa8cf0c059ef9
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-11-08 15:01:36 +0100

    lint fixes

--- packages/apps/graph/src/graph/Query/transfers.ts
+++ packages/apps/graph/src/graph/Query/transfers.ts
@@ -1,5 +1,5 @@
 import { prismaClient } from '@db/prismaClient';
-import { Prisma } from '@prisma/client';
+import type { Prisma } from '@prisma/client';
 import { normalizeError } from '@utils/errors';
 import { builder } from '../builder';
 
