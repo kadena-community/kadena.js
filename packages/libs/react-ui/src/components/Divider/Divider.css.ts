7407d431ba7f9766d90568f8bfee6f3ba8af4233
Eileen Guo <eileen.m.guo@gmail.com>
2023-11-13 10:39:17 +0100

    updating divider styles

--- packages/libs/react-ui/src/components/Divider/Divider.css.ts
+++ packages/libs/react-ui/src/components/Divider/Divider.css.ts
@@ -1,9 +1,15 @@
+import { sprinkles } from '@theme/sprinkles.css';
 import { vars } from '@theme/vars.css';
 import { style } from '@vanilla-extract/css';
 
-export const dividerClass = style({
-  borderColor: `${vars.colors.$borderDefault}`,
-  borderWidth: '1px',
-  margin: `${vars.sizes.$10} 0`,
-  borderBottomWidth: '0',
-});
+export const dividerClass = style([
+  sprinkles({
+    backgroundColor: '$borderDefault',
+    width: '100%',
+    marginY: '$10',
+    border: 'none',
+  }),
+  {
+    height: vars.borderWidths.$sm,
+  },
+]);
