4f48c10f72fdc41c897522671cac2e575d1449d2
Mohamed Ismail <ismailreality1@gmail.com>
2023-11-13 11:16:44 +0100

    chore: add build scripts + update package.json

---
+++ packages/tools/docs-tools/src/index.ts
@@ -0,0 +1,17 @@
+import remarkAdmonitions from './remarkAdmonition';
+import remarkCheckForCodeTitle from './remarkCheckForCodeTitle';
+import remarkFigureOutOfParagraph from './remarkFigureOutOfParagraph';
+import remarkFixAbsoluteLinks from './remarkFixAbsoluteLinks';
+import remarkTwitter from './remarkTwitter';
+import remarkYoutube from './remarkYoutube';
+
+export {
+  remarkAdmonitions,
+  remarkCheckForCodeTitle,
+  remarkFigureOutOfParagraph,
+  remarkFixAbsoluteLinks,
+  remarkTwitter,
+  remarkYoutube,
+}
+
+export type * from './types';
