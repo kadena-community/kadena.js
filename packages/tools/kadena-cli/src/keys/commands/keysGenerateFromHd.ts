4dd6c5eb02f0d617420630a5742a07813f35bf60
neel <bbnillo@gmail.com>
2023-11-23 12:51:18 +0100

    initial commit

---
+++ packages/tools/kadena-cli/src/keys/commands/keysGenerateFromHd.ts
@@ -0,0 +1,25 @@
+import type { Command } from 'commander';
+import debug from 'debug';
+import { createCommand } from '../../utils/createCommand.js';
+import { globalOptions } from '../../utils/globalOptions.js';
+
+// TO-DO: Implement this command
+// choices: [
+//    > Generate Public key from HD key
+//    > Generate Public and Private key from HD key
+//    > Generate Public and Private key from newly generated HD key
+//   'Exit'
+// ],
+
+export const createGenerateFromHdCommand: (
+  program: Command,
+  version: string,
+) => void = createCommand(
+  'genfromhd',
+  'Generate key(s) from HD key',
+  [globalOptions.keyPassword(), globalOptions.keyFilename()],
+  async (config) => {
+    debug('generate-from-hid:action')({ config });
+    console.log('generate from hd: not implemented yet');
+  },
+);
--- packages/tools/kadena-cli/src/prompts/keys.ts
+++ packages/tools/kadena-cli/src/prompts/keys.ts
@@ -1,12 +1,7 @@
 import { input, select } from '@inquirer/prompts';
 import { program } from 'commander';
-import type { ICustomKeysetsChoice } from '../keys/utils/keysetHelpers.js';
 import type { ICustomNetworkChoice } from '../networks/utils/networkHelpers.js';
-import {
-  capitalizeFirstLetter,
-  getExistingKeysets,
-  isAlphabetic,
-} from '../utils/helpers.js';
+import { capitalizeFirstLetter, isAlphabetic } from '../utils/helpers.js';
 
 export async function keyAlias(): Promise<string> {
   return await input({
@@ -40,38 +35,4 @@ export async function keyAskForKeyType(): Promise<string> {
   });
 
   return keyTypeChoice.toLowerCase();
-}
-
-export async function keysetSelectPrompt(
-  isOptional: boolean = false,
-): Promise<string | 'skip'> {
-  const existingKeysets: ICustomKeysetsChoice[] = await getExistingKeysets();
-
-  const choices: ICustomKeysetsChoice[] = existingKeysets.map((keyset) => ({
-    value: keyset.value,
-    name: keyset.name,
-  }));
-
-  if (isOptional) {
-    choices.push({
-      value: 'skip',
-      name: 'Keyset is optional. Continue to next step',
-    });
-  }
-
-  choices.push({ value: 'createNewKeyset', name: 'Create a new keyset' });
-
-  const selectedKeyset = await select({
-    message: 'Select a keyset',
-    choices: choices,
-  });
-
-  if (selectedKeyset === 'createNewKeyset') {
-    await program.parseAsync(['', '', 'keysets', 'create']);
-    return keysetSelectPrompt(isOptional);
-  } else if (selectedKeyset !== 'skip') {
-    return selectedKeyset;
-  }
-
-  return 'skip';
 }
