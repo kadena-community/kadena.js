0a13c5c3a3ee3810149abbeb22c106b94871d1d9
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-23 15:54:30 +0100

    Move tests to src folder but exclude them from build

--- packages/tools/shared-config/vitest.integration.config.js
+++ packages/tools/shared-config/vitest.integration.config.js
@@ -2,9 +2,8 @@ const { default: tsconfigPaths } = require('vite-tsconfig-paths');
 const { defineConfig } = require('vitest/config');
 
 module.exports = defineConfig({
-  plugins: [tsconfigPaths({ projects: ['./integration-tests/tsconfig.test.json'] })],
   test: {
-    include: ['integration-tests/*.int.{test,spec}.{ts,tsx}'],
+    include: ['src/integration-tests/*.int.{test,spec}.{ts,tsx}'],
     globals: false,
   },
 });
