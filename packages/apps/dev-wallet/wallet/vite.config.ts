a64c0f37f746b031d82327717e0b306e75c4b867
Albert G <516972+alber70g@users.noreply.github.com>
2023-11-23 14:26:54 +0100

    fix(dev-wallet): optimize monorepo dependencies (see: https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies)

--- packages/apps/dev-wallet/wallet/vite.config.ts
+++ packages/apps/dev-wallet/wallet/vite.config.ts
@@ -1,11 +1,42 @@
-import commonjs from '@rollup/plugin-commonjs';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
+import path from 'path';
 import { defineConfig, UserConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+/**
+ * Get list of monorepo packages from package.json non-dev-dependencies
+ * @returns {string[]} - list of package names
+ */
+function getMonorepoPackagesFromPackageJson() {
+  const packageJson = fs.readFileSync(
+    path.resolve(__dirname, './package.json'),
+    'utf-8',
+  );
+  const { dependencies } = JSON.parse(packageJson);
+  const packages = Object.keys(dependencies).filter((key) =>
+    dependencies[key].startsWith('workspace:'),
+  );
+
+  return packages;
+}
+
+const monorepoPackages = getMonorepoPackagesFromPackageJson();
+monorepoPackages.push('@kadena/client/fp');
+
 export const config: UserConfig = {
-  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin(), commonjs()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+
+  optimizeDeps: {
+    include: [...monorepoPackages],
+  },
+
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/, ...monorepoPackages],
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
