64c9bfd96bac5be26e241b5a4e13ab9cfa0a1bda
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-24 14:28:51 +0100

    [test] add additional assertions

--- packages/tools/integration-tests/src/projects/graph/tests/account.test.ts
+++ packages/tools/integration-tests/src/projects/graph/tests/account.test.ts
@@ -8,8 +8,16 @@ describe('Account', () => {
     const response = await request(baseUrl)
       .post('')
       .send(getAccountQuery)
-    console.log(response)
     expect(response.statusCode).toBe(200)
-    expect(response.text).toHaveProperty('data')
+    expect(response.body).toHaveProperty('data')
+    expect(response.body.data).toHaveProperty('account')
+    expect(response.body.data.account).toHaveProperty('accountName')
+    expect(response.body.data.account).toHaveProperty('moduleName')
+    expect(response.body.data.account).toHaveProperty('__typename')
+    expect(response.body.data.account).toHaveProperty('id')
+    expect(response.body.data.account).toHaveProperty('transactions')
+    expect(response.body.data.account).toHaveProperty('transfers')
+    expect(response.body.data.account).toHaveProperty('totalBalance')
+    expect(response.body.data.account).toHaveProperty('chainAccounts')
       });
   });
