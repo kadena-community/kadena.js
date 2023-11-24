1d20fa7bcc64137ead65fe945b42e86a5ea289c6
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-24 14:48:46 +0100

    [chore] update eslint config

--- packages/tools/integration-tests/.eslintrc.js
+++ packages/tools/integration-tests/.eslintrc.js
@@ -3,12 +3,7 @@ require('@rushstack/eslint-config/patch/modern-module-resolution');
 module.exports = {
   extends: [
     '@kadena-dev/eslint-config/profile/lib',
-    'plugin:playwright/recommended',
   ],
   parserOptions: { tsconfigRootDir: __dirname },
-  ignorePatterns: ['vitest.*.ts'],
-  rules: {
-    '@kadena-dev/typedef-var': 'off',
-    '@rushstack/typedef-var': 'off',
-  },
+  ignorePatterns: ['vitest.projects.ts'],
 };
