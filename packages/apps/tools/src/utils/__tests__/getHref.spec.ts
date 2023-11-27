16f7859786ff38553dfb591372a26d40cf1b6956
kristina.spasevska <kristina.spasevska@deptagency.com>
2023-11-27 16:23:25 +0100

    test: add unit test for getHref util

---
+++ packages/apps/tools/src/utils/__tests__/getHref.spec.ts
@@ -0,0 +1,18 @@
+import Cookies from 'js-cookie';
+import { describe, expect, test, vi } from 'vitest';
+import { getHref }  from '../getHref';
+
+describe('getHref', () => {
+    test('getting # when no correct pathname is provided and there is no href from the item', () => {
+        const result = getHref('');
+        expect(result).toBe('#');
+    });
+
+    test('getting href of first item when there is no path from URL', () => {
+        const pathname = '/';
+        const result = getHref(pathname, 'faucet');
+        expect(result).toBe('/faucet/new');
+    });
+});
+
+
