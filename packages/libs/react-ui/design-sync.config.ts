194bfacd61c8e2ba21f453a07bf434843a32c3ef
Salama Ashoush <salamaashoush@gmail.com>
2023-11-27 17:12:13 +0100

    [@react-ui] Setup design sync cli

---
+++ packages/libs/react-ui/design-sync.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, responsiveExtension } from '@design-sync/cli';
+import { vanillaExtractPlugin } from '@design-sync/vanilla-extract-plugin';
+
+const breakpoints = {
+  xs: '{kda.foundation.breakpoint.xs}',
+  sm: '{kda.foundation.breakpoint.sm}',
+  md: '{kda.foundation.breakpoint.md}',
+  lg: '{kda.foundation.breakpoint.lg}',
+  xl: '{kda.foundation.breakpoint.xl}',
+  xxl: '{kda.foundation.breakpoint.xxl}',
+};
+
+const breakpointKeys = Object.keys(breakpoints);
+
+export default defineConfig({
+  uri: 'github:kadena-community/design-system/tokens#main',
+  out: 'src/styles/tokens',
+  defaultMode: 'light',
+  requiredModes: ['light', 'dark'],
+  plugins: [
+    vanillaExtractPlugin({
+      contractName: 'tokens',
+      createGlobalThemes: false,
+      createGlobalContract: false,
+    }),
+  ],
+  schemaExtensions: [
+    responsiveExtension({
+      breakpoints,
+      filter: ({ type, path }) =>
+        type === 'typography' &&
+        breakpointKeys.some((key) => path.endsWith(key)),
+    }),
+  ],
+  prettify: true,
+});
