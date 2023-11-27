c3cbde868c972197ca5f71885578aee44316e5f9
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-27 15:51:04 +0100

    [test] use simulation to set up data and format package

--- packages/tools/integration-tests/.eslintrc.js
+++ packages/tools/integration-tests/.eslintrc.js
@@ -1,9 +1,7 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: [
-    '@kadena-dev/eslint-config/profile/lib',
-  ],
+  extends: ['@kadena-dev/eslint-config/profile/lib'],
   parserOptions: { tsconfigRootDir: __dirname },
   ignorePatterns: ['vitest.projects.ts'],
   rules: {
--- packages/tools/integration-tests/src/projects/graph/testdata/devnet/simulation/file.ts
+++ packages/tools/integration-tests/src/projects/graph/testdata/devnet/simulation/file.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import type { ParseResult } from 'papaparse';
+import { parse } from 'papaparse';
 import path from 'path';
 import { devnetConfig } from '../config';
 
@@ -28,7 +30,7 @@ export function createDir(directory: string): void {
   }
 }
 
-export function cleanDir(directory: string): void  {
+export function cleanDir(directory: string): void {
   const file = path.join(directory, 'testdata.csv');
   if (fs.existsSync(file)) {
     fs.unlinkSync(file);
@@ -46,7 +48,7 @@ export function createFile(filename: string): string {
   };
   const directory = path.join(__dirname, `${devnetConfig.LOG_FOLDERNAME}/`);
   createDir(directory);
-  cleanDir(directory)
+  cleanDir(directory);
   const filepath = path.join(directory, filename);
   const headers = `${Object.keys(exampleData).join(',')}\n`;
   fs.writeFileSync(filepath, headers);
@@ -59,4 +61,28 @@ export function appendToFile(
 ): void {
   const dataString = Object.values(data).join(',');
   fs.appendFileSync(filepath, `${dataString}\n`);
+}
+
+export interface ITestData {
+  timestamp: number;
+  from: string;
+  to: string;
+  amount: number;
+  requestKey: string;
+  type: string;
+}
+
+export function parseLogs() {
+  const pathToFile = path.join(
+    __dirname,
+    `${devnetConfig.LOG_FOLDERNAME}/testdata.csv`,
+  );
+  const file = fs.readFileSync(pathToFile, 'utf8');
+  const parsedResult = parse(file, {
+    header: true,
+    complete: function (results: ParseResult<ITestData>) {
+      return results;
+    },
+  });
+  return parsedResult.data;
 }
