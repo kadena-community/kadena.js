a85e0c0ecfac19786c39895364fad34119b8561b
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-11-27 13:13:12 +0100

    second test

---
+++ packages/tools/integration-tests/src/projects/graph/tests/block.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { describe, expect, test } from 'vitest';
+import { grapHost } from '../testdata/constants/network';
+import { getAccountQuery } from '../testdata/queries/getAccount';
+
+describe('Block', () => {
+  test('getAccount', async () => {
+
+   const response = await request(grapHost)
+      .post('')
+      .send(getAccountQuery)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toHaveProperty('data')
+    expect(response.body.data).toHaveProperty('account')
+    expect(response.body.data.account).toHaveProperty('accountName')
+    expect(response.body.data.account).toHaveProperty('moduleName')
+    expect(response.body.data.account).toHaveProperty('__typename')
+    expect(response.body.data.account).toHaveProperty('id')
+    expect(response.body.data.account).toHaveProperty('transactions')
+    expect(response.body.data.account).toHaveProperty('transfers')
+    expect(response.body.data.account).toHaveProperty('totalBalance')
+    expect(response.body.data.account).toHaveProperty('chainAccounts')
+      });
+  });
+
