a6ab980e23144ab35b6f219ce21f7d7d2f760015
Steven Straatemans <steven@straatemans.nl>
2023-11-28 15:23:45 +0100

    setup testing

--- packages/apps/proof-of-us/src/utils/env.ts
+++ packages/apps/proof-of-us/src/utils/env.ts
@@ -1,9 +1,10 @@
-export const env = (() => {
-  const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
+const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
+const TESTNUMBER = Number(process.env.NEXT_PUBLIC_TESTNUMBER);
 
-  if (!TRACKING_ID) throw Error('NEXT_PUBLIC_TRACKING_ID is not set');
+if (!TRACKING_ID) throw Error('NEXT_PUBLIC_TRACKING_ID is not set');
+if (!TESTNUMBER) throw Error('NEXT_PUBLIC_TESTNUMBER is not set');
 
-  return {
-    TRACKING_ID,
-  };
-})();
+export const env = {
+  TRACKING_ID,
+  TESTNUMBER,
+};
