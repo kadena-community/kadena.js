85670b8dfe4d2a4fcff4463a155a366c32627f65
Nil Amrutlal <nil.amrutlal@deptagency.com>
2023-11-28 11:07:30 +0100

    get marmelate template

--- packages/apps/graph/src/devnet/get-marmalade-templates.ts
+++ packages/apps/graph/src/devnet/get-marmalade-templates.ts
@@ -2,16 +2,23 @@ import { dotenv } from '../utils/dotenv';
 import { downloadGitFilesFromFolder } from '../utils/downlaod-git-files';
 import { logger } from './helper';
 
+export async function getMarmaladeFiles(
+  destinationPath: string = process.cwd(),
+): Promise<void> {
+  await getMarmaladeTemplates(destinationPath);
+  await getCodeFiles(destinationPath);
+}
+
 export async function getMarmaladeTemplates(
   destinationPath: string = process.cwd(),
-) {
+): Promise<void> {
   logger.info('Downloading marmalade templates');
   try {
     await downloadGitFilesFromFolder(
       {
         owner: dotenv.MARMALADE_TEMPLATE_OWNER,
         name: dotenv.MARMALADE_TEMPLATE_REPO,
-        folderPath: dotenv.MARMALADE_TEMPLATE_PATH,
+        path: dotenv.MARMALADE_TEMPLATE_PATH,
         branch: dotenv.MARMALADE_TEMPLATE_BRANCH,
       },
       destinationPath,
@@ -21,3 +28,7 @@ export async function getMarmaladeTemplates(
     throw error;
   }
 }
+
+export async function getCodeFiles(
+  templateDestinationPath: string = process.cwd(),
+) {}
