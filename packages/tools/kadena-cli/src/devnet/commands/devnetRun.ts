1efe38a208884116027d09eb3e41ddb92bf09820
Jesse van Muijden <jessevanmuijden@gmail.com>
2023-12-11 13:15:59 -0400

    chore(kadena-cli): improve devnet command error handling

--- packages/tools/kadena-cli/src/devnet/commands/devnetRun.ts
+++ packages/tools/kadena-cli/src/devnet/commands/devnetRun.ts
@@ -4,7 +4,7 @@ import { globalOptions } from '../../utils/globalOptions.js';
 import chalk from 'chalk';
 import type { CreateCommandReturnType } from '../../utils/createCommand.js';
 import { createCommand } from '../../utils/createCommand.js';
-import { runDevnet } from '../utils/docker.js';
+import { guardDocker, runDevnet } from '../utils/docker.js';
 
 export const runDevnetCommand: CreateCommandReturnType = createCommand(
   'run',
@@ -13,21 +13,14 @@ export const runDevnetCommand: CreateCommandReturnType = createCommand(
   async (config) => {
     debug('devnet-run:action')({ config });
 
-    try {
-      runDevnet(config.devnetConfig);
+    guardDocker();
 
-      console.log(
-        chalk.green(
-          `\nThe devnet configuration "${config.devnet}" is running.\n`,
-        ),
-      );
-    } catch (e) {
-      console.log(
-        chalk.red(
-          'Running devnet requires Docker. Please install or run Docker and try again.',
-        ),
-      );
-      return;
-    }
+    runDevnet(config.devnetConfig);
+
+    console.log(
+      chalk.green(
+        `\nThe devnet configuration "${config.devnet}" is running.\n`,
+      ),
+    );
   },
 );
