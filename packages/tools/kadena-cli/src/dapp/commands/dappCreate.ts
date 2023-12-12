06454a08a0fbbea0aa8de968086956e59f3f87c4
Mohamed Ismail <ismailreality1@gmail.com>
2023-12-12 11:10:26 +0100

    feat(dapp): use spawnsync to inherit the stdout

--- packages/tools/kadena-cli/src/dapp/commands/dappCreate.ts
+++ packages/tools/kadena-cli/src/dapp/commands/dappCreate.ts
@@ -1,10 +1,10 @@
-import { execSync } from 'child_process';
+import chalk from 'chalk';
+import { spawnSync } from 'child_process';
 import type { Command } from 'commander';
 import debug from 'debug';
 import { existsSync } from 'fs';
 import { join } from 'path';
 
-import chalk from 'chalk';
 import { createCommand } from '../../utils/createCommand.js';
 import { globalOptions } from '../../utils/globalOptions.js';
 
@@ -16,25 +16,23 @@ export const createDappCommand: (program: Command, version: string) => void =
     async (config, args) => {
       const projectDir = join(process.cwd(), args[0]);
       const { dappTemplate } = config;
-      console.log({
-        args,
-        projectDir,
-      });
-      debug('dapp-create-command')({ config });
       const isFolderExists = existsSync(projectDir);
-      console.log(`isFolderExists: ${isFolderExists}`);
+
       if (isFolderExists) {
         console.error(chalk.red(`Project directory ${args[0]} already exists`));
         process.exit(1);
       }
 
-      console.log(`Creating ${dappTemplate} project at ${args[0]}`);
-      // generate-project -n [project-name] -t [template-name]
-      execSync(
-        `npx @kadena/create-kadena-app generate-project -n ${args[0]} -t ${dappTemplate}`,
-      );
-
-      console.log(chalk.green(`Project ${args[0]} created successfully`));
-      return;
+      const cmd = 'npx';
+      const cmdArgs = [
+        '--no-install',
+        '@kadena/create-kadena-app',
+        'generate-project',
+        '-n',
+        args[0],
+        '-t',
+        dappTemplate,
+      ];
+      spawnSync(cmd, cmdArgs, { stdio: 'inherit' });
     },
   );
