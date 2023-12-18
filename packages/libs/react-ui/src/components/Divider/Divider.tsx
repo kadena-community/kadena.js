331dd51f450388d0ccbc469831fa2cabd58e4a35
Salama Ashoush <salamaashoush@gmail.com>
2023-12-18 12:31:27 +0100

    Use aria hook for divider

--- packages/libs/react-ui/src/components/Divider/Divider.tsx
+++ packages/libs/react-ui/src/components/Divider/Divider.tsx
@@ -1,10 +1,16 @@
 import cn from 'classnames';
 import type { ComponentPropsWithRef, FC } from 'react';
 import React from 'react';
+import { useSeparator } from 'react-aria';
 import { dividerClass } from './Divider.css';
 
 export interface IDividerProps extends ComponentPropsWithRef<'hr'> {}
 
 export const Divider: FC<IDividerProps> = ({ className, ...props }) => {
-  return <hr className={cn(dividerClass, className)} {...props} />;
+  const { separatorProps } = useSeparator({
+    ...props,
+    elementType: 'hr',
+    orientation: 'horizontal',
+  });
+  return <hr className={cn(dividerClass, className)} {...separatorProps} />;
 };
