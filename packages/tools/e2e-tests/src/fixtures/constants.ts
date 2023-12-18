794222f002e952027400e018c3a675c2092acc37
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-12-18 15:31:08 +0100

    [test] update faucet tests

--- packages/tools/e2e-tests/src/fixtures/constants.ts
+++ packages/tools/e2e-tests/src/fixtures/constants.ts
@@ -1,1 +1,4 @@
 export const ns = 'common';
+export const devnetHost = 'http://localhost:8080'
+export const networkId = 'fast-development'
+
--- packages/tools/e2e-tests/src/helpers/accounts.helper.ts
+++ packages/tools/e2e-tests/src/helpers/accounts.helper.ts
@@ -1,5 +1,9 @@
-import type { ChainId } from '@kadena/client';
+import { createSignWithKeypair, type ChainId } from '@kadena/client';
+import { createAccount } from '@kadena/client-utils/coin';
 import { genKeyPair } from '@kadena/cryptography-utils';
+import { expect } from '@playwright/test';
+import { sender00Account } from '../fixtures/accounts.fixture';
+import { devnetHost, networkId } from '../fixtures/constants';
 import type { IAccountWithSecretKey } from '../types/accounts';
 
 export async function generateAccount(
@@ -13,4 +17,32 @@ export async function generateAccount(
     guard: keyPair.publicKey,
     secretKey: keyPair.secretKey as string,
   };
+}
+export async function createAccountOnChain(chain: ChainId): Promise<IAccountWithSecretKey> {
+  const generatedAccount = await generateAccount(chain);
+  const creationTask = await createAccount(
+    {
+      account: generatedAccount.account,
+      keyset: {
+        pred: 'keys-all',
+        keys: [generatedAccount.publicKey],
+      },
+      gasPayer: {
+        account: sender00Account.account,
+        publicKeys: [sender00Account.publicKey],
+      },
+      chainId: chain,
+    },
+    {
+      host: devnetHost,
+      defaults: {
+        networkId: networkId,
+      },
+      sign: createSignWithKeypair([sender00Account]),
+    },
+  );
+  const result = await creationTask.executeTo();
+  console.log(result)
+  expect(result).toBe('Write succeeded');
+  return generatedAccount
 }
