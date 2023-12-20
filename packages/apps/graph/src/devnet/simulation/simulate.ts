12b718c2772412ebd36c6fd98dd364aac60e3012
MRVDH <maarten.vandenhoven@deptagency.com>
2023-12-20 09:39:06 +0100

    fix return type of get balance

--- packages/apps/graph/src/devnet/simulation/simulate.ts
+++ packages/apps/graph/src/devnet/simulation/simulate.ts
@@ -141,12 +141,12 @@ export async function simulate({
           counter = 0;
         }
 
-        const balance = (await getBalance(
+        const balance = parseFloat((await getBalance(
           account.account,
           dotenv.NETWORK_ID,
           account.chainId || dotenv.SIMULATE_DEFAULT_CHAIN_ID,
           dotenv.NETWORK_HOST,
-        )) as number;
+        ))?.decimal || '0');
 
         // using a random number safety gap to avoid underflowing the account
         const amountWithSafetyGap = amount + getRandomNumber(seededRandomNo, 1);
