b935079604d1fd87618eb2a032faaf228bf37631
Sam Ferrero <ferrerocompanylimited@gmail.com>
2023-12-21 11:52:55 +0100

    chore:lint

--- packages/libs/react-ui/src/components/Form/FormFieldWrapper/FormFieldHelper/FormFieldHelper.tsx
+++ packages/libs/react-ui/src/components/Form/FormFieldWrapper/FormFieldHelper/FormFieldHelper.tsx
@@ -1,6 +1,5 @@
 import { SystemIcon } from '@components/Icon';
-import type { FocusableElement } from '@react-types/shared';
-import type { DOMAttributes, FC } from 'react';
+import type { FC } from 'react';
 import React from 'react';
 import { helperClass, helperIconClass } from './FormFieldHelper.css';
 
