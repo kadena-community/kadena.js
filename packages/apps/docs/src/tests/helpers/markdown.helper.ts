245db6c1cbc0f7259bee458404517d826b0b84d2
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-12-29 12:27:35 +0100

    [chore] formatting

--- packages/apps/docs/src/tests/helpers/markdown.helper.ts
+++ packages/apps/docs/src/tests/helpers/markdown.helper.ts
@@ -1,43 +1,36 @@
 const parseMD = require('parse-md').default;
 import fs from 'fs';
 
+export async function extractMetadataFromMarkdown(
+  parentPath: string,
+  childPath?: string,
+  grandChildPath?: string,
+): Promise<Metadata[]> {
+  const metadata: Metadata[] = [];
 
+  const parentFile = fs.readFileSync(parentPath, 'utf8');
+  const parent = parseMD(parentFile);
+  metadata.push(parent.metadata);
 
-
- export async function extractMetadataFromMarkdown(
-    parentPath: string,
-    childPath?: string,
-    grandChildPath?: string,
-  ): Promise<Metadata[]> {
-    const metadata: Metadata[] = [];
-
-
-    const parentFile = fs.readFileSync(parentPath, 'utf8');
-    const parent  = parseMD(parentFile);
-    metadata.push(parent.metadata);
-
-    if (childPath) {
-      const childFile = fs.readFileSync(childPath, 'utf8');
-      const child  = parseMD(childFile);
-      metadata.push(child.metadata);
-    }
-
-    if (grandChildPath) {
-      const grandChildFile = fs.readFileSync(grandChildPath, 'utf8');
-      const  grandChild  = parseMD(grandChildFile);
-      metadata.push(grandChild.metadata);
-    }
-    return metadata
+  if (childPath) {
+    const childFile = fs.readFileSync(childPath, 'utf8');
+    const child = parseMD(childFile);
+    metadata.push(child.metadata);
   }
 
+  if (grandChildPath) {
+    const grandChildFile = fs.readFileSync(grandChildPath, 'utf8');
+    const grandChild = parseMD(grandChildFile);
+    metadata.push(grandChild.metadata);
+  }
+  return metadata;
+}
 
-
-
-export type Metadata = {
+export interface Metadata {
   title: string;
   description: string;
   menu: string;
   label: string;
   order: number;
   layout: 'full';
-};
+}
