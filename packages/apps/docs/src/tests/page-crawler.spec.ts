e6fce10423217a50f56e1abb9f03aa05c564270a
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-12-29 12:17:30 +0100

    [test] clean up spec file

--- packages/apps/docs/src/tests/page-crawler.spec.ts
+++ packages/apps/docs/src/tests/page-crawler.spec.ts
@@ -1,5 +1,4 @@
 import { test } from '@playwright/test';
-import MenuHelper from './helpers/menu.helper';
 import TreeHelper from './helpers/tree.helper';
 import CookieHelper from './helpers/cookie.helper';
 
@@ -8,7 +7,6 @@ const pages = ['kadena', 'build', 'pact', 'chainweb', 'marmalade', 'contribute']
 
 for (const pageToCheck of pages) {
 test(`Page: ${pageToCheck} - All Markdown files should be present in the menu`, async ({ page }) => {
-    const menuHelper = new MenuHelper(page)
     const treeHelper = new TreeHelper(page)
     const cookieHelper = new CookieHelper(page)
 
