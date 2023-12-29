5b60a25381333f4a1036aae6721c73b7381a603a
Ghislain Gabrielse <Ghislain@Kadena.io>
2023-12-29 11:53:40 +0100

    [test] update run script for docs and playwright config to run headless in CI

--- packages/apps/docs/playwright.config.ts
+++ packages/apps/docs/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   workers: process.env.CI ? 4 : 1,
   reporter: [['github'], ['list'], ['html', { open: 'never' }]],
   use: {
-    headless: !!process.env.CI,
+    headless: process.env.CI != undefined,
     baseURL: 'http://localhost:3000/',
     channel: 'chromium',
     trace: 'retain-on-failure',
