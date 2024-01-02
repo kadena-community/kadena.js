74545aae41c444b09ddd5f9c4e7e892d7d77a188
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-01-02 15:48:31 +0000

    added non fungible queries and adapted fungible queries

---
+++ packages/apps/graph-client/src/graphql/fields/chain-non-fungible-account.graph.ts
@@ -0,0 +1,27 @@
+import type { DocumentNode } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+export const CORE_CHAIN_NON_FUNGIBLE_ACCOUNT_FIELDS: DocumentNode = gql`
+  fragment CoreChainNonFungibleAccountFields on ChainNonFungibleAccount {
+    chainId
+  }
+`;
+
+export const ALL_CHAIN_NON_FUNGIBLE_ACCOUNT_FIELDS: DocumentNode = gql`
+  ${CORE_CHAIN_NON_FUNGIBLE_ACCOUNT_FIELDS}
+
+  fragment AllChainNonFungibleAccountFields on ChainNonFungibleAccount {
+    ...CoreChainNonFungibleAccountFields
+    accountName
+    guard {
+      keys
+      predicate
+    }
+    nonFungibles {
+      balance
+      id
+    }
+
+    # transactions {}
+  }
+`;
--- packages/apps/graph-client/src/graphql/queries.graph.ts
+++ packages/apps/graph-client/src/graphql/queries.graph.ts
@@ -4,7 +4,9 @@ import {
   ALL_CHAIN_ACCOUNT_FIELDS,
   CORE_CHAIN_ACCOUNT_FIELDS,
 } from './fields/chain-account.graph';
+import { CORE_CHAIN_NON_FUNGIBLE_ACCOUNT_FIELDS } from './fields/chain-non-fungible-account.graph';
 import { CORE_MINER_KEY_FIELDS } from './fields/miner-key.graph';
+import { ALL_NON_FUNGIBLE_ACCOUNT_FIELDS } from './fields/non-fungible-account.graph';
 import { CORE_TRANSACTION_FIELDS } from './fields/transaction.graph';
 import { CORE_TRANSFER_FIELDS } from './fields/transfer.graph';
 
@@ -79,14 +81,14 @@ export const getRecentHeights: DocumentNode = gql`
   }
 `;
 
-export const getAccount: DocumentNode = gql`
+export const getFungibleAccount: DocumentNode = gql`
   ${ALL_ACCOUNT_FIELDS}
   ${CORE_CHAIN_ACCOUNT_FIELDS}
   ${CORE_TRANSACTION_FIELDS}
   ${CORE_TRANSFER_FIELDS}
 
-  query getAccount($fungibleName: String!, $accountName: String!) {
-    account(fungibleName: $fungibleName, accountName: $accountName) {
+  query getFungibleAccount($fungibleName: String!, $accountName: String!) {
+    fungibleAccount(fungibleName: $fungibleName, accountName: $accountName) {
       ...AllAccountFields
       chainAccounts {
         ...CoreChainAccountFields
@@ -119,17 +121,17 @@ export const getAccount: DocumentNode = gql`
   }
 `;
 
-export const getChainAccount: DocumentNode = gql`
+export const getChainFungibleAccount: DocumentNode = gql`
   ${CORE_TRANSACTION_FIELDS}
   ${CORE_TRANSFER_FIELDS}
   ${ALL_CHAIN_ACCOUNT_FIELDS}
 
-  query getChainAccount(
+  query getChainFungibleAccount(
     $fungibleName: String!
     $accountName: String!
     $chainId: String!
   ) {
-    chainAccount(
+    chainFungibleAccount(
       fungibleName: $fungibleName
       accountName: $accountName
       chainId: $chainId
@@ -252,5 +254,61 @@ export const getTransfers: DocumentNode = gql`
 export const estimateGasLimit: DocumentNode = gql`
   query estimateGasLimit($transaction: PactTransaction!) {
     gasLimitEstimate(transaction: $transaction)
+  }
+`;
+
+export const getNonFungibleAccount: DocumentNode = gql`
+  ${ALL_NON_FUNGIBLE_ACCOUNT_FIELDS}
+  ${CORE_CHAIN_NON_FUNGIBLE_ACCOUNT_FIELDS}
+  ${CORE_TRANSACTION_FIELDS}
+
+  query getNonFungibleAccount($accountName: String!) {
+    nonFungibleAccount(accountName: $accountName) {
+      ...AllNonFungibleAccountFields
+      chainAccounts {
+        ...CoreChainNonFungibleAccountFields
+        guard {
+          keys
+          predicate
+        }
+        nonFungibles {
+          balance
+          id
+        }
+        transactions {
+          edges {
+            node {
+              ...CoreTransactionFields
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getChainNonFungibleAccount: DocumentNode = gql`
+  ${CORE_CHAIN_NON_FUNGIBLE_ACCOUNT_FIELDS}
+  ${CORE_TRANSACTION_FIELDS}
+
+  query getChainNonFungibleAccount($accountName: String!, $chainId: String!) {
+    chainNonFungibleAccount(accountName: $accountName, chainId: $chainId) {
+      ...CoreChainNonFungibleAccountFields
+      guard {
+        keys
+        predicate
+      }
+      nonFungibles {
+        balance
+        id
+      }
+      transactions {
+        edges {
+          node {
+            ...CoreTransactionFields
+          }
+        }
+      }
+    }
   }
 `;
