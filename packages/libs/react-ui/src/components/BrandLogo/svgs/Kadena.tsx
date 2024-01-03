6bc234443b04fa50570cd95536bb9b7c06a6e253
kristina.spasevska <kristina.spasevska@deptagency.com>
2024-01-03 09:49:17 +0100

    update: new brand logos

--- packages/libs/react-ui/src/components/BrandLogo/svgs/Kadena.tsx
+++ packages/libs/react-ui/src/components/BrandLogo/svgs/Kadena.tsx
@@ -1,52 +1,20 @@
 import type { SVGProps } from 'react';
 import * as React from 'react';
-
 export const KadenaLogo: React.FC<SVGProps<SVGSVGElement>> = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="40"
-    height="40"
-    viewBox="0 0 40 40"
-    fill="none"
-    aria-label="Kadena"
-  >
-    <g clipPath="url(#clip0_3_15)">
-      <path
-        d="M31.5523 6.62952H23.7098L14.4796 16.3155V24.3859L31.5523 6.62952Z"
-        fill="url(#paint0_linear_3_15)"
-      />
-      <path d="M14.4796 33.3705H7.8501V6.62952H14.4796V33.3705Z" fill="white" />
-      <path
-        d="M32.1499 33.3705L21.256 17.3386L16.8195 21.9499L24.4518 33.3705H32.1499Z"
-        fill="url(#paint1_linear_3_15)"
-      />
+  <svg xmlns="http://www.w3.org/2000/svg" id="body_1" width="24" height="24">
+
+    <g transform="matrix(0.6 0 0 0.6 0 0)">
+      <g transform="matrix(1 0 0 1 0 0)">
+        <g transform="matrix(1 0 0 1 0 0)">
+          <g>
+            <path d="M34.410156 39.746094L22.492188 39.746094L22.484375 39.73828L22.390625 39.652344L7.617188 25.816406L13.664062 20L34.308594 39.652344L34.410156 39.746094z" stroke="none" fill="#4A9079" fill-rule="nonzero" />
+            <path d="M34.410156 0.25L22.492188 0.25L22.484375 0.257812L22.390625 0.34375L7.617188 14.179688L13.664062 20L34.308594 0.34375L34.410156 0.25z" stroke="none" fill="#4A9079" fill-rule="nonzero" />
+            <path d="M7.621094 20.875L7.621094 39.746094L7.613281 39.73828L7.523438 39.652344L0.335938 32.835938L0.316406 32.820312L0.214844 32.722656L0.214844 7.277344L0.316406 7.179688L0.335938 7.164062L7.523438 0.34375L7.613281 0.257812L7.621094 0.25L7.621094 20.875z" stroke="none" fill="#4A9079" fill-rule="nonzero" />
+            <path d="M0.214844 20L0.210938 20L0.210938 20.875L0.214844 20.875L0.214844 20z" stroke="none" fill="#4A9079" fill-rule="nonzero" />
+            <path d="M7.621094 20L7.617188 20L7.617188 20.875L7.621094 20.875L7.621094 20z" stroke="none" fill="#4A9079" fill-rule="nonzero" />
+          </g>
+        </g>
+      </g>
     </g>
-    <defs>
-      <linearGradient
-        id="paint0_linear_3_15"
-        x1="14.4796"
-        y1="15.5077"
-        x2="31.5523"
-        y2="15.5077"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop />
-        <stop offset="0.6" stopColor="white" />
-      </linearGradient>
-      <linearGradient
-        id="paint1_linear_3_15"
-        x1="15.5989"
-        y1="20.6432"
-        x2="29.3973"
-        y2="29.6024"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop />
-        <stop offset="0.6" stopColor="white" />
-      </linearGradient>
-      <clipPath id="clip0_3_15">
-        <rect width="40" height="40" fill="white" />
-      </clipPath>
-    </defs>
   </svg>
 );
