0299dc8ed9753774e420d00fd423d2927accf89d
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-01-04 19:49:36 +0100

    dont set name?

--- .github/actions/devnet/action.sh
+++ .github/actions/devnet/action.sh
@@ -16,4 +16,4 @@ exec docker run \
   -v "/var/run/docker.sock":"/var/run/docker.sock" \
   -v /home/runner/work/_temp:/home/runner/work/_temp \
   -e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY 	\
-  -d -p 8080:8080 "kadena/devnet" --name devnet --health-cmd="curl --verbose --fail http://localhost:8080/info || exit 1"
+  -d -p 8080:8080 "kadena/devnet" --health-cmd="curl --verbose --fail http://localhost:8080/info || exit 1"
