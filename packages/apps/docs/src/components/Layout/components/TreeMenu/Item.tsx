769e2d93ae350917f9ce5eed74f5f19d577f8894
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-01-04 09:51:04 +0100

    [fix] resolve eslint errors and migrate test-id to data-testid

--- packages/apps/docs/src/components/Layout/components/TreeMenu/Item.tsx
+++ packages/apps/docs/src/components/Layout/components/TreeMenu/Item.tsx
@@ -25,9 +25,8 @@ export const Item: FC<IItem> = ({ item, level }) => {
   );
 
   return (
-    //TODO: Check with Steven for a good way to do this.
-    <li // eslint-disable-next-line react/no-unknown-property
-      test-id={`menuItem-${level}`}
+    <li
+      data-testid={`menuItem-${level}`}
       className={classNames(listItemClass, listItemVariants[`l${level}`])}
     >
       <Link className={classes} href={item.root} data-active={item.isActive}>
