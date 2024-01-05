b424cc942e46b2a72f487d5f842142fb5bd32d12
Sander Looijenga <slooijenga@anwb.nl>
2024-01-05 14:11:07 +0100

    Show selected value in Storybook entry

--- packages/libs/react-ui/src/components/Form/ComboBox/ComboBox.stories.tsx
+++ packages/libs/react-ui/src/components/Form/ComboBox/ComboBox.stories.tsx
@@ -1,7 +1,8 @@
+import { Box } from '@components/Layout';
+import { Text } from '@components/Typography';
 import { onLayer1 } from '@storyDecorators';
 import type { Meta, StoryObj } from '@storybook/react';
 import { tokens } from '@theme/index';
-import { vars } from '@theme/vars.css';
 import React from 'react';
 import { Item } from 'react-stately';
 import type { IComboBoxProps } from './ComboBox';
@@ -20,20 +21,29 @@ type Story = StoryObj;
 export const Test: Story = {
   name: 'ComboBox',
   render: () => {
+    const [selectedValue, setSelectedValue] = React.useState<string | null>(
+      'Nothing selected yet',
+    );
     return (
-      <ComboBox
-        label="Favorite Animal"
-        allowsCustomValue
-        id="my-combobox"
-        width={tokens.kda.foundation.layout.content.maxWidth}
-      >
-        <Item key="red panda">Red Panda</Item>
-        <Item key="cat">Cat</Item>
-        <Item key="dog">Dog</Item>
-        <Item key="aardvark">Aardvark</Item>
-        <Item key="kangaroo">Kangaroo</Item>
-        <Item key="snake">Snake</Item>
-      </ComboBox>
+      <>
+        <ComboBox
+          label="Favorite Animal"
+          allowsCustomValue
+          id="my-combobox"
+          width={tokens.kda.foundation.layout.content.maxWidth}
+          onInputChange={(value) => setSelectedValue(value)}
+        >
+          <Item key="red panda">Red Panda</Item>
+          <Item key="cat">Cat</Item>
+          <Item key="dog">Dog</Item>
+          <Item key="aardvark">Aardvark</Item>
+          <Item key="kangaroo">Kangaroo</Item>
+          <Item key="snake">Snake</Item>
+        </ComboBox>
+        <Box marginBottom={'$4'} />
+        <Text>Selected value:</Text>
+        <Text>{selectedValue}</Text>
+      </>
     );
   },
 };
