6f93d7350ee44ea459bd6b34cd26bf422931b071
Sam Ferrero <ferrerocompanylimited@gmail.com>
2024-01-08 10:16:47 +0100

    chore: breadcrumbs on docs;

--- packages/apps/docs/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/apps/docs/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import type { IMenuItem } from '@kadena/docs-tools';
-import { Box, Breadcrumbs as StyledBreadcrumbs } from '@kadena/react-ui';
+import { Box, BreadcrumbsContainer, BreadcrumbsItem } from '@kadena/react-ui';
 import Link from 'next/link';
 import type { FC } from 'react';
 import React, { useMemo } from 'react';
@@ -50,19 +50,19 @@ export const Breadcrumbs: FC<IProps> = ({ menuItems }) => {
 
   return (
     <Box data-cy="breadcrumbs" marginTop="$10" marginBottom="$4">
-      <StyledBreadcrumbs.Root>
+      <BreadcrumbsContainer>
         {items.map((item, idx) =>
           idx < items.length - 1 ? (
-            <StyledBreadcrumbs.Item key={`${item.root}${idx}`} asChild>
+            <BreadcrumbsItem key={`${item.root}${idx}`} asChild>
               <Link href={`${item.root}`}>{item.title}</Link>
-            </StyledBreadcrumbs.Item>
+            </BreadcrumbsItem>
           ) : (
-            <StyledBreadcrumbs.Item key={`${item.root}${idx}`}>
+            <BreadcrumbsItem key={`${item.root}${idx}`}>
               {item.title}
-            </StyledBreadcrumbs.Item>
+            </BreadcrumbsItem>
           ),
         )}
-      </StyledBreadcrumbs.Root>
+      </BreadcrumbsContainer>
     </Box>
   );
 };
