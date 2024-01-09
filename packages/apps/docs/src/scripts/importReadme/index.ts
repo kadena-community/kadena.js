5a7c4c8e5d5d6685e3cc87a728ecc8073088f352
Steven Straatemans <steven@straatemans.nl>
2024-01-09 12:44:20 +0100

    fix build

--- packages/apps/docs/src/scripts/importReadme/index.ts
+++ packages/apps/docs/src/scripts/importReadme/index.ts
@@ -1,10 +1,4 @@
-import type { IScriptResult } from '../types';
-import { importReadMes } from '../utils';
 import { TEMP_DIR, promiseExec } from '../utils/build';
-import { importRepo } from './importRepo';
-
-const errors: string[] = [];
-const success: string[] = [];
 
 export const removeRepoDomain = (repo: string): string =>
   repo.replace(/https:\/\/github.com/, '');
@@ -19,20 +13,4 @@ export const clone = async (repo: string): Promise<void> => {
   } catch (e) {
     await Promise.resolve();
   }
-};
-
-export const importAllReadmes = async (): Promise<IScriptResult> => {
-  const monorepoCount = 0;
-  let outsideCount = 0;
-
-  for (const item of importReadMes) {
-    outsideCount++;
-    await importRepo(item);
-  }
-
-  success.push(
-    `Docs imported from monorepo(${monorepoCount}) and outside repos(${outsideCount})`,
-  );
-
-  return { success, errors };
 };
