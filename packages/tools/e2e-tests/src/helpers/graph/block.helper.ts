a2ab318009d1b48bc1268a58b1fef160be3376a5
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-01-10 10:38:24 +0100

    [test] migrate Graph tests to playwright

--- packages/tools/e2e-tests/src/helpers/graph/block.helper.ts
+++ packages/tools/e2e-tests/src/helpers/graph/block.helper.ts
@@ -1,10 +1,12 @@
 import { getTransactionsByRequestKeyQuery } from '@fixtures/graph/testdata/queries/getTransactions';
 import { sendQuery } from '@helpers/graph/request.helper';
+import { APIRequestContext } from '@playwright/test';
 
 export async function getBlockHash(
+  request: APIRequestContext,
   requestKey: string | undefined,
 ): Promise<string> {
   const query = getTransactionsByRequestKeyQuery(requestKey);
-  const queryResult = await sendQuery(query);
-  return queryResult.body.data.transactions.edges[0].node.block.hash;
+  const queryResult = await sendQuery(request, query);
+  return queryResult.transactions.edges[0].node.block.hash;
 }
