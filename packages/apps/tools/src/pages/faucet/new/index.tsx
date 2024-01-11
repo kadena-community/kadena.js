d2e205d6da72c7f3b194b53c90eac23ea3f5456b
Sander Looijenga <slooijenga@anwb.nl>
2024-01-11 14:08:25 +0100

    Use placeholderData instead and reset notification

--- packages/apps/tools/src/pages/faucet/new/index.tsx
+++ packages/apps/tools/src/pages/faucet/new/index.tsx
@@ -99,8 +99,13 @@ const NewAccountFaucetPage: FC = () => {
     queryKey: ['accountName', pubKeys, chainID, pred],
     queryFn: () => createPrincipal(pubKeys, chainID, pred),
     enabled: pubKeys.length > 0,
-    initialData: '',
+    placeholderData: '',
+    keepPreviousData: true,
   });
+
+  useEffect(() => {
+    setRequestStatus({ status: 'idle' });
+  }, [pubKeys.length]);
 
   const {
     register,
