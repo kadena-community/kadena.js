fae05943d13141981360ce3e93a606c10573caf1
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-01-11 15:09:49 +0100

    [chore] update test after merge conflict

--- packages/tools/e2e-tests/src/tests/tools-app/faucet.spec.ts
+++ packages/tools/e2e-tests/src/tests/tools-app/faucet.spec.ts
@@ -1,29 +1,65 @@
 import { expect } from '@playwright/test';
-import { accountsData } from '../../fixtures/data/accounts';
-import { pollFinished, pollInProgress } from '../../fixtures/mocks/poll.mocks';
-import { sendMocks } from '../../fixtures/mocks/send.mocks';
-import { test } from '../../page-objects';
+import { test } from '../../support/fixtures/test.fixture';
+import {
+  createAccount,
+  generateAccount,
+} from '../../support/helpers/accounts.helper';
 
-test('Fund existing account @mocks', async ({ page, toolsApp, mockHelper }) => {
+test.beforeEach(async ({ page, toolsApp, context }) => {
   await test.step('Open Tools and navigate to Faucet', async () => {
     await page.goto('/');
-    await toolsApp.homePage.header.setNetwork('Testnet');
-    await toolsApp.homePage.header.goTo('Faucet');
-  });
-
-  await test.step('Fund account on chain 0.', async () => {
-    await mockHelper.mockResponse('**/send', sendMocks);
-    await toolsApp.faucetPage.asidePanel.navigateTo('Fund Existing Account');
-    await toolsApp.faucetPage.fundExistingAccount(accountsData.publicKey, '0');
-
-    await mockHelper.mockResponse('**/poll', pollInProgress);
-    await expect(
-      page.getByText('Transaction is being processed...'),
-    ).toBeVisible();
-
-    await mockHelper.mockResponse('**/poll', pollFinished);
-    await expect(
-      page.getByText('Transaction successfully completed'),
-    ).toBeVisible();
+    await toolsApp.homePage.header.setNetwork('devnet');
+    await toolsApp.homePage.header.goToPage('Faucet');
   });
 });
+
+const accountTypes = [
+  { type: 'k:', NumberOfKeys: 1 },
+  { type: 'w:', NumberOfKeys: 2 },
+];
+
+for (const accountType of accountTypes) {
+  test(`Create and fund ${accountType.type} account`, async ({
+    page,
+    toolsApp,
+  }) => {
+    const account = await generateAccount(accountType.NumberOfKeys, '0');
+    await test.step('Create account on chain 0.', async () => {
+      await toolsApp.faucetPage.asidePanel.clickPageLink('Fund New Account');
+      await toolsApp.faucetPage.CreateFundAccount(account);
+      await expect(
+        await toolsApp.faucetPage.notificationComponent.getTitle(),
+      ).toHaveText('Transaction is being processed...');
+    });
+    await test.step('Account has been created', async () => {
+      await expect(
+        await toolsApp.faucetPage.notificationComponent.getTitle(),
+      ).toHaveText('Transaction successfully completed');
+    });
+  });
+
+  test(`Fund existing ${accountType.type} account`, async ({
+    page,
+    toolsApp,
+    i18n,
+  }) => {
+    await test.step('Fund account on chain 0.', async () => {
+      const createdAccount = await createAccount(accountType.NumberOfKeys, '0');
+      await toolsApp.faucetPage.asidePanel.clickPageLink(
+        'Fund Existing Account',
+      );
+      await toolsApp.faucetPage.fundExistingAccount(
+        createdAccount.account,
+        '0',
+      );
+      await expect(
+        await toolsApp.faucetPage.notificationComponent.getTitle(),
+      ).toHaveText('Transaction is being processed...');
+    });
+    await test.step('Account has been funded', async () => {
+      await expect(
+        await toolsApp.faucetPage.notificationComponent.getTitle(),
+      ).toHaveText('Transaction successfully completed');
+    });
+  });
+}
