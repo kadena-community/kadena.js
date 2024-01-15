1b541c4bbd349c7eb69205baf89fb4bb96ed6bee
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-01-15 13:25:59 +0100

    removed config

--- packages/tools/kadena-cli/src/devnet/utils/simulation/config.ts
+++
@@ -1,9 +0,0 @@
-import { ChainId } from '@kadena/types';
-
-export const SIMULATION_CONFIG = {
-  LOG_FOLDERNAME: `${process.cwd()}/simulation-logs}`,
-  DEFAULT_CHAIN_ID: '0' as ChainId,
-  NETWORK_HOST: 'http://localhost:8080',
-  NETWORK_ID: 'fast-development',
-  CHAIN_COUNT: 20,
-};
