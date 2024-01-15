1310d0cd6293a6194ec6717a4765f5bb3422b633
Steven Straatemans <steven@straatemans.nl>
2024-01-15 18:25:24 +0100

    fix article style padding

--- packages/apps/docs/src/components/Layout/components/articleStyles.css.ts
+++ packages/apps/docs/src/components/Layout/components/articleStyles.css.ts
@@ -23,8 +23,6 @@ export const contentClass = style([
   atoms({
     display: 'flex',
     position: 'relative',
-
-    paddingInline: 'md',
     overflow: 'hidden',
     width: '100%',
     height: '100%',
