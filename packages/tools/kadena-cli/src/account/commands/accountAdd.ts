d1f50f3af466e72646887ab03796a596275b1b4b
Mohamed Ismail <ismailreality1@gmail.com>
2024-01-17 13:21:08 +0100

    feat(account): store account data to alias file

--- packages/tools/kadena-cli/src/account/commands/accountAdd.ts
+++ packages/tools/kadena-cli/src/account/commands/accountAdd.ts
@@ -1,8 +1,23 @@
 import debug from 'debug';
 import type { Command } from 'commander';
+import path from 'path';
+import chalk from 'chalk';
+import yaml from 'js-yaml';
 
+import { services } from '../../services/index.js';
 import { createCommand } from "../../utils/createCommand.js";
 import { globalOptions } from "../../utils/globalOptions.js";
+import { defaultAccountPath } from '../../constants/account.js';
+import { sanitizeFilename } from '../../utils/helpers.js';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function writeAlias(config: any, filePath: string): Promise<void> {
+  await services.filesystem.ensureDirectoryExists(filePath);
+  await services.filesystem.writeFile(
+    filePath,
+    yaml.dump(config),
+  );
+}
 
 export const addAccountCommand: (program: Command, version: string) => void = createCommand(
   'add',
@@ -18,5 +33,23 @@ export const addAccountCommand: (program: Command, version: string) => void = cr
   ],
   async (config) => {
     debug('account-add:action')({ config });
+    const sanitizedAlias = sanitizeFilename(config.accountAlias).toLowerCase();
+    const filePath = path.join(defaultAccountPath, `${sanitizedAlias}.yaml`);
+
+    if(await services.filesystem.fileExists(filePath)) {
+      console.log(
+        chalk.yellow(
+          `\nThe existing account configuration "${config.accountAlias}" will not be updated.\n`,
+        ),
+      );
+      return;
+    }
+
+    await writeAlias(config, filePath);
+    console.log(
+      chalk.green(
+      `\nThe account configuration "${config.accountAlias}" has been saved.\n`,
+      ),
+    );
   }
 );
