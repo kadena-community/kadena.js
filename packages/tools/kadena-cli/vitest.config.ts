066d4055a8db3faf6846eccd63b24a93d90d4f29
Mohamed Ismail <ismailreality1@gmail.com>
2024-01-23 09:51:39 +0100

    feat(account): fix test coverage setup

--- packages/tools/kadena-cli/vitest.config.ts
+++ packages/tools/kadena-cli/vitest.config.ts
@@ -7,10 +7,19 @@ export default defineConfig({
     globals: true,
     include: ['src/**/*.test.ts'],
     setupFiles: ['./vitest.setup.js', './src/setup.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-    },
     threads: false, // To prevent error in tests using jsdom environment: Module did not self-register: canvas.node
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 0,
+        functions: 0,
+        branches: 0,
+        statements: 0,
+      },
+      include: ['src/**/*.ts'],
+      exclude: ['**/mocks/**'],
+    },
   },
 });
