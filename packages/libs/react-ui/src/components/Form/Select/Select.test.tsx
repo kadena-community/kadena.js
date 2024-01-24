645ccedff5f6bec6018e145ef8f063bbd638ecbc
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-01-24 12:25:29 +0100

    chore: Formatting

--- packages/libs/react-ui/src/components/Form/Select/Select.test.tsx
+++ packages/libs/react-ui/src/components/Form/Select/Select.test.tsx
@@ -8,68 +8,84 @@ import userEvent from '@testing-library/user-event';
 // TODO: add more tests
 
 describe('Select', () => {
-  it('renders without errors', () => {
-    render(
-      <Select id="select-without-errors" selectedKey="1" aria-label="select">
-        <SelectItem key="1">Option 1</SelectItem>
-        <SelectItem key="2">Option 2</SelectItem>
-      </Select>,
-    );
+  it(
+    'renders without errors',
+    () => {
+      render(
+        <Select id="select-without-errors" selectedKey="1" aria-label="select">
+          <SelectItem key="1">Option 1</SelectItem>
+          <SelectItem key="2">Option 2</SelectItem>
+        </Select>,
+      );
 
-    const selectField = screen.getByLabelText('select');
-    expect(selectField).toBeInTheDocument();
-  }, {timeout: 6000});
+      const selectField = screen.getByLabelText('select');
+      expect(selectField).toBeInTheDocument();
+    },
+    { timeout: 6000 },
+  );
 
-  it('renders the provided children options when is open', async () => {
-    render(
-      <Select id="renders-child-options" selectedKey="1" aria-label="select">
-        <SelectItem key="1">Option 1</SelectItem>
-        <SelectItem key="2">Option 2</SelectItem>
-      </Select>,
-    );
+  it(
+    'renders the provided children options when is open',
+    async () => {
+      render(
+        <Select id="renders-child-options" selectedKey="1" aria-label="select">
+          <SelectItem key="1">Option 1</SelectItem>
+          <SelectItem key="2">Option 2</SelectItem>
+        </Select>,
+      );
 
-    const select = screen.getByLabelText('select');
-    await userEvent.click(select);
-    const option1 = screen.getByRole('option', {
-      name: 'Option 1',
-    });
-    const option2 = screen.getByRole('option', {
-      name: 'Option 2',
-    });
+      const select = screen.getByLabelText('select');
+      await userEvent.click(select);
+      const option1 = screen.getByRole('option', {
+        name: 'Option 1',
+      });
+      const option2 = screen.getByRole('option', {
+        name: 'Option 2',
+      });
 
-    expect(option1).toBeInTheDocument();
-    expect(option2).toBeInTheDocument();
-  }, {timeout: 6000})
+      expect(option1).toBeInTheDocument();
+      expect(option2).toBeInTheDocument();
+    },
+    { timeout: 6000 },
+  );
 
-  it('invokes the onChange event handler when an option is selected', async () => {
-    const handleChange = vi.fn();
-    render(
-      <Select
-        id="on-change-select"
-        selectedKey="1"
-        onSelectionChange={handleChange}
-        aria-label="select"
-      >
-        <SelectItem key="1">Option 1</SelectItem>
-        <SelectItem key="2">Option 2</SelectItem>
-      </Select>,
-    );
+  it(
+    'invokes the onChange event handler when an option is selected',
+    async () => {
+      const handleChange = vi.fn();
+      render(
+        <Select
+          id="on-change-select"
+          selectedKey="1"
+          onSelectionChange={handleChange}
+          aria-label="select"
+        >
+          <SelectItem key="1">Option 1</SelectItem>
+          <SelectItem key="2">Option 2</SelectItem>
+        </Select>,
+      );
 
-    const selectContainer = screen.getByLabelText('select');
-    await userEvent.click(selectContainer);
-    await userEvent.click(screen.getByRole('option', { name: 'Option 2' }));
-    expect(handleChange).toHaveBeenCalledTimes(1);
-  }, {timeout: 6000})
+      const selectContainer = screen.getByLabelText('select');
+      await userEvent.click(selectContainer);
+      await userEvent.click(screen.getByRole('option', { name: 'Option 2' }));
+      expect(handleChange).toHaveBeenCalledTimes(1);
+    },
+    { timeout: 6000 },
+  );
 
-  it('disables the select element when disabled prop is true', () => {
-    render(
-      <Select selectedKey="1" isDisabled label="Cool label">
-        <SelectItem key="1">Option 1</SelectItem>
-        <SelectItem key="2">Option 2</SelectItem>
-      </Select>,
-    );
+  it(
+    'disables the select element when disabled prop is true',
+    () => {
+      render(
+        <Select selectedKey="1" isDisabled label="Cool label">
+          <SelectItem key="1">Option 1</SelectItem>
+          <SelectItem key="2">Option 2</SelectItem>
+        </Select>,
+      );
 
-    const selectButton = screen.getByRole('button') as HTMLButtonElement;
-    expect(selectButton.disabled).toBe(true);
-  }, {timeout: 6000})
+      const selectButton = screen.getByRole('button') as HTMLButtonElement;
+      expect(selectButton.disabled).toBe(true);
+    },
+    { timeout: 6000 },
+  );
 });
