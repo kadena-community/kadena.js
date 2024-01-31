4bf65d7a6e3771dc2f5b70cf3dc3e123519065bf
Steven Straatemans <steven@straatemans.nl>
2024-01-31 16:27:57 +0100

    temp

--- packages/apps/proof-of-us/src/components/Mint/Mint.tsx
+++ packages/apps/proof-of-us/src/components/Mint/Mint.tsx
@@ -1,37 +1,39 @@
+import { PROOFOFUS_QR_EVENT_URL } from '@/constants';
 import { useAccount } from '@/hooks/account';
 import { useProofOfUs } from '@/hooks/proofOfUs';
+import { env } from '@/utils/env';
 import type { FC } from 'react';
+import { useState } from 'react';
 
 interface IProps {
   proofOfUs: IProofOfUs;
 }
 
 export const Mint: FC<IProps> = ({ proofOfUs }) => {
-  const { addSignee, removeSignee, getSigneeAccount, isConnected } =
-    useProofOfUs();
-  const { account } = useAccount();
+  const [isMinting, setIsMinting] = useState(false);
+  const [isMinted, setIsMinted] = useState(false);
 
-  const handleJoin = async () => {
-    addSignee({ tokenId: proofOfUs.tokenId });
-  };
+  const handleMint = () => {
+    setIsMinting(true);
 
-  const handleRemove = async () => {
-    if (!account) return;
-    removeSignee({
-      tokenId: proofOfUs.tokenId,
-      signee: getSigneeAccount(account),
-    });
+    //TODO: this should be an actual minting process
+    setTimeout(() => {
+      setIsMinting(false);
+      setIsMinted(true);
+    }, 5000);
   };
   return (
     <>
-      <section>
-        {!isConnected() ? (
-          <button onClick={handleJoin}>join</button>
-        ) : (
-          <button onClick={handleRemove}>remove</button>
-        )}
-      </section>
       scanned Proof Of Us with ID ({proofOfUs.tokenId})
+      <div>
+        {!isMinted ? (
+          <button disabled={isMinting} onClick={handleMint}>
+            Mint!
+          </button>
+        ) : (
+          <div>Congrats!!!</div>
+        )}
+      </div>
     </>
   );
 };
--- packages/apps/proof-of-us/src/constants.ts
+++ packages/apps/proof-of-us/src/constants.ts
@@ -1,2 +1,3 @@
 export const ACCOUNT_COOKIE_NAME = 'waccount';
 export const PROOFOFUS_QR_URL = '/scan';
+export const PROOFOFUS_QR_EVENT_URL = '/mint';
