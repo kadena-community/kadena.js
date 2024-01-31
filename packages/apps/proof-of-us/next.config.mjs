7220ccb668c71d1959b16f06440a6c2a09d5c433
Steven <steven@straatemans.nl>
2024-01-31 12:54:37 +0100

    feat(pou): start editor (#1527)

--- packages/apps/proof-of-us/next.config.mjs
+++ packages/apps/proof-of-us/next.config.mjs
@@ -5,6 +5,15 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+      canvas: 'commonjs canvas',
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
 };
 
 export default withVanillaExtract(nextConfig);
