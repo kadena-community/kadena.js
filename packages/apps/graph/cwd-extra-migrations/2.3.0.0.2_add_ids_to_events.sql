98845878b6031b3cc8e944f8f5dc3813e9930588
MRVDH <maarten.vandenhoven@deptagency.com>
2024-01-31 11:55:58 +0100

    [@kadena/graph] Alter migrations

--- packages/apps/graph/cwd-extra-migrations/2.3.0.0.2_add_ids_to_events.sql
+++ packages/apps/graph/cwd-extra-migrations/2.3.0.0.2_add_ids_to_events.sql
@@ -1,26 +1,26 @@
 
 -- Add the id column if it doesnt exist
 ALTER TABLE
-  public.events
+  events
 ADD
   COLUMN IF NOT EXISTS id INT;
 
 -- Create a sequence for the id column if not already created
-CREATE SEQUENCE IF NOT EXISTS public.events_id_seq OWNED BY public.events.id;
+CREATE SEQUENCE IF NOT EXISTS events_id_seq OWNED BY events.id;
 
 
 -- Update existing entries with unique id's
 UPDATE
-  public.events
+  events
 SET
-  id = COALESCE(id, nextval('public.events_id_seq'))
+  id = COALESCE(id, nextval('events_id_seq'))
 WHERE
   id IS NULL;
 
 
 -- Make the id column not null
 ALTER TABLE
-  public.events
+  events
 ALTER COLUMN
   id
 SET NOT NULL;
@@ -29,17 +29,17 @@ SET NOT NULL;
 -- Set value of the sequence
 SELECT
   setval(
-    'public.events_id_seq',
-    COALESCE(MAX(public.events.id), 0) + 1,
+    'events_id_seq',
+    COALESCE(MAX(events.id), 0) + 1,
     false
   )
-FROM public.events;
+FROM events;
 
 
 --Set the default value of the id column to the next value of the sequence
 ALTER TABLE
-  public.events
+  events
 ALTER COLUMN
   id
 SET
-  DEFAULT nextval('public.events_id_seq');
+  DEFAULT nextval('events_id_seq');
