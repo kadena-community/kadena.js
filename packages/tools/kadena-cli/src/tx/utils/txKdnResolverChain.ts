0951fb4851c533b8790076f7940c045ec0b17afe
neel <bbnillo@gmail.com>
2024-02-02 08:38:54 +0100

    fixes

---
+++ packages/tools/kadena-cli/src/tx/utils/txKdnResolverChain.ts
@@ -0,0 +1,102 @@
+import type { ChainId, IClient, ICommandResult } from '@kadena/client';
+import { Pact, createClient } from '@kadena/client';
+
+interface INameToAddressResponse {
+  address: string;
+}
+interface IAddressToNameResponse {
+  name: string;
+}
+
+const client = (
+  networkConfig: Record<string, string>,
+  chainId: ChainId,
+): IClient =>
+  createClient(
+    `${networkConfig.networkHost}/chainweb/0.0/${networkConfig.networkId}/chain/${chainId}/pact`,
+  );
+
+export async function kdnResolveNameToAddress(
+  name: string,
+  networkConfig: Record<string, string>,
+  chainId: ChainId,
+): Promise<string | undefined> {
+  try {
+    const transaction = Pact.builder
+      .execution(
+        Pact.modules['n_32faa22a75da53789d48dcbcb124a11c8f8651a8.kadena-names'][
+          'get-address'
+        ](name),
+      )
+      .setMeta({ chainId: chainId })
+      .setNetworkId(networkConfig.networkId)
+      .createTransaction();
+
+    const response: ICommandResult = await client(networkConfig, chainId).local(
+      transaction,
+      {
+        preflight: false,
+        signatureVerification: false,
+      },
+    );
+
+    const data = parseChainResponse<INameToAddressResponse>(
+      response,
+      'address',
+    );
+    if (data.address === null) {
+      return undefined;
+    }
+    return data.address;
+  } catch (error) {
+    console.error('Error in kdnResolveNameToAddress:', error);
+    throw error;
+  }
+}
+
+export async function kdnResolveAddressToName(
+  address: string,
+  networkConfig: Record<string, string>,
+  chainId: ChainId,
+): Promise<string | undefined> {
+  try {
+    const transaction = Pact.builder
+      .execution(
+        Pact.modules['n_32faa22a75da53789d48dcbcb124a11c8f8651a8.kadena-names'][
+          'get-name'
+        ](address),
+      )
+      .setMeta({ chainId: chainId })
+      .setNetworkId(networkConfig.networkId)
+      .createTransaction();
+
+    const response: ICommandResult = await client(networkConfig, chainId).local(
+      transaction,
+      {
+        preflight: false,
+        signatureVerification: false,
+      },
+    );
+
+    const data = parseChainResponse<IAddressToNameResponse>(response, 'name');
+    if (data.name === null) {
+      return undefined;
+    }
+    return data.name;
+  } catch (error) {
+    console.error('Error in kdnResolveAddressToName:', error);
+    throw error;
+  }
+}
+
+function parseChainResponse<T>(response: ICommandResult, subject: string): T {
+  if (response.result?.status === 'success') {
+    return response.result.data as T;
+  } else if (response.result?.status === 'failure') {
+    const errorMessage = `Failed to retrieve ${subject}: ${JSON.stringify(
+      response.result.error,
+    )}`;
+    throw new Error(errorMessage);
+  }
+  throw new Error(`Failed to retrieve ${subject}: Unknown error`);
+}
