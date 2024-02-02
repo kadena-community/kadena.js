4eb5d441b4639458648598ad9ddcc6ad3f7c7436
Sander Looijenga <sander.looijenga@kadena.io>
2024-02-02 17:54:57 +0100

    Fix Tracker -> Finisher routing issue (#1571)
    * Use a Button for styling, but route ourselves
    
    * Remove unused import
    
    ---------
    
    Co-authored-by: Sander Looijenga <slooijenga@anwb.nl>

--- packages/apps/tools/src/pages/transactions/cross-chain-transfer-tracker/index.tsx
+++ packages/apps/tools/src/pages/transactions/cross-chain-transfer-tracker/index.tsx
@@ -26,7 +26,6 @@ import {
   Grid,
   GridItem,
   Heading,
-  Link,
   Notification,
   NotificationButton,
   NotificationFooter,
@@ -321,15 +320,17 @@ const CrossChainTransferTracker: FC = () => {
             alignItems={'flex-start'}
             flexDirection={'row-reverse'}
           >
-            <Link
+            <Button
               title={t('Finish Transaction')}
-              href={`/transactions/cross-chain-transfer-finisher?reqKey=${requestKey}`}
+              onPress={() => {
+                window.location.href = `/transactions/cross-chain-transfer-finisher?reqKey=${requestKey}`;
+              }}
               endIcon={<SystemIcon.Link />}
               color="positive"
               variant="contained"
             >
               {t('Finish Transaction')}
-            </Link>
+            </Button>
           </Stack>
         </div>
       ) : null}
