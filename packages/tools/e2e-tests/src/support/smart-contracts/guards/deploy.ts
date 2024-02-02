d267a7ef23b4854d3bfac125e14a52e027980b5b
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-02-02 22:53:14 +0100

    chore/formatting

--- packages/tools/e2e-tests/src/support/smart-contracts/guards/deploy.ts
+++ packages/tools/e2e-tests/src/support/smart-contracts/guards/deploy.ts
@@ -8,8 +8,7 @@ import {
 export const deployGuards = async (chainId: ChainId) => {
   // Depends on where you're deploying, whether it's a redeploy or not (on Testnet it's not an upgrade, on (a "fresh") DevNet it is)
   const upgrade = false;
-await createUtilNamespace({ chainId: chainId, upgrade });
-    await deployGaurdsContract({ chainId, upgrade, namespace: 'util' });
-    await deployGuards1Contract({ chainId, upgrade, namespace: 'util' });
-
+  await createUtilNamespace({ chainId: chainId, upgrade });
+  await deployGaurdsContract({ chainId, upgrade, namespace: 'util' });
+  await deployGuards1Contract({ chainId, upgrade, namespace: 'util' });
 };
