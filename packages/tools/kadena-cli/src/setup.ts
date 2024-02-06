698a2bc7512496ce83f541c9f2adb4f42f884f93
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-02-06 09:53:13 +0100

    Revert "[@kadena-cli]feat(account): add account manual and from wallet sub commands (#1494)"
    This reverts commit bf09d11616a6b80c36210479885b125340d6c873.

--- packages/tools/kadena-cli/src/setup.ts
+++
@@ -1,6 +0,0 @@
-import { afterAll, afterEach, beforeAll } from 'vitest';
-import { server } from './mocks/server.js';
-
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
