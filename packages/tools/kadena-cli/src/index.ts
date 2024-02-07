37aa1b7ca8612a346411785e7c40e1f29f41b494
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-02-07 17:03:25 +0100

    fix: dynamic import ttys only when used

--- packages/tools/kadena-cli/src/index.ts
+++ packages/tools/kadena-cli/src/index.ts
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import { readFileSync } from 'node:fs';
-import ttys from 'ttys';
 
 import { loadProgram } from './program.js';
 
 async function main(): Promise<void> {
-  let stdin: string = '';
-  try {
-    stdin = readFileSync(0, 'utf8');
-  } catch (e) {
-    /* empty */
+  if (process.stderr.isTTY) {
+    const ttys = await import('ttys');
+    let stdin: string = '';
+    try {
+      stdin = readFileSync(0, 'utf8');
+    } catch (e) {
+      /* empty */
+    }
+
+    process.argv.push(stdin);
+
+    await loadProgram(new Command()).parseAsync();
+
+    // Without this the stdin stream will not close on it's own
+    ttys.stdin.destroy();
+  } else {
+    await loadProgram(new Command()).parseAsync();
   }
-
-  process.argv.push(stdin);
-
-  await loadProgram(new Command()).parseAsync();
-
-  // Without this the stdin stream will not close on it's own
-  ttys.stdin.destroy();
 }
 
 main().catch(console.error);
