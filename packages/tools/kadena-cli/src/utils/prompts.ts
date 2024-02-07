a12c1b127342082ca02219508b893fdf4ee6b7eb
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-02-07 17:12:13 +0100

    fix: dynamic import ttys only when used

--- packages/tools/kadena-cli/src/utils/prompts.ts
+++ packages/tools/kadena-cli/src/utils/prompts.ts
@@ -1,19 +1,43 @@
 import * as inquirer from '@inquirer/prompts';
-import ttys from 'ttys';
+import type ttys from 'ttys';
 
-export const INQUIRER_OPTIONS = {
-  input: ttys.stdin,
-  output: process.stderr,
+interface INQUIREROPTIONS {
+  input: typeof ttys.stdin;
+  output: typeof process.stderr;
+}
+
+let cache: INQUIREROPTIONS;
+
+const getInquirerOptions = async (): Promise<INQUIREROPTIONS> => {
+  if (cache !== undefined) return cache;
+  // eslint-disable-next-line require-atomic-updates
+  cache = {
+    input: (await import('ttys')).stdin,
+    output: process.stderr,
+  };
+  return cache;
 };
 
-export const checkbox: typeof inquirer.checkbox = (config, context) =>
-  inquirer.checkbox(config, { ...context, ...INQUIRER_OPTIONS });
+export const checkbox: typeof inquirer.checkbox = (async (config, context) =>
+  inquirer.checkbox(config, {
+    ...context,
+    ...(await getInquirerOptions()),
+  })) as typeof inquirer.checkbox;
 
-export const input: typeof inquirer.input = (config, context) =>
-  inquirer.input(config, { ...context, ...INQUIRER_OPTIONS });
+export const input: typeof inquirer.input = (async (config, context) =>
+  inquirer.input(config, {
+    ...context,
+    ...(await getInquirerOptions()),
+  })) as typeof inquirer.input;
 
-export const select: typeof inquirer.select = (config, context) =>
-  inquirer.select(config, { ...context, ...INQUIRER_OPTIONS });
+export const select: typeof inquirer.select = (async (config, context) =>
+  inquirer.select(config, {
+    ...context,
+    ...(await getInquirerOptions()),
+  })) as typeof inquirer.select;
 
-export const password: typeof inquirer.password = (config, context) =>
-  inquirer.password(config, { ...context, ...INQUIRER_OPTIONS });
+export const password: typeof inquirer.password = (async (config, context) =>
+  inquirer.password(config, {
+    ...context,
+    ...(await getInquirerOptions()),
+  })) as typeof inquirer.password;
