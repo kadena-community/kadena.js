56ed7f43c7b4e80b5897af67c6fbe6ad066a2adf
Sander Looijenga <slooijenga@anwb.nl>
2024-02-08 15:43:51 +0100

    Use useAsyncCallback in Ledger context

--- packages/apps/tools/src/context/ledger-context.tsx
+++ packages/apps/tools/src/context/ledger-context.tsx
@@ -1,52 +1,56 @@
 import TransportWebHID from '@ledgerhq/hw-transport-webhid';
 import { listen } from '@ledgerhq/logs';
 import type { FC } from 'react';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext } from 'react';
+import { useAsyncCallback } from 'react-async-hook';
 
 import type Transport from '@ledgerhq/hw-transport';
 
 const LedgerContext = createContext<{
   transport: Transport | null;
   connect: (verboseLogging?: boolean) => void;
-}>({ transport: null, connect: () => {} });
+  error?: Error;
+  loading: boolean;
+}>({ transport: null, connect: () => {}, loading: false });
 
 const LedgerContextProvider: FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [transport, setTransport] = useState<Transport | null>(null);
+  const connectLedger = async (verboseLogging = false) => {
+    const transport = await TransportWebHID.create();
 
-  const connect = async (verboseLogging = false) => {
-    try {
-      const transport = await TransportWebHID.create();
-
-      //listen to the events which are sent by the Ledger packages in order to debug the app
-      if (verboseLogging) {
-        listen((log) => console.log('Ledger listen:', log));
-      }
-
-      setTransport(transport);
-
-      transport.on('disconnect', () => {
-        console.log('useLedgerTransport disconnected');
-        setTransport(null);
-      });
-    } catch (error) {
-      console.log(
-        'Something went wrong with the Ledger device connection',
-        error,
-      );
+    // listen to the events which are sent by the Ledger packages in order to debug the app
+    if (verboseLogging) {
+      listen((log) => console.log('Ledger listen:', log));
     }
+
+    transport.on('disconnect', () => {
+      console.log('useLedgerTransport disconnected');
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      reset();
+    });
+
+    return transport;
   };
 
+  const {
+    result,
+    execute: connect,
+    error,
+    loading,
+    reset,
+  } = useAsyncCallback(connectLedger);
+
   return (
-    <LedgerContext.Provider value={{ transport, connect }}>
+    <LedgerContext.Provider
+      value={{ transport: result || null, connect, error, loading }}
+    >
       {children}
     </LedgerContext.Provider>
   );
 };
 
 const useLedgerTransport = () => {
-  console.log('useLedgerTransport');
   const context = useContext(LedgerContext);
 
   if (context === undefined) {
