98c21803123bdf8ea162f052be6cbdc342174807
Steven Straatemans <steven@straatemans.nl>
2024-02-08 12:40:21 +0100

    start

--- packages/apps/proof-of-us/src/pages/api/upload.ts
+++ packages/apps/proof-of-us/src/pages/api/upload.ts
@@ -22,7 +22,7 @@ export default async function handler(
 
   if (!background) {
     return res.status(404).json({
-      message: 'background not found',
+      message: 'background not found!',
     });
   }
 
