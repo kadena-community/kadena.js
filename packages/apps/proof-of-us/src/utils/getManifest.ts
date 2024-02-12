3d5f7b42b61b3ddbf27ba0cb813fd009a783670c
Steven Straatemans <steven@straatemans.nl>
2024-02-12 08:31:36 +0100

    update manifest

--- packages/apps/proof-of-us/src/utils/getManifest.ts
+++ packages/apps/proof-of-us/src/utils/getManifest.ts
@@ -5,7 +5,22 @@ export const getManifest = (proofOfUs: IProofOfUsData, url: string): {} => {
     name: 'My NFT',
     description: 'This is my non-fungible token.',
     image: url,
-    authors: signees.map((signee) => ({ name: signee.displayName })),
+    authors: signees.map((signee) => ({
+      name: signee.label ? signee.label : signee.displayName,
+    })),
+    properties: {
+      avatar: {
+        backgroundColor: proofOfUs.backgroundColor,
+      },
+      signees: signees.map((signee) => ({
+        name: signee.label ? signee.label : signee.displayName,
+        position: {
+          xPercentage: signee.position?.xPercentage,
+          yPercentage: signee.position?.yPercentage,
+        },
+        socialLinks: signee.socialLinks?.map((link) => link),
+      })),
+    },
     collection: {
       name: 'Proof Of Us',
       family: 'Art',
