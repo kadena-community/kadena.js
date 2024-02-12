71cd2ef29bd7fc762544c4117d4a7f131e8b1525
Steven Straatemans <steven@straatemans.nl>
2024-02-12 09:11:15 +0100

    small stylinh the attendance card

--- packages/apps/proof-of-us/src/components/AttendanceTicket/AttendanceTicket.tsx
+++ packages/apps/proof-of-us/src/components/AttendanceTicket/AttendanceTicket.tsx
@@ -8,20 +8,33 @@ interface IProps {
 export const AttendanceTicket: FC<IProps> = ({ token }) => {
   const date = new Date(token.properties.date);
   return (
-    <section
-      className={ticketClass}
-      style={{
-        backgroundImage: `url("${token.image}")`,
-        backgroundColor: token.properties.avatar?.backgroundColor,
-        color: token.properties.avatar?.color,
-      }}
-    >
-      <h4>{token.name}</h4>
+    <>
+      <svg viewBox="0 0 160 90" width="1">
+        <defs>
+          <clipPath id="path" clipPathUnits="objectBoundingBox">
+            <path
+              style={{ fill: 'rgb(0,7,255)' }}
+              transform="scale(0.0063, 0.011)"
+              d="M159.764,30.921c-0.029,0 -0.059,0 -0.088,0c-7.771,0 -14.079,6.309 -14.079,14.079c-0,7.77 6.308,14.079 14.079,14.079c0.029,-0 0.059,-0 0.088,-0l0,31.008l-159.642,0l-0,-89.817l159.642,0l0,30.651Z"
+            ></path>
+          </clipPath>
+        </defs>
+      </svg>
+      <section
+        className={ticketClass}
+        style={{
+          backgroundImage: `url("${token.image}")`,
+          backgroundColor: token.properties.avatar?.backgroundColor,
+          color: token.properties.avatar?.color,
+        }}
+      >
+        <h4>{token.name}</h4>
 
-      <div>
-        <h5>Date</h5>
-        {date.toLocaleDateString()}
-      </div>
-    </section>
+        <div>
+          <h5>Date</h5>
+          {date.toLocaleDateString()}
+        </div>
+      </section>
+    </>
   );
 };
--- packages/apps/proof-of-us/src/components/AttendanceTicket/style.css.ts
+++ packages/apps/proof-of-us/src/components/AttendanceTicket/style.css.ts
@@ -1,12 +1,19 @@
+import { atoms } from '@kadena/react-ui/styles';
 import { style } from '@vanilla-extract/css';
 
-export const ticketClass = style({
-  position: 'relative',
-  width: '100vw',
-  maxWidth: '800px',
-  aspectRatio: '16/9',
-  background: 'lightgrey',
-  backgroundRepeat: 'none',
-  backgroundSize: 'cover',
-  borderRadius: '8px',
-});
+export const ticketClass = style([
+  atoms({
+    borderRadius: 'md',
+  }),
+  {
+    position: 'relative',
+    width: '100vw',
+    maxWidth: '800px',
+    aspectRatio: '16/9',
+    background: 'lightgrey',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    clipPath: 'url(#path)',
+  },
+]);
