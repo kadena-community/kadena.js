b64d87e826085887a56bd8a56a7e4bd84fb33e7d
kristina.spasevska <kristina.spasevska@deptagency.com>
2024-02-15 11:49:35 +0100

    feat: refactor tx details

--- packages/apps/tools/src/pages/transactions/transfer/transaction-details.tsx
+++ packages/apps/tools/src/pages/transactions/transfer/transaction-details.tsx
@@ -1,5 +1,5 @@
-// import { AccountHoverTag } from '@/components/Global';
 // import { useLedgerPublicKey } from '@/hooks/use-ledger-public-key';
+import type { IExecutionPayloadObject } from '@kadena/client';
 import {
   Card,
   Stack,
@@ -8,64 +8,22 @@ import {
   ToggleButton,
   TrackerCard,
 } from '@kadena/react-ui';
+import type { ICap } from '@kadena/types';
 import React, { useState } from 'react';
 
-const TransactionDetails = (transaction: any) => {
-  const tx = {
-    payload: {
-      exec: {
-        code: '(n_d8cbb935f9cd9d2399a5886bb08caed71f9bad49.coin-faucet.create-and-request-coin "k:18c12f868e4cb825e2cad57e44aa588f8a1d2f392a2a945b50c51634a5429045" (read-keyset "new_keyset") 100.0)',
-        data: {
-          new_keyset: {
-            keys: [
-              '18c12f868e4cb825e2cad57e44aa588f8a1d2f392a2a945b50c51634a5429045',
-            ],
-            pred: 'keys-all',
-          },
-        },
-      },
-    },
-    nonce: 'kjs:nonce:1707567506729',
-    signers: [
-      {
-        pubKey:
-          '332cffee708e0e2b390feb30f81c61d573ff0d8db18f10a218ae9dd34c1cbdc7',
-        scheme: 'ED25519',
-        clist: [
-          {
-            name: 'n_d8cbb935f9cd9d2399a5886bb08caed71f9bad49.coin-faucet.GAS_PAYER',
-            args: [
-              'k:18c12f868e4cb825e2cad57e44aa588f8a1d2f392a2a945b50c51634a5429045',
-              { int: 1 },
-              { decimal: '1.0' },
-            ],
-          },
-          {
-            name: 'coin.TRANSFER',
-            args: [
-              'c:Ecwy85aCW3eogZUnIQxknH8tG8uXHM5QiC__jeI0nWA',
-              'k:18c12f868e4cb825e2cad57e44aa588f8a1d2f392a2a945b50c51634a5429045',
-              { decimal: '100.0' },
-            ],
-          },
-        ],
-      },
-    ],
-    meta: {
-      gasLimit: 2500,
-      gasPrice: 1e-8,
-      sender: 'c:Ecwy85aCW3eogZUnIQxknH8tG8uXHM5QiC__jeI0nWA',
-      ttl: 28800,
-      creationTime: 1707567506,
-      chainId: '0',
-    },
-    networkId: 'testnet04',
-  };
-
+export interface ITransactionDetails {
+  transaction: any;
+}
+const TransactionDetails = ({
+  transaction,
+}: ITransactionDetails): React.JSX.Element | null => {
   // const [keyId, setKeyId] = useState<number>();
   // const publicKey = useLedgerPublicKey(keyId);
   const [txDetailsExpanded, setTxDetailsExpanded] = useState<boolean>(false);
 
+  if (!transaction) return null;
+
+  // @ts-ignore
   return (
     <Card fullWidth={true}>
       <Stack flexDirection={'column'} gap={'sm'}>
@@ -88,7 +46,20 @@ const TransactionDetails = (transaction: any) => {
             labelValues={[
               {
                 label: 'Raw',
-                value: JSON.stringify(tx),
+                value: JSON.stringify(transaction),
+              },
+            ]}
+          />
+        ) : null}
+
+        {transaction.payload ? (
+          <TrackerCard
+            variant="vertical"
+            labelValues={[
+              {
+                label: 'Code',
+                value: (transaction.payload as IExecutionPayloadObject).exec
+                  .code,
               },
             ]}
           />
@@ -98,46 +69,40 @@ const TransactionDetails = (transaction: any) => {
           variant="vertical"
           labelValues={[
             {
-              label: 'Code',
-              value: tx.payload.exec.code,
-            },
-          ]}
-        />
-
-        <TrackerCard
-          variant="vertical"
-          labelValues={[
-            {
               label: 'Network',
-              value: tx.networkId,
+              value: transaction.networkId,
             },
           ]}
         />
         <Text>Capabilities</Text>
 
-        {tx.signers.map((signer) =>
-          signer.clist.map((cap) => (
-            <div key={cap.name}>
-              <TrackerCard
-                variant="horizontal"
-                labelValues={[
-                  {
-                    label: 'Name',
-                    value: cap.name,
-                  },
-                  {
-                    label: 'Arguments',
-                    value: cap.args.map((item) => item.toString()).join(','),
-                  },
-                  {
-                    label: 'Signer',
-                    value: signer.pubKey,
-                  },
-                ]}
-              />
-            </div>
-          )),
-        )}
+        {transaction.signers
+          ? transaction.signers.map((signer: any) =>
+              signer.clist.map((cap: ICap) => (
+                <div key={cap.name}>
+                  <TrackerCard
+                    variant="horizontal"
+                    labelValues={[
+                      {
+                        label: 'Name',
+                        value: cap.name,
+                      },
+                      {
+                        label: 'Arguments',
+                        value: cap.args
+                          .map((item) => item.toString())
+                          .join(','),
+                      },
+                      {
+                        label: 'Signer',
+                        value: signer.pubKey,
+                      },
+                    ]}
+                  />
+                </div>
+              )),
+            )
+          : null}
       </Stack>
     </Card>
   );
