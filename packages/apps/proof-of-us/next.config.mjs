af6ce2dc8eed7df7e277226057ac63f8fffd3670
Albert G <516972+alber70g@users.noreply.github.com>
2024-02-15 12:16:24 +0100

    chore(pou): add graph integration

--- packages/apps/proof-of-us/next.config.mjs
+++ packages/apps/proof-of-us/next.config.mjs
@@ -14,6 +14,14 @@ const nextConfig = {
     // config.infrastructureLogging = { debug: /PackFileCache/ };
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/graph',
+        destination: 'https://graph.kadena.network/graphql'
+      },
+    ];
+  },
 };
 
 export default withVanillaExtract(nextConfig);
