a5d18789aaff97ba2abb5f3a94eb46f2eaca631a
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-02-15 16:24:48 +0100

    try catch token processing

--- packages/apps/graph/src/graph/objects/token.ts
+++ packages/apps/graph/src/graph/objects/token.ts
@@ -1,5 +1,6 @@
 import { getTokenInfo } from '@devnet/simulation/marmalade/get-token-info';
 import type { ChainId } from '@kadena/types';
+import { normalizeError } from '@utils/errors';
 import { builder } from '../builder';
 
 export default builder.objectType('Token', {
@@ -11,17 +12,21 @@ export default builder.objectType('Token', {
     info: t.field({
       type: 'TokenInfo',
       nullable: true,
-      resolve: async (parent) => {
-        const tokenInfo = await getTokenInfo(
-          parent.id,
-          parent.chainId.toString() as ChainId,
-        );
+      async resolve(parent) {
+        try {
+          const tokenInfo = await getTokenInfo(
+            parent.id,
+            parent.chainId.toString() as ChainId,
+          );
 
-        if (!tokenInfo) {
-          return null;
+          if (!tokenInfo) {
+            return null;
+          }
+
+          return tokenInfo;
+        } catch (error) {
+          throw normalizeError(error);
         }
-
-        return tokenInfo;
       },
     }),
   }),
