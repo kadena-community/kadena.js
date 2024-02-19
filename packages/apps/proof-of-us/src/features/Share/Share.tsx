b9e530f18e1588fc2ccf9207a80e69ba923ef285
Steven Straatemans <steven@straatemans.nl>
2024-02-19 11:38:20 +0100

    make link to block explorer

--- packages/apps/proof-of-us/src/features/Share/Share.tsx
+++ packages/apps/proof-of-us/src/features/Share/Share.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { AttendanceTicket } from '@/components/AttendanceTicket/AttendanceTicket';
 import { SocialShare } from '@/components/SocialShare/SocialShare';
+import { env } from '@/utils/env';
 
 import type { FC } from 'react';
 
@@ -19,6 +20,15 @@ export const Share: FC<IProps> = ({ token, proofOfUs }) => {
         </>
       )}
 
+      <div>
+        find your token on the chain:{' '}
+        <a
+          href={`https://explorer.chainweb.com/${env.NETWORKNAME}/eventsearch?q=${token?.['token-id']}`}
+          target="_blank"
+        >
+          click here
+        </a>
+      </div>
       {proofOfUs && <pre>{JSON.stringify(proofOfUs, null, 2)}</pre>}
       {token && <pre>{JSON.stringify(token, null, 2)}</pre>}
     </div>
