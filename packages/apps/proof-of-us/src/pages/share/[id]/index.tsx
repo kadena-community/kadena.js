9f533e8fbf533ec1773e09877a370530b76c9ce1
Steven Straatemans <steven@straatemans.nl>
2024-02-19 10:22:20 +0100

    social share

--- packages/apps/proof-of-us/src/pages/share/[id]/index.tsx
+++ packages/apps/proof-of-us/src/pages/share/[id]/index.tsx
@@ -3,6 +3,7 @@ import { Share } from '@/features/Share/Share';
 import { fetchManifestData } from '@/utils/fetchManifestData';
 import { getProofOfUs } from '@/utils/proofOfUs';
 import type { GetServerSidePropsContext, NextPage } from 'next';
+import { Head } from 'next/document';
 interface IProps {
   params: {
     id: string;
@@ -14,6 +15,25 @@ interface IProps {
 const Page: NextPage<IProps> = ({ params, token, proofOfUs }) => {
   return (
     <>
+      <Head>
+        <meta
+          name="twitter:title"
+          content={`${proofOfUs?.name} | Proof Of Us (Powered by Kadena)`}
+        />
+         <meta name="twitter:description" content={proofOfUs?.description} />
+         <meta property="og:title" content={`${proofOfUs?.name} | Proof Of Us (Powered by Kadena)`} />
+          <meta property="og:description" content={proofOfUs?.description}  />
+        <meta
+          name="twitter:url"
+          content={`${process.env.NEXT_PUBLIC_URL}/share/${params.id}`}
+        />
+        <meta
+          property="og:url"
+          content={`${process.env.NEXT_PUBLIC_URL}/share/${params.id}`}
+        />
+        <meta name="twitter:image" content={proofOfUs?.image} />
+        <meta property="og:image" content={proofOfUs?.image} />
+      </Head>
       <ProofOfUsProvider>
         <Share eventId={params.id} token={token} proofOfUs={proofOfUs} />;
       </ProofOfUsProvider>
