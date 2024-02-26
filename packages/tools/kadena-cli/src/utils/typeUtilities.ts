cb5b9d760e7efe26abf7046dcb89b61984e9c95c
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-02-26 17:31:08 +0100

    chore(kadena-cli): remove unused utility types

--- packages/tools/kadena-cli/src/utils/typeUtilities.ts
+++ packages/tools/kadena-cli/src/utils/typeUtilities.ts
@@ -1,30 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type First<T extends any[]> = T extends [infer One]
-  ? One
-  : // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  T extends [infer HD, ...any[]]
-  ? HD
-  : never;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
-export type Tail<T extends any[]> = T extends [infer _]
-  ? []
-  : // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  T extends [infer _, ...infer TL]
-  ? TL
-  : never;
-
-/** @deprecated use Awaited instead */
-export type Pure<T> = T extends Promise<infer R> ? R : T;
-
 export type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
-
-export type Combine2<A, B> = {
-  [K in keyof (A | B)]: A[K] | B[K];
-} & Omit<A, keyof B> &
-  Omit<B, keyof A>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Fn = (...args: any[]) => unknown;
