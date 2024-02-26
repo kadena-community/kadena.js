729c0928699df5155c391588785add68a937b95a
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-02-26 16:04:51 +0100

    change gasPrice to float

--- packages/apps/graph/src/graph/objects/meta.ts
+++ packages/apps/graph/src/graph/objects/meta.ts
@@ -6,7 +6,7 @@ builder.objectType('Meta', {
     sender: t.exposeString('sender'),
     chainId: t.exposeString('chainId'),
     gasLimit: t.expose('gasLimit', { type: 'BigInt' }),
-    gasPrice: t.exposeInt('gasPrice'),
+    gasPrice: t.expose('gasPrice', { type: 'Float' }),
     ttl: t.expose('ttl', { type: 'BigInt' }),
     creationTime: t.expose('creationTime', { type: 'DateTime' }),
   }),
