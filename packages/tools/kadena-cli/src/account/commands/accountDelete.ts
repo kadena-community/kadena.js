9bb500b79a344eccef725aed67079f0f93819764
Mohamed Ismail <ismailreality1@gmail.com>
2024-02-27 16:00:39 +0100

    feat(account): delete directory for account delete all

--- packages/tools/kadena-cli/src/account/commands/accountDelete.ts
+++ packages/tools/kadena-cli/src/account/commands/accountDelete.ts
@@ -1,4 +1,4 @@
-import { join, parse } from 'path';
+import { join } from 'path';
 import { ACCOUNT_DIR } from '../../constants/config.js';
 import { services } from '../../services/index.js';
 import type { CommandResult } from '../../utils/command.util.js';
@@ -8,42 +8,41 @@ import { log } from '../../utils/logger.js';
 import { accountOptions } from '../accountOptions.js';
 import { isEmpty } from '../utils/addHelpers.js';
 
-async function getAllAccountAliases(): Promise<string[]> {
-  const files = await services.filesystem.readDir(ACCOUNT_DIR);
-  return files.map((file) => parse(file).name);
+async function deleteAccountDir(): Promise<CommandResult<null>> {
+  try {
+    await services.filesystem.deleteDirectory(ACCOUNT_DIR);
+    return {
+      data: null,
+      success: true,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      errors: ['Failed to delete all keys'],
+    };
+  }
 }
 
 async function removeAccount(
   accountAlias: string,
-): Promise<CommandResult<string[]>> {
-  const deletedFiles = [];
-  const nonDeletedFiles = [];
-  const aliases =
-    accountAlias === 'all'
-      ? await getAllAccountAliases()
-      : [accountAlias.trim()];
-  for (const alias of aliases) {
-    const filePath = join(ACCOUNT_DIR, `${alias}.yaml`);
-    if (await services.filesystem.fileExists(filePath)) {
-      await services.filesystem.deleteFile(filePath);
-      deletedFiles.push(alias);
-    } else {
-      nonDeletedFiles.push(alias);
-    }
+): Promise<CommandResult<null>> {
+  if (accountAlias === 'all') {
+    return await deleteAccountDir();
   }
 
-  const warnings =
-    nonDeletedFiles.length === 1
-      ? `\nThe account alias "${nonDeletedFiles[0]}" does not exist`
-      : `\nThe following account aliases do not exist:\n${nonDeletedFiles.join(
-          '\n',
-        )}`;
-
-  return {
-    data: deletedFiles,
-    success: true,
-    warnings: nonDeletedFiles.length > 0 ? [warnings] : undefined,
-  };
+  const filePath = join(ACCOUNT_DIR, `${accountAlias}.yaml`);
+  if (await services.filesystem.fileExists(filePath)) {
+    await services.filesystem.deleteFile(filePath);
+    return {
+      data: null,
+      success: true,
+    };
+  } else {
+    return {
+      success: false,
+      errors: [`The account alias "${accountAlias}" does not exist`],
+    };
+  }
 }
 
 export const createAccountDeleteCommand = createCommand(
@@ -77,12 +76,10 @@ export const createAccountDeleteCommand = createCommand(
 
     const result = await removeAccount(accountAlias);
     assertCommandError(result);
-    if (result.data.length > 0) {
-      const deleteSuccessMsg =
-        accountAlias === 'all'
-          ? 'All account aliases has been deleted'
-          : `The selected account alias "${accountAlias}" has been deleted`;
-      log.info(log.color.green(`\n${deleteSuccessMsg}`));
-    }
+    const deleteSuccessMsg =
+      accountAlias === 'all'
+        ? 'All account aliases has been deleted'
+        : `The selected account alias "${accountAlias}" has been deleted`;
+    log.info(log.color.green(`\n${deleteSuccessMsg}`));
   },
 );
