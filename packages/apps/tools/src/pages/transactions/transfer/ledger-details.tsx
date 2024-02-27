d0f98ec9525c8763524a364d7578b031dc85ad46
kristina.spasevska <kristina.spasevska@deptagency.com>
2024-02-27 12:01:46 +0100

    feat: add info with tooltip about keys

--- packages/apps/tools/src/pages/transactions/transfer/ledger-details.tsx
+++ packages/apps/tools/src/pages/transactions/transfer/ledger-details.tsx
@@ -2,9 +2,21 @@ import { AccountHoverTag } from '@/components/Global';
 // import { useLedgerPublicKey } from '@/hooks/use-ledger-public-key';
 import { Toggle } from '@/components/Global/Toggle';
 import { derivationModes } from '@/hooks/use-ledger-public-key';
-import { Combobox, ComboboxItem, Stack, SystemIcon } from '@kadena/react-ui';
+import {
+  marginBottomOnError,
+  tooltipInfoContainer,
+} from '@/pages/transactions/transfer/styles.css';
+import { SystemIcons } from '@kadena/react-components';
+import {
+  Combobox,
+  ComboboxItem,
+  Stack,
+  SystemIcon,
+  Tooltip,
+} from '@kadena/react-ui';
+import classNames from 'classnames';
 import useTranslation from 'next-translate/useTranslation';
-import React from 'react';
+import React, { useState } from 'react';
 
 const options = Array.from({ length: 10 }, (_, i) => ({
   id: `ledger-key-${i}`,
@@ -31,6 +43,7 @@ const LedgerDetails = ({
     ? derivationModes[1]
     : derivationModes[0];
 
+  const [errorLedgerKey, setErrorLedgerKey] = useState<boolean>(false);
   const setLegacyOn = () => {
     setLegacyToggleOn(!legacyToggleOn);
   };
@@ -38,22 +51,39 @@ const LedgerDetails = ({
   return (
     <>
       <Stack flexDirection={'row'} justifyContent={'space-between'}>
-        <Combobox
-          allowsCustomValue
-          startIcon={<SystemIcon.KeyIconFilled />}
-          label="Key ID"
-          onInputChange={async (value) => {
-            console.log('onInputChange', value);
-            await getPublicKey({
-              keyId: parseInt(value, 10),
-              derivationMode,
-            });
-            setKeyId(value);
-          }}
-          defaultItems={options}
-        >
-          {(item) => <ComboboxItem key={item.id}>{item.name}</ComboboxItem>}
-        </Combobox>
+        <Stack alignItems={'flex-end'} gap={'md'}>
+          <Combobox
+            allowsCustomValue
+            startIcon={<SystemIcon.KeyIconFilled />}
+            label="Key Index"
+            onInputChange={async (value) => {
+              await getPublicKey({
+                keyId: parseInt(value, 10),
+                derivationMode,
+              });
+
+              if (isNaN(+value)) return setErrorLedgerKey(true);
+              setKeyId(value);
+              setErrorLedgerKey(false);
+            }}
+            defaultItems={options}
+            isInvalid={errorLedgerKey}
+            errorMessage={errorLedgerKey ? 'Enter number from 1 to 99' : ''}
+          >
+            {(item) => <ComboboxItem key={item.id}>{item.name}</ComboboxItem>}
+          </Combobox>
+          <div
+            className={classNames(
+              tooltipInfoContainer,
+              errorLedgerKey ? marginBottomOnError : null,
+            )}
+          >
+            <Tooltip content={t('ledger tooltip content')} position={'top'}>
+              <SystemIcons.Information />
+            </Tooltip>
+          </div>
+        </Stack>
+
         <Toggle
           label={t('is Legacy')}
           toggled={legacyToggleOn}
