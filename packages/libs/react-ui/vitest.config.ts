eab06e08a424ee1c88a664985519915c26609c6c
code-capi <info@mulderr.com>
2024-02-28 12:53:39 +0100

    fix: react in scope for tests and set correct jsx config for ts

--- packages/libs/react-ui/vitest.config.ts
+++ packages/libs/react-ui/vitest.config.ts
@@ -1,9 +1,13 @@
 import baseConfig from '@kadena-dev/shared-config/vitest.config';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig, mergeConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react'
 
 const localConfig = defineConfig({
-  plugins: [vanillaExtractPlugin({ emitCssInSsr: true })],
+  plugins: [vanillaExtractPlugin({ emitCssInSsr: true }), react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   test: {
     setupFiles: ['vitest.setup.ts'],
     environment: 'happy-dom',
