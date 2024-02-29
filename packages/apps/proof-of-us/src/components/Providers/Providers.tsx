3a718cf2c1f59ea06435f487233d0e525f1fb3b8
Steven Straatemans <steven@straatemans.nl>
2024-02-29 11:34:23 +0100

    remove graphql

--- packages/apps/proof-of-us/src/components/Providers/Providers.tsx
+++ packages/apps/proof-of-us/src/components/Providers/Providers.tsx
@@ -1,7 +1,5 @@
 'use client';
 import { localStorageProvider } from '@/utils/localStorageProvider';
-import type { NormalizedCacheObject } from '@apollo/client';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import { ThemeProvider } from 'next-themes';
 import type { FC, PropsWithChildren } from 'react';
@@ -13,29 +11,19 @@ import { TokenProvider } from '../TokenProvider/TokenProvider';
 
 // next/apollo-link bug: https://github.com/dotansimha/graphql-yoga/issues/2194
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { YogaLink } = require('@graphql-yoga/apollo-link');
-
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: new YogaLink({
-    endpoint: '/graph',
-  }),
-  cache: new InMemoryCache(),
-});
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
   return (
     <SWRConfig value={{ provider: localStorageProvider }}>
-      <ApolloProvider client={client}>
-        <ToastProvider>
-          <AccountProvider>
-            <ThemeProvider>
-              <TokenProvider>
-                <ProofOfUsProvider>{children}</ProofOfUsProvider>
-              </TokenProvider>
-            </ThemeProvider>
-          </AccountProvider>
-        </ToastProvider>
-      </ApolloProvider>
+      <ToastProvider>
+        <AccountProvider>
+          <ThemeProvider>
+            <TokenProvider>
+              <ProofOfUsProvider>{children}</ProofOfUsProvider>
+            </TokenProvider>
+          </ThemeProvider>
+        </AccountProvider>
+      </ToastProvider>
     </SWRConfig>
   );
 };
