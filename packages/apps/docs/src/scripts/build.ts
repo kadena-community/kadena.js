fdfef6e0c02c92404706d77a9c20098a661e9693
Steven Straatemans <steven@straatemans.nl>
2024-03-04 17:06:14 +0100

    fix the build

--- packages/apps/docs/src/scripts/build.ts
+++ packages/apps/docs/src/scripts/build.ts
@@ -17,7 +17,7 @@ const runPrettier = async (): Promise<IScriptResult> => {
   const errors: string[] = [];
 
   const { stderr } = await promiseExec(
-    `prettier ./public/sitemap.xml --write && prettier ./src/pages --write  && prettier ./src/_generated/**/*.json --write`,
+    `prettier ./src/pages --write  && prettier ./src/_generated/**/*.json --write`,
   );
 
   if (stderr) {
