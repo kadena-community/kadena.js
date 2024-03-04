4661d09806433d811ddd807cb55a373005f61955
Javad Khalilian <khalilian.javad@gmail.com>
2024-03-04 10:53:46 +0100

    chore: build file

--- packages/libs/toolbox/build.config.ts
+++ packages/libs/toolbox/build.config.ts
@@ -1,4 +1,24 @@
-import { defineBuildConfig } from 'unbuild';
+import { readdirSync } from 'fs';
+import { BuildEntry, defineBuildConfig } from 'unbuild';
+
+const entries: BuildEntry[] = readdirSync('./src').flatMap((entry) => {
+  return [
+    {
+      builder: 'mkdist',
+      input: `./src/${entry}`,
+      outDir: `./dist/${entry}/esm`,
+      cleanDist: true,
+      format: 'esm',
+    },
+    {
+      builder: 'mkdist',
+      input: `./src/${entry}`,
+      outDir: `./dist/${entry}/cjs`,
+      cleanDist: true,
+      format: 'cjs',
+    },
+  ];
+});
 
 export default defineBuildConfig({
   entries: [
