3adcf869b3cf67c4390a764b7397566a998adee7
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-03-05 13:05:01 +0100

    chore(kadena-cli): ask pw again for import and fix output for sign

--- packages/tools/kadena-cli/src/tx/utils/txSignWithAliasFile.ts
+++ packages/tools/kadena-cli/src/tx/utils/txSignWithAliasFile.ts
@@ -172,7 +172,7 @@ export async function signWithAliasFile(
   assertCommandError(result);
 
   if (result.data.commands.length === 1) {
-    log.output(JSON.stringify(result.data.commands[0], null, 2));
+    log.output(JSON.stringify(result.data.commands[0].command, null, 2));
   }
   result.data.commands.forEach((tx) => {
     log.info(`Signed transaction saved to ${tx.path}`);
