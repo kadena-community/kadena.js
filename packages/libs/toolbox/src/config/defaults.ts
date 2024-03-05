8f8dbc49d7af8eb52978ae9d058641022db8c082
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-05 21:51:29 +0100

    chore: format

--- packages/libs/toolbox/src/config/defaults.ts
+++ packages/libs/toolbox/src/config/defaults.ts
@@ -1,5 +1,5 @@
-import { defu } from "defu";
-import { join } from "node:path";
+import { defu } from 'defu';
+import { join } from 'node:path';
 import type {
   ChainwebMiningClientConfig,
   ChainwebNetworkConfig,
@@ -11,79 +11,79 @@ import type {
   PactServerNetworkConfig,
   PactToolboxConfigObj,
   Signer,
-} from "./config";
-import { createChainwebRpcUrl } from "./utils";
+} from './config';
+import { createChainwebRpcUrl } from './utils';
 
 export const defaultSigners: Signer[] = [
   {
-    account: "sender00",
+    account: 'sender00',
     publicKey:
-      "368820f80c324bbc7c2b0610688a7da43e39f91d118732671cd9c7500ff43cca",
+      '368820f80c324bbc7c2b0610688a7da43e39f91d118732671cd9c7500ff43cca',
     secretKey:
-      "251a920c403ae8c8f65f59142316af3c82b631fba46ddea92ee8c95035bd2898",
+      '251a920c403ae8c8f65f59142316af3c82b631fba46ddea92ee8c95035bd2898',
   },
   {
-    account: "sender01",
+    account: 'sender01',
     publicKey:
-      "6be2f485a7af75fedb4b7f153a903f7e6000ca4aa501179c91a2450b777bd2a7",
+      '6be2f485a7af75fedb4b7f153a903f7e6000ca4aa501179c91a2450b777bd2a7',
     secretKey:
-      "2beae45b29e850e6b1882ae245b0bab7d0689ebdd0cd777d4314d24d7024b4f7",
+      '2beae45b29e850e6b1882ae245b0bab7d0689ebdd0cd777d4314d24d7024b4f7',
   },
   {
-    account: "sender02",
+    account: 'sender02',
     publicKey:
-      "3a9dd532d73dace195dbb64d1dba6572fb783d0fdd324685e32fbda2f89f99a6",
+      '3a9dd532d73dace195dbb64d1dba6572fb783d0fdd324685e32fbda2f89f99a6',
     secretKey:
-      "9b54e924f7acdb03ad4e471308f9a512dac26a50398b41cab8bfe7a496804dbd",
+      '9b54e924f7acdb03ad4e471308f9a512dac26a50398b41cab8bfe7a496804dbd',
   },
   {
-    account: "sender03",
+    account: 'sender03',
     publicKey:
-      "43f2adb1de192000cb3777bacc7f983b6614fd9c1715cd44cd484b6d3a0d34c8",
+      '43f2adb1de192000cb3777bacc7f983b6614fd9c1715cd44cd484b6d3a0d34c8',
     secretKey:
-      "a477c755abcaaac7bc9aadfc616c0cb7a8a9df9e15adc168e39c247c648544ac",
+      'a477c755abcaaac7bc9aadfc616c0cb7a8a9df9e15adc168e39c247c648544ac',
   },
   {
-    account: "sender04",
+    account: 'sender04',
     publicKey:
-      "2d70aa4f697c3a3b8dd6d97745ac074edcfd0eb65c37774cde25135483bea71e",
+      '2d70aa4f697c3a3b8dd6d97745ac074edcfd0eb65c37774cde25135483bea71e',
     secretKey:
-      "1685ef1cb774a517fdfd5c08cf1ae898084d8ad39b04717a4b5b27c2a3190a75",
+      '1685ef1cb774a517fdfd5c08cf1ae898084d8ad39b04717a4b5b27c2a3190a75',
   },
   {
-    account: "sender05",
+    account: 'sender05',
     publicKey:
-      "f09d8f6394aea425fe6783d88cd81363d8017f16afd3711c575be0f5cd5c9bb9",
+      'f09d8f6394aea425fe6783d88cd81363d8017f16afd3711c575be0f5cd5c9bb9',
     secretKey:
-      "2505b87dca474955597fed38178df9e66b16b0c8dc140817c0ad75bd14be32d4",
+      '2505b87dca474955597fed38178df9e66b16b0c8dc140817c0ad75bd14be32d4',
   },
   {
-    account: "sender06",
+    account: 'sender06',
     publicKey:
-      "5ffc1f7fef7a44738625762f75a4229454951e03f2afc6f81309c0c1bdf9ee6f",
+      '5ffc1f7fef7a44738625762f75a4229454951e03f2afc6f81309c0c1bdf9ee6f',
     secretKey:
-      "c2873d7226845228a6422d15599aa9371a6cd105d29f6157c07d4e9f5b5d7a3c",
+      'c2873d7226845228a6422d15599aa9371a6cd105d29f6157c07d4e9f5b5d7a3c',
   },
   {
-    account: "sender07",
+    account: 'sender07',
     publicKey:
-      "4c31dc9ee7f24177f78b6f518012a208326e2af1f37bb0a2405b5056d0cad628",
+      '4c31dc9ee7f24177f78b6f518012a208326e2af1f37bb0a2405b5056d0cad628',
     secretKey:
-      "f1c1923e49cb23d15fe45bdc3f65d7fc1d031ce50dd81bb5085bdd2c63364d7f",
+      'f1c1923e49cb23d15fe45bdc3f65d7fc1d031ce50dd81bb5085bdd2c63364d7f',
   },
   {
-    account: "sender08",
+    account: 'sender08',
     publicKey:
-      "63b2eba4ed70d4612d3e7bc90db2fbf4c76f7b074363e86d73f0bc617f8e8b81",
+      '63b2eba4ed70d4612d3e7bc90db2fbf4c76f7b074363e86d73f0bc617f8e8b81',
     secretKey:
-      "e0018d4e39736c9c22400cdd88ef60f0ca203466bb27b8435a72e0868289690d",
+      'e0018d4e39736c9c22400cdd88ef60f0ca203466bb27b8435a72e0868289690d',
   },
   {
-    account: "sender09",
+    account: 'sender09',
     publicKey:
-      "c59d9840b0b66090836546b7eb4a73606257527ec8c2b482300fd229264b07e6",
+      'c59d9840b0b66090836546b7eb4a73606257527ec8c2b482300fd229264b07e6',
     secretKey:
-      "adbe3793a0daf70c7e7a5d59349e0f51d928178de55c6328302ef5b628ed448b",
+      'adbe3793a0daf70c7e7a5d59349e0f51d928178de55c6328302ef5b628ed448b',
   },
 ];
 
@@ -93,7 +93,7 @@ export const defaultKeysets: Record<string, KeysetConfig> =
       ...acc,
       [signer.account]: {
         keys: [signer.publicKey],
-        pred: "keys-all",
+        pred: 'keys-all',
       },
     }),
     {},
@@ -110,11 +110,11 @@ export function createPactServerConfig(
 ): Required<PactServerConfig> {
   const defaults = {
     port: 9091,
-    logDir: ".pact-toolbox/logs",
-    persistDir: ".pact-toolbox/persist",
+    logDir: '.pact-toolbox/logs',
+    persistDir: '.pact-toolbox/persist',
     verbose: true,
     pragmas: [],
-    execConfig: ["DisablePact44", "AllowReadInLocal"],
+    execConfig: ['DisablePact44', 'AllowReadInLocal'],
   };
   return {
     ...defaults,
@@ -124,23 +124,23 @@ export function createPactServerConfig(
 
 export const defaultDevNetContainer = {
   port: 9090,
-  image: "kadena/devnet",
-  tag: "latest",
+  image: 'kadena/devnet',
+  tag: 'latest',
   // volume: 'kadena_devnet',
-  name: "devnet",
+  name: 'devnet',
 };
 
 export function createLocalNetworkConfig(
   overrides?: Partial<PactServerNetworkConfig>,
 ): PactServerNetworkConfig {
   const defaults = {
-    type: "pact-server",
-    rpcUrl: "http://localhost:{port}",
-    networkId: "local",
+    type: 'pact-server',
+    rpcUrl: 'http://localhost:{port}',
+    networkId: 'local',
     signers: defaultSigners,
     keysets: defaultKeysets,
-    chainId: "0",
-    senderAccount: "sender00",
+    chainId: '0',
+    senderAccount: 'sender00',
     autoStart: true,
     proxyPort: 8080,
     serverConfig: createPactServerConfig(),
@@ -163,13 +163,13 @@ export function createDevNetNetworkConfig(
   }
 
   const defaults = {
-    type: "chainweb-devnet",
+    type: 'chainweb-devnet',
     rpcUrl: createChainwebRpcUrl(),
-    networkId: "development",
-    chainId: "0",
+    networkId: 'development',
+    chainId: '0',
     signers: defaultSigners,
     keysets: defaultKeysets,
-    senderAccount: "sender00",
+    senderAccount: 'sender00',
     autoStart: true,
     onDemandMining: false,
     containerConfig: defaultDevNetContainer,
@@ -179,32 +179,32 @@ export function createDevNetNetworkConfig(
   return defu(overrides ?? {}, defaults) as DevNetworkConfig;
 }
 
-export const chainwebConfigDir = join(process.cwd(), ".pact-toolbox");
+export const chainwebConfigDir = join(process.cwd(), '.pact-toolbox');
 export function createChainwebNodeConfig(
   overrides?: Partial<ChainwebNodeConfig>,
   configDir = chainwebConfigDir,
 ): ChainwebNodeConfig {
   const defaults = {
     persistDb: true,
-    configFile: join(configDir, "chainweb-node.common.yaml"),
-    p2pCertificateChainFile: join(configDir, "devnet-bootstrap-node.cert.pem"),
-    p2pCertificateKeyFile: join(configDir, "devnet-bootstrap-node.key.pem"),
-    p2pHostname: "bootstrap-node",
+    configFile: join(configDir, 'chainweb-node.common.yaml'),
+    p2pCertificateChainFile: join(configDir, 'devnet-bootstrap-node.cert.pem'),
+    p2pCertificateKeyFile: join(configDir, 'devnet-bootstrap-node.key.pem'),
+    p2pHostname: 'bootstrap-node',
     p2pPort: 1789,
     bootstrapReachability: 1,
-    clusterId: "devnet-minimal",
+    clusterId: 'devnet-minimal',
     p2pMaxSessionCount: 1,
     mempoolP2pMaxSessionCount: 1,
     knownPeerInfo:
-      "YNo8pXthYQ9RQKv1bbpQf2R5LcLYA3ppx2BL2Hf8fIM@bootstrap-node:1789",
-    logLevel: "info",
+      'YNo8pXthYQ9RQKv1bbpQf2R5LcLYA3ppx2BL2Hf8fIM@bootstrap-node:1789',
+    logLevel: 'info',
     enableMiningCoordination: true,
     miningPublicKey:
-      "f89ef46927f506c70b6a58fd322450a936311dc6ac91f4ec3d8ef949608dbf1f",
+      'f89ef46927f506c70b6a58fd322450a936311dc6ac91f4ec3d8ef949608dbf1f',
     headerStream: true,
     rosetta: false,
     allowReadsInLocal: true,
-    databaseDirectory: join(process.cwd(), ".pact-toolbox/chainweb/db"),
+    databaseDirectory: join(process.cwd(), '.pact-toolbox/chainweb/db'),
     disablePow: true,
     servicePort: 1848,
   } as ChainwebNodeConfig;
@@ -219,11 +219,11 @@ export function createChainWebMiningClientConfig(
 ): ChainwebMiningClientConfig {
   const defaults = {
     publicKey:
-      "f89ef46927f506c70b6a58fd322450a936311dc6ac91f4ec3d8ef949608dbf1f",
-    worker: "on-demand",
+      'f89ef46927f506c70b6a58fd322450a936311dc6ac91f4ec3d8ef949608dbf1f',
+    worker: 'on-demand',
     constantDelayBlockTime: 5,
     threadCount: 1,
-    logLevel: "info",
+    logLevel: 'info',
     noTls: true,
     onDemandPort: 9090,
     stratumPort: 1917,
@@ -238,13 +238,13 @@ export function createLocalChainwebNetworkConfig(
   overrides?: Partial<LocalChainwebNetworkConfig>,
 ): LocalChainwebNetworkConfig {
   const defaults = {
-    type: "chainweb-local",
+    type: 'chainweb-local',
     rpcUrl: createChainwebRpcUrl(),
-    networkId: "development",
-    chainId: "0",
+    networkId: 'development',
+    chainId: '0',
     signers: defaultSigners,
     keysets: defaultKeysets,
-    senderAccount: "sender00",
+    senderAccount: 'sender00',
     autoStart: true,
     nodeConfig: createChainwebNodeConfig(),
     miningClientConfig: createChainWebMiningClientConfig(),
@@ -259,15 +259,15 @@ export function createChainwebNetworkConfig(
   overrides?: Partial<ChainwebNetworkConfig>,
 ): ChainwebNetworkConfig {
   const defaults = {
-    type: "chainweb",
+    type: 'chainweb',
     rpcUrl: createChainwebRpcUrl({
-      host: "https://testnet.chainweb.com",
+      host: 'https://testnet.chainweb.com',
     }),
-    networkId: "testnet04",
-    chainId: "0",
+    networkId: 'testnet04',
+    chainId: '0',
     signers: [],
     keysets: {},
-    senderAccount: "",
+    senderAccount: '',
     ...defaultMeta,
   } as ChainwebNetworkConfig;
 
@@ -277,15 +277,15 @@ export function createTestNetNetworkConfig(
   overrides?: Partial<ChainwebNetworkConfig>,
 ): ChainwebNetworkConfig {
   const defaults = {
-    type: "chainweb",
+    type: 'chainweb',
     rpcUrl: createChainwebRpcUrl({
-      host: "https://testnet.chainweb.com",
+      host: 'https://testnet.chainweb.com',
     }),
-    networkId: "testnet04",
-    chainId: "0",
+    networkId: 'testnet04',
+    chainId: '0',
     signers: [],
     keysets: {},
-    senderAccount: "",
+    senderAccount: '',
     ...defaultMeta,
   } as ChainwebNetworkConfig;
 
@@ -296,15 +296,15 @@ export function createMainNetNetworkConfig(
   overrides?: Partial<ChainwebNetworkConfig>,
 ): ChainwebNetworkConfig {
   const defaults = {
-    type: "chainweb",
+    type: 'chainweb',
     rpcUrl: createChainwebRpcUrl({
-      host: "https://mainnet.chainweb.com",
+      host: 'https://mainnet.chainweb.com',
     }),
-    networkId: "mainnet01",
-    chainId: "0",
+    networkId: 'mainnet01',
+    chainId: '0',
     signers: [],
     keysets: {},
-    senderAccount: "",
+    senderAccount: '',
     ...defaultMeta,
   } as ChainwebNetworkConfig;
 
@@ -312,11 +312,11 @@ export function createMainNetNetworkConfig(
 }
 
 export const defaultConfig: PactToolboxConfigObj = {
-  defaultNetwork: "local",
+  defaultNetwork: 'local',
   networks: {},
-  contractsDir: "pact",
-  scriptsDir: "scripts",
-  preludes: ["kadena/chainweb"],
+  contractsDir: 'pact',
+  scriptsDir: 'scripts',
+  preludes: ['kadena/chainweb'],
   deployPreludes: true,
   downloadPreludes: false,
 };
