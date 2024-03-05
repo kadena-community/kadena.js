3fa3c333c8aa6c2cd132a1375bb5f9182ba7de4b
Sam Ferrero <ferrerocompanylimited@gmail.com>
2024-03-05 15:26:07 +0000

    chore: header test

--- packages/apps/dev-wallet/src/App/layout.tsx
+++ packages/apps/dev-wallet/src/App/layout.tsx
@@ -1,12 +1,56 @@
-import { Stack, Text } from '@kadena/react-ui';
-import { FC } from 'react';
+import {
+  KadenaLogo,
+  NavHeader,
+  NavHeaderLink,
+  NavHeaderLinkList,
+  NavHeaderSelect,
+  SelectItem,
+  Stack,
+  SystemIcon,
+  Text,
+} from '@kadena/react-ui';
+import { FC, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { layout } from './layout.css';
 
+const mockNetworkItems: string[] = ['DevNet', 'Testnet', 'Mainnet'];
+
 export const Layout: FC = () => {
+  const [value, setValue] = useState<string>(mockNetworkItems[0]);
+
   return (
     <div className={layout}>
-      <Stack
+      <NavHeader
+        logo={
+          <a href="">
+            <KadenaLogo height={40} />
+          </a>
+        }
+      >
+        <NavHeaderLinkList>
+          <NavHeaderLink key="home" href="/">
+            DX-Wallet
+          </NavHeaderLink>
+          <NavHeaderLink key="home" href="/select-profile">
+            Profiles
+          </NavHeaderLink>
+        </NavHeaderLinkList>
+
+        <NavHeaderSelect
+          aria-label="Select Network"
+          selectedKey={value}
+          onSelectionChange={(value: any) => setValue(value)}
+          startIcon={<SystemIcon.Earth />}
+        >
+          {mockNetworkItems.map((network) => (
+            <SelectItem key={network} textValue={network}>
+              {network}
+            </SelectItem>
+          ))}
+        </NavHeaderSelect>
+      </NavHeader>
+
+      {/* <Stack
         as="nav"
         flexDirection="row"
         justifyContent="space-between"
@@ -19,7 +63,7 @@ export const Layout: FC = () => {
             <Text bold>DX-Wallet</Text>
           </Link>
         </Stack>
-      </Stack>
+      </Stack> */}
       <Outlet />
       <div id="modalportal"></div>
     </div>
