47bb0380310b2dc23e91825247f15977f67bed84
Steven Straatemans <steven@straatemans.nl>
2024-03-05 16:13:15 +0100

    image enlarge with animation

--- packages/apps/docs/src/components/Markdown/Figure/Figure.tsx
+++ packages/apps/docs/src/components/Markdown/Figure/Figure.tsx
@@ -1,16 +1,30 @@
-import { Text } from '@kadena/react-ui';
+import { Dialog, Text } from '@kadena/react-ui';
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import type { FC } from 'react';
 import React, { useState } from 'react';
-import { figCaption, figure, figureImg } from './styles.css';
+import {
+  figCaption,
+  figure,
+  figureImg,
+  imageModalAltTextClass,
+  imageModalClass,
+} from './styles.css';
 
 interface IProps {
   alt: string;
   src: string;
 }
 
+const spring = {
+  type: 'spring',
+  damping: 9,
+  stiffness: 120,
+};
+
 export const Figure: FC<IProps> = ({ alt, src }) => {
   const [dimension, setDimension] = useState({ width: 0, height: 0 });
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleLoad: React.ReactEventHandler<HTMLImageElement> = (e) => {
     setDimension({
@@ -18,28 +32,68 @@ export const Figure: FC<IProps> = ({ alt, src }) => {
       height: e.currentTarget.naturalHeight,
     });
   };
+
+  const handleOpenModal: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    e.preventDefault();
+    setIsModalOpen(true);
+  };
+
   return (
-    <figure className={figure}>
-      <Image
-        className={figureImg}
-        src={src}
-        alt={alt}
-        width={dimension.width}
-        height={dimension.height}
-        sizes="100vw"
-        placeholder="blur"
-        blurDataURL="/assets/blur.jpg"
-        onLoad={handleLoad}
-      />
-      {alt ? (
-        <figcaption className={figCaption}>
-          <Text variant="smallest" as="span">
+    <AnimatePresence>
+      <Dialog
+        className={imageModalClass}
+        isOpen={isModalOpen}
+        isDismissable={false}
+        onOpenChange={() => setIsModalOpen(false)}
+      >
+        <motion.img
+          layoutId={src}
+          initial={{ ...spring, scale: 0.5 }}
+          animate={{ ...spring, scale: 1 }}
+          exit={{ ...spring, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          key={src}
+          className={figureImg}
+          src={src}
+          alt={alt}
+          width={dimension.width}
+          height={dimension.height}
+          onLoad={handleLoad}
+        />
+        {alt ? (
+          <Text className={imageModalAltTextClass} variant="smallest" as="span">
             {alt}
           </Text>
-        </figcaption>
-      ) : (
-        <></>
-      )}
-    </figure>
+        ) : (
+          <></>
+        )}
+      </Dialog>
+
+      <figure className={figure} onClick={handleOpenModal}>
+        <motion.div layoutId={src}>
+          <Image
+            className={figureImg}
+            src={src}
+            alt={alt}
+            width={dimension.width}
+            height={dimension.height}
+            sizes="100vw"
+            placeholder="blur"
+            blurDataURL="/assets/blur.jpg"
+            onLoad={handleLoad}
+          />
+        </motion.div>
+
+        {alt ? (
+          <figcaption className={figCaption}>
+            <Text variant="smallest" as="span">
+              {alt}
+            </Text>
+          </figcaption>
+        ) : (
+          <></>
+        )}
+      </figure>
+    </AnimatePresence>
   );
 };
--- packages/apps/docs/src/components/Markdown/Figure/styles.css.ts
+++ packages/apps/docs/src/components/Markdown/Figure/styles.css.ts
@@ -42,13 +42,14 @@ export const figureImg = style([
 export const figCaption = style([
   atoms({
     textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
   }),
   {
     marginBlockEnd: tokens.kda.foundation.size.n8,
     marginInline: tokens.kda.foundation.size.n4,
     ...responsiveStyle({
       md: {
-        maxWidth: '50%',
         marginBlockStart: tokens.kda.foundation.size.n2,
         marginInlineStart: 0,
         marginInlineEnd: 0,
@@ -68,4 +69,46 @@ globalStyle(
 
 globalStyle(`table figure`, {
   margin: '0 !important',
+});
+
+export const imageModalClass = style([
+  atoms({
+    width: '100%',
+  }),
+  {
+    outline: 0,
+    maxWidth: '100vw',
+    maxHeight: '100vh',
+    backgroundColor: 'transparent',
+    border: 0,
+    paddingInline: tokens.kda.foundation.spacing.xs,
+    ...responsiveStyle({
+      md: {
+        paddingInline: tokens.kda.foundation.spacing.xl,
+      },
+    }),
+  },
+]);
+export const imageModalAltTextClass = style([
+  atoms({
+    marginBlock: 'md',
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  }),
+  {
+    color: tokens.kda.foundation.color.text.base.inverse.default,
+  },
+]);
+
+globalStyle(`${imageModalClass} button`, {
+  top: `calc(0px - ${tokens.kda.foundation.spacing.sm})`,
+  right: 0,
+  color: tokens.kda.foundation.color.text.base.inverse.default,
+
+  ...responsiveStyle({
+    md: {
+      paddingInline: tokens.kda.foundation.spacing.xl,
+    },
+  }),
 });
