8215bb3d3e9bd6953690536ebb1b8a67bc2d6ac5
Sam Ferrero <ferrerocompanylimited@gmail.com>
2024-03-05 17:03:55 +0000

    chore: nav

--- packages/apps/dev-wallet/src/App/layout.tsx
+++ packages/apps/dev-wallet/src/App/layout.tsx
@@ -5,13 +5,10 @@ import {
   NavHeaderLinkList,
   NavHeaderSelect,
   SelectItem,
-  Stack,
   SystemIcon,
-  Text,
 } from '@kadena/react-ui';
 import { FC, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import { layout } from './layout.css';
+import { Outlet } from 'react-router-dom';
 
 const mockNetworkItems: string[] = ['DevNet', 'Testnet', 'Mainnet'];
 
@@ -19,7 +16,7 @@ export const Layout: FC = () => {
   const [value, setValue] = useState<string>(mockNetworkItems[0]);
 
   return (
-    <div className={layout}>
+    <>
       <NavHeader
         logo={
           <a href="">
@@ -50,22 +47,8 @@ export const Layout: FC = () => {
         </NavHeaderSelect>
       </NavHeader>
 
-      {/* <Stack
-        as="nav"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        padding={'sm'}
-        backgroundColor="base.default"
-      >
-        <Stack gap="sm" padding="sm">
-          <Link to="/">
-            <Text bold>DX-Wallet</Text>
-          </Link>
-        </Stack>
-      </Stack> */}
       <Outlet />
       <div id="modalportal"></div>
-    </div>
+    </>
   );
 };
