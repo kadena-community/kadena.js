c7b837dcb4c7b8e66ee84c9e4855cb7a5370aa4a
Steven <steven@straatemans.nl>
2024-03-05 11:18:25 +0100

    fix(docs): remove react-components lib (#1788)

--- packages/libs/react-components/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-// This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-config/patch/modern-module-resolution');
-
-module.exports = {
-  extends: ['@kadena-dev/eslint-config/profile/react'],
-  parserOptions: { tsconfigRootDir: __dirname },
-};
