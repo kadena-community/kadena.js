221b557e23d5c19a312763d52048c73043711904
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-03-07 14:46:59 +0100

    fix: remove changes related to transfers

--- packages/apps/graph/cwd-extra-migrations/2.3.0.0.6_drop_transfer_indexes.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS transfers_block_requestkey;
-DROP INDEX IF EXISTS transfers_from_acct;
-DROP INDEX IF EXISTS transfers_from_acct_height_idx;
-DROP INDEX IF EXISTS transfers_to_acct;
-DROP INDEX IF EXISTS transfers_to_acct_height_idx_idx;
--- packages/apps/graph/cwd-extra-migrations/2.3.0.0.7_create_transfer_indexes.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX transfers_from_acct_height ON transfers (from_acct, height DESC);
-CREATE INDEX transfers_to_acct_height ON transfers (to_acct, height DESC);
