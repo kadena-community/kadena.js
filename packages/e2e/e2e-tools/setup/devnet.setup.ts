c6a6b57335f6fc0cfc21306d039bfb369b3fe272
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-13 14:42:00 +0100

    feat: add setup for ledger account

--- packages/e2e/e2e-tools/setup/devnet.setup.ts
+++ packages/e2e/e2e-tools/setup/devnet.setup.ts
@@ -1,3 +1,5 @@
+import { ledgerAccount } from '@kadena-dev/e2e-base/src/constants/accounts.constants';
+import { createAccount } from '@kadena-dev/e2e-base/src/helpers/client-utils/accounts.helper';
 import { deployFaucetContract } from '@kadena-dev/e2e-base/src/smart-contracts/faucet/deploy';
 import { deployGuards } from '@kadena-dev/e2e-base/src/smart-contracts/guards/deploy';
 import { deployGasStation } from '@kadena-dev/e2e-base/src/smart-contracts/kadena-xchain-gas/deploy';
@@ -28,5 +30,15 @@ setup('Deploy Faucet', async () => {
   await setup.step('Deploy Faucet Contract on chain 1', async () => {
     const deploymentStatus = await deployFaucetContract('1');
     expect(deploymentStatus).toEqual('success');
+  });
+});
+
+setup('Setup Ledger Account', async () => {
+  await setup.step('Setup Ledger account on chain 0', async () => {
+    const creationTask = await createAccount(
+      ledgerAccount,
+      ledgerAccount.chains[0],
+    );
+    expect(creationTask.result.status).toEqual('success');
   });
 });
