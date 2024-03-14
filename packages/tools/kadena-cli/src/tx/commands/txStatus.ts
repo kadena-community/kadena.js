2f9a941e452fce7c7ea191eb40900a9507618cff
Mohamed Ismail <ismailreality1@gmail.com>
2024-03-14 12:11:52 +0100

    feat(transaction): print results in table format

--- packages/tools/kadena-cli/src/tx/commands/txStatus.ts
+++ packages/tools/kadena-cli/src/tx/commands/txStatus.ts
@@ -16,25 +16,64 @@ export const getTxStatus = async (config: {
   networkConfig: INetworkCreateOptions;
 }): Promise<CommandResult<ICommandResult>> => {
   try {
-    const { listen } = createClient(
+    const { pollStatus } = createClient(
       `${config.networkConfig.networkHost}/chainweb/0.0/${config.networkConfig.networkId}/chain/${config.chainId}/pact`,
     );
-    const result = await listen({
-      requestKey: config.requestKey,
-      chainId: config.chainId,
-      networkId: config.networkConfig.networkId,
-    });
+    const result = await pollStatus(
+      {
+        requestKey: config.requestKey,
+        chainId: config.chainId,
+        networkId: config.networkConfig.networkId,
+      },
+      { timeout: 25000 },
+    );
 
     return {
       success: true,
-      data: result,
+      data: result[config.requestKey],
     };
   } catch (error) {
+    const errorMessage =
+      error.message === 'TIME_OUT_REJECT'
+        ? `Transaction request for ${config.requestKey} is timed out. Please check your "chainID" input and try again.`
+        : `Transaction request for ${config.requestKey} is failed with : ${error.message}`;
     return {
       success: false,
-      errors: [error.message],
+      errors: [errorMessage],
     };
   }
+};
+
+export const generateTabularData = (
+  chainId: string,
+  result: ICommandResult,
+): { header: string[]; rows: string[][] } => {
+  const eventsData = result.events?.map((event) => {
+    const { name, module, params } = event;
+    const moduleName = module.namespace
+      ? `${module.namespace}.${module.name}`
+      : module.name;
+    const eventName = `${moduleName}.${name}`;
+    return [`Event:${eventName}`, params.join('\n')];
+  });
+  const events =
+    eventsData === undefined ? [['Events', 'N/A']] : [...eventsData];
+
+  const data = [
+    ['Chain ID', chainId.toString()],
+    ['Transaction Status', result.result.status],
+    ['Transaction ID', result.txId?.toString() ?? 'N/A'],
+    ['Gas', result.gas.toString()],
+    ['Block Height', result.metaData?.blockHeight?.toString() ?? 'N/A'],
+    ...events,
+  ];
+
+  return {
+    // Currently based on header only the columns are aligned.
+    // So adding empty header to keep the column alignment.
+    header: ['', ''],
+    rows: data,
+  };
 };
 
 export const createTxStatusCommand: (
@@ -61,14 +100,16 @@ export const createTxStatusCommand: (
     const { status } = result.data.result;
     const outputColor = status === 'success' ? log.color.green : log.color.red;
     log.info(outputColor(`Transaction Status: ${status}`));
-    if (status === 'success') {
-      log.info(outputColor(`Data: ${result.data.result.data}`));
-    } else {
+
+    if (status === 'failure') {
       const errorMessage =
         'message' in result.data.result.error
           ? result.data.result.error.message
           : result.data.result.error;
       log.info(outputColor(`Error: ${errorMessage}`));
     }
+
+    const { header, rows } = generateTabularData(config.chainId, result.data);
+    log.output(log.generateTableString(header, rows));
   },
 );
