c451ebb14ce23ae13d69fef118275d81a9bdcbad
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-14 16:35:42 +0100

    feat: add ledger e2e tests

--- packages/e2e/e2e-base/src/page-objects/react-ui/listBox.component.ts
+++ packages/e2e/e2e-base/src/page-objects/react-ui/listBox.component.ts
@@ -1,18 +1,20 @@
-import type { Page } from '@playwright/test';
+import type { Locator, Page } from '@playwright/test';
 
 export class ListBoxComponent {
   private readonly _page: Page;
+  private _parentLocator: Locator;
 
-  public constructor(page: Page) {
+  public constructor(page: Page, parentLocator: Locator) {
     this._page = page;
+    this._parentLocator = parentLocator;
   }
 
   public async setValueForListBox(
     btnLabel: string,
     listLabel: string,
-    value: string,
+    value: string | RegExp,
   ): Promise<void> {
-    await this._page.getByRole('button', { name: btnLabel }).click();
+    await this._parentLocator.getByRole('button', { name: btnLabel }).click();
     await this._page
       .getByRole('listbox', { name: listLabel })
       .getByRole('option', { name: value, exact: true })
