783427c853561c59d08ecca8cc3a89b0053c2438
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-15 15:48:15 +0100

    fix: use regular sandbox without instant mining for now

--- packages/e2e/e2e-graph/playwright.config.ts
+++ packages/e2e/e2e-graph/playwright.config.ts
@@ -5,12 +5,12 @@ const config: PlaywrightTestConfig = {
   ...baseConfig,
   webServer: {
     command: `pnpm --filter @kadena/graph start:generate`,
-    url: 'http://localhost:3000/graphql',
+    url: 'http://localhost:4000/graphql',
     reuseExistingServer: process.env.CI === undefined,
     stdout: 'pipe',
     stderr: 'pipe',
     env: {
-      PORT: '3000',
+      PORT: '4000',
     },
   },
   projects: [
