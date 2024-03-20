766dff0040238a74f674fce0095c8354892b28d1
r-mulder <info@mulderr.com>
2024-03-20 21:38:24 +0100

    chore:upgrade docs storybook

--- packages/apps/docs/.storybook/main.ts
+++ packages/apps/docs/.storybook/main.ts
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 const path = require('path');
 import { StorybookConfig } from '@storybook/react-webpack5';
 import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin';
@@ -5,28 +6,18 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-controls',
-    '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm',
-    'storybook-dark-mode',
-    'storybook-addon-next-router',
-    {
-      name: 'storybook-addon-swc',
-      options: {
-        enable: true,
-        enableSwcLoader: true,
-        enableSwcMinify: true,
-        swcLoaderOptions: {},
-        swcMinifyOptions: {},
-      },
-    },
-  ],
+  addons: [getAbsolutePath("@storybook/addon-a11y"), getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-controls"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm"), getAbsolutePath("storybook-dark-mode"), {
+    name: 'storybook-addon-swc',
+    options: {
+      enable: true,
+      enableSwcLoader: true,
+      enableSwcMinify: true,
+      swcLoaderOptions: {},
+      swcMinifyOptions: {}
+    }
+  }],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {},
   },
   docs: {
@@ -87,3 +78,7 @@ const config: StorybookConfig = {
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
