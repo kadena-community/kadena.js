7fd17c4c698d701d2d189660f38e56484317304b
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-21 19:15:09 +0100

    set permissions for tpmfs volume

--- .github/actions/devnet/action.sh
+++ .github/actions/devnet/action.sh
@@ -10,7 +10,7 @@ echo "docker run --name=devnet -d --health-cmd='curl --verbose --fail http://loc
   -v /home/runner/work/_temp:/home/runner/work/_temp \
   $INPUT_MIGRATIONS \
   -e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY 	\
-  --tmpfs /data \
+  --tmpfs /data:rw \
   -p 8080:8080 -p 5432:5432 \"kadena/devnet:experimental-smart-mining.graph\""
 
 exec docker run --name=devnet -d --health-cmd='curl --verbose --fail http://localhost:8080/info || exit 1' \
@@ -18,7 +18,7 @@ exec docker run --name=devnet -d --health-cmd='curl --verbose --fail http://loca
   -v /home/runner/work/_temp:/home/runner/work/_temp \
   $INPUT_MIGRATIONS \
   -e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY 	\
-  --tmpfs /data \
+  --tmpfs /data:wr \
   -p 8080:8080 -p 5432:5432 "kadena/devnet:experimental-smart-mining.graph"
 
 echo "Waiting for devnet to be healthy..."
