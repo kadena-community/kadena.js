0d26b562721df9e06d6e97f385fde8b927fa0c94
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-21 17:20:22 +0100

    add tpmfs volume

--- .github/actions/devnet/action.sh
+++ .github/actions/devnet/action.sh
@@ -10,14 +10,16 @@ echo "docker run --name=devnet -d --health-cmd='curl --verbose --fail http://loc
   -v /home/runner/work/_temp:/home/runner/work/_temp \
   $INPUT_MIGRATIONS \
   -e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY 	\
-  -p 8080:8080 -p 5432:5432 \"kadena/devnet\""
+  --mount type=tmpfs,destination=/data \
+  -p 8080:8080 -p 5432:5432 \"kadena/devnet:experimental-smart-mining.graph\""
 
 exec docker run --name=devnet -d --health-cmd='curl --verbose --fail http://localhost:8080/info || exit 1' \
   -v "/var/run/docker.sock":"/var/run/docker.sock" \
   -v /home/runner/work/_temp:/home/runner/work/_temp \
   $INPUT_MIGRATIONS \
   -e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY 	\
-  -p 8080:8080 -p 5432:5432 "kadena/devnet"
+  --mount type=tmpfs,destination=/data \
+  -p 8080:8080 -p 5432:5432 "kadena/devnet:experimental-smart-mining.graph"
 
 echo "Waiting for devnet to be healthy..."
 until [ "`docker inspect -f {{.State.Health.Status}} devnet`"=="healthy" ]; do
