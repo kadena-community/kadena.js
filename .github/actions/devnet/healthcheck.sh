9505a3f7f4f7417f71ca1ddbfd2f0faa9250a761
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-23 12:56:45 +0100

    refactor action to a composite action

--- .github/actions/devnet/healthcheck.sh
+++ .github/actions/devnet/healthcheck.sh
@@ -2,13 +2,13 @@
 counter=0
 while [[ $counter -lt 30 ]]; do
     echo "Checking if all objects are running"
-    response=$(curl -X 'GET' 'http://localhost:9999/process/chainweb-data' -H 'accept: application/json')
+    response=$(curl 'http://localhost:9999/process/chainweb-data')
     is_ready=$(echo "$response" | jq -r '.is_ready')
     if [[ "$is_ready" == "Ready" ]]; then
-        echo "All objects are running"
+        echo "Sandbox healthy"
         exit 0  # Success exit code
     else
-        echo "Some objects are not running"
+        echo "Sandbox not healthy, retrying in 10 seconds..."
         sleep 10  # Sleep for 10 seconds
         counter=$((counter+1))
     fi
