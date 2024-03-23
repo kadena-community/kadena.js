c8a3d49a7bdb90363f271425d616f1086374d7ae
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-03-23 13:14:01 +0100

    improve health check

--- .github/actions/devnet/healthcheck.sh
+++ .github/actions/devnet/healthcheck.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 counter=0
-while [[ $counter -lt 30 ]]; do
-    echo "Checking if all objects are running"
-    response=$(curl 'http://localhost:9999/process/chainweb-data')
+max_attempts=30
+retry_interval=10
+
+while [[ $counter -lt $max_attempts ]]; do
+    echo "Checking sandbox health..."
+    response=$(curl -s 'http://localhost:9999/process/chainweb-data')
     is_ready=$(echo "$response" | jq -r '.is_ready')
+
     if [[ "$is_ready" == "Ready" ]]; then
         echo "Sandbox healthy"
         exit 0  # Success exit code
     else
-        echo "Sandbox not healthy, retrying in 10 seconds..."
-        sleep 10  # Sleep for 10 seconds
+        echo "Sandbox not healthy, retrying in $retry_interval seconds..."
+        sleep $retry_interval
         counter=$((counter+1))
     fi
 done
 
-echo "Sandbox not healthy, exiting."
+echo "Sandbox not healthy after $max_attempts attempts, exiting."
 exit 1  # Error exit code
