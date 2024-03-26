417c74e0b5af66112e06dfdc3c89fb4fb1143b7e
Sander Looijenga <slooijenga@anwb.nl>
2024-03-26 12:51:45 +0100

    Add type predicate function and use it

--- packages/apps/tools/src/utils/pact.ts
+++ packages/apps/tools/src/utils/pact.ts
@@ -1,5 +1,15 @@
 import type { ICommandResult } from '@kadena/chainweb-node-client';
+import type { IPactExec } from '@kadena/types';
+
+export const isContinuationResponse = (
+  continuation: ICommandResult['continuation'],
+): continuation is IPactExec => {
+  return continuation !== null;
+};
 
 export const isCrossChainResponse = (response: ICommandResult): boolean => {
-  return response.continuation !== null && response.continuation.yield !== null;
+  return (
+    isContinuationResponse(response.continuation) &&
+    response.continuation.yield !== null
+  );
 };
