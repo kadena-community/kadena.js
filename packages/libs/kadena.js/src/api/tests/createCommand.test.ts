5b6652dfa2936ea0197b69e0232838097a877d24
Albert G <516972+alber70g@users.noreply.github.com>
2024-03-28 13:34:45 +0100

    chore(monorepo): remove `libs/kadena.js` package

--- packages/libs/kadena.js/src/api/tests/createCommand.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest';
-import { createCommand } from '../createCommand';
-import { command, payload, signature } from './mockdata/execCommand';
-
-test('Takes in signature objects and stringified Pact object, and outputs a Signed Pact Command', () => {
-  const actual = createCommand([signature], JSON.stringify(payload));
-
-  expect(actual).toEqual(command);
-});
--- packages/libs/kadena.js/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './api/attachSignature';
-export * from './api/createCommand';
-export * from './api/createContCommand';
-export * from './api/createExecCommand';
-export * from './api/prepareContCommand';
-export * from './api/prepareExecCommand';
-export * from './api/pullAndCheckHashs';
-export { pullSigner } from './api/pullSigner';
-export * from './wallet/createCap';
