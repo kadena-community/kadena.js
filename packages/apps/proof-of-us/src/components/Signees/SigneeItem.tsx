6baf8541fbce892ec32edb0dd2f9eb24c4b97eaa
Steven Straatemans <steven@straatemans.nl>
2024-04-04 11:19:22 +0200

    fix the saved position bullet colors

--- packages/apps/proof-of-us/src/components/Signees/SigneeItem.tsx
+++ packages/apps/proof-of-us/src/components/Signees/SigneeItem.tsx
@@ -1,3 +1,4 @@
+import { getColorStyle } from '@/utils/getColor';
 import { Stack } from '@kadena/react-ui';
 import classNames from 'classnames';
 import type { FC } from 'react';
@@ -18,21 +19,6 @@ interface IProps {
   isMultiple: boolean;
 }
 
-const TRANSPARENT = '7D';
-const colors = [
-  '#2898BD',
-  '#5E4DB2',
-  '#E56910',
-  '#943D73',
-  '#09326C',
-  '#8F7EE7',
-  '#50253F',
-  '#A54800',
-];
-const getColors = (idx: number) => {
-  return colors[idx % 8] + TRANSPARENT;
-};
-
 export const SigneeItem: FC<IProps> = ({
   name,
   accountName,
@@ -40,13 +26,14 @@ export const SigneeItem: FC<IProps> = ({
   socialLink,
   isMultiple,
 }) => {
-  const colorStyle = {
-    '--bulletColor': getColors(idx),
-  } as any;
   return (
     <li className={classNames(isMultiple ? multipleSigneeClass : itemClass)}>
       <Stack width="100%">
-        <div className={bulletClass} data-position={idx} style={colorStyle} />
+        <div
+          className={bulletClass}
+          data-position={idx}
+          style={getColorStyle(idx)}
+        />
         <div className={classNames(titleClass, ellipsClass)}>{name}</div>
       </Stack>
       <div className={classNames(nameClass, ellipsClass)}>{accountName}</div>
