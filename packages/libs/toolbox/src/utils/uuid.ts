2e57b41a07cb3417ab20b5a538e43c09f4532c61
MRVDH <maarten.vandenhoven@deptagency.com>
2024-04-22 14:18:30 +0200

    formatting

--- packages/libs/toolbox/src/utils/uuid.ts
+++ packages/libs/toolbox/src/utils/uuid.ts
@@ -10,10 +10,10 @@ export function nanoid(t = 21) {
         (e &= 63) < 36
           ? e.toString(36)
           : e < 62
-          ? (e - 26).toString(36).toUpperCase()
-          : e > 62
-          ? '-'
-          : '_'),
+            ? (e - 26).toString(36).toUpperCase()
+            : e > 62
+              ? '-'
+              : '_'),
     '',
   );
 }
