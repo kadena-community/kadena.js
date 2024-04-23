34fc39c61255816e7e25c1118c71e90032fe67d8
MRVDH <maarten.vandenhoven@deptagency.com>
2024-04-23 15:34:54 +0200

    reverted commander to 11

--- packages/tools/kadena-cli/src/networks/networkOptions.ts
+++ packages/tools/kadena-cli/src/networks/networkOptions.ts
@@ -11,7 +11,7 @@ export const networkOptions = {
     prompt: networks.networkNamePrompt,
     validation: z.string(),
     option: new Option(
-      '-a, --network-name <networkName>',
+      '-n, --network-name <networkName>',
       'Kadena network (e.g. "mainnet")',
     ),
     transform: (networkName: string) => {
