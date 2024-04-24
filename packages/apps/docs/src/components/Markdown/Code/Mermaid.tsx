b58aa2a3b7b7d4ff2542c617b1a89d5ae809a830
Steven Straatemans <steven@straatemans.nl>
2024-04-24 15:55:43 +0200

    add mermaid styling of codeblock

---
+++ packages/apps/docs/src/components/Markdown/Code/Mermaid.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import mermaid from 'mermaid';
+import type { FC, PropsWithChildren } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { code, mermaidClass } from './style.css';
+
+export const Mermaid: FC<PropsWithChildren> = ({ children, ...props }) => {
+  const ref = useRef<HTMLPreElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.innerHTML = ref.current.innerText;
+      mermaid.initialize({
+        theme: 'base',
+        startOnLoad: true,
+        themeVariables: {
+          primaryColor: '#2767a3',
+          primaryTextColor: '#fff',
+          primaryBorderColor: '#fff',
+          lineColor: '#fff',
+          secondaryColor: '#006100',
+        },
+      });
+    }
+  }, [ref]);
+
+  return (
+    <pre
+      ref={ref}
+      className={classNames(code, mermaidClass, 'mermaid')}
+      {...props}
+    >
+      {children}
+    </pre>
+  );
+};
