81c25539b1e4c1f1e84f6dd3eac8c4b272d5e33d
Steven <steven@straatemans.nl>
2024-04-24 13:41:44 +0200

    fix(docs): refactor validatelinks script (#1999)

--- packages/apps/docs/src/scripts/watchdocs.ts
+++ packages/apps/docs/src/scripts/watchdocs.ts
@@ -5,16 +5,19 @@ import { movePages } from './movePages';
 import type { IEventType } from './movePages/singlePage';
 import { moveSinglePage } from './movePages/singlePage';
 import { initFunc } from './utils/build';
+import { validateLinks } from './validateLinks';
 
 const runSingleChange = async (path: string): Promise<void> => {
   await initFunc(moveSinglePage(path), 'move a single page');
   await initFunc(fixLocalLinksSingle(path), 'Fix local links for single page');
+  await initFunc(validateLinks, 'Validate Links');
 };
 
 const runAll = async (event: IEventType, path: string): Promise<void> => {
   await initFunc(movePages, 'Move all pages from docs with config.yaml');
   await initFunc(createDocsTree, 'Create docs tree');
   await initFunc(fixLocalLinks, 'Fix local links from the config.yaml');
+  await initFunc(validateLinks, 'Validate Links');
 };
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
