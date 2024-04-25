0f9d13be0481de1bcad2abfc7b64c8e5f121182f
Steven Straatemans <steven@straatemans.nl>
2024-04-25 15:38:34 +0200

    fixing render and rerender

--- packages/apps/docs/src/components/Markdown/Code/Mermaid.tsx
+++ packages/apps/docs/src/components/Markdown/Code/Mermaid.tsx
@@ -4,25 +4,30 @@ import type { FC, PropsWithChildren } from 'react';
 import React, { useEffect, useRef } from 'react';
 import { code, mermaidClass } from './style.css';
 
+mermaid.initialize({
+  theme: 'base',
+  startOnLoad: true,
+  themeVariables: {
+    primaryColor: '#2767a3',
+    primaryTextColor: '#fff',
+    primaryBorderColor: '#fff',
+    lineColor: '#fff',
+    secondaryColor: '#006100',
+  },
+});
+
 export const Mermaid: FC<PropsWithChildren> = ({ children, ...props }) => {
   const ref = useRef<HTMLPreElement>(null);
 
   useEffect(() => {
     if (ref.current) {
       ref.current.innerHTML = ref.current.innerText;
-      mermaid.initialize({
-        theme: 'base',
-        startOnLoad: true,
-        themeVariables: {
-          primaryColor: '#2767a3',
-          primaryTextColor: '#fff',
-          primaryBorderColor: '#fff',
-          lineColor: '#fff',
-          secondaryColor: '#006100',
-        },
-      });
+
+      setTimeout(() => {
+        mermaid.contentLoaded();
+      }, 100);
     }
-  }, [ref]);
+  }, []);
 
   return (
     <pre
