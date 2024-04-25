f3b72d63de2c8c88717395e095a1f6e16e1f08ad
Robin Mulder <116208434+r-mulder@users.noreply.github.com>
2024-04-25 13:56:07 +0200

    Feat(react UI)/new input components (#1987)
    Feat: update TextField and NumberField components to the new branding look and feel
    
    ---------
    
    Co-authored-by: Sander Looijenga <slooijenga@anwb.nl>

--- packages/apps/proof-of-us/src/components/TextField/TextField.tsx
+++ packages/apps/proof-of-us/src/components/TextField/TextField.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import type { FC } from 'react';
 import { textClass } from './style.css';
 
-export const TextField: FC<ITextFieldProps> = ({ ...props }) => {
+export const TextField: FC<Omit<ITextFieldProps, 'size'>> = ({ ...props }) => {
   return (
     <input {...props} className={classNames(textClass, props.className)} />
   );
