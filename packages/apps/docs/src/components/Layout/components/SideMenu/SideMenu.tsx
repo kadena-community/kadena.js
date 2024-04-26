bc4941f5566ad7d5db85c95537a26457b3d7f493
Steven <steven@straatemans.nl>
2024-04-26 09:00:07 +0200

    feat(docs): show all the menu items in leftsidemenu (#2004)

--- packages/apps/docs/src/components/Layout/components/SideMenu/SideMenu.tsx
+++ packages/apps/docs/src/components/Layout/components/SideMenu/SideMenu.tsx
@@ -1,23 +1,15 @@
 import { EVENT_NAMES, analyticsEvent } from '@/utils/analytics';
 import type { IMenuItem } from '@kadena/docs-tools';
-import { Box, Heading, SystemIcon, TextField } from '@kadena/react-ui';
+import { Box, SystemIcon, TextField } from '@kadena/react-ui';
 import { atoms } from '@kadena/react-ui/styles';
-import classNames from 'classnames';
 import { useRouter } from 'next/router';
 import type { FC, KeyboardEvent } from 'react';
 import React from 'react';
 import { MainTreeItem } from '../TreeMenu/MainTreeItem';
 import { TreeList } from '../TreeMenu/TreeList';
 import { MenuCard } from './MenuCard';
-import { ListLink } from './components/ListLink';
 import { ShowOnMobile } from './components/ShowOnMobile';
-import {
-  listClass,
-  listItemClass,
-  sideMenuClass,
-  sideMenuTitleButtonClass,
-  sideMenuTitleClass,
-} from './sideMenu.css';
+import { sideMenuClass } from './sideMenu.css';
 import { useSideMenu } from './useSideMenu';
 
 interface IProps {
@@ -26,14 +18,9 @@ interface IProps {
 }
 
 export const SideMenu: FC<IProps> = ({ closeMenu, menuItems }) => {
-  const { active, clickMenu, clickSubMenu, setActive, treeRef } = useSideMenu(
-    closeMenu,
-    menuItems,
-  );
+  const { active, clickSubMenu, treeRef } = useSideMenu(closeMenu, menuItems);
   const router = useRouter();
   const MagnifierIcon = SystemIcon.Magnify;
-
-  const activeItem = menuItems.find((item) => item.isMenuOpen);
 
   const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>): void => {
     e.preventDefault();
@@ -51,20 +38,6 @@ export const SideMenu: FC<IProps> = ({ closeMenu, menuItems }) => {
 
   return (
     <div className={sideMenuClass}>
-      {active === 0 && (
-        <div className={sideMenuTitleClass}>
-          <Heading as="h5">Kadena Docs</Heading>
-        </div>
-      )}
-      {active === 1 && (
-        <button
-          type="button"
-          onClick={() => setActive(0)}
-          className={classNames(sideMenuTitleClass, sideMenuTitleButtonClass)}
-        >
-          <Heading as="h5">{activeItem?.menu}</Heading>
-        </button>
-      )}
       <ShowOnMobile>
         <Box marginInline="md" marginBlockStart="md" marginBlockEnd="xl">
           {/* TODO: Replace with SearchField */}
@@ -80,35 +53,19 @@ export const SideMenu: FC<IProps> = ({ closeMenu, menuItems }) => {
           />
         </Box>
       </ShowOnMobile>
-      <MenuCard cyTestId="sidemenu-main" active={active} idx={0}>
-        <ul className={listClass}>
-          {menuItems.map((item) => (
-            <li key={item.root} className={listItemClass}>
-              <ListLink
-                onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
-                  clickMenu(e, item)
-                }
-                href={item.root}
-                hasSubMenu={!!item.children?.length}
-              >
-                {item.menu}
-              </ListLink>
-            </li>
+
+      <MenuCard
+        cyTestId="sidemenu-submenu"
+        active={active}
+        idx={1}
+        onClick={clickSubMenu}
+      >
+        <TreeList ref={treeRef} root={true} level="l0">
+          {menuItems.map((menu) => (
+            <MainTreeItem key={menu.root} item={menu} root={false} level={0} />
           ))}
-        </ul>
+        </TreeList>
       </MenuCard>
-      {activeItem && (
-        <MenuCard
-          cyTestId="sidemenu-submenu"
-          active={active}
-          idx={1}
-          onClick={clickSubMenu}
-        >
-          <TreeList ref={treeRef} root={true}>
-            <MainTreeItem item={activeItem} root={true} />
-          </TreeList>
-        </MenuCard>
-      )}
     </div>
   );
 };
