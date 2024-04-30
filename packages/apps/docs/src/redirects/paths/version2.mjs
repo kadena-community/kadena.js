b56e8f514d53e78fcf863d43bc859dc0ab6395fc
Lisa Gunn <lisa@kadena.io>
2024-04-30 12:37:33 -0700

    Added redirects to version2, added crypto utils page, remove imported crypto utils pages

--- packages/apps/docs/src/redirects/paths/version2.mjs
+++ packages/apps/docs/src/redirects/paths/version2.mjs
@@ -1,7 +1,380 @@
 export const version2PathUrlRedirects = [
   {
+    source: '/kadena',
+    destination: '/learn',
+    permanent: true,
+  },
+  {
     source: '/kadena/:slug',
     destination: '/learn/:slug',
+    permanent: true,
+  },
+  {
+    source: '/kadena/kda',
+    destination: '/invest',
+    permanent: true,
+  },
+  {
+    source: '/kadena/kda/manage-kda',
+    destination: '/invest',
+    permanent: true,
+  },
+  {
+    source: '/kadena/kda/kda-concepts',
+    destination: '/learn/accounts-keys',
+    permanent: true,
+  },
+  {
+    source: '/kadena/exchanges',
+    destination: '/invest/exchanges',
+    permanent: true,
+  },
+  {
+    source: '/kadena/exchanges/:slug',
+    destination: '/invest/exchanges',
+    permanent: true,
+  },
+  {
+    source: '/kadena/wallets',
+    destination: '/invest/wallets',
+    permanent: true,
+  },
+  {
+    source: '/kadena/wallets/:slug',
+    destination: '/invest/wallets/:slug',
+    permanent: true,
+  },
+  {
+    source: '/kadena/wallets/chainweaver/:slug',
+    destination: '/invest/wallets/chainweaver',
+    permanent: true,
+  },
+  {
+    source: '/kadena/wallets/kadena-spirekey',
+    destination: '/build/authentication',
+    permanent: true,
+  },
+  {
+    source: '/kadena/client',
+    destination: '/reference/kadena-client',
+    permanent: true,
+  },
+  {
+    source: '/kadena/client/:slug',
+    destination: '/reference/kadena-client',
+    permanent: true,
+  },
+  {
+    source: '/kadena/client-examples',
+    destination: '/reference/kadena-client',
+    permanent: true,
+  },
+  {
+    source: '/kadena/kadenajs',
+    destination: '/reference/kadena-client',
+    permanent: true,
+  },
+  {
+    source: '/kadena/kadenajs/:slug',
+    destination: '/reference/kadena-client',
+    permanent: true,
+  },
+  {
+    source: '/kadena/code-of-conduct',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source: '/build/quickstart/:slug',
+    destination: '/build/onboard',
+    permanent: true,
+  },
+  {
+    source: '/build/kda-cli',
+    destination: '/build/frontend',
+    permanent: true,
+  },
+  {
+    source: '/build/kda-cli/:slug',
+    destination: '/build/frontend',
+    permanent: true,
+  },
+  {
+    source: '/build/cryptography-utils',
+    destination: '/reference/kadena-client/cryptographic-utils',
+    permanent: true,
+  },
+  {
+    source: '/build/cryptography-utils/api',
+    destination: '/reference/kadena-client/cryptographic-utils',
+    permanent: true,
+  },
+  {
+    source: '/build/cryptography-utils/crypto-api',
+    destination: '/reference/kadena-client/cryptographic-utils',
+    permanent: true,
+  },
+  {
+    source: '/build/cookbook',
+    destination: '/build/templates',
+    permanent: true,
+  },
+  {
+    source: '/build/cookbook/cookbook',
+    destination: '/build/templates',
+    permanent: true,
+  },
+  {
+    source: '/build/guides',
+    destination: '/build',
+    permanent: true,
+  },
+  {
+    source: '/build/guides/:slug',
+    destination: '/build',
+    permanent: true,
+  },
+  {
+    source: '/build/resources',
+    destination: '/build',
+    permanent: true,
+  },
+  {
+    source: '/build/resources/kuro-layer-2',
+    destination: '/build',
+    permanent: true,
+  },
+  {
+    source: '/build/guides/election-dapp-tutorial/:slug',
+    destination: '/build/election/:slug',
+    permanent: true,
+  }, 
+  {
+    source: '/marmalade',
+    destination: '/build/nft-marmalade',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/what-is-marmalade',
+    destination: '/build/nft-marmalade',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/quick-start',
+    destination: '/build/nft-marmalade/get-started',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/marmalade-v1',
+    destination: '/build/nft-marmalade',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/:slug',
+    destination: '/build/nft-marmalade/:slug',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/marmalade-v1/:slug',
+    destination: '/build/nft-marmalade',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/metadata/:slug',
+    destination: '/build/nft-marmalade/metadata',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/auctions',
+    destination: '/build/nft-marmalade/sales/auctions',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/auctions/:slug',
+    destination: '/build/nft-marmalade/sales/auctions',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/concrete-policies/collection-policy',
+    destination: '/build/nft-marmalade/policies',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/concrete-policies/guard-policy',
+    destination: '/build/nft-marmalade/policies',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/concrete-policies/non-fungible-policy',
+    destination: '/build/nft-marmalade/policies',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/concrete-policies/royalty-policy',
+    destination: '/build/nft-marmalade/policies',
+    permanent: true,
+  },
+  {
+    source: '/marmalade/architecture/:slug',
+    destination: '/build/nft-marmalade/architecture',
+    permanent: true,
+  },
+  {
+    source: '/pact',
+    destination: '/build/pact',
+    permanent: true,
+  },
+  {
+    source: '/pact/overview',
+    destination: '/build/pact',
+    permanent: true,
+  },
+  {
+    source: '/pact/beginner',
+    destination: '/build/pact',
+    permanent: true,
+  },
+  {
+    source: '/pact/intermediate',
+    destination: '/build/pact',
+    permanent: true,
+  },
+  {
+    source: '/pact/:slug1',
+    destination: '/build/pact',
+    permanent: true,
+  },
+  {
+    source: '/pact/:slug1/:slug2',
+    destination: '/build/pact/:slug1/:slug2',
+    permanent: true,
+  },
+  {
+    source: '/pact/beginner/:slug',
+    destination: '/build/pact/:slug',
+    permanent: true,
+  },
+  {
+    source: '/pact/beginner/web-editor',
+    destination: '/build/pact/dev-network',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/rest-api',
+    destination: '/reference/rest-api',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/rest-api:slug',
+    destination: '/reference/rest-api',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/concepts',
+    destination: '/build/pact/advanced',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/syntax',
+    destination: '/reference/syntax',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/time-formats',
+    destination: '/reference/functions/time',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/functions',
+    destination: '/reference/functions',
+    permanent: true,
+  },
+  {
+    source: '/pact/beginner/:slug',
+    destination: '/build/pact/:slug',
+    permanent: true,
+  },
+  {
+    source: '/pact/intermediate/:slug',
+    destination: '/build/pact/:slug',
+    permanent: true,
+  },
+  {
+    source: '/chainweb',
+    destination: '/deploy',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/:slug',
+    destination: '/deploy/chainweb/:slug',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/compact-database',
+    destination: '/deploy/chainweb/compact-a-database',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/node-client',
+    destination: '/reference/chainweb',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/node-client/api',
+    destination: '/reference/chainweb/node-client',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/stream-client',
+    destination: '/reference/chainweb/stream-client',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/js-binding',
+    destination: '/reference/chainweb/js-binding',
+    permanent: true,
+  },
+  {
+    source: '/chainweb/js-binding/api',
+    destination: '/reference/chainweb/js-binding',
+    permanent: true,
+  },
+
+  {
+    source: '/contribute',
+    destination: '/participate',
+    permanent: true,
+  },
+  {
+    source: '/contribute/node',
+    destination: '/deploy',
+    permanent: true,
+  },
+
+  {
+    source: '/contribute/node/:slug',
+    destination: '/deploy',
+    permanent: true,
+  },
+  {
+    source: '/contribute/docs',
+    destination: '/participate/docs',
+    permanent: true,
+  },
+
+  {
+    source: '/contribute/kadena-dao',
+    destination: '/participate',
+    permanent: true,
+  },
+  {
+    source: '/contribute/ambassadors',
+    destination: '/participate/community',
+    permanent: true,
+  },
+  {
+    source: '/contribute/ambassadors/:slug',
+    destination: '/participate/community',
     permanent: true,
   },
   {
@@ -12,6 +385,46 @@ export const version2PathUrlRedirects = [
   {
     source: '/pact/reference/:slug1/:slug2/:slug3',
     destination: '/reference/:slug1/:slug2/:slug3',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/property-checking',
+    destination: '/reference/property-checking',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/properties-and-invariants',
+    destination: '/reference/property-checking/properties-and-invariants',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/pacts',
+    destination: '/reference/pacts',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/pact-repl-cli',
+    destination: '/reference/pact-repl-cli',
+    permanent: true,
+  },
+  {
+    source: '/pact/pactjs/api',
+    destination: '/reference/kadena-client/pactjs',
+    permanent: true,
+  },
+  {
+    source: '/pact/pactjs/utils',
+    destination: '/reference/kadena-client/pactjs-utils',
+    permanent: true,
+  },
+  {
+    source: '/pact/cli/api',
+    destination: '/reference/kadena-client/pactjs-cli',
+    permanent: true,
+  },
+  {
+    source: '/pact/pactjs-generator/api',
+    destination: '/reference/kadena-client/pactjs-generator',
     permanent: true,
   },
   {
@@ -27,6 +440,11 @@ export const version2PathUrlRedirects = [
   {
     source: '/pact/reference/properties-and-invariants/logical-operators',
     destination: '/reference/property-checking/logical',
+    permanent: true,
+  },
+  {
+    source: '/pact/reference/properties-and-invariants/list-operators',
+    destination: '/reference/property-checking/list',
     permanent: true,
   },
 ];
