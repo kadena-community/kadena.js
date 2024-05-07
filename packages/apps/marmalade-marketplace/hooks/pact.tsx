0911819dab8ef2d1f4b05e8b1f5648b9e2219171
ggobugi27 <ggobugi27@github.com>
2024-05-07 19:06:50 +0900

    add marmalade-marketplace app

---
+++ packages/apps/marmalade-marketplace/hooks/pact.tsx
@@ -0,0 +1,5 @@
+import type { IPollResponse, IUnsignedCommand } from '@kadena/client';
+import { Pact } from '@kadena/client';
+import { PactNumber } from '@kadena/pactjs';
+import type { IPollResponse, IUnsignedCommand } from '@kadena/client';
+import { createToken, mintToken, burnToken } from '@kadena/client-utils/marmalade';
---
+++ packages/apps/marmalade-marketplace/pages/index.tsx
@@ -0,0 +1,103 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className={styles.title}>
+          Marmalade Example Application
+        </h1>
+
+        <div className={styles.grid}>
+
+          <a className={styles.card}>
+            <h3>Token Management</h3>
+          </a>
+
+          <a className={styles.card}>
+            <h3>Marketplace</h3>
+          </a>
+
+        </div>
+      </main>
+
+      <footer>
+
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
