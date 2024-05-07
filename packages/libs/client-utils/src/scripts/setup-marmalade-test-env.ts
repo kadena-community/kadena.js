c93dd89d66910c52b3831295a84c0d4c19066a52
Ivan Magaš <imagas96@gmail.com>
2024-05-07 15:41:52 +0200

    chore: remove whitelisting sale from test setup script

--- packages/libs/client-utils/src/scripts/setup-marmalade-test-env.ts
+++ packages/libs/client-utils/src/scripts/setup-marmalade-test-env.ts
@@ -1,15 +1,8 @@
-import { Pact, createSignWithKeypair } from '@kadena/client';
-import {
-  addSigner,
-  composePactCommand,
-  execution,
-  setMeta,
-} from '@kadena/client/fp';
+import { createSignWithKeypair } from '@kadena/client';
 import type { ChainId } from '@kadena/types';
 import { describeModule } from '../built-in';
 import { sourceAccount } from '../built-in/test/test-data';
 import { transferCreate } from '../coin';
-import { preflightClient, submitClient } from '../core';
 import type { IClientConfig } from '../core/utils/helpers';
 import {
   secondaryTargetAccount,
@@ -18,60 +11,6 @@ import {
 import { deployMarmalade } from '../nodejs';
 
 const chainId: ChainId = '0';
-
-const whitelistSale = async (config: IClientConfig, saleContract: string) => {
-  let saleWhitelisted = false;
-
-  try {
-    await preflightClient(config)(
-      composePactCommand(
-        execution(
-          Pact.modules['marmalade-v2.policy-manager']['retrieve-sale'](
-            saleContract,
-          ),
-        ),
-        addSigner([sender00Account.publicKey], (signFor) => [
-          signFor('coin.GAS'),
-        ]),
-        setMeta({ senderAccount: sender00Account.account, chainId }),
-      ),
-    ).execute();
-
-    saleWhitelisted = true;
-  } catch (error) {
-    console.log(`Sale "${saleContract}" not whitelisted, whitelisting now`);
-  }
-
-  if (!saleWhitelisted) {
-    try {
-      await submitClient({
-        host: 'http://127.0.0.1:8080',
-        defaults: {
-          networkId: 'development',
-        },
-        sign: createSignWithKeypair([sender00Account]),
-      })(
-        composePactCommand(
-          execution(
-            Pact.modules['marmalade-v2.policy-manager']['add-sale-whitelist'](
-              () => saleContract,
-            ),
-          ),
-          addSigner([sender00Account.publicKey], (signFor) => [
-            signFor('coin.GAS'),
-            signFor('marmalade-v2.policy-manager.SALE-WHITELIST', saleContract),
-          ]),
-          setMeta({ senderAccount: sender00Account.account, chainId }),
-        ),
-      ).execute();
-
-      saleWhitelisted = true;
-    } catch (error) {
-      console.log(`Error whitelisting the sale contract "${saleContract}"`);
-      throw error;
-    }
-  }
-};
 
 const main = async () => {
   console.log('Setting up marmalade test environment');
@@ -101,9 +40,6 @@ const main = async () => {
       deleteFilesAfterDeployment: true,
     });
   }
-
-  await whitelistSale(config, 'marmalade-sale.conventional-auction');
-  await whitelistSale(config, 'marmalade-sale.dutch-auction');
 
   const [resultSourceAccount, resultTargetAccount] = await Promise.all([
     transferCreate(
