3b0b870d25554a9efd71a7aad6f9b5d965c4b1dc
Nil Amrutlal <nil.amrutlal@deptagency.com>
2024-05-08 17:09:04 +0200

    fix: remove testing code

--- packages/apps/explorer/src/pages/index.tsx
+++ packages/apps/explorer/src/pages/index.tsx
@@ -1,15 +1,6 @@
-import { useBlockQuery } from '@/__generated__/sdk';
 import React from 'react';
 
 const Home: React.FC = () => {
-  const { data: block, error: blockError } = useBlockQuery({
-    variables: {
-      hash: 'KSngxzXprxY8SMrH0ABajE8cbOvy_yZVtLgNUkxC3pg',
-    },
-  });
-
-  console.log(block, blockError);
-
   return (
     <>
       <p>K:Explorer</p>
