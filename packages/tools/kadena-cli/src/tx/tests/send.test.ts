ca5be160896d3646e9488f6164a4363695624cfb
Bart <45095973+barthuijgen@users.noreply.github.com>
2024-05-08 09:49:45 +0200

    feat(kadena-cli): fix legacy signing and add more tests (#2070)
    Co-authored-by: Bart Huijgen <barthuijgen@users.noreply.github.com>

--- packages/tools/kadena-cli/src/tx/tests/send.test.ts
+++ packages/tools/kadena-cli/src/tx/tests/send.test.ts
@@ -8,6 +8,7 @@ import { createTransaction } from '../commands/txCreateTransaction.js';
 import { signTransactionFileWithKeyPairAction } from '../utils/txSignWithKeypair.js';
 
 describe('tx send', () => {
+  // skipped because usage of live chainweb-api (only used for manual testing)
   it.skip('Prompts relevant values and sends transaction to chain', async () => {
     const publicKey =
       '2619fafe33b3128f38a4e4aefe6a5559371b18b6c25ac897aff165ce14b241b3';
