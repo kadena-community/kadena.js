72b2bad63553351d6448fc099114f9f35755ae9f
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-05-13 16:32:59 +0200

    feat(kadena-cli): add bundler

---
+++ packages/tools/kadena-cli/rollup.config.mjs
@@ -0,0 +1,14 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: './lib',
+    intro: 'const navigator = {};',
+    format: 'es',
+  },
+  plugins: [resolve(), commonjs(), typescript(), json()],
+};
