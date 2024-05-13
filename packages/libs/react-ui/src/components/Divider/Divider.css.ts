ad90f690a27ec1f57c030faa8c8b4eac76aaeecb
r-mulder <info@mulderr.com>
2024-05-13 17:00:33 +0200

    feat: add variant options with new colors

--- packages/libs/react-ui/src/components/Divider/Divider.css.ts
+++ packages/libs/react-ui/src/components/Divider/Divider.css.ts
@@ -1,15 +1,34 @@
-import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 import { atoms } from '../../styles/atoms.css';
 import { tokens } from '../../styles/index';
 
-export const dividerClass = style([
-  atoms({
-    width: '100%',
-    marginBlock: 'lg',
-    border: 'none',
-  }),
-  {
-    backgroundColor: tokens.kda.foundation.color.border.base.bold,
-    height: tokens.kda.foundation.border.width.hairline,
+export const dividerClass = recipe({
+  base: [
+    atoms({
+      width: '100%',
+      marginBlock: 'lg',
+      border: 'none',
+    }),
+    {
+      height: tokens.kda.foundation.border.width.hairline,
+    },
+  ],
+  variants: {
+    variant: {
+      subtle: {
+        backgroundColor: tokens.kda.foundation.color.border.base.subtle,
+      },
+      base: {
+        backgroundColor: tokens.kda.foundation.color.border.base.default,
+      },
+      bold: { backgroundColor: tokens.kda.foundation.color.border.base.bold },
+      boldest: {
+        backgroundColor: tokens.kda.foundation.color.border.base.boldest,
+      },
+      highContrast: {
+        backgroundColor:
+          tokens.kda.foundation.color.border.base['high-contrast'],
+      },
+    },
   },
-]);
+});
