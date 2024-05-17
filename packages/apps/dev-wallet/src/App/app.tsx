2b6f66cf4528bdde06005d265ccffa43d0957006
Sam Ferrero <ferrerocompanylimited@gmail.com>
2024-05-17 13:56:17 +0100

    chore: added hook and removed theme provider

--- packages/apps/dev-wallet/src/App/app.tsx
+++ packages/apps/dev-wallet/src/App/app.tsx
@@ -1,23 +1,14 @@
-import { darkThemeClass } from '@kadena/react-ui/styles';
-import { ThemeProvider } from 'next-themes';
-import { WalletProvider } from '@/modules/wallet/wallet.provider';
 import { DatabaseProvider } from '@/modules/db/db.provider';
+import { WalletProvider } from '@/modules/wallet/wallet.provider';
+import { useTheme } from '../hooks/useTheme';
 import { Routes } from './routes';
 
 function Providers({ children }: { children: React.ReactNode }) {
+  useTheme();
   return (
-    <ThemeProvider
-      attribute="class"
-      value={{
-        light: 'light',
-        dark: darkThemeClass,
-      }}
-      defaultTheme="dark"
-    >
-      <DatabaseProvider>
-        <WalletProvider>{children}</WalletProvider>
-      </DatabaseProvider>
-    </ThemeProvider>
+    <DatabaseProvider>
+      <WalletProvider>{children}</WalletProvider>
+    </DatabaseProvider>
   );
 }
 
---
+++ packages/apps/dev-wallet/src/hooks/useTheme.ts
@@ -0,0 +1,143 @@
+// Duplicated from docs until this gets exported from react-ui
+import { darkThemeClass } from '@kadena/react-ui/styles';
+import { useCallback, useEffect, useState } from 'react';
+const MEDIA = '(prefers-color-scheme: dark)';
+const storageKey = 'theme';
+const isServer = typeof window === 'undefined';
+const defaultTheme = 'system';
+
+export type ITheme = 'dark' | 'light' | 'system';
+interface IUseThemeProps {
+  /**
+   * lockedTheme will lock the theme for the whole application.
+   * It will overwrite the localstorage and even the switching of themes in your system will not update the theme
+   * IMPORTANT: using the useTheme twice in a application can lead to unexpected race conditions
+   * You would need to lock the theme on all the uses of this hook with the same ITheme
+   */
+  lockedTheme?: ITheme;
+  /**
+   * overwriteTheme can be used when you want 1 particular component to have a fixed theme
+   * IMPORTANT: this will also overwrite the lockedTheme for that component
+   */
+  overwriteTheme?: ITheme;
+}
+interface IUseThemeReturnProps {
+  theme: ITheme | undefined;
+  setTheme: (value: ITheme) => void;
+}
+
+const getTheme = (key: string) => {
+  if (isServer) return undefined;
+  let theme: ITheme | undefined = undefined;
+  try {
+    theme = (localStorage.getItem(key) as ITheme) || undefined;
+  } catch (e) {
+    console.error('localStorage is not suported in this browser');
+  }
+  return theme || defaultTheme;
+};
+
+const getSystemTheme = (e?: MediaQueryList | MediaQueryListEvent): ITheme => {
+  if (!e) e = window.matchMedia(MEDIA);
+  const isDark = e.matches;
+  const systemTheme = isDark ? 'dark' : 'light';
+  return systemTheme;
+};
+
+export const useTheme = ({
+  overwriteTheme,
+  lockedTheme,
+}: IUseThemeProps = {}): IUseThemeReturnProps => {
+  const [theme, setThemeState] = useState(() =>
+    lockedTheme ? lockedTheme : getTheme(storageKey),
+  );
+
+  const applyTheme = useCallback((theme: ITheme) => {
+    let resolved = theme;
+    console.log('applying');
+
+    if (!resolved) return;
+
+    // If theme is system, resolve it before setting theme
+    if (theme === 'system') {
+      resolved = getSystemTheme();
+    }
+    if (lockedTheme) {
+      resolved = lockedTheme;
+    }
+
+    const name = resolved;
+    const d = document.documentElement;
+
+    d.classList.remove('light');
+    d.classList.remove(darkThemeClass);
+    d.classList.add(name === 'dark' ? darkThemeClass : 'light');
+  }, []);
+
+  const setTheme = (
+    value: ITheme,
+    updateLocalStorage: boolean = true,
+  ): void => {
+    const resolved = lockedTheme ? lockedTheme : value;
+    setThemeState(resolved);
+    applyTheme(resolved);
+
+    if (updateLocalStorage) {
+      window.localStorage.setItem(storageKey, resolved);
+      window.dispatchEvent(new Event(storageKey));
+    }
+  };
+
+  const handleMediaQuery = useCallback(
+    (e: MediaQueryListEvent | MediaQueryList) => {
+      const resolved = lockedTheme ? lockedTheme : getSystemTheme(e);
+      setTheme(resolved);
+    },
+    [],
+  );
+
+  // Always listen to System preference
+  useEffect(() => {
+    if (lockedTheme) {
+      setTheme(lockedTheme);
+      return;
+    }
+
+    const media = window.matchMedia(MEDIA);
+    const theme = window.localStorage.getItem(storageKey) as ITheme;
+    if (theme) {
+      setTheme(theme);
+    } else {
+      handleMediaQuery(media);
+    }
+
+    media.addEventListener('change', handleMediaQuery);
+
+    return () => media.removeEventListener('change', handleMediaQuery);
+  }, []);
+
+  const storageListener = useCallback((event: StorageEvent | Event) => {
+    if (event.type !== storageKey && 'key' in event && event.key !== storageKey)
+      return;
+
+    // If default theme set, use it if localstorage === null (happens on local storage manual deletion)
+    const theme = lockedTheme
+      ? lockedTheme
+      : (window.localStorage.getItem(storageKey) as ITheme) || defaultTheme;
+    setTheme(theme, false);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener(storageKey, storageListener);
+    window.addEventListener('storage', storageListener);
+    return () => {
+      window.removeEventListener(storageKey, storageListener);
+      window.removeEventListener('storage', storageListener);
+    };
+  }, [storageListener]);
+
+  return {
+    theme: overwriteTheme ? overwriteTheme : theme,
+    setTheme,
+  };
+};
