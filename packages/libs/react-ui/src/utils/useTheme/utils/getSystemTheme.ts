06262511d1c79bcc69159122997b07878ad5b803
Steven Straatemans <steven@straatemans.nl>
2024-05-22 15:20:16 +0200

    fix

--- packages/libs/react-ui/src/utils/useTheme/utils/getSystemTheme.ts
+++ packages/libs/react-ui/src/utils/useTheme/utils/getSystemTheme.ts
@@ -1,4 +1,5 @@
-import { MEDIA } from './constants';
+import type { ITheme } from './constants';
+import { MEDIA, Themes } from './constants';
 
 export const getSystemTheme = (
   e?: MediaQueryList | MediaQueryListEvent,
@@ -6,6 +7,6 @@ export const getSystemTheme = (
   if (!e) e = window.matchMedia(MEDIA);
 
   const isDark = e.matches;
-  const systemTheme = isDark ? 'dark' : 'light';
+  const systemTheme = isDark ? Themes.dark : Themes.light;
   return systemTheme;
 };
