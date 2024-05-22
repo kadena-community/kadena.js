cfb7c5bab29290f6601f6e6bddeaf17e18332db4
Steven <steven@straatemans.nl>
2024-05-22 17:35:17 +0200

    fix(docs): remove the theme provider (#2137)

--- packages/apps/docs/src/pages/_app.tsx
+++ packages/apps/docs/src/pages/_app.tsx
@@ -11,9 +11,7 @@ import { MenuProvider } from '@/hooks/useMenu/MenuProvider';
 import { getLayout } from '@/utils/getLayout';
 import type { IPageMeta, IPageProps } from '@kadena/docs-tools';
 import { RouterProvider } from '@kadena/react-ui';
-import { darkThemeClass } from '@kadena/react-ui/styles';
 import { MDXProvider } from '@mdx-js/react';
-import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -107,25 +105,15 @@ export const MyApp = ({
         <link rel="apple-touch-icon" href="/assets/favicons/icon@192.png?1" />
       </Head>
       <MDXProvider components={markDownComponents}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="light"
-          value={{
-            light: 'light',
-            dark: darkThemeClass,
-          }}
-        >
-          <RouterProvider navigate={router.push}>
-            <MenuProvider>
-              <Header menuItems={props.headerMenuItems} />
-              <CookieConsent />
-              <Layout {...props}>
-                <Component {...props} />
-              </Layout>
-            </MenuProvider>
-          </RouterProvider>
-        </ThemeProvider>
+        <RouterProvider navigate={router.push}>
+          <MenuProvider>
+            <Header menuItems={props.headerMenuItems} />
+            <CookieConsent />
+            <Layout {...props}>
+              <Component {...props} />
+            </Layout>
+          </MenuProvider>
+        </RouterProvider>
       </MDXProvider>
       <Analytics />
     </>
