52e9774c2c429f83e9ff2cae5957d4cbab3b0299
Vasilika Klimova <2607lika@gmail.com>
2024-05-22 15:19:22 +0200

    Feat / Dev wallet/ onboarding (#2133)
    * feat(dev-wallet): Styles for onboarding
    
    * feat(dev-wallet): Simple password validation
    
    * feat(dev-wallet): Remove comments
    
    * feat(dev-wallet): Fix styles
    
    * feat(dev-wallet): password cnfirmation
    
    * feat(dev-wallet): Validation
    
    * feat(dev-wallet): Validation
    
    * fix(dev-wallet): show error messages in create profile form
    
    ---------
    
    Co-authored-by: likita <vasilika-ne@yandex.ru>
    Co-authored-by: Javad Khalilian <khalilian.javad@gmail.com>

---
+++ packages/apps/dev-wallet/src/Components/AuthCard/style.css.ts
@@ -0,0 +1,28 @@
+import { atoms } from '@kadena/react-ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const authCard = style([
+  atoms({
+    padding: 'xxl',
+  }),
+  {
+    borderRadius: '1px',
+    textAlign: 'left',
+    backgroundColor: 'rgba(255, 255, 255, 0.03)',
+    border: '1px solid rgba(255, 255, 255, 0.1)',
+  },
+]);
+
+export const backBtnClass = style([
+  atoms({
+    textDecoration: 'none',
+    color: 'text.base.default',
+  }),
+]);
+
+export const iconStyle = style([
+  atoms({
+    fontSize: 'sm',
+    color: 'text.base.default',
+  }),
+]);
