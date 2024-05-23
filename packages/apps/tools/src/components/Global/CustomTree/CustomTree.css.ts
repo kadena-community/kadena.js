291987bb987fbf56f401e78d3f39f807f3efb041
Sander Looijenga <sander.looijenga@kadena.io>
2024-05-23 16:17:43 +0200

    Add ellipsis truncating to Tree items

--- packages/apps/tools/src/components/Global/CustomTree/CustomTree.css.ts
+++ packages/apps/tools/src/components/Global/CustomTree/CustomTree.css.ts
@@ -1,8 +1,12 @@
+import { ellipsis, token } from '@kadena/react-ui/styles';
 import { style } from '@vanilla-extract/css';
 
-export const itemTitleStyle = style({
-  flex: 1,
-});
+export const itemTitleStyle = style([
+  ellipsis,
+  {
+    flex: 1,
+  },
+]);
 
 export const itemContainerStyle = style({
   // cursor: 'pointer',
