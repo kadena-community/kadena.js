7f964fc7cd2953060577a2f72280fabde1e7cd42
Steven Straatemans <steven@straatemans.nl>
2024-05-23 14:26:16 +0200

    fix the headers

--- packages/apps/docs/src/scripts/createPactDocs/index.ts
+++ packages/apps/docs/src/scripts/createPactDocs/index.ts
@@ -16,36 +16,6 @@ import {
   success,
 } from './utils/constants';
 
-const completeContentCreator = () => {
-  let content = '';
-
-  const get = () => content;
-
-  const set = (newContent: string) => {
-    content = `${content} /n/n ${newContent}`;
-  };
-
-  const createPage = () => {};
-
-  return { get, set, createPage };
-};
-const completeContent = completeContentCreator();
-
-// all the headers in this content need to have the depth increased by 1
-const fixHeaders = (content: string) => {
-  // Regular expression to match markdown headers
-  const headerRegex = /^(#+)(.*)$/gm;
-
-  // Function to replace matched headers with increased depth
-  const replaceHeader = (match: string, hashes: string, headerText: string) => {
-    const newHashes = `${hashes}# `; // Increase the depth by adding one more '#'
-    return `${newHashes}${headerText.trim()}`; // Trim header text to remove leading/trailing whitespace
-  };
-
-  // Replace headers in the markdown string
-  return content.replace(headerRegex, replaceHeader);
-};
-
 // TODO: this is temporary to checkout the correct branch. we can use the normal clone function when done
 export const clone = async (repo: string): Promise<void> => {
   try {
@@ -99,12 +69,8 @@ const copyPages = async (
       const destination = `./src/pages${page.destination}/index.md`;
       const pageContent = fs.readFileSync(destination, 'utf8');
 
-      const newContent = pageContent.replace(
-        CONTENTPLACEHOLDER,
-        fixHeaders(content),
-      );
+      const newContent = pageContent.replace(CONTENTPLACEHOLDER, content);
 
-      completeContent.set(newContent);
       fs.writeFileSync(destination, newContent);
     }
 
@@ -120,8 +86,6 @@ export const createPactDocs = async (): Promise<IScriptResult> => {
 
   const pages = loadConfigPages();
   await copyPages(pages);
-
-  completeContent.createPage();
 
   if (!errors.length) {
     success.push('pact docs created');
