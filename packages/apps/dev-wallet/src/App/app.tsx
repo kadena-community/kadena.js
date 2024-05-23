96d0047db9103f1425e9fe2216e786048e55356f
Sam Ferrero <ferrerocompanylimited@gmail.com>
2024-05-23 10:40:54 +0100

    chore: init theme

--- packages/apps/dev-wallet/src/App/app.tsx
+++ packages/apps/dev-wallet/src/App/app.tsx
@@ -1,12 +1,15 @@
 import { DatabaseProvider } from '@/modules/db/db.provider';
 import { WalletProvider } from '@/modules/wallet/wallet.provider';
+import { useTheme } from '@kadena/react-ui';
 import { Routes } from './routes';
 
 function Providers({ children }: { children: React.ReactNode }) {
+  // initialize the theme
+  useTheme();
   return (
-      <DatabaseProvider>
-        <WalletProvider>{children}</WalletProvider>
-      </DatabaseProvider>
+    <DatabaseProvider>
+      <WalletProvider>{children}</WalletProvider>
+    </DatabaseProvider>
   );
 }
 
