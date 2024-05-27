ab586e7ac6b754cc9df809f5be1d8b0111d80668
Sander Looijenga <sander.looijenga@kadena.io>
2024-05-27 09:11:42 +0200

    Fix display of badge

--- packages/apps/tools/src/components/Global/CustomTree/CustomTree.css.ts
+++ packages/apps/tools/src/components/Global/CustomTree/CustomTree.css.ts
@@ -5,8 +5,18 @@ export const itemTitleStyle = style([
   ellipsis,
   {
     flex: 1,
+    flexGrow: 2,
   },
 ]);
+
+export const itemBadgeStyle = style({
+  flex: 1,
+  flexGrow: 4,
+  padding: '0 4px',
+  fontSize: '12px',
+  lineHeight: '16px',
+  // fontFamily: token('typography.family.monospaceFont'),
+});
 
 export const itemContainerStyle = style({
   // cursor: 'pointer',
