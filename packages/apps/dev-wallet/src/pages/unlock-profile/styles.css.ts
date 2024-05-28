b648892dd79abd7828e2cb08e8be992569bdb196
lkononenko <liubov.kononenko@gmail.com>
2024-05-28 10:08:12 +0200

    style: use atoms for margin

--- packages/apps/dev-wallet/src/pages/unlock-profile/styles.css.ts
+++ packages/apps/dev-wallet/src/pages/unlock-profile/styles.css.ts
@@ -1,17 +1,20 @@
 import { style } from '@vanilla-extract/css';
-import { tokens } from '@kadena/react-ui/styles';
+import { tokens, atoms } from '@kadena/react-ui/styles';
 
 export const passwordContainer = style([
+  atoms({
+    marginBlockStart: 'md',
+  }),
   {
-    marginTop: tokens.kda.foundation.spacing.md,
     minHeight: '100px'
   },
 ]);
 
 export const profileContainer = style([
+  atoms({
+    marginBlockEnd: 'md',
+  }),
   {
     border: `1px solid ${tokens.kda.foundation.color.neutral.n20}`,
-    marginBottom: tokens.kda.foundation.spacing.md,
   },
 ]);
-
