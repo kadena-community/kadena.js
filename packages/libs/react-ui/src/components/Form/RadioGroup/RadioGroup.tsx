bc109f56d25cb9cedc9419803a612d061f090560
r-mulder <info@mulderr.com>
2024-05-29 13:57:03 +0200

    feat: add radiogroup and radio components

---
+++ packages/libs/react-ui/src/components/Form/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,34 @@
+import { RecipeVariants } from '@vanilla-extract/recipes';
+import React, { ReactElement, createContext } from 'react';
+import { AriaRadioGroupProps, useRadioGroup } from 'react-aria';
+import { RadioGroupState, useRadioGroupState } from 'react-stately';
+import { Field } from '../Field/Field';
+import { groupClass } from './Radio.css';
+
+type Direction = NonNullable<RecipeVariants<typeof groupClass>>['direction'];
+
+export interface IRadioGroupProps extends AriaRadioGroupProps {
+  children: ReactElement[] | ReactElement;
+  direction: Direction;
+  isReadOnly?: boolean;
+}
+
+export const RadioContext = createContext<RadioGroupState | null>(null);
+
+export function RadioGroup(props: IRadioGroupProps) {
+  const { children, direction = 'row', isReadOnly } = props;
+  const state = useRadioGroupState(props);
+  const { radioGroupProps, ...fieldprops } = useRadioGroup(props, state);
+
+  return (
+    <div {...radioGroupProps}>
+      <div className={groupClass({ direction })}>
+        <RadioContext.Provider value={state}>
+          {React.Children.map(children, (child) =>
+            React.cloneElement(child, { isReadOnly }),
+          )}
+        </RadioContext.Provider>
+      </div>
+    </div>
+  );
+}
