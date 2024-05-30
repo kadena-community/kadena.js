4d2f3a2efc676f524b86cf42326cca1acdb4adaf
Mohamed Ismail <ismailreality1@gmail.com>
2024-05-30 10:47:47 +0200

    test: improve msw setup by removing global handlers + exposing helpers

--- packages/tools/kadena-cli/src/mocks/server.ts
+++ packages/tools/kadena-cli/src/mocks/server.ts
@@ -1,5 +1,71 @@
+import { HttpResponse, http } from 'msw';
 import { setupServer } from 'msw/node';
-import { handlers } from './handlers.js';
 
-// This configures a Service Worker with the given request handlers.
-export const server = setupServer(...handlers);
+interface IHandlerOptions {
+  network: string;
+  chainId: string;
+  endpoint: string;
+  response: unknown;
+  status: number;
+  method?: 'post' | 'get';
+  httpProtocol?: 'http' | 'https';
+}
+
+interface IDynamicHandlerOptions extends IHandlerOptions {
+  getResponse: (req: Request) => Promise<unknown>;
+}
+
+const createHandler = ({
+  network,
+  chainId,
+  response,
+  status,
+  endpoint = 'local',
+  method = 'post',
+  httpProtocol = 'https',
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}: IHandlerOptions): any => {
+  const url = `${httpProtocol}://${network}.chainweb.com/chainweb/0.0/${network}/${chainId}/pact/api/v1/${endpoint}`;
+  return http[method](url, async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return HttpResponse.json(response as any, { status });
+  });
+};
+
+const createDynamicHandler = ({
+  network,
+  chainId,
+  status,
+  endpoint = 'local',
+  method = 'post',
+  httpProtocol = 'https',
+  getResponse,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}: IDynamicHandlerOptions): any => {
+  const url = `${httpProtocol}://${network}.chainweb.com/chainweb/0.0/${network}/${chainId}/pact/api/v1/${endpoint}`;
+  return http[method](url, async ({ request }) => {
+    const response = await getResponse(request);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return HttpResponse.json(response as any, { status });
+  });
+};
+
+const server = setupServer();
+
+const useHandler = (options: IHandlerOptions): void => {
+  const handler = createHandler(options);
+  server.use(handler);
+};
+
+const useDynamicHandler = (options: IDynamicHandlerOptions): void => {
+  const handler = createDynamicHandler(options);
+  server.use(handler);
+};
+
+export {
+  createDynamicHandler,
+  createHandler,
+  server,
+  useDynamicHandler,
+  useHandler,
+};
