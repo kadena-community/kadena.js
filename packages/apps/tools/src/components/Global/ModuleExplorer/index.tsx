735e5fd7bc01a42e6741eb202b7acaa4c263218b
Sander Looijenga <sander.looijenga@kadena.io>
2024-05-30 11:55:53 +0200

    Re-add previous functionality + grand cleanup

--- packages/apps/tools/src/components/Global/ModuleExplorer/index.tsx
+++ packages/apps/tools/src/components/Global/ModuleExplorer/index.tsx
@@ -1,23 +1,20 @@
 import type { IncompleteModuleModel } from '@/pages/modules/explorer/utils';
-import { contractParser, IModule } from '@kadena/pactjs-generator';
-import React, { useCallback, useState } from 'react';
-import { TreeItem } from '../CustomTree/CustomTree';
-import type { IEditorProps } from './editor';
-import Editor from './editor';
+import { isModule } from '@/pages/modules/explorer/utils';
+import type { ChainwebChainId } from '@kadena/chainweb-node-client';
+import { contractParser } from '@kadena/pactjs-generator';
+import React, { useState } from 'react';
+import type { TreeItem } from '../CustomTree/CustomTree';
 import type { ISidePanelProps } from './SidePanel';
 import SidePanel from './SidePanel';
+import type { IEditorProps } from './editor';
+import Editor from './editor';
 import { containerStyle } from './styles.css';
 import type { IChainModule } from './types';
-import { getModulesMap } from './utils';
 
 export interface IModuleExplorerProps {
-  // modules: IChainModule[];
-  // openedModules: IEditorProps['openedModules'];
-  // onModuleClick: ISidePanelProps['onResultClick'];
-  // onInterfaceClick: ISidePanelProps['onInterfaceClick'];
-  // onInterfacesExpand: ISidePanelProps['onInterfacesExpand'];
-  // onModuleExpand: ISidePanelProps['onModuleExpand'];
-  // onActiveModuleChange: IEditorProps['onActiveModuleChange'];
+  openedModules: IEditorProps['openedModules'];
+  onModuleClick: ISidePanelProps<IncompleteModuleModel>['onModuleClick'];
+  onActiveModuleChange: IEditorProps['onActiveModuleChange'];
   // onTabClose: IEditorProps['onTabClose'];
   items: ISidePanelProps<IncompleteModuleModel>['data'];
   onReload: ISidePanelProps<IncompleteModuleModel | Outline>['onReload'];
@@ -120,14 +117,16 @@ const chainModuleToOutlineTreeItems = (
   return treeItems;
 };
 
-// eslint-disable-next-line react/function-component-definition
-function ModuleExplorer({
+const ModuleExplorer = ({
   items,
   onReload,
   onExpandCollapse,
-}: IModuleExplorerProps) {
+  onActiveModuleChange,
+  openedModules: _openedModules,
+}: IModuleExplorerProps) => {
   const [activeModule, setActiveModule] = useState<IChainModule>();
-  const [openedModules, setOpenedModules] = useState<IChainModule[]>([]);
+  const [openedModules, setOpenedModules] =
+    useState<IChainModule[]>(_openedModules);
   let outlineItems: TreeItem<Outline>[] = [];
 
   if (activeModule) {
@@ -143,153 +142,67 @@ function ModuleExplorer({
             title: 'Explorer',
             key: 'explorer',
             label: 'Explorer',
-            data: {
-              name: 'Explorer',
-              chainId: '0',
-            },
+            data: 'explorer',
           },
           {
             title: 'Outline',
             key: 'outline',
             label: 'Outline',
             children: outlineItems,
-            data: {
-              name: 'Outline',
-              chainId: '0',
-            },
+            data: 'outline',
           },
         ]}
         onReload={onReload}
-        onModuleClick={(x) => {
-          const [network, moduleName] = (x.key as string).split('.');
-          const chainModule: IChainModule = {
-            code: x.data.code,
-            chainId: x.data.chainId,
-            moduleName,
-            hash: x.data.hash,
-            network,
-          };
-          setActiveModule(chainModule);
-          setOpenedModules([chainModule]);
+        onModuleClick={({ data }) => {
+          if (isModule(data)) {
+            const chainModule: IChainModule = {
+              code: data.code,
+              chainId: data.chainId,
+              moduleName: data.name,
+              hash: data.hash,
+              network: data.networkId,
+            };
+            setActiveModule(chainModule);
+            setOpenedModules((prev) => {
+              const alreadyOpened = prev.find((openedModule) => {
+                return (
+                  openedModule.moduleName === chainModule.moduleName &&
+                  openedModule.chainId === chainModule.chainId &&
+                  openedModule.network === chainModule.network
+                );
+              });
+
+              if (alreadyOpened) {
+                return prev;
+              }
+
+              return [...prev, chainModule];
+            });
+          }
         }}
         onExpandCollapse={onExpandCollapse}
       />
-      {/* <SidePanel>
-        <Item></Item>
-        <Item></Item>
-      </SidePanel> */}
       <Editor
         openedModules={openedModules}
         activeModule={activeModule}
         onActiveModuleChange={(module) => {
-          // onActiveModuleChange(module);
-          // setActiveModule(module);
+          onActiveModuleChange(module);
+          setActiveModule(module);
         }}
         onTabClose={(module) => {
-          // setOpenedModules(
-          //   openedModules.filter((openedModule) => {
-          //     return (
-          //       `${openedModule.moduleName}-${openedModule.chainId}-${openedModule.network}` !==
-          //       `${module.moduleName}-${module.chainId}-${module.network}`
-          //     );
-          //   }),
-          // );
+          setOpenedModules(
+            openedModules.filter((openedModule) => {
+              return (
+                `${openedModule.moduleName}-${openedModule.chainId}-${openedModule.network}` !==
+                `${module.moduleName}-${module.chainId}-${module.network}`
+              );
+            }),
+          );
           // onTabClose(module);
         }}
       />
     </div>
   );
-}
-
-// const ModuleExplorer = ({
-//   modules,
-//   openedModules: fetchedModules,
-//   onModuleClick,
-//   onInterfaceClick,
-//   onInterfacesExpand,
-//   onModuleExpand,
-//   onActiveModuleChange,
-//   onTabClose,
-// }: IModuleExplorerProps): React.JSX.Element => {
-//   const [activeModule, setActiveModule] = useState<IChainModule>();
-//   const results = getModulesMap(modules);
-//   const [openedModules, setOpenedModules] =
-//     useState<IChainModule[]>(fetchedModules);
-
-//   const updateOpenedModules = useCallback(
-//     (result: IChainModule) => {
-//       setOpenedModules((prev) => {
-//         const alreadyOpened = prev.find((openedModule) => {
-//           return (
-//             openedModule.moduleName === result.moduleName &&
-//             openedModule.chainId === result.chainId &&
-//             openedModule.network === result.network
-//           );
-//         });
-
-//         if (alreadyOpened) {
-//           return prev;
-//         }
-
-//         let add = result;
-
-//         const enhanced = modules.find((module) => {
-//           return (
-//             module.moduleName === result.moduleName &&
-//             module.chainId === result.chainId
-//           );
-//         });
-
-//         if (enhanced) {
-//           add = enhanced;
-//         }
-
-//         return [...prev, add];
-//       });
-//     },
-//     [modules],
-//   );
-
-//   return (
-//     <div className={containerStyle}>
-//       <SidePanel
-//         data={[]}
-//         // results={results}
-//         // onResultClick={(result) => {
-//         //   updateOpenedModules(result);
-//         //   onModuleClick(result);
-//         //   setActiveModule(result);
-//         // }}
-//         // onInterfaceClick={(result) => {
-//         //   updateOpenedModules(result);
-//         //   onInterfaceClick(result);
-//         //   setActiveModule(result);
-//         // }}
-//         // onInterfacesExpand={onInterfacesExpand}
-//         // onModuleExpand={onModuleExpand}
-//         // selectedModule={activeModule}
-//       />
-//       <Editor
-//         openedModules={openedModules}
-//         activeModule={activeModule}
-//         onActiveModuleChange={(module) => {
-//           onActiveModuleChange(module);
-//           setActiveModule(module);
-//         }}
-//         onTabClose={(module) => {
-//           setOpenedModules(
-//             openedModules.filter((openedModule) => {
-//               return (
-//                 `${openedModule.moduleName}-${openedModule.chainId}-${openedModule.network}` !==
-//                 `${module.moduleName}-${module.chainId}-${module.network}`
-//               );
-//             }),
-//           );
-//           onTabClose(module);
-//         }}
-//       />
-//     </div>
-//   );
-// };
+};
 
 export default ModuleExplorer;
