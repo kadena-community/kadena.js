983c02dd9b61167350afdfb549ad63ad86f7354a
Ghislain Gabriëlse <72976986+Ghislain89@users.noreply.github.com>
2024-06-03 15:56:36 +0200

    Test/proof of us (#1969)
    * chore: add proof-of-us test that creates a proof with an initiator and 1 signer.
    
    * remove buffer from root deps
    
    * chore: add test steps and sign with 5 users
    
    * chore update timeouts
    
    * test: add tags to tests
    
    * uodates
    
    * remove custom params
    
    * chore: update test and fix lockfile
    
    * set trace to default
    
    * chore: Formatting
    
    * chore: add missing labels
    
    * chore: dependencies
    
    * chore: formatting
    
    * chore: Formatting

--- packages/e2e/e2e-base/src/fixtures/shared/test.fixture.ts
+++ packages/e2e/e2e-base/src/fixtures/shared/test.fixture.ts
@@ -1,4 +1,5 @@
 import { mergeTests } from '@playwright/test';
 import { test as pageObjectsFixture } from './page-obects.fixture';
+import { test as personaFixture } from './persona.fixture';
 
-export const test = mergeTests(pageObjectsFixture);
+export const test = mergeTests(pageObjectsFixture, personaFixture);
