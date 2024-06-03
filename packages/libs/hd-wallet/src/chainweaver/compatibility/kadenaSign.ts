a240f44fbaa915c578da0620e57f0e0c3cece5db
Javad Khalilian <khalilian.javad@gmail.com>
2024-06-03 11:15:41 +0200

    hd-wallet: renamed massage to hash in kadenaSign for better readability (#2122)
    * refactor(hd-wallet): rename function variables for more readability
    
    * refactor(hd-wallet): rename function variables for more readability

--- packages/libs/hd-wallet/src/chainweaver/compatibility/kadenaSign.ts
+++ packages/libs/hd-wallet/src/chainweaver/compatibility/kadenaSign.ts
@@ -4,12 +4,12 @@ import { kadenaSign as originalKadenaSign } from '../kadena-crypto.js';
 
 export const kadenaSign = async (
   password: string | Uint8Array,
-  message: string,
+  hash: string, // base64 message
   secretKey: EncryptedString | Uint8Array,
 ): Promise<Uint8Array> => {
   return await originalKadenaSign(
     password,
-    Buffer.from(message, 'base64'),
+    Buffer.from(hash, 'base64'),
     await kadenaDecrypt(password, secretKey),
   );
 };
--- packages/libs/hd-wallet/src/chainweaver/compatibility/kadenaSignFromRootKey.ts
+++ packages/libs/hd-wallet/src/chainweaver/compatibility/kadenaSignFromRootKey.ts
@@ -4,20 +4,20 @@ import { kadenaSign } from '../vendor/kadena-crypto.cjs';
 import { kadenaGenKeypair } from './kadenaGenKeypair.js';
 
 /**
- * Sign a message with a root key and the index of the keypair to use
+ * Sign a base64 message with a root key and the index of the keypair to use
  * @param password
- * @param message
+ * @param hash // base64 hash
  * @param rootKey
  * @param index
  * @returns signature
  */
 export async function kadenaSignFromRootKey(
   password: string | Uint8Array,
-  message: string,
+  hash: string,
   rootKey: EncryptedString,
   index: number,
 ): Promise<Uint8Array> {
   const { secretKey } = await kadenaGenKeypair(password, rootKey, index);
   const secret = await kadenaDecrypt(password, secretKey);
-  return kadenaSign(password, Buffer.from(message, 'base64'), secret);
+  return kadenaSign(password, Buffer.from(hash, 'base64'), secret);
 }
