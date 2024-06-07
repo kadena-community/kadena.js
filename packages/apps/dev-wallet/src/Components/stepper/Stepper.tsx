26646e11111411cc5d41cc27b4296e17d940074f
Sam Ferrero <ferrerocompanylimited@gmail.com>
2024-06-07 18:08:53 +0100

    chore: added data atributes for css

--- packages/apps/dev-wallet/src/Components/stepper/Stepper.tsx
+++ packages/apps/dev-wallet/src/Components/stepper/Stepper.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 export const Stepper = ({
   steps,
@@ -9,14 +9,30 @@ export const Stepper = ({
   steps: { id: string; route: string; title?: string; elements: ReactNode }[];
 }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const isCompleted = (index: number) =>
+    index > steps.findIndex((step) => step.route === location.pathname);
+
   if (initialPath) {
     navigate(initialPath);
   }
   return (
     <>
-      {/* style this steps, if they have a title it will render bellow otherwise its just a line to show some progress */}
+      {/* style this steps, if they have a title it will render bellow otherwise its just a line to show some progress
+      we can use the selectors like
+      selectors: {
+        '&[data-active="true"]': css
+      }
+      */}
       {steps.map((step, index) => (
-        <span key={index}> {step.title}</span>
+        <span
+          key={index}
+          data-completed={isCompleted(index)}
+          data-active={location.pathname === step.route}
+        >
+          {' '}
+          {step.title}
+        </span>
       ))}
       <Routes>
         {steps.map((step) => (
