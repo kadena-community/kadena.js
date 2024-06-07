3f228b68f340fff4becfc70be2b76436c9b36c67
Steven Straatemans <steven@straatemans.nl>
2024-06-07 16:37:38 +0200

    cleanup

--- packages/apps/explorer/src/services/format.ts
+++ packages/apps/explorer/src/services/format.ts
@@ -6,7 +6,6 @@ export function formatNumberWithUnit(number: number, unit?: string): string {
   }
   const units = ['', 'K', 'M', 'B', 'T', 'P', 'E'];
   const unitIndex = Math.floor(Math.log10(Math.abs(number)) / 3);
-  console.log(unitIndex);
   const formattedNumber = (number / Math.pow(1000, unitIndex)).toFixed(2);
   return `${formattedNumber} ${unit !== undefined ? unit : units[unitIndex] ? units[unitIndex] : ''}`;
 }
