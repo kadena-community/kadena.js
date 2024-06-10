bef0da83c8772764578ac42d0cd79e9a435c538a
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-06-10 17:06:43 +0200

    test(kadena-cli): improve tx local test

--- packages/tools/kadena-cli/src/tx/tests/local.test.ts
+++ packages/tools/kadena-cli/src/tx/tests/local.test.ts
@@ -1,57 +1,48 @@
+import type { ICommandPayload } from '@kadena/types';
+import { HttpResponse, http } from 'msw';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { assertCommandError } from '../../utils/command.util.js';
-import { mockPrompts, runCommand } from '../../utils/test.util.js';
-import { defaultTemplates } from '../commands/templates/templates.js';
-import { createAndWriteTransaction } from '../commands/txCreateTransaction.js';
-import { signTransactionFileWithKeyPairAction } from '../utils/txSignWithKeypair.js';
+import { server } from '../../mocks/server.js';
+import { safeJsonParse } from '../../utils/globalHelpers.js';
+import { runCommand } from '../../utils/test.util.js';
+
+const getCommand = (body: string): ICommandPayload | null => {
+  const json = safeJsonParse(body);
+  const cmd =
+    json !== null && typeof json === 'object' && 'cmd' in json
+      ? (json.cmd as string)
+      : null;
+  return cmd !== null ? safeJsonParse<ICommandPayload>(cmd) : null;
+};
 
 describe('tx local', () => {
-  // Mock msw endpoint,
-
-  const publicKey =
-    '2619fafe33b3128f38a4e4aefe6a5559371b18b6c25ac897aff165ce14b241b3';
-  const secretKey =
-    'c4e33c93182268c5ef79979493c7d834c81e62ceed22f8ea235cc776c3da0a43';
-  const targetAccount =
-    'k:00b34067644479c769b48b4cc9b2c732e48fc9aeb82d06ecd52dc783550de54d';
-
   beforeEach(async () => {
     vi.clearAllMocks();
   });
 
-  // todo
-  it.skip('Submits a valid transaction and receives success response', async () => {
-    const transaction = await createAndWriteTransaction(
-      defaultTemplates.transfer,
-      {
-        'account:from': `k:${publicKey}`,
-        'account:to': targetAccount,
-        'decimal:amount': '0.01',
-        'chain-id': '1',
-        'key:from': publicKey,
-        'network:networkId': 'testnet04',
-      },
-      null,
+  it('Submits a valid transaction and receives success response', async () => {
+    server.use(
+      http.post(
+        'https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/pact/api/v1/local',
+        async (req): Promise<HttpResponse> => {
+          const cmd = getCommand(await req.request.text());
+          if (
+            cmd &&
+            'exec' in cmd.payload &&
+            cmd.payload.exec.code === '(+ 1 1)'
+          ) {
+            return HttpResponse.json({
+              result: { status: 'success', data: 2 },
+            });
+          } else {
+            return HttpResponse.json(
+              { result: { status: 'failure', error: { message: 'error' } } },
+              { status: 400 },
+            );
+          }
+        },
+      ),
     );
-
-    assertCommandError(transaction);
-
-    await signTransactionFileWithKeyPairAction({
-      files: [transaction.data.filePath],
-      keyPairs: [{ publicKey, secretKey }],
-    });
-
-    mockPrompts({
-      input: {
-        'Enter ChainId': '1',
-      },
-      select: {
-        'Select network': 'testnet04',
-      },
-    });
-
-    const { stdout } = await runCommand(`tx local '(+ 1 1)'`);
-
-    expect(stdout.includes('Local transaction on network')).toBe(true);
+    const { stdout } = await runCommand(['tx', 'local', '(+ 1 1)']);
+    expect(stdout).toBe('2');
   });
 });
