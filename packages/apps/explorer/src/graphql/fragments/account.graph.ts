51e34b300644b8731c12b125f0630b891c28fe61
Nil Amrutlal <112877112+nil-amrutlal-dept@users.noreply.github.com>
2024-06-10 15:17:55 +0200

    Queries and fragments needed for main seach (#2259)
    * feat: added queries needed for main search
    
    * chore: changeset file
    
    * fix: lint fixes

---
+++ packages/apps/explorer/src/graphql/fragments/account.graph.ts
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+import type { DocumentNode } from 'graphql';
+
+export const CORE_ACCOUNT_FIELDS: DocumentNode = gql`
+  fragment CoreAccountFields on FungibleAccount {
+    accountName
+    totalBalance
+    chainAccounts {
+      chainId
+      guard {
+        keys
+        predicate
+      }
+    }
+  }
+`;
