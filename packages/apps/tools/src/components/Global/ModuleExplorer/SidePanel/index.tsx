54e8852c49558354081e8e44b112844a421bd50e
Sander Looijenga <sander.looijenga@kadena.io>
2024-06-13 11:07:42 +0200

    Initial search bar functionality

--- packages/apps/tools/src/components/Global/ModuleExplorer/SidePanel/index.tsx
+++ packages/apps/tools/src/components/Global/ModuleExplorer/SidePanel/index.tsx
@@ -1,11 +1,25 @@
 import {
   MonoArrowDropDown,
   MonoArrowRight,
+  MonoManageSearch,
   MonoMenuOpen,
+  MonoSearch,
   MonoVerticalSplit,
 } from '@kadena/react-icons/system';
-import { Button, Heading, Stack } from '@kadena/react-ui';
-import React from 'react';
+
+import type { ChainwebNetworkId } from '@kadena/chainweb-node-client';
+import {
+  Box,
+  Button,
+  Heading,
+  Select,
+  SelectItem,
+  Stack,
+  Text,
+  TextField,
+} from '@kadena/react-ui';
+import React, { useState } from 'react';
+import { useDebounce } from 'react-use';
 import CustomAccordion from '../../CustomAccordion/CustomAccordion';
 import type { ICustomTreeProps, TreeItem } from '../../CustomTree/CustomTree';
 import CustomTree from '../../CustomTree/CustomTree';
@@ -14,6 +28,8 @@ import {
   headingStyles,
   iconStyles,
   itemStyle,
+  searchResultQueryStyles,
+  searchResultsStyles,
 } from './styles.css';
 
 export interface ISidePanelProps<T> {
@@ -22,6 +38,7 @@ export interface ISidePanelProps<T> {
   onReload: ICustomTreeProps<T>['onReload'];
   onModuleClick: (module: TreeItem<T>) => void;
   onExpandCollapse: (item: TreeItem<T>, expanded: boolean) => void;
+  onSearch: ISearchBarProps['onSearch'];
 }
 
 function SidePanel<T>({
@@ -29,7 +46,9 @@ function SidePanel<T>({
   onReload,
   onModuleClick,
   onExpandCollapse,
+  onSearch,
 }: ISidePanelProps<T>) {
+  const [showSearch, setShowSearch] = useState(true || false);
   return (
     <CustomAccordion
       items={items}
@@ -56,6 +75,17 @@ function SidePanel<T>({
             <Heading variant="h6" className={headingStyles}>
               {item.data.title}
             </Heading>
+            {item.data.supportsSearch ? (
+              <Button
+                isCompact
+                variant="transparent"
+                onPress={() => {
+                  setShowSearch(!showSearch);
+                }}
+              >
+                <MonoSearch />
+              </Button>
+            ) : null}
             <Button
               isCompact
               variant="transparent"
@@ -65,17 +95,78 @@ function SidePanel<T>({
             </Button>
           </Stack>
           {item.isExpanded ? (
-            <CustomTree
-              data={item.data.children}
-              onReload={onReload}
-              onItemClick={onModuleClick}
-              onExpandCollapse={onExpandCollapse}
-              {...item.accessibilityProps}
-            />
+            <>
+              {showSearch ? (
+                <Search
+                  networks={item.data.children.map(
+                    (x) => x.key as ChainwebNetworkId,
+                  )}
+                  onSearch={onSearch}
+                />
+              ) : null}
+              <CustomTree
+                items={item.data.children}
+                onReload={onReload}
+                onItemClick={onModuleClick}
+                onExpandCollapse={onExpandCollapse}
+                {...item.accessibilityProps}
+              />
+            </>
           ) : null}
         </>
       )}
     </CustomAccordion>
+  );
+}
+
+interface ISearchBarProps {
+  networks: ChainwebNetworkId[];
+  onSearch: (query: string) => number;
+}
+
+function Search({ networks, onSearch }: ISearchBarProps) {
+  console.log('Search render', networks);
+  const [searchHits, setSearchHits] = useState(0);
+  const [query, setQuery] = useState('');
+  // const deferredQuery = useDeferredValue(query);
+  useDebounce(
+    () => {
+      const hits = onSearch(query);
+      setSearchHits(hits);
+    },
+    1000,
+    [query],
+  );
+
+  return (
+    <Box padding="sm">
+      <Stack>
+        <TextField
+          startVisual={<MonoManageSearch />}
+          onChange={(x) => {
+            console.log('onChange', { x });
+            // const hits = onSearch(x.target.value);
+            // console.log('onChange2', { hits });
+
+            // setSearchHits(hits);
+
+            setQuery(x.target.value);
+          }}
+        />
+        <Select>
+          <SelectItem>HI</SelectItem>
+        </Select>
+      </Stack>
+      {query.length ? (
+        <Text className={searchResultsStyles} as="p" size="smallest" bold>
+          {`${searchHits} Modules found for `}
+          <Text
+            size="smallest"
+            className={searchResultQueryStyles}
+          >{`"${query}"`}</Text>
+        </Text>
+      ) : null}
+    </Box>
   );
 }
 
