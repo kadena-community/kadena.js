55f8d846c398a49a5b40415f681cbaffed20a8b8
Sander Looijenga <sander.looijenga@kadena.io>
2024-06-13 11:08:33 +0200

    Initial search & filtering modules list functionality

--- packages/apps/tools/src/components/Global/ModuleExplorer/index.tsx
+++ packages/apps/tools/src/components/Global/ModuleExplorer/index.tsx
@@ -2,7 +2,14 @@ import type {
   IncompleteModuleModel,
   ModuleModel,
 } from '@/hooks/use-module-query';
-import React, { useState } from 'react';
+import {
+  mapToTreeItems,
+  modelsToTreeMap,
+} from '@/pages/modules/explorer/utils';
+import type { ChainwebNetworkId } from '@kadena/chainweb-node-client';
+import type { FuseResult, IFuseOptions } from 'fuse.js';
+import Fuse from 'fuse.js';
+import React, { useCallback, useMemo, useState } from 'react';
 import type { TreeItem } from '../CustomTree/CustomTree';
 import type { ISidePanelProps } from './SidePanel';
 import SidePanel from './SidePanel';
@@ -18,12 +25,41 @@ export interface IModuleExplorerProps {
   onModuleClick: ISidePanelProps<IncompleteModuleModel>['onModuleClick'];
   onActiveModuleChange: IEditorProps['onActiveModuleChange'];
   // onTabClose: IEditorProps['onTabClose'];
-  items: ISidePanelProps<IncompleteModuleModel>['data'];
+  // items: Omit<ISidePanelProps<IncompleteModuleModel>['items'], 'children'>;
+  items: Omit<TreeItem<IncompleteModuleModel[]>, 'children'>[];
   onReload: ISidePanelProps<IncompleteModuleModel | Outline>['onReload'];
   onExpandCollapse: ISidePanelProps<
     IncompleteModuleModel | Outline
   >['onExpandCollapse'];
 }
+
+const generateDataMap = (
+  items: IModuleExplorerProps['items'],
+): Map<ChainwebNetworkId, IncompleteModuleModel[]> => {
+  const dataMap = new Map<ChainwebNetworkId, IncompleteModuleModel[]>();
+
+  items.forEach((item) => {
+    const networkId = item.key as ChainwebNetworkId;
+    dataMap.set(networkId, item.data);
+  });
+
+  return dataMap;
+};
+
+const searchResultsToDataMap = (
+  results: FuseResult<IncompleteModuleModel>[],
+): Map<ChainwebNetworkId, IncompleteModuleModel[]> => {
+  return results.reduce<Map<ChainwebNetworkId, IncompleteModuleModel[]>>(
+    (acc, result) => {
+      const networkId = result.item.networkId;
+      const data = acc.get(networkId) || [];
+      data.push(result.item);
+      acc.set(networkId, data);
+      return acc;
+    },
+    new Map(),
+  );
+};
 
 const ModuleExplorer = ({
   items,
@@ -39,11 +75,90 @@ const ModuleExplorer = ({
   const [openedModules, setOpenedModules] =
     useState<ModuleModel[]>(_openedModules);
 
+  // const [data, setData] = useState<
+  //   Map<ChainwebNetworkId, IncompleteModuleModel[]>
+  // >(generateDataMap(items));
+  const data = generateDataMap(items);
+  const [filteredData, setFilteredData] = useState<
+    Map<ChainwebNetworkId, IncompleteModuleModel[]>
+  >(new Map());
+
+  console.log('rerender ModuleExplorer', data);
+
+  const mapped = items.map((item) => {
+    const networkId = item.key as ChainwebNetworkId;
+    return {
+      ...item,
+      data: {
+        name: item.key as string,
+        chainId: '0' as const,
+        networkId: networkId,
+      },
+      children: mapToTreeItems(
+        modelsToTreeMap(
+          filteredData.get(networkId) || data.get(networkId) || [],
+        ),
+        item.key as string,
+        !filteredData.get(networkId),
+      ),
+    };
+  });
+
   let outlineItems: TreeItem<Outline>[] = [];
 
   if (activeModule) {
-    outlineItems = moduleToOutlineTreeItems(activeModule, items);
+    outlineItems = moduleToOutlineTreeItems(activeModule, mapped);
   }
+
+  console.log('rerender ModuleExplorer', items);
+
+  const allData = useMemo(() => {
+    return items.flatMap((item) => item.data);
+  }, [items]);
+
+  const onSearch = useCallback(
+    (searchQuery: string) => {
+      console.log('onSearcg', { searchQuery, allData });
+      const fuseOptions: IFuseOptions<IncompleteModuleModel> = {
+        // isCaseSensitive: false,
+        includeScore: true,
+        // shouldSort: true,
+        // includeMatches: false,
+        // findAllMatches: false,
+        // minMatchCharLength: 1,
+        // location: 0,
+        // threshold: 0.6, // 0.4 is the minimum to e.g. match "fuacet" to "faucet"
+        // distance: 100,
+        // useExtendedSearch: false,
+        ignoreLocation: true,
+        // ignoreFieldNorm: false,
+        // fieldNormWeight: 1,
+        keys: [
+          { name: 'title', getFn: (item) => item.name },
+          { name: 'hash', getFn: (item) => item.hash ?? '' },
+        ],
+      };
+
+      const fuse = new Fuse(allData, fuseOptions);
+
+      // const y = fuse.search({
+      //   title: searchQuery,
+      //   // hash: searchQuery,
+      // });
+      const y = fuse.search(searchQuery);
+
+      console.log('searchResultComplete', y);
+      console.log(
+        'searchResult',
+        y.map((x) => x.item.name),
+      );
+
+      setFilteredData(searchResultsToDataMap(y));
+
+      return y.length;
+    },
+    [allData],
+  );
 
   return (
     <div className={containerStyle}>
@@ -52,8 +167,9 @@ const ModuleExplorer = ({
           {
             title: 'Explorer',
             key: 'explorer',
-            children: items,
+            children: mapped,
             data: 'explorer',
+            supportsSearch: true,
           },
           {
             title: 'Outline',
@@ -62,6 +178,7 @@ const ModuleExplorer = ({
             data: 'outline',
           },
         ]}
+        onSearch={onSearch}
         onReload={onReload}
         onModuleClick={({ data }) => {
           if (isCompleteModule(data)) {
--- packages/apps/tools/src/pages/modules/explorer/index.tsx
+++ packages/apps/tools/src/pages/modules/explorer/index.tsx
@@ -1,4 +1,3 @@
-import type { TreeItem } from '@/components/Global/CustomTree/CustomTree';
 import ModuleExplorer from '@/components/Global/ModuleExplorer';
 import type { IEditorProps } from '@/components/Global/ModuleExplorer/editor';
 import { isModuleLike } from '@/components/Global/ModuleExplorer/types';
@@ -21,7 +20,7 @@ import type {
   InferGetServerSidePropsType,
 } from 'next/types';
 import React, { useCallback, useEffect } from 'react';
-import { getQueryValue, mapToTreeItems, modelsToTreeMap } from './utils';
+import { getQueryValue } from './utils';
 
 const QueryParams = {
   MODULE: 'module',
@@ -89,27 +88,36 @@ const ModuleExplorerPage = (
   const mainnetModulesQuery = useModulesQuery('mainnet01');
   const testnetModulesQuery = useModulesQuery('testnet04');
 
-  let mappedMainnet: TreeItem<IncompleteModuleModel>[] = [];
-  let amountOfMainnetModules = 0;
+  // let mappedMainnet: TreeItem<IncompleteModuleModel>[] = [];
+  // let amountOfMainnetModules = 0;
 
-  if (mainnetModulesQuery.isSuccess) {
-    mappedMainnet = mapToTreeItems(
-      modelsToTreeMap(mainnetModulesQuery.data),
-      'mainnet',
-    );
-    amountOfMainnetModules = mainnetModulesQuery.data.length;
-  }
+  // if (mainnetModulesQuery.isSuccess) {
+  //   mappedMainnet = mapToTreeItems(
+  //     modelsToTreeMap(mainnetModulesQuery.data),
+  //     'mainnet',
+  //   );
+  //   amountOfMainnetModules = mainnetModulesQuery.data.length;
+  // }
 
-  let mappedTestnet: TreeItem<IncompleteModuleModel>[] = [];
-  let amountOfTestnetModules = 0;
+  // let mappedTestnet: TreeItem<IncompleteModuleModel>[] = [];
+  // let amountOfTestnetModules = 0;
 
-  if (testnetModulesQuery.isSuccess) {
-    mappedTestnet = mapToTreeItems(
-      modelsToTreeMap(testnetModulesQuery.data),
-      'testnet',
-    );
-    amountOfTestnetModules = testnetModulesQuery.data.length;
-  }
+  // if (testnetModulesQuery.isSuccess) {
+  //   console.log(
+  //     'lets search',
+  //     new Fuse(testnetModulesQuery.data, {
+  //       keys: ['name'],
+  //       includeScore: true,
+  //       // findAllMatches: true,
+  //     }).search('fuacet'),
+  //   );
+
+  //   mappedTestnet = mapToTreeItems(
+  //     modelsToTreeMap(testnetModulesQuery.data),
+  //     'testnet',
+  //   );
+  //   amountOfTestnetModules = testnetModulesQuery.data.length;
+  // }
 
   // const customNetworks = networksData.filter(
   //   (n) => n.networkId !== 'mainnet01' && n.networkId !== 'testnet04',
@@ -161,21 +169,23 @@ const ModuleExplorerPage = (
         items={[
           {
             title: 'Mainnet',
-            key: 'mainnet',
-            children: mappedMainnet,
-            data: { name: 'mainnet01', chainId: '0', networkId: 'mainnet01' },
+            key: 'mainnet01',
+            // children: mappedMainnet,
+            // data: { name: 'mainnet01', chainId: '0', networkId: 'mainnet01' },
+            data: mainnetModulesQuery.data!,
             isLoading: mainnetModulesQuery.isFetching,
             supportsReload: true,
-            label: amountOfMainnetModules,
+            label: mainnetModulesQuery.data?.length ?? 0,
           },
           {
             title: 'Testnet',
-            key: 'testnet',
-            children: mappedTestnet,
-            data: { name: 'testnet04', chainId: '0', networkId: 'testnet04' },
+            key: 'testnet04',
+            // children: mappedTestnet,
+            // data: { name: 'testnet04', chainId: '0', networkId: 'testnet04' },
+            data: testnetModulesQuery.data!,
             isLoading: testnetModulesQuery.isFetching,
             supportsReload: true,
-            label: amountOfTestnetModules,
+            label: testnetModulesQuery.data?.length ?? 0,
           },
         ]}
         onReload={(treeItem) => {
