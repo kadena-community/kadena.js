2266ddfc2cf0bb5fdaaf430de5f400bcf0dfaf4d
Ghislain Gabriëlse <72976986+Ghislain89@users.noreply.github.com>
2024-06-13 21:53:01 +0200

    chore: remove obsolete packages (#2009)
    * chore: remove obsolete packages
    
    * chore: fix lockfile
    
    * chore: fix merge conflicts
    
    * chore: fix more merge conflicts
    
    * chore: lockfile
    
    * remove obsolete package files
    
    * remove obsolete files
    
    * fix: marmalade test pnpm script

--- packages/libs/toolbox/src/prelude/init.handlebars.ts
+++
@@ -1,7 +0,0 @@
-export const template = `{{#each preludes as |prelude|}}
-(load "{{prelude}}/install.repl")
-{{/each}}
-(load "tools/test-accounts.repl")
-(env-gasmodel "table")
-(env-gaslimit {{gasLimit}})
-(print "Initialized gas model 'table'")`;
