a0d3a7dbf6e20ca1997b1861bff050af3edb0b7f
Vasilika Klimova <2607lika@gmail.com>
2024-06-18 12:24:20 +0200

    feat(dev-wallet): Dashboard (#2262)
    * feat(dev-wallet): Dashboard
    
    * refactor(hd-wallet): keep account loged in
    
    * refactor(dev-wallet): use localstorage instead of sessionStorage
    
    * feat(dev-wallet): session
    
    * chore: lint
    
    ---------
    
    Co-authored-by: likita <vasilika-ne@yandex.ru>
    Co-authored-by: Javad Khalilian <khalilian.javad@gmail.com>

---
+++ packages/apps/dev-wallet/src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,43 @@
+import { MonoDataThresholding, MonoSignature } from '@kadena/react-icons';
+import { Box, Stack, Text } from '@kadena/react-ui';
+import type { FC } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import {
+  sidebarClass,
+  sidebarLinkClass,
+  sidebarMenuClass,
+  sidebarMenuOptionClass,
+} from './style.css.ts';
+
+export const Sidebar: FC = () => {
+  useParams();
+  return (
+    <Box paddingBlockStart="xxl" padding="xl" className={sidebarClass}>
+      <aside>
+        <Text transform="uppercase" size="smallest">
+          Pages
+        </Text>
+        <Box marginBlockStart="md">
+          <ul className={sidebarMenuClass}>
+            <li className={sidebarMenuOptionClass}>
+              <NavLink to="/" className={sidebarLinkClass}>
+                <Stack alignItems="center" gap="md">
+                  <MonoDataThresholding></MonoDataThresholding>
+                  Dashboard
+                </Stack>
+              </NavLink>
+            </li>
+            <li className={sidebarMenuOptionClass}>
+              <NavLink to="/sig-builder" className={sidebarLinkClass}>
+                <Stack alignItems="center" gap="md">
+                  <MonoSignature></MonoSignature>
+                  Sig Builder
+                </Stack>
+              </NavLink>
+            </li>
+          </ul>
+        </Box>
+      </aside>
+    </Box>
+  );
+};
---
+++ packages/apps/dev-wallet/src/utils/color.ts
@@ -0,0 +1,6 @@
+export const cardColor = 'rgba(255, 255, 255, 0.03)';
+export const cardBackgroundColorHover = 'rgba(255, 255, 255, 0.07)';
+export const cardColorHover = 'rgba(255, 255, 255, 0.2)';
+export const linkBlockColor = 'rgba(255, 255, 255, 0.6)';
+export const cardHoverColor = 'rgba(255, 255, 255, 0.1)';
+export const sidebarTextColorClass = 'rgba(255, 255, 255, 0.4)'; // #FFFFFF66
