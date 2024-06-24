a5e69f3dcc18e7fccb3c96af9a80363890bebbb2
Nil Amrutlal <112877112+nil-amrutlal-dept@users.noreply.github.com>
2024-06-24 15:44:13 +0200

    GraphQL Query Dialog Viewer (Feat) (#2315)
    * wip: query dialog
    
    * fix: add graph to path in env host variables
    
    * chore: changeset file
    
    * wip: added query context to app
    
    * feat: added setQueries on block-table
    
    * feat: remove prop from graphql viewer; used context instead
    
    * fix: remove duplicate graphqlviewer
    
    * feat: add graphquerydialog to navbar and add fragments to queries
    
    * feat: set queries in detail pages
    
    * fix: linting
    
    * fix: type in variable

--- packages/apps/explorer/src/graphql/queries/blocks-from-heights.graph.ts
+++ packages/apps/explorer/src/graphql/queries/blocks-from-heights.graph.ts
@@ -1,7 +1,10 @@
 import { gql } from '@apollo/client';
 import type { DocumentNode } from 'graphql';
+import { CORE_BLOCK_FIELDS } from '../fragments/block.graph';
 
 export const coreBlocksFromHeights: DocumentNode = gql`
+  ${CORE_BLOCK_FIELDS}
+
   query blocksFromHeights(
     $startHeight: Int!
     $endHeight: Int
