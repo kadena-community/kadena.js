3ae9e42418ee90d7a38c8657ceeca9dcec8af74d
Steven Straatemans <steven@straatemans.nl>
2024-06-27 16:15:17 +0200

    add content

--- packages/apps/explorer/src/components/footer/footer.tsx
+++ packages/apps/explorer/src/components/footer/footer.tsx
@@ -7,6 +7,8 @@ import { Heading, Stack, Text } from '@kadena/react-ui';
 import type { FC } from 'react';
 import React from 'react';
 import Logo from '../logo/logo';
+import FooterColumn from './footer-column';
+import FooterLink from './footer-link';
 import {
   doubleContentClass,
   footerClass,
@@ -23,7 +25,7 @@ const Footer: FC = () => {
       flexDirection="column"
       gap="xxxl"
     >
-      <Stack width="100%" flexDirection={{ xs: 'column', md: 'row' }}>
+      <Stack width="100%" flexDirection={{ xs: 'column', sm: 'row' }}>
         <Stack flex={1} flexDirection="column" gap="lg" marginInlineEnd="lg">
           <Logo />
           <Text>
@@ -31,7 +33,7 @@ const Footer: FC = () => {
             secure, and efficient smart contracts.
           </Text>
           <Stack flexDirection="column" marginBlockEnd="md">
-            <Heading as="h5">Socials</Heading>
+            <Heading as="h6">Socials</Heading>
             <Stack gap="sm">
               <a
                 className={socialLinkClass}
@@ -41,6 +43,7 @@ const Footer: FC = () => {
               >
                 <MonoLogoX />
               </a>
+
               <a
                 className={socialLinkClass}
                 href="https://www.linkedin.com/company/kadena-llc/mycompany/"
@@ -62,30 +65,95 @@ const Footer: FC = () => {
         </Stack>
         <Stack
           className={doubleContentClass}
+          gap="xl"
           flexDirection={{ xs: 'column', lg: 'row' }}
         >
-          <Stack flex={1} marginBlockStart="sm">
-            <Heading as="h5">Build</Heading>
-          </Stack>
-          <Stack flex={1} marginBlockStart="sm">
-            <Heading as="h5">Blockchain</Heading>
-          </Stack>
+          <FooterColumn title="Build">
+            <FooterLink href="https://www.kadena.io/marmalade">
+              Marmalade
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/spirekey">
+              Kadena SpireKey
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/tools">Tools</FooterLink>
+            <FooterLink href="https://chainweaver.kadena.network/">
+              Chainweaver Wallet
+            </FooterLink>
+            <FooterLink href="https://explorer.chainweb.com/mainnet">
+              Block Explorer
+            </FooterLink>
+            <FooterLink href="https://www.npmjs.com/package/@kadena/client">
+              Kadena Client
+            </FooterLink>
+            <FooterLink href="https://tools.kadena.io/">
+              Builder Tools
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/ecosystem">
+              Ecosystem
+            </FooterLink>
+            <FooterLink href="https://docs.kadena.io/">
+              Developer Docs
+            </FooterLink>
+          </FooterColumn>
+          <FooterColumn title="Blockchain">
+            <FooterLink href="https://www.kadena.io/chainweb">
+              Chainweb
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/pact">Pact</FooterLink>
+            <FooterLink href="https://www.kadena.io/ecosystem">
+              Ecosystem
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/defi">Defi</FooterLink>
+          </FooterColumn>
         </Stack>
 
         <Stack
           className={doubleContentClass}
+          gap="xl"
           flexDirection={{ xs: 'column', lg: 'row' }}
         >
-          <Stack flex={1} marginBlockStart="sm">
-            <Heading as="h5">Learn</Heading>
-          </Stack>
-          <Stack flex={1} marginBlockStart="sm">
-            <Heading as="h5">Participate</Heading>
-          </Stack>
+          <FooterColumn title="Learn">
+            <FooterLink href="https://www.kadena.io/blog">Blog</FooterLink>
+            <FooterLink href="https://www.kadena.io/whitepapers">
+              Whitepapers
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/academy">
+              Academy
+            </FooterLink>
+            <FooterLink href="https://docs.kadena.io/">
+              Developer Docs
+            </FooterLink>
+          </FooterColumn>
+          <FooterColumn title="Participate">
+            <FooterLink href="https://www.kadena.io/community">
+              Community
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/kda-token">
+              KDA Token
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/grants">Grants</FooterLink>
+            <FooterLink href="https://www.kadena.io/cabinet">
+              Kadena Cabinet
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/connect">
+              Kadena Connect
+            </FooterLink>
+            <FooterLink href="https://www.kadena.io/beyond-the-block">
+              Beyod the Block
+            </FooterLink>
+          </FooterColumn>
         </Stack>
-        <Stack flex={1} marginBlockStart="sm">
-          <Heading as="h5">Company</Heading>
-        </Stack>
+        <FooterColumn title="Company">
+          <FooterLink href="https://www.kadena.io/about">
+            About Kadena
+          </FooterLink>
+          <FooterLink href="https://boards.greenhouse.io/kadenallc">
+            Careers
+          </FooterLink>
+          <FooterLink href="https://www.kadena.io/media-kit">
+            Media Kit
+          </FooterLink>
+        </FooterColumn>
       </Stack>
       <Stack
         width="100%"
