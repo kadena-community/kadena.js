0077f9b06286329620e082db47634448bebf73ef
Steven <steven@straatemans.nl>
2024-07-01 13:32:11 +0200

    fix(explorer): the search params (#2345)

--- packages/apps/explorer/src/components/search/search-component/search-component.css.ts
+++ packages/apps/explorer/src/components/search/search-component/search-component.css.ts
@@ -22,3 +22,19 @@ export const searchInputClass = style([
 export const searchBadgeBoxClass = style({
   width: '20%',
 });
+
+export const editingBoxClass = style([
+  atoms({
+    position: 'absolute',
+    display: 'grid',
+    borderStyle: 'solid',
+    borderWidth: 'hairline',
+    backgroundColor: 'base.@active',
+    fontSize: 'sm',
+    fontFamily: 'primaryFont',
+  }),
+  {
+    top: '45px',
+    width: '100%',
+  },
+]);
