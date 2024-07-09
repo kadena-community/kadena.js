f6ac0a8a5e87b17a56d561149e2c62911e2f4900
Javad Khalilian <khalilian.javad@gmail.com>
2024-07-09 11:07:44 +0200

    feat(dev-wallet): discover account  modal

--- packages/apps/dev-wallet/src/pages/transfer/components/DiscoverdAccounts.tsx
+++ packages/apps/dev-wallet/src/pages/transfer/components/DiscoverdAccounts.tsx
@@ -1,24 +1,47 @@
-import { Button, Dialog, Heading, Stack, Text } from '@kadena/kode-ui';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  Stack,
+  Text,
+} from '@kadena/kode-ui';
 import { FC } from 'react';
 import { IReceiverAccount } from '../utils';
+import { discoverdAccountClass } from './style.css';
 
 export const DiscoverdAccounts: FC<{
   accounts: IReceiverAccount[];
   onSelect: (selected: IReceiverAccount) => void;
 }> = ({ accounts, onSelect }) => (
-  <Dialog size="sm" isOpen={true}>
-    <Stack flexDirection={'column'} gap={'sm'}>
-      <Heading variant="h2">Select the receiver account</Heading>
-      <Text>
-        There are {accounts.length} accounts associated with the same address on
-        the Blockchian. Please select the account you want to use.
-      </Text>
-      {accounts.map((account) => (
-        <Stack key={account.address} justifyContent={'space-between'}>
-          <Text>{account.address}</Text>
-          <Button onPress={() => onSelect(account)}>Select</Button>
+  <Dialog size="lg" isOpen={true}>
+    <DialogHeader>Select the receiver account</DialogHeader>
+    <DialogContent>
+      <Stack flexDirection={'column'} gap={'sm'}>
+        <Text>
+          There are {accounts.length} accounts associated with the same address
+          on the Blockchian. Please select the account you want to use.
+        </Text>
+        <Text>Address: {accounts[0].address}</Text>
+        <Stack flexDirection={'column'}>
+          {accounts.map((account) => (
+            <Stack flexDirection={'column'} className={discoverdAccountClass}>
+              <Text color="emphasize">
+                Chains: (
+                {account.chains.map(({ chainId }) => chainId).join(', ')})
+              </Text>
+              <Text color="emphasize">Balance: {account.overallBalance}</Text>
+              <Text bold color="emphasize">
+                Guard
+              </Text>
+              <pre>{JSON.stringify(account.keyset.guard, null, 2)}</pre>
+              <Stack justifyContent={'flex-end'}>
+                <Button onPress={() => onSelect(account)}>Select</Button>
+              </Stack>
+            </Stack>
+          ))}
         </Stack>
-      ))}
-    </Stack>
+      </Stack>
+    </DialogContent>
   </Dialog>
 );
