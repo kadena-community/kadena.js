8814ef1a6cc3a446c0ac47508022ceab0ed3f712
Steven Straatemans <steven@straatemans.nl>
2024-07-10 20:07:55 +0200

    fix the build

--- packages/apps/explorer/src/pages/index.tsx
+++ packages/apps/explorer/src/pages/index.tsx
@@ -1,36 +1,8 @@
-import { useRouter } from '@/components/routing/useRouter';
-import type { INetwork } from '@/context/networks-context';
-import { selectedNetworkKey, useNetwork } from '@/context/networks-context';
-import Cookies from 'js-cookie';
+import { selectedNetworkKey } from '@/context/networks-context';
 import type { GetServerSideProps } from 'next';
 import type React from 'react';
-import { useEffect } from 'react';
 
 const Home: React.FC = () => {
-  const router = useRouter();
-  const { setActiveNetwork, networks } = useNetwork();
-
-  // useEffect(() => {
-  //   if (!localStorage.getItem(selectedNetworkKey)) {
-  //     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  //     router.replace(`/${networks[0].networkId}`);
-  //     return;
-  //   }
-  //   const network: INetwork = JSON.parse(
-  //     localStorage.getItem(selectedNetworkKey) ?? '{}',
-  //   );
-
-  //   if (!network) {
-  //     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  //     router.replace(`/`);
-  //     return;
-  //   }
-
-  //   setActiveNetwork(network.networkId);
-  //   // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  //   router.replace(`/${network.networkId}`);
-  // }, [router.asPath]);
-
   return null;
 };
 
