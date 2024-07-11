ff9a3138f9666639a54f61b16df5ea886e8df091
Albert G <516972+alber70g@users.noreply.github.com>
2024-07-11 16:58:29 +0200

    chore(graph): add sentry to @kadena/graph (#2376)
    * chore(graph): add sentry to @kadena/graph
    
    * chore: formatting
    
    * chore: add environment for sentry (defaults to mainnet)
    
    * remove NODE_ENV from environment, makes it confusing.
    
    ---------
    
    Co-authored-by: Ghislain Gabrielse <Ghislain@Kadena.io>

--- packages/apps/graph/src/index.ts
+++ packages/apps/graph/src/index.ts
@@ -9,10 +9,13 @@ moduleAlias.addAliases({
   '@devnet': `${__dirname}/devnet`,
 });
 
+import { AttributeNames } from '@pothos/tracing-sentry';
+import * as Sentry from '@sentry/node';
 import { SystemCheckError, runSystemsCheck } from '@services/systems-check';
 import { dotenv } from '@utils/dotenv';
 import type { ExecutionArgs } from 'graphql';
 import { useServer } from 'graphql-ws/lib/use/ws';
+import type { Plugin } from 'graphql-yoga';
 import { createYoga } from 'graphql-yoga';
 import 'json-bigint-patch';
 import { createServer } from 'node:http';
@@ -30,10 +33,41 @@ if (dotenv.NODE_ENV === 'development') {
 
 const schema = builder.toSchema();
 
-const plugins = [extensionsPlugin()];
+const plugins: Plugin[] = [extensionsPlugin()];
 
 if (dotenv.COMPLEXITY_EXPOSED) {
   plugins.push(complexityPlugin(schema));
+}
+
+const tracingPlugin: Plugin = {
+  onExecute: ({ setExecuteFn, executeFn }) => {
+    setExecuteFn((options) =>
+      Sentry.startSpan(
+        {
+          op: 'graphql.execute',
+          name: options.operationName ?? '<unnamed operation>',
+          forceTransaction: true,
+          attributes: {
+            [AttributeNames.OPERATION_NAME]: options.operationName ?? undefined,
+            // [AttributeNames.SOURCE]: print(options.document),
+          },
+        },
+        () => executeFn(options),
+      ),
+    );
+  },
+};
+
+if (dotenv.SENTRY_DSN) {
+  console.log(
+    ` ✔ starting with sentry ${dotenv.NODE_ENV}-${dotenv.ENVIRONMENT}`,
+  );
+  Sentry.init({
+    dsn: dotenv.SENTRY_DSN,
+    tracesSampleRate: 1,
+    environment: dotenv.ENVIRONMENT,
+  });
+  plugins.push(tracingPlugin);
 }
 
 const yogaApp = createYoga({
