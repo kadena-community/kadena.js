3fe1af9f4bb6ecde9341285af792d33112efc662
Isa Ozler <isaozler@gmail.com>
2024-07-17 18:15:21 +0200

    Storybook brand updates

---
+++ packages/libs/kode-ui/.storybook/global.css.ts
@@ -0,0 +1,11 @@
+import { globalStyle, vars } from "../src/styles";
+
+globalStyle('pre.prismjs > div, pre.prismjs > div span, code', {
+  overflowWrap: 'break-word',
+  fontFamily: `${vars.fonts.$mono} !important`,
+  fontWeight: vars.fontWeights.$medium,
+});
+
+globalStyle('code', {
+  fontWeight: vars.fontWeights.$semiBold,
+});
