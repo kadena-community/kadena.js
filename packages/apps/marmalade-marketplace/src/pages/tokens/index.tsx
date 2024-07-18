e1e94c924094a2b9765cf3a35ecf9c64b542941f
Jermaine <jermaine150@gmail.com>
2024-07-18 10:11:18 +0200

    Create token improvements (#2416)
    * Create token styling and bugfixes
    
    * Create token improvs and w:account retrieval
    
    * Fixing tests

--- packages/apps/marmalade-marketplace/src/pages/tokens/index.tsx
+++ packages/apps/marmalade-marketplace/src/pages/tokens/index.tsx
@@ -1,9 +1,5 @@
-import React, { useState, FormEvent } from 'react';
-import { Heading , Select, SelectItem} from '@kadena/kode-ui';
-import Layout from '@/components/Layout';
+import React, { useState } from 'react';
 import CreateToken from '@/components/CreateToken';
-
-import MintToken from '@/components/MintToken';
 
 export default function TokenPages() {
   const [tokenFunction, setTokenFunction] = useState("Create Token")
