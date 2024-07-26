dc9530c8f7b9981b2330c5fb1c2c06631f0f7708
jermaine150 <jermaine150@gmail.com>
2024-07-26 12:29:41 +0200

    Connect wallet component and cleanup header

--- packages/apps/marmalade-marketplace/src/components/MarketplaceHeader/style.css.ts
+++ packages/apps/marmalade-marketplace/src/components/MarketplaceHeader/style.css.ts
@@ -1,4 +1,3 @@
-
 import { style } from '@vanilla-extract/css';
 import { tokens } from '@kadena/kode-ui/styles';
 
@@ -8,33 +7,4 @@ export const navHeader = style({
 
 export const navHeaderLink = style({
   fontSize: tokens.kda.foundation.typography.fontSize.base,
-});
-
-export const walletButton = style({
-  height: '40px',
-});
-
-export const walletButtonBadge = style({
-  color: 'gray',
-});
-
-export const selectContainer = style({
-  display: 'flex',
-  alignItems: 'center',
-  position: 'relative',
-});
-
-export const walletOption = style({
-  display: 'flex',
-  width: '40px',
-  alignItems: 'center',
-  marginLeft: '0',
-  backgroundColor: '#52FFC6',
-  padding: '0 1em',
-  outline: 'none',
-});
-
-export const walletContainer = style({
-  display: 'flex',
-  alignItems: 'center',
 });
