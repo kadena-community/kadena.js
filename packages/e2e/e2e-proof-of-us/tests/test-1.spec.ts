17bab3f169802377e47d6d6013a4c4775b5c7886
Steven Straatemans <steven@straatemans.nl>
2024-07-29 10:45:44 +0200

    add extra test

--- packages/e2e/e2e-proof-of-us/tests/test-1.spec.ts
+++ packages/e2e/e2e-proof-of-us/tests/test-1.spec.ts
@@ -5,6 +5,7 @@ const proofTitle = 'Super Fancy Title';
 let shareUrl: string;
 let initiatorCredential: object;
 let signerCredential: object;
+let signer2Credential: object;
 
 const TESTURL = 'http://localhost:3000/';
 
@@ -28,8 +29,9 @@ test('1 Initiator, 1 signers. all participants sign -> Should be able to mint th
     expect(shareUrl).toBeDefined();
   });
 
-  await test.step('Create an account for the signer', async () => {
+  await test.step('Scan the QR code and create an account for all signers', async () => {
     await signer1.goto(shareUrl);
+
     const popupPromise = signer1.waitForEvent('popup');
     await proofOfusApp.loginToMintWithSigner(signer1);
 
@@ -38,10 +40,6 @@ test('1 Initiator, 1 signers. all participants sign -> Should be able to mint th
       signerPopup,
       true,
     );
-  });
-
-  await test.step('Scan the QR code and create an account for all signers', async () => {
-    await signer1.goto(shareUrl);
   });
 
   await test.step('Initiate the Signing Process with Initiator', async () => {
@@ -69,6 +67,105 @@ test('1 Initiator, 1 signers. all participants sign -> Should be able to mint th
         timeout: 120000,
       }),
       expect(signer1.getByRole('heading', { name: proofTitle })).toBeVisible({
+        timeout: 120000,
+      }),
+    ]);
+  });
+
+  await test.step('DEBUG: log link to block explorer', async () => {
+    await proofOfusApp.logBlockExplorerUrl(initiator);
+  });
+});
+
+test('1 Initiator, 2 signers. all participants sign -> Should be able to mint the connection token @xs', async ({
+  initiator,
+  signer1,
+  signer2,
+  spirekeyApp,
+  proofOfusApp,
+}) => {
+  await test.step('Create an account for the initiator and create proof', async () => {
+    await initiator.goto(TESTURL);
+    const popupPromise = initiator.waitForEvent('popup');
+    await proofOfusApp.loginToMintWith(initiator);
+    const initiatorPopup = await popupPromise;
+    initiatorCredential = await spirekeyApp.createSpireKeyAccountFor(
+      initiatorPopup,
+      true,
+    );
+
+    shareUrl = await proofOfusApp.createProofWith(initiator, proofTitle);
+    expect(shareUrl).toBeDefined();
+  });
+
+  await test.step('Scan the QR code and create an account for all signers', async () => {
+    await Promise.all([
+      (async () => {
+        await signer1.goto(shareUrl);
+
+        const popupPromise = signer1.waitForEvent('popup');
+        await proofOfusApp.loginToMintWithSigner(signer1);
+
+        const signerPopup = await popupPromise;
+        signerCredential = await spirekeyApp.createSpireKeyAccountFor(
+          signerPopup,
+          true,
+        );
+      })(),
+      (async () => {
+        await signer2.goto(shareUrl);
+
+        const popupPromise = signer2.waitForEvent('popup');
+        await proofOfusApp.loginToMintWithSigner(signer2);
+
+        const signerPopup = await popupPromise;
+        signer2Credential = await spirekeyApp.createSpireKeyAccountFor(
+          signerPopup,
+          true,
+        );
+      })(),
+    ]);
+  });
+
+  await test.step('Initiate the Signing Process with Initiator', async () => {
+    await proofOfusApp.startSigningProcessWith(initiator);
+  });
+
+  await test.step('Sign with the Signer', async () => {
+    await Promise.all([
+      (async () => {
+        const popupPromise = signer1.waitForEvent('popup');
+
+        await proofOfusApp.signProofWith(signer1);
+        const signerPopup = await popupPromise;
+        await spirekeyApp.signTransaction(signerPopup, signerCredential);
+      })(),
+      (async () => {
+        const popupPromise = signer2.waitForEvent('popup');
+
+        await proofOfusApp.signProofWith(signer2);
+        const signerPopup = await popupPromise;
+        await spirekeyApp.signTransaction(signerPopup, signer2Credential);
+      })(),
+    ]);
+  });
+
+  await test.step('Sign with Initiator & Mint NFT', async () => {
+    const popupPromise = initiator.waitForEvent('popup');
+    await proofOfusApp.signAndMintWith(initiator);
+    const initiatorPopup = await popupPromise;
+    await spirekeyApp.signTransaction(initiatorPopup, initiatorCredential);
+  });
+
+  await test.step('The Proof should be succesfully minted for the initiator as well as all the signers', async () => {
+    await Promise.all([
+      expect(initiator.getByRole('heading', { name: proofTitle })).toBeVisible({
+        timeout: 120000,
+      }),
+      expect(signer1.getByRole('heading', { name: proofTitle })).toBeVisible({
+        timeout: 120000,
+      }),
+      expect(signer2.getByRole('heading', { name: proofTitle })).toBeVisible({
         timeout: 120000,
       }),
     ]);
