3edda69f3221e9eafb2592f0c93196177235ecbe
Steven Straatemans <steven@straatemans.nl>
2024-08-19 09:22:54 +0200

    fix layout of transaction to full

--- packages/apps/explorer/src/pages/[networkSlug]/transaction/[requestKey].tsx
+++ packages/apps/explorer/src/pages/[networkSlug]/transaction/[requestKey].tsx
@@ -67,7 +67,7 @@ const Transaction: React.FC = () => {
   }, [loading, data, error]);
 
   return (
-    <Layout layout="default">
+    <Layout>
       {innerData && innerData.transaction ? (
         <>
           <LayoutHeader>
