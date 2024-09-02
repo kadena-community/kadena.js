459a3e410441689b3a358e53f4a9c37cc1b79b6b
Steven Straatemans <steven@straatemans.nl>
2024-09-02 10:51:38 +0200

    get balance

--- packages/apps/tools/src/scripts/faucetCron/index.ts
+++ packages/apps/tools/src/scripts/faucetCron/index.ts
@@ -1,8 +1,13 @@
+import { createClient, Pact } from '@kadena/client';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const channelId = process.env.SLACK_CHANNELID;
 const tokenId = process.env.SLACK_TOKEN;
+
+const client = createClient(
+  'https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/1/pact',
+);
 
 const sendMessage = async () => {
   const result = await fetch('https://slack.com/api/chat.postMessage', {
@@ -41,10 +46,61 @@ const sendMessage = async () => {
   const data = await result.json();
   return data;
 };
+const sendErrorMessage = async () => {
+  const result = await fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Authorization: `Bearer ${tokenId}`,
+    },
+    body: JSON.stringify({
+      channel: `${channelId}`,
+      blocks: JSON.stringify([
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'We were unable to retrieve the faucet account balance. \n There seems to be an issue with Chainweaver',
+          },
+        },
+      ]),
+    }),
+  });
+
+  const data = await result.json();
+  return data;
+};
+
+const getFaucetAccount = async () => {
+  const transaction = Pact.builder
+    .execution(
+      `(coin.get-balance "c:Ecwy85aCW3eogZUnIQxknH8tG8uXHM5QiC__jeI0nWA")`,
+    )
+    .setNetworkId('testnet04')
+    .setMeta({
+      chainId: `9`,
+    })
+    .createTransaction();
+
+  return await client.local(transaction, {
+    preflight: false,
+    signatureVerification: false,
+  });
+};
 
 const runJob = async () => {
-  const result = await sendMessage();
-  console.log({ result });
+  const accountResult = await getFaucetAccount();
+
+  console.log(accountResult);
+
+  return;
+  if (accountResult.result.status !== 'success') {
+    await sendErrorMessage();
+    return;
+  }
+
+  //   const result = await sendMessage();
+  //   console.log({ result });
 };
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
