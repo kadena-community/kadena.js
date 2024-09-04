03057bd7d981a5b0d24322e911a5c358bf3f9637
Steven Straatemans <steven@straatemans.nl>
2024-09-04 09:50:43 +0200

    test cron

--- packages/apps/tools/src/scripts/faucetCron/constants.ts
+++ packages/apps/tools/src/scripts/faucetCron/constants.ts
@@ -1,7 +1,7 @@
 export const channelId = process.env.SLACK_CHANNELID;
 export const tokenId = process.env.SLACK_TOKEN;
 export const faucetAccount = 'c:Ecwy85aCW3eogZUnIQxknH8tG8uXHM5QiC__jeI0nWA';
-export const MINBALANCE = 1000;
+export const MINBALANCE = 5000;
 
 export interface IChainAccount {
   balance: number;
