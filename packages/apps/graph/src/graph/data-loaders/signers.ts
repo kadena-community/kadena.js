bff417fe26ecf91b4ce1b6369de12a0e329a0bb1
Albert G <516972+alber70g@users.noreply.github.com>
2024-09-04 10:05:56 +0200

    feat(graph): use dataloader for signers

--- packages/apps/graph/src/graph/data-loaders/signers.ts
+++ packages/apps/graph/src/graph/data-loaders/signers.ts
@@ -1,5 +1,7 @@
+import { prismaClient } from '@db/prisma-client';
 import type { Signer } from '@prisma/client';
-import { getSigners } from '@services/chainweb-node/signer';
+import { getMempoolTransactionSigners } from '@services/chainweb-node/mempool';
+import { nullishOrEmpty } from '@utils/nullish-or-empty';
 import DataLoader from 'dataloader';
 
 interface ISignersKey {
@@ -10,12 +12,71 @@ interface ISignersKey {
 
 export const signersLoader = new DataLoader<ISignersKey, Signer[]>(
   async (keys: readonly ISignersKey[]) => {
-    const results = await Promise.all(
-      keys.map(({ requestKey, blockHash, chainId }) => {
-        return getSigners(requestKey, blockHash, chainId);
-      }),
+    const mempoolKeys = keys.filter(({ blockHash }) =>
+      nullishOrEmpty(blockHash),
+    );
+    const transactionKeys = keys.filter(
+      ({ blockHash }) => !nullishOrEmpty(blockHash),
     );
 
-    return results;
+    const mempoolPromises = mempoolKeys.map(({ requestKey, chainId }) => {
+      return getMempoolTransactionSigners(requestKey, chainId);
+    });
+
+    const transactionsPromise = prismaClient.signer.findMany({
+      where: {
+        requestKey: {
+          in: transactionKeys.map(({ requestKey }) => requestKey),
+        },
+      },
+    });
+
+    await Promise.all([...mempoolPromises, transactionsPromise]);
+
+    const mempoolResults = await Promise.all(mempoolPromises);
+    const transactionResults = await transactionsPromise;
+
+    const txSignersByRequestkey = transactionResults.reduce(
+      (acc, result) => {
+        acc[result.requestKey] = [...(acc[result.requestKey] || []), result];
+        return acc;
+      },
+      {} as Record<string, Signer[]>,
+    );
+
+    const mempoolSignersByRequestkey = mempoolResults.reduce(
+      (acc, result) => {
+        acc[result[0].requestKey] = result;
+        return acc;
+      },
+      {} as Record<string, Signer[]>,
+    );
+
+    return keys.map(({ requestKey, blockHash }) => {
+      if (nullishOrEmpty(blockHash)) {
+        return mempoolSignersByRequestkey[requestKey] || [];
+      } else {
+        return txSignersByRequestkey[requestKey] || [];
+      }
+    });
   },
 );
+
+async function getSigners(
+  requestKey: string,
+  blockHash: string,
+  chainId: string,
+): Promise<Signer[]> {
+  // This is needed because if the transaction is in the mempool, the
+  // signers are not stored in the database and there is no status.
+  // If blockHash has a value, we do not check the mempool for signers or status
+  if (nullishOrEmpty(blockHash)) {
+    return await getMempoolTransactionSigners(requestKey, chainId.toString());
+  } else {
+    return await prismaClient.signer.findMany({
+      where: {
+        requestKey: requestKey,
+      },
+    });
+  }
+}
