b57b5ed5c561f9c4c51aa43e9953bb705df262d5
Steven Straatemans <steven@straatemans.nl>
2024-09-04 11:05:49 +0200

    what is going on?

--- packages/apps/tools/src/app/api/faucet/route.route.ts
+++ packages/apps/tools/src/app/api/faucet/route.route.ts
@@ -1,6 +1,7 @@
 import { runJob } from '@/scripts/faucetCron';
+import { channelId, tokenId } from '@/scripts/faucetCron/constants';
 
 export async function GET(request: Request) {
   await runJob();
-  return new Response(`Hello`);
+  return new Response(`Hello ${channelId} - ${tokenId}`);
 }
