3da2a8940dbdf560282b2f2dfc3472ba689987b7
Steven Straatemans <steven@straatemans.nl>
2024-09-04 11:00:10 +0200

    fix tests

--- packages/apps/tools/src/scripts/faucetCron/__tests__/messages.test.ts
+++ packages/apps/tools/src/scripts/faucetCron/__tests__/messages.test.ts
@@ -45,13 +45,11 @@ describe('messages', () => {
       } as IAccount;
 
       await sendMessage(account);
-      const body = JSON.parse(mocks.fetch.mock.calls[0][1].body);
+      const body = JSON.parse(mocks.fetch.mock.calls[0][1].body) as any;
       expect(mocks.fetch).toBeCalledTimes(1);
-      expect(body).toEqual({
-        blocks:
-          '[{"type":"header","text":{"type":"plain_text","text":"Low Faucet alert! 🚨"}},{"type":"section","accessory":{"type":"image","image_url":"https://media.giphy.com/media/ZNnnp4wa17dZrDQKKI/giphy.gif?cid=790b7611li34xwh3ghrh6h6xwketcjop0mjayanqbp0n1enh&ep=v1_gifs_search&rid=giphy.gif&ct=g","alt_text":"0.1"},"text":{"type":"mrkdwn","text":"The faucet seems to be running low on funds (TESTNET):\\n *chain 0:* (500 KDA)\\n*chain 2:* (1000 KDA)\\n*chain 3:* (909 KDA)"}}]',
-        channel: 'C07KYLJ92E4',
-      });
+      expect(body.blocks).toEqual(
+        '[{"type":"header","text":{"type":"plain_text","text":"Low Faucet alert! 🚨"}},{"type":"section","accessory":{"type":"image","image_url":"https://media.giphy.com/media/ZNnnp4wa17dZrDQKKI/giphy.gif?cid=790b7611li34xwh3ghrh6h6xwketcjop0mjayanqbp0n1enh&ep=v1_gifs_search&rid=giphy.gif&ct=g","alt_text":"0.1"},"text":{"type":"mrkdwn","text":"The faucet seems to be running low on funds (TESTNET):\\n *chain 0:* (500 KDA)\\n*chain 2:* (1000 KDA)\\n*chain 3:* (909 KDA)"}}]',
+      );
     });
   });
 });
