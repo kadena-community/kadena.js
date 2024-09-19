80ac706636160402ef5e4c5608dcbd0d6c4a93bb
Steven <steven@straatemans.nl>
2024-09-19 09:13:25 +0200

    feat(ui): Tile component (#2525)
    * add tile component
    
    * fix build
    
    * add focus outline style
    
    * add Tile to build
    
    * add onClick functionality for Tile
    
    * onclick should not be required

---
+++ packages/libs/kode-ui/src/components/Tile/Tile.css.ts
@@ -0,0 +1,40 @@
+import { atoms, style, token } from '../../styles';
+
+export const tileClass = style([
+  atoms({
+    borderRadius: 'sm',
+    borderWidth: 'hairline',
+    borderStyle: 'solid',
+    borderColor: 'base.subtle',
+    padding: 'n4',
+    width: '100%',
+    listStyleType: 'none',
+  }),
+  {
+    backgroundColor: token('color.background.layer.default'),
+    selectors: {
+      'button&': {
+        cursor: 'pointer',
+      },
+      '&:not([data-disabled="true"]):hover, &:active:hover': {
+        backgroundColor: token('color.background.brand.primary.subtle'),
+        borderColor: token('color.border.base.@hover'),
+      },
+      '&:focus, &[data-focused="true"], &[data-focus-visible="true"]': {
+        outline: `${token('color.border.tint.outline')} solid ${token('border.width.normal')}`,
+        outlineOffset: token('border.width.normal'),
+        backgroundColor: token('color.background.layer.default'),
+        borderColor: token('color.border.base.@hover'),
+        borderWidth: '10px',
+      },
+
+      '&:active, &[data-status="active"]': {
+        backgroundColor: token('color.background.brand.primary.subtlest'),
+        borderColor: token('color.border.tint.@focus'),
+      },
+      '&:disabled, &[data-disabled="true"]': {
+        opacity: '.2',
+      },
+    },
+  },
+]);
