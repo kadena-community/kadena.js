b1e488c893ced8ca2088191bbbfa290756fb6cb1
Albert G <516972+alber70g@users.noreply.github.com>
2024-10-08 12:11:10 +0200

    feat(dev-wallet): export legacy functions for `kadenaChangePassword` and `kadenaGenKeypair` (#2555)

--- packages/libs/hd-wallet/src/chainweaver/compatibility/kadenaChangePassword.ts
+++ packages/libs/hd-wallet/src/chainweaver/compatibility/kadenaChangePassword.ts
@@ -1,10 +1,11 @@
 import type { EncryptedString } from '../../index.js';
 import { kadenaDecrypt } from '../../index.js';
+import type { BinaryLike } from '../../utils/crypto.js';
 import { kadenaChangePassword as originalKadenaChangePassword } from '../kadena-crypto.js';
 import { encryptLegacySecretKey } from './encryption.js';
 
 export const kadenaChangePassword = async (
-  secretKey: EncryptedString,
+  secretKey: BinaryLike,
   oldPassword: string,
   newPassword: string,
 ): Promise<EncryptedString> => {
