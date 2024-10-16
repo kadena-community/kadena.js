f4832a8173d1f91b4d04d0a81d85ee65f763eecc
Steven Straatemans <steven@straatemans.nl>
2024-10-16 12:09:56 +0200

    stories for all formatters

--- packages/libs/kode-ui/src/patterns/CompactTable/CompactTable.stories.tsx
+++ packages/libs/kode-ui/src/patterns/CompactTable/CompactTable.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { onLayer2 } from '../../storyDecorators';
-import { Link, MediaContextProvider } from './../../components';
+import { MediaContextProvider } from './../../components';
 import type { ICompactTableProps } from './CompactTable';
 import { CompactTable } from './CompactTable';
-import { CompactTableFormatters } from './Formatters';
+import { CompactTableFormatters } from './TableFormatters';
 
 const meta: Meta<ICompactTableProps> = {
   title: 'Patterns/CompactTable',
@@ -25,6 +25,44 @@ const meta: Meta<ICompactTableProps> = {
   },
 };
 
+const data = [
+  {
+    node: {
+      status: 'success',
+      requestKey: 'he-man',
+      parameters: 'param1',
+      block: {
+        height: 10,
+      },
+      balance: {
+        balance: '1093.33',
+      },
+    },
+  },
+  {
+    node: {
+      status: 'success',
+      requestKey: 'masters of the universe',
+      parameters: 'param2',
+      block: {
+        height: 11,
+      },
+    },
+  },
+  {
+    node: {
+      requestKey: 'skeletor',
+      parameters: 'param3',
+      block: {
+        height: 12,
+      },
+      balance: {
+        balance: '45.2222',
+      },
+    },
+  },
+];
+
 export default meta;
 type Story = StoryObj<ICompactTableProps>;
 
@@ -34,35 +72,6 @@ export const Primary: Story = {
     isLoading: false,
   },
   render: ({ isLoading }) => {
-    const data = [
-      {
-        node: {
-          requestKey: 'key1',
-          parameters: 'param1',
-          block: {
-            height: 10,
-          },
-        },
-      },
-      {
-        node: {
-          requestKey: 'key2',
-          parameters: 'param2',
-          block: {
-            height: 11,
-          },
-        },
-      },
-      {
-        node: {
-          requestKey: 'key3',
-          parameters: 'param3',
-          block: {
-            height: 12,
-          },
-        },
-      },
-    ];
     return (
       <MediaContextProvider>
         <CompactTable
@@ -97,35 +106,6 @@ export const FormatLink: Story = {
     isLoading: false,
   },
   render: ({ isLoading }) => {
-    const data = [
-      {
-        node: {
-          requestKey: 'he-man',
-          parameters: 'param1',
-          block: {
-            height: 10,
-          },
-        },
-      },
-      {
-        node: {
-          requestKey: 'masters of the universe',
-          parameters: 'param2',
-          block: {
-            height: 11,
-          },
-        },
-      },
-      {
-        node: {
-          requestKey: 'skeletor',
-          parameters: 'param3',
-          block: {
-            height: 12,
-          },
-        },
-      },
-    ];
     return (
       <MediaContextProvider>
         <CompactTable
@@ -148,6 +128,136 @@ export const FormatLink: Story = {
               label: 'Parameters',
               key: 'node.parameters',
               width: '45%',
+            },
+          ]}
+          data={data}
+        />
+      </MediaContextProvider>
+    );
+  },
+};
+
+export const FormatStatus: Story = {
+  name: 'FormatStatus',
+  args: {
+    isLoading: false,
+  },
+  render: ({ isLoading }) => {
+    return (
+      <MediaContextProvider>
+        <CompactTable
+          isLoading={isLoading}
+          fields={[
+            {
+              label: 'Status',
+              key: 'node.status',
+              width: '15%',
+              render: CompactTableFormatters.FormatStatus(),
+            },
+            {
+              label: 'Height',
+              key: 'node.block.height',
+              width: '15%',
+            },
+            {
+              label: 'RequestKey',
+              key: 'node.requestKey',
+              width: '40%',
+              render: CompactTableFormatters.FormatLink({
+                url: `https://kadena.io`,
+              }),
+            },
+            {
+              label: 'Parameters',
+              key: 'node.parameters',
+              width: '30%',
+            },
+          ]}
+          data={data}
+        />
+      </MediaContextProvider>
+    );
+  },
+};
+
+export const FormatMultiStepTx: Story = {
+  name: 'FormatMultiStepTx',
+  args: {
+    isLoading: false,
+  },
+  render: ({ isLoading }) => {
+    return (
+      <MediaContextProvider>
+        <CompactTable
+          isLoading={isLoading}
+          fields={[
+            {
+              label: 'Status',
+              key: 'node.status',
+              width: '15%',
+              render: CompactTableFormatters.FormatMultiStepTx(),
+            },
+            {
+              label: 'Height',
+              key: 'node.block.height',
+              width: '15%',
+            },
+            {
+              label: 'RequestKey',
+              key: 'node.requestKey',
+              width: '40%',
+              render: CompactTableFormatters.FormatLink({
+                url: `https://kadena.io`,
+              }),
+            },
+            {
+              label: 'Parameters',
+              key: 'node.parameters',
+              width: '30%',
+            },
+          ]}
+          data={data}
+        />
+      </MediaContextProvider>
+    );
+  },
+};
+
+export const FormatAmount: Story = {
+  name: 'FormatAmount',
+  args: {
+    isLoading: false,
+  },
+  render: ({ isLoading }) => {
+    return (
+      <MediaContextProvider>
+        <CompactTable
+          isLoading={isLoading}
+          fields={[
+            {
+              label: 'Height',
+              key: 'node.block.height',
+              width: '15%',
+            },
+            {
+              label: 'RequestKey',
+              key: 'node.requestKey',
+              width: '30%',
+              render: CompactTableFormatters.FormatLink({
+                url: `https://kadena.io`,
+              }),
+            },
+            {
+              label: 'Parameters',
+              key: 'node.parameters',
+              width: '30%',
+            },
+            {
+              label: 'Balance',
+              key: 'node.balance.balance',
+              width: '25%',
+              render: CompactTableFormatters.FormatAmount(),
+              align: 'end',
             },
           ]}
           data={data}
--- packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatAmount.tsx
+++ packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatAmount.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import type { ICompactTableFormatterProps } from './types';
 
 export const FormatAmount = (): FC<ICompactTableFormatterProps> => {
-  const Component: FC<ICompactTableFormatterProps> = ({ value }) => (
-    <>{value} KDA</>
-  );
+  const Component: FC<ICompactTableFormatterProps> = ({ value }) => {
+    if (!value) return null;
+    return <>{parseFloat(value).toLocaleString()} KDA</>;
+  };
   return Component;
 };
