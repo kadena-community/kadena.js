addb44a74488f08d7071bfd7f1b2034659000fc8
Albert G <516972+alber70g@users.noreply.github.com>
2024-10-16 17:12:43 +0200

    WIP: fix transfers query

--- packages/apps/graph/src/graph/objects/fungible-account.ts
+++ packages/apps/graph/src/graph/objects/fungible-account.ts
@@ -9,6 +9,18 @@ import { normalizeError } from '@utils/errors';
 import { networkData } from '@utils/network';
 import { builder } from '../builder';
 import { fungibleAccountDetailsLoader } from '../data-loaders/fungible-account-details';
+
+const keysToCamel = <T extends Record<string, unknown>>(obj: T) => {
+  return Object.keys(obj).reduce(
+    (acc, key) => {
+      acc[key.replace(/_(\w)/g, (m, p1) => p1.toUpperCase())] = obj[key];
+      return acc;
+    },
+    {} as Record<string, unknown>,
+  );
+};
+
+import { generateTransferFilter } from '../query/transfers';
 import type {
   IFungibleAccount,
   IFungibleChainAccount,
@@ -166,7 +178,9 @@ export default builder.node(
             return await prismaClient.transfer.count({
               where: {
                 OR: [
-                  { senderAccount: parent.accountName },
+                  {
+                    senderAccount: parent.accountName,
+                  },
                   {
                     receiverAccount: parent.accountName,
                   },
@@ -179,21 +193,94 @@ export default builder.node(
           }
         },
 
+        /*
+          amount          Decimal      @db.Decimal
+  blockHash       String       @map("block") @db.VarChar
+  chainId         BigInt       @map("chainid")
+  senderAccount   String       @map("from_acct") @db.VarChar
+  height          BigInt
+  orderIndex      BigInt       @map("idx")
+  moduleHash      String       @map("modulehash") @db.VarChar
+  moduleName      String       @map("modulename") @db.VarChar
+  requestKey      String       @map("requestkey") @db.VarChar
+  receiverAccount String       @map("to_acct") @db.VarChar
+        */
+
         async resolve(condition, parent) {
+          console.log('condition', JSON.stringify(condition, null, 2));
+
           try {
-            return await prismaClient.transfer.findMany({
+            //             const result = (
+            //               (await prismaClient.$queryRaw`
+            // WITH tr AS (
+            //   -- First subquery: Transfers where the account is the sender
+            //   SELECT
+            //     amount,
+            //     block,
+            //     chainid,
+            //     from_acct,
+            //     height,
+            //     idx,
+            //     modulehash,
+            //     modulename,
+            //     requestkey,
+            //     to_acct
+            //   FROM transfers
+            //   WHERE from_acct = ${parent.accountName}
+            //     -- AND (:minHeight IS NULL OR height >= :minHeight)
+            //     -- AND (:maxHeight IS NULL OR height <= :maxHeight)
+
+            //   UNION
+
+            //   -- Second subquery: Transfers where the account is the receiver
+            //   SELECT
+            //     amount,
+            //     block,
+            //     chainid,
+            //     from_acct,
+            //     height,
+            //     idx,
+            //     modulehash,
+            //     modulename,
+            //     requestkey,
+            //     to_acct
+            //   FROM transfers
+            //   WHERE to_acct = ${parent.accountName}
+            //   AND from_acct != ${parent.accountName}
+            //     -- AND (:minHeight IS NULL OR height >= :minHeight)
+            //     -- AND (:maxHeight IS NULL OR height <= :maxHeight)
+            // )
+            // SELECT
+            //   amount,
+            //   block AS block_hash,
+            //   chainid AS chain_id,
+            //   from_acct AS sender_account,
+            //   height,
+            //   idx AS order_index,
+            //   modulehash AS module_hash,
+            //   modulename AS module_name,
+            //   requestkey AS request_key,
+            //   to_acct AS receiver_account
+            // FROM tr
+            // -- WHERE modulename = :tspToken
+            //   -- AND (:tspChainId IS NULL OR chainid = :tspChainId)
+            // ORDER BY
+            //   height DESC
+            // LIMIT 20;
+            // `) as any
+            //             ).map(keysToCamel);
+
+            const result = await prismaClient.transfer.findMany({
               ...condition,
-              where: {
-                OR: [
-                  { senderAccount: parent.accountName },
-                  { receiverAccount: parent.accountName },
-                ],
-                moduleName: parent.fungibleName,
-              },
+              where: generateTransferFilter({
+                accountName: parent.accountName,
+              }),
               orderBy: {
                 height: 'desc',
               },
             });
+            console.log('result', JSON.stringify(result, null, 2));
+            return result;
           } catch (error) {
             throw normalizeError(error);
           }
@@ -202,3 +289,84 @@ export default builder.node(
     }),
   },
 );
+
+
+const createTransfersPaginatedQuery = () => {
+  return prismaClient.$queryRaw`
+  WITH transfers_by_acct AS (
+	SELECT
+		*
+	FROM
+		(
+			SELECT
+				*
+			FROM
+				transfers
+			WHERE
+				from_acct = 'k:22fbc926b497bf3ecc7199b923039e74cdeda12726e1dc4f443953007cf8cc43'
+			ORDER BY
+				height DESC,
+				requestkey DESC,
+				idx ASC
+		) as t0
+	UNION
+	ALL
+	SELECT
+		*
+	FROM
+		(
+			SELECT
+				*
+			FROM
+				transfers
+			WHERE
+				to_acct = 'k:22fbc926b497bf3ecc7199b923039e74cdeda12726e1dc4f443953007cf8cc43'
+			ORDER BY
+				height DESC,
+				requestkey DESC,
+				idx ASC
+		) AS t1
+)
+SELECT
+	amount,
+	block AS block_hash,
+	chainid AS chain_id,
+	from_acct AS sender_account,
+	height,
+	idx AS order_index,
+	modulehash AS module_hash,
+	modulename AS module_name,
+	requestkey AS request_key,
+	to_acct AS receiver_account
+FROM
+	transfers_by_acct
+WHERE
+	(
+		"height" <= (
+			SELECT
+				"height"
+			FROM
+				"transfers"
+			WHERE
+				(
+					block,
+					chainid,
+					idx,
+					modulehash,
+					requestkey
+				) = (
+					'I_Oyt-xi7BdmO9hsATRClofuFE-OpVKAFYtT6Vk8oXc',
+					1,
+					0,
+					'rE7DU8jlQL9x_MPYuniZJf5ICBTAEHAIFQCB4blofP4',
+					'-gE_ZELs7JDixNbvLkfgUy6d0DR-3c54SJe7SG-Xm3A'
+				)
+		)
+	)
+ORDER BY
+	height DESC,
+	requestkey DESC,
+	idx ASC OFFSET 100
+LIMIT
+	100;`
+}
