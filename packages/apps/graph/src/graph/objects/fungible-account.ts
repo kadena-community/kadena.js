ac1c2af1bdff8580fd3aff21771fdc3d9e2cf5dd
Albert G <516972+alber70g@users.noreply.github.com>
2024-10-16 17:12:43 +0200

    WIP: fix transfers query

--- packages/apps/graph/src/graph/objects/fungible-account.ts
+++ packages/apps/graph/src/graph/objects/fungible-account.ts
@@ -9,6 +9,7 @@ import { normalizeError } from '@utils/errors';
 import { networkData } from '@utils/network';
 import { builder } from '../builder';
 import { fungibleAccountDetailsLoader } from '../data-loaders/fungible-account-details';
+
 import type {
   IFungibleAccount,
   IFungibleChainAccount,
@@ -17,6 +18,193 @@ import {
   FungibleAccountName,
   FungibleChainAccountName,
 } from '../types/graphql-types';
+
+const keysToCamel = <T extends Record<string, unknown>>(obj: T) => {
+  return Object.keys(obj).reduce(
+    (acc, key) => {
+      acc[key.replace(/_(\w)/g, (m, p1) => p1.toUpperCase())] = obj[key];
+      return acc;
+    },
+    {} as Record<string, unknown>,
+  );
+};
+interface ITransferQueryConditions {
+  select?: Record<string, boolean>;
+  take: number;
+  skip: number;
+  cursor?: {
+    blockHash_chainId_orderIndex_moduleHash_requestKey: {
+      blockHash: string;
+      chainId: number;
+      orderIndex: number;
+      moduleHash: string;
+      requestKey: string;
+    };
+  };
+}
+
+const getTransfers = (
+  accountName: string,
+  condition: ITransferQueryConditions | undefined,
+) => {
+  // The query with the pagination cursor is composed separately
+  // as Prisma rightfully does not allow dynamic statements in sql queries
+  if (!condition?.cursor) {
+    return prismaClient.$queryRaw`
+      WITH transfers_by_acct AS (
+
+      SELECT
+        *
+      FROM
+        (
+
+          SELECT
+            *
+          FROM
+            transfers
+
+          WHERE
+            from_acct = ${accountName}
+          ORDER BY
+            height DESC,
+            requestkey DESC,
+            idx ASC
+        ) as t0
+
+      UNION
+
+      SELECT
+        *
+      FROM
+        (
+
+          SELECT
+            *
+          FROM
+            transfers
+
+          WHERE
+            to_acct = ${accountName}
+          ORDER BY
+            height DESC,
+            requestkey DESC,
+            idx ASC
+        ) AS t1
+      )
+
+      SELECT
+        DISTINCT on (height, requestkey, idx)
+        amount,
+        block AS block_hash,
+        chainid AS chain_id,
+        from_acct AS sender_account,
+        height,
+        idx AS order_index,
+        modulehash AS module_hash,
+        modulename AS module_name,
+        requestkey AS request_key,
+        to_acct AS receiver_account
+      FROM
+        transfers_by_acct
+
+      ORDER BY
+        height DESC,
+        requestkey DESC,
+        idx ASC
+      OFFSET ${condition?.skip || 0}
+      LIMIT ${condition?.take || 21};`;
+  } else {
+    const { blockHash, chainId, orderIndex, moduleHash, requestKey } =
+      condition.cursor.blockHash_chainId_orderIndex_moduleHash_requestKey;
+
+    return prismaClient.$queryRaw`
+      WITH transfers_by_acct AS (
+
+      SELECT
+        *
+      FROM
+        (
+
+          SELECT
+            *
+          FROM
+            transfers
+
+          WHERE
+            from_acct = ${accountName}
+          ORDER BY
+            height DESC,
+            requestkey DESC,
+            idx ASC
+        ) as t0
+
+      UNION ALL
+
+      SELECT
+        *
+      FROM
+        (
+
+          SELECT
+            *
+          FROM
+            transfers
+
+          WHERE
+              to_acct = ${accountName}
+          ORDER BY
+            height DESC,
+            requestkey DESC,
+            idx ASC
+        ) AS t1
+      )
+
+      SELECT
+        amount,
+        block AS block_hash,
+        chainid AS chain_id,
+        from_acct AS sender_account,
+        height,
+        idx AS order_index,
+        modulehash AS module_hash,
+        modulename AS module_name,
+        requestkey AS request_key,
+        to_acct AS receiver_account
+      FROM
+        transfers_by_acct
+      WHERE
+      (
+        "height" <= (
+
+          SELECT
+            "height"
+          FROM
+            "transfers"
+          WHERE
+            (
+              block,
+              chainid,
+              idx,
+              modulehash,
+              requestkey
+            ) = (
+              ${blockHash},
+              ${chainId},
+              ${orderIndex},
+              ${moduleHash},
+              ${requestKey}
+            )
+          )
+        )
+
+      ORDER BY
+        height DESC,
+        requestkey DESC,
+        idx ASC
+      OFFSET ${condition?.skip || 0}
+      LIMIT ${condition?.take || 21};`;
+  }
+};
 
 export default builder.node(
   builder.objectRef<IFungibleAccount>(FungibleAccountName),
@@ -166,7 +354,9 @@ export default builder.node(
             return await prismaClient.transfer.count({
               where: {
                 OR: [
-                  { senderAccount: parent.accountName },
+                  {
+                    senderAccount: parent.accountName,
+                  },
                   {
                     receiverAccount: parent.accountName,
                   },
@@ -180,20 +370,15 @@ export default builder.node(
         },
 
         async resolve(condition, parent) {
+          console.log('condition', JSON.stringify(condition, null, 2));
+
           try {
-            return await prismaClient.transfer.findMany({
-              ...condition,
-              where: {
-                OR: [
-                  { senderAccount: parent.accountName },
-                  { receiverAccount: parent.accountName },
-                ],
-                moduleName: parent.fungibleName,
-              },
-              orderBy: {
-                height: 'desc',
-              },
-            });
+            const result = (
+              (await getTransfers(parent.accountName, condition)) as any
+            ).map(keysToCamel);
+
+            console.log('result', JSON.stringify(result, null, 2));
+            return result;
           } catch (error) {
             throw normalizeError(error);
           }
