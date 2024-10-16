c34691237c7db948d1037551e7eae4cff26e40b2
Albert G <516972+alber70g@users.noreply.github.com>
2024-10-16 17:12:43 +0200

    fix(graph): performance improvement on the `transfers` query

--- packages/apps/graph/src/graph/query/transfers.ts
+++ packages/apps/graph/src/graph/query/transfers.ts
@@ -4,24 +4,29 @@ import { getDefaultConnectionComplexity } from '@services/complexity';
 import { normalizeError } from '@utils/errors';
 import { builder } from '../builder';
 
-const generateTransferFilter = (args: {
+export const generateTransferFilter = (args: {
   accountName?: string | null | undefined;
   fungibleName?: string | null | undefined;
   chainId?: string | null | undefined;
   requestKey?: string | null | undefined;
   blockHash?: string | null | undefined;
-}): Prisma.TransferWhereInput => ({
-  ...(args.accountName && {
-    OR: [
-      { senderAccount: args.accountName },
-      { receiverAccount: args.accountName },
-    ],
-  }),
-  ...(args.fungibleName && { moduleName: args.fungibleName }),
-  ...(args.chainId && { chainId: parseInt(args.chainId) }),
-  ...(args.requestKey && { requestKey: args.requestKey }),
-  ...(args.blockHash && { blockHash: args.blockHash }),
-});
+}): Prisma.TransferWhereInput => {
+  console.log('returning generateTransferFilter');
+  return {
+    ...(args.accountName && {
+      OR: [
+        {
+          senderAccount: args.accountName,
+        },
+        { receiverAccount: args.accountName },
+      ],
+    }),
+    ...(args.fungibleName && { moduleName: args.fungibleName }),
+    ...(args.chainId && { chainId: parseInt(args.chainId) }),
+    ...(args.requestKey && { requestKey: args.requestKey }),
+    ...(args.blockHash && { blockHash: args.blockHash }),
+  };
+};
 
 builder.queryField('transfers', (t) =>
   t.prismaConnection({
@@ -77,6 +82,7 @@ builder.queryField('transfers', (t) =>
       }
     },
     async resolve(query, __parent, args) {
+      console.log('query', JSON.stringify(query, null, 2));
       try {
         return await prismaClient.transfer.findMany({
           ...query,
