4fcab6dcd25a2ac51250e1c2a7d2a1c08ab8f34e
Steven Straatemans <steven@straatemans.nl>
2024-10-25 13:07:36 +0200

    animate

--- packages/apps/proof-of-us/src/components/AttendanceTicket/AttendanceTicket.tsx
+++ packages/apps/proof-of-us/src/components/AttendanceTicket/AttendanceTicket.tsx
@@ -83,6 +83,9 @@ export const AttendanceTicket: FC<IProps> = ({ data, share }) => {
       <motion.div
         layoutId={data.image}
         className={ticketClass}
+        transition={{ delay: 1, duration: 0.5 }}
+        initial={{ opacity: 0, y: '-50%' }}
+        animate={{ opacity: 1, y: 0 }}
         style={{
           backgroundImage: `url("${getIPFSLink(data.image)}")`,
           backgroundColor: data.properties?.avatar?.backgroundColor,
