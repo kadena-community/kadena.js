afb1021e384acf4b11de56c40bf6fced97af50ba
Javad Khalilian <khalilian.javad@gmail.com>
2024-10-26 07:35:39 +0200

    chore(dw): bump db version

--- packages/apps/dev-wallet/src/config.ts
+++ packages/apps/dev-wallet/src/config.ts
@@ -5,5 +5,9 @@ export const config = {
   DB: {
     DB_VERSION: 37,
     DB_NAME: 'dev-wallet',
+    // This should be used carefully, as it will wipe the database on version change
+    // I have added this for development purposes, We should remove this and write
+    // a migration script
+    DB_WIPE_ON_VERSION_CHANGE: true,
   },
 };
