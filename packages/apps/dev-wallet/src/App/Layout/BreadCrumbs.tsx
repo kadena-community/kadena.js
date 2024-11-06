bc8aa56a46ba7c1e2acf3bea02e9551c8561c168
Javad Khalilian <khalilian.javad@gmail.com>
2024-11-06 13:19:32 +0100

    fix(dw): breadcrumbs icon (#2637)

--- packages/apps/dev-wallet/src/App/Layout/BreadCrumbs.tsx
+++ packages/apps/dev-wallet/src/App/Layout/BreadCrumbs.tsx
@@ -1,5 +1,9 @@
 import { MonoWindow } from '@kadena/kode-icons/system';
-import { BreadcrumbsItem, Breadcrumbs as BreadcrumbsUI } from '@kadena/kode-ui';
+import {
+  BreadcrumbsItem,
+  Breadcrumbs as BreadcrumbsUI,
+  Text,
+} from '@kadena/kode-ui';
 import { useLayout } from '@kadena/kode-ui/patterns';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
@@ -9,7 +13,11 @@ export const BreadCrumbs: FC = () => {
 
   return (
     <BreadcrumbsUI
-      icon={breadCrumbs.length ? breadCrumbs[0].visual : <MonoWindow />}
+      icon={
+        <Text>
+          {breadCrumbs.length ? breadCrumbs[0].visual : <MonoWindow />}
+        </Text>
+      }
     >
       <>
         <BreadcrumbsItem component={Link} href="/">
