3e03fab7f65105c4b195bf37ff6537c5c453a5fd
Steven Straatemans <steven@straatemans.nl>
2024-11-07 10:51:40 +0100

    breadcrumbs with a network badge

--- packages/apps/dev-wallet/src/Components/SideBarBreadcrumbs/SideBarBreadcrumbs.tsx
+++ packages/apps/dev-wallet/src/Components/SideBarBreadcrumbs/SideBarBreadcrumbs.tsx
@@ -1,4 +1,5 @@
-import { IBreadcrumbsProps } from '@kadena/kode-ui';
+import { useWallet } from '@/modules/wallet/wallet.hook';
+import { Badge, IBreadcrumbsProps } from '@kadena/kode-ui';
 import { SideBarBreadcrumbs as SideBarBreadcrumbsUI } from '@kadena/kode-ui/patterns';
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
@@ -7,14 +8,19 @@ export const SideBarBreadcrumbs: FC<IBreadcrumbsProps> = ({
   children,
   ...props
 }) => {
+  const { activeNetwork } = useWallet();
+
   return (
     <SideBarBreadcrumbsUI {...props}>
-      {React.Children.map(children, (child) => {
-        if (!React.isValidElement(child)) {
-          return null;
-        }
-        return React.cloneElement(child, { ...child.props, component: Link });
-      })}
+      <>
+        <Badge size="sm" style="highContrast">{`${activeNetwork?.name}`}</Badge>
+        {React.Children.map(children, (child) => {
+          if (!React.isValidElement(child)) {
+            return null;
+          }
+          return React.cloneElement(child, { ...child.props, component: Link });
+        })}
+      </>
     </SideBarBreadcrumbsUI>
   );
 };
