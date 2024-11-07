afc961fc471c1faea9de45591060344c49867cfd
Steven Straatemans <steven@straatemans.nl>
2024-11-07 13:35:50 +0100

    fix builds

--- packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatJsonParse.tsx
+++ packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatJsonParse.tsx
@@ -5,10 +5,14 @@ import type { ICompactTableFormatterProps } from './types';
 
 export const FormatJsonParse = () => {
   const Component = ({ value }: ICompactTableFormatterProps) => {
-    const valueString = typeof value === 'string' ? JSON.parse(value) : value;
+    let v = value;
+    if (typeof value === 'string') {
+      v = !!value && value?.length > 0 ? JSON.parse(value) : value;
+    }
+
     return (
       <Text variant="code" className={dataFieldClass}>
-        {!!value && value?.length > 0 ? JSON.parse(valueString) : value}
+        {v}
       </Text>
     );
   };
--- packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatMultiStepTx.tsx
+++ packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatMultiStepTx.tsx
@@ -1,10 +1,13 @@
 import { MonoAirlineStops } from '@kadena/kode-icons/system';
 import React from 'react';
 import type { ICompactTableFormatterProps } from './types';
+import { valueToString } from './utils';
 
 export const FormatMultiStepTx = () => {
   const Component = ({ value }: ICompactTableFormatterProps) => {
-    return value ? <MonoAirlineStops title="Multistep Tx" /> : undefined;
+    return valueToString(value) ? (
+      <MonoAirlineStops title="Multistep Tx" />
+    ) : undefined;
   };
   return Component;
 };
