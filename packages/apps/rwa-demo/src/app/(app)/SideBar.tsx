9be8b9ff45a71b8ff317428ed95cdf1a63688d5a
Steven Straatemans <steven@straatemans.nl>
2024-11-11 16:26:28 +0100

    logout

--- packages/apps/rwa-demo/src/app/(app)/SideBar.tsx
+++ packages/apps/rwa-demo/src/app/(app)/SideBar.tsx
@@ -1,21 +1,50 @@
-import { MonoApps, MonoDarkMode, MonoLightMode } from '@kadena/kode-icons';
-import { Button, Themes, useTheme } from '@kadena/kode-ui';
+import { useAccount } from '@/hooks/account';
+import {
+  MonoApps,
+  MonoDarkMode,
+  MonoLightMode,
+  MonoLogout,
+} from '@kadena/kode-icons';
+import {
+  Button,
+  ContextMenu,
+  ContextMenuItem,
+  Themes,
+  useTheme,
+} from '@kadena/kode-ui';
 import {
   SideBarItem,
   SideBarItemsInline,
   SideBar as SideBarLayout,
 } from '@kadena/kode-ui/patterns';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import type { FC } from 'react';
+import { useEffect } from 'react';
 import { KLogo } from './KLogo';
 
 export const SideBar: FC = () => {
   const { theme, setTheme } = useTheme();
+  const { logout, account, isMounted } = useAccount();
+  const router = useRouter();
 
   const toggleTheme = (): void => {
     const newTheme = theme === Themes.dark ? Themes.light : Themes.dark;
     setTheme(newTheme);
   };
+
+  const handleLogout = () => {
+    logout();
+    router.push('/login');
+  };
+
+  useEffect(() => {
+    if (!isMounted) return;
+    if (!account) {
+      router.push('/login');
+    }
+  }, [isMounted]);
+
   return (
     <SideBarLayout
       logo={
@@ -38,6 +67,13 @@ export const SideBar: FC = () => {
       context={
         <>
           <SideBarItemsInline>
+            <ContextMenu trigger={<Button variant="outlined">Profile</Button>}>
+              <ContextMenuItem
+                endVisual={<MonoLogout />}
+                label="Logout"
+                onClick={handleLogout}
+              />
+            </ContextMenu>
             <SideBarItem
               visual={theme === 'dark' ? <MonoDarkMode /> : <MonoLightMode />}
               onPress={toggleTheme}
