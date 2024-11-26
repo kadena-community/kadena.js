ef9b9ac3ee8f47e5170ca5ffe21df4d5f57765a4
Steven Straatemans <steven@straatemans.nl>
2024-11-26 14:03:52 +0100

    add transaction animation to freeze account

--- packages/apps/rwa-demo/src/components/FreezeInvestor/FreezeInvestor.tsx
+++ packages/apps/rwa-demo/src/components/FreezeInvestor/FreezeInvestor.tsx
@@ -7,6 +7,7 @@ import { MonoPause, MonoPlayArrow } from '@kadena/kode-icons';
 import { Button } from '@kadena/kode-ui';
 import type { FC } from 'react';
 import React, { useEffect, useState } from 'react';
+import { SendTransactionAnimation } from '../SendTransactionAnimation/SendTransactionAnimation';
 import { TransactionPendingIcon } from '../TransactionPendingIcon/TransactionPendingIcon';
 
 interface IProps {
@@ -43,10 +44,11 @@ export const FreezeInvestor: FC<IProps> = ({ investorAccount }) => {
       const client = getClient();
       const res = await client.submit(signedTransaction);
 
-      await addTransaction({
+      const transaction = await addTransaction({
         ...res,
         type: 'FREEZE-ADDRESS',
       });
+      return transaction;
     } catch (e: any) {
       setIsLoading(false);
     }
@@ -57,8 +59,13 @@ export const FreezeInvestor: FC<IProps> = ({ investorAccount }) => {
   }, [frozen]);
 
   return (
-    <Button startVisual={getVisual(frozen, isLoading)} onPress={handleFreeze}>
-      Pause Account
-    </Button>
+    <SendTransactionAnimation
+      onPress={handleFreeze}
+      trigger={
+        <Button startVisual={getVisual(frozen, isLoading)}>
+          {frozen ? 'Unfreeze account' : 'Freeze account'}
+        </Button>
+      }
+    ></SendTransactionAnimation>
   );
 };
