976fb5a2737408c8ac8ce7126a23d0e812b8f165
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-12-04 10:44:08 +0100

    feat(wallet-sdk): query getTrasnfer per chain

--- packages/libs/wallet-sdk/src/services/graphql/transfer.query.ts
+++ packages/libs/wallet-sdk/src/services/graphql/transfer.query.ts
@@ -1,21 +1,6 @@
 import { graphql } from '../../gql/gql.js';
 
-export const TRANSFER_QUERY = graphql(`
-  query accountTransfers($accountName: String!, $fungibleName: String) {
-    lastBlockHeight
-    fungibleAccount(accountName: $accountName, fungibleName: $fungibleName) {
-      transfers(first: 100) {
-        edges {
-          node {
-            ...TransferFields
-            crossChainTransfer {
-              ...TransferFields
-            }
-          }
-        }
-      }
-    }
-  }
+export const TRANSFER_FIELDS_FRAGMENT = graphql(`
   fragment TransferFields on Transfer {
     amount
     chainId
@@ -69,6 +54,50 @@ export const TRANSFER_QUERY = graphql(`
   }
 `);
 
+export const ACCOUNT_TRANSFER_QUERY = graphql(`
+  query accountTransfers($accountName: String!, $fungibleName: String) {
+    lastBlockHeight
+    fungibleAccount(accountName: $accountName, fungibleName: $fungibleName) {
+      transfers(first: 100) {
+        edges {
+          node {
+            ...TransferFields
+            crossChainTransfer {
+              ...TransferFields
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+export const ACCOUNT_CHAIN_TRANSFER_QUERY = graphql(`
+  query accountChainTransfers(
+    $accountName: String!
+    $chainId: String
+    $fungibleName: String
+    $first: Int
+  ) {
+    lastBlockHeight
+    transfers(
+      accountName: $accountName
+      chainId: $chainId
+      fungibleName: $fungibleName
+      first: $first
+    ) {
+      edges {
+        node {
+          ...TransferFields
+          crossChainTransfer {
+            ...TransferFields
+          }
+        }
+      }
+    }
+  }
+`);
+
 export const TRANSFER_REQUESTKEY_QUERY = graphql(`
   query accountTransferRequestKey($requestKey: String!, $accountName: String) {
     lastBlockHeight
@@ -76,58 +105,6 @@ export const TRANSFER_REQUESTKEY_QUERY = graphql(`
       edges {
         node {
           ...TransferFields
-        }
-      }
-    }
-  }
-
-  fragment TransferFields on Transfer {
-    amount
-    chainId
-    orderIndex
-    receiverAccount
-    requestKey
-    senderAccount
-    moduleName
-    block {
-      hash
-      height
-      creationTime
-    }
-    transaction {
-      cmd {
-        networkId
-        payload {
-          __typename
-          ... on ExecutionPayload {
-            code
-            data
-          }
-          ... on ContinuationPayload {
-            step
-            pactId
-          }
-        }
-        signers {
-          clist {
-            name
-            args
-          }
-        }
-      }
-      result {
-        __typename
-        ... on TransactionResult {
-          goodResult
-          badResult
-          events {
-            edges {
-              node {
-                name
-                parameters
-              }
-            }
-          }
         }
       }
     }
