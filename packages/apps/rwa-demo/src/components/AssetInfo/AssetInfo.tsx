a9a510699cae2c78aeb1f4fe8e188a4591ee0689
Steven Straatemans <steven@straatemans.nl>
2024-12-10 09:55:00 +0100

    copy the asset info

--- packages/apps/rwa-demo/src/components/AssetInfo/AssetInfo.tsx
+++ packages/apps/rwa-demo/src/components/AssetInfo/AssetInfo.tsx
@@ -1,15 +1,24 @@
 import { useAsset } from '@/hooks/asset';
-import { MonoPause, MonoPlayArrow } from '@kadena/kode-icons';
+import { env } from '@/utils/env';
+import { MonoPause, MonoPlayArrow, MonoVpnLock } from '@kadena/kode-icons';
 import { Button, Heading, Stack } from '@kadena/kode-ui';
 import type { FC } from 'react';
+import { CopyButton } from '../CopyButton/CopyButton';
 import { SupplyCount } from '../SupplyCount/SupplyCount';
 
 export const AssetInfo: FC = () => {
   const { paused, asset } = useAsset();
+
   if (!asset) return;
   return (
     <Stack width="100%" flexDirection="column">
-      <Heading as="h3">{asset.contractName}</Heading>
+      <Stack width="100%" gap="sm" alignItems="center" marginBlock="md">
+        <MonoVpnLock />
+        <Heading as="h3">{asset.contractName}</Heading>
+        <CopyButton
+          value={`${env.URL}/assets/create/${asset?.namespace}/${asset?.contractName}`}
+        />
+      </Stack>
       <Stack width="100%" alignItems="center" gap="md">
         <Button isDisabled>
           {paused ? (
