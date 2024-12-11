adde949eca384f5030f8a2bc2ee7f987fc58abd6
Steven Straatemans <steven@straatemans.nl>
2024-12-11 13:46:49 +0100

    remove comments

--- packages/apps/rwa-demo/src/services/createContract.ts
+++ packages/apps/rwa-demo/src/services/createContract.ts
@@ -43,15 +43,3 @@ export const createContract = async (
     .setNetworkId(getNetwork().networkId)
     .createTransaction();
 };
-
-// (define-namespace (ns.create-principal-namespace (read-keyset 'keyset))
-//     (read-keyset 'keyset)
-//     (read-keyset 'keyset)
-//   )
-//   (namespace (ns.create-principal-namespace (read-keyset 'keyset)))
-
-//       (let ((keyset-name:string (format "{}.{}" [(ns.create-principal-namespace (read-keyset 'keyset)) 'admin-keyset]) ))
-//     (define-keyset keyset-name (read-keyset 'keyset))
-//     (enforce-keyset keyset-name)
-//     keyset-name
-//   )
