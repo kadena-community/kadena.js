746e8bd24e89b7cd7597bdcbf9b90958995e9bd6
Steven Straatemans <steven@straatemans.nl>
2024-12-17 14:38:15 +0100

    add extra check on deleting an investor. there should be no balance

--- packages/apps/rwa-demo/src/hooks/deleteInvestor.ts
+++ packages/apps/rwa-demo/src/hooks/deleteInvestor.ts
@@ -1,7 +1,9 @@
+import type { IWalletAccount } from '@/components/AccountProvider/AccountType';
 import type { ITransaction } from '@/components/TransactionsProvider/TransactionsProvider';
 import { interpretErrorMessage } from '@/components/TransactionsProvider/TransactionsProvider';
 import type { IDeleteIdentityProps } from '@/services/deleteIdentity';
 import { deleteIdentity } from '@/services/deleteIdentity';
+import { getBalance } from '@/services/getBalance';
 import { getClient } from '@/utils/client';
 import { useNotifications } from '@kadena/kode-ui/patterns';
 import { useEffect, useState } from 'react';
@@ -9,12 +11,24 @@ import { useAccount } from './account';
 import { useAsset } from './asset';
 import { useTransactions } from './transactions';
 
-export const useDeleteInvestor = () => {
-  const { account, sign, accountRoles, isMounted } = useAccount();
+export const useDeleteInvestor = ({
+  investorAccount,
+}: {
+  investorAccount?: string;
+}) => {
+  const {
+    account,
+    sign,
+    accountRoles,
+    isMounted: isAccountMounted,
+  } = useAccount();
   const { paused } = useAsset();
   const { addTransaction } = useTransactions();
   const { addNotification } = useNotifications();
   const [isAllowed, setIsAllowed] = useState(false);
+  const [notAllowedReason, setNotAllowedReason] = useState('');
+  const [balance, setBalance] = useState<number | undefined>(undefined);
+  const [isMounted, setIsMounted] = useState(false);
 
   const submit = async (
     data: IDeleteIdentityProps,
@@ -44,10 +58,42 @@ export const useDeleteInvestor = () => {
     }
   };
 
-  useEffect(() => {
-    if (!isMounted) return;
-    setIsAllowed(!paused && accountRoles.isWhitelistManager());
-  }, [paused, account?.address, isMounted]);
+  const init = async (account: IWalletAccount, investorAccount: string) => {
+    if (!account || !investorAccount || isMounted) return;
+    const res = await getBalance({ investorAccount, account: account! });
 
-  return { submit, isAllowed };
+    if (typeof res === 'number') {
+      setBalance(res);
+    }
+    setIsMounted(true);
+  };
+
+  useEffect(() => {
+    if (!account || !investorAccount) return;
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    init(account, investorAccount);
+  }, [account?.address, investorAccount]);
+
+  useEffect(() => {
+    if (!isAccountMounted || !isMounted) return;
+
+    //if the investorAccount is given, we are checking that we are allowed to remove THIS investor
+    //when there is a balance of tokens on this investor,we are not allowed to remove it
+    if (investorAccount) {
+      const result =
+        !paused && accountRoles.isWhitelistManager() && balance === 0;
+      setIsAllowed(result);
+      if (!result) {
+        setNotAllowedReason(
+          `There is still a balance of ${balance} tokens on this account`,
+        );
+      }
+      return;
+    }
+
+    setIsAllowed(!paused && accountRoles.isWhitelistManager());
+  }, [paused, account?.address, isMounted, balance]);
+
+  return { submit, isAllowed, notAllowedReason };
 };
