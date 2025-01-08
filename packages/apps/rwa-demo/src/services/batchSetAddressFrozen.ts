3d7d20a710596c9ba6a4ed0420d974efce66fe89
Steven <steven@straatemans.nl>
2025-01-08 13:21:08 +0100

    feat(rwa): batch freeze unfreeze investors (#2776)

---
+++ packages/apps/rwa-demo/src/services/batchSetAddressFrozen.ts
@@ -0,0 +1,38 @@
+import type { IWalletAccount } from '@/components/AccountProvider/AccountType';
+import { getNetwork } from '@/utils/client';
+import { getAsset } from '@/utils/getAsset';
+import { getPubkeyFromAccount } from '@/utils/getPubKey';
+import { Pact } from '@kadena/client';
+
+export interface IBatchSetAddressFrozenProps {
+  investorAccounts: string[];
+  pause: boolean;
+}
+
+export const batchSetAddressFrozen = async (
+  data: IBatchSetAddressFrozenProps,
+  account: IWalletAccount,
+) => {
+  return Pact.builder
+    .execution(
+      `(${getAsset()}.batch-set-address-frozen (read-msg 'investors) (read-msg 'pause))
+      `,
+    )
+    .addData('investors', data.investorAccounts)
+    .addData(
+      'pause',
+      data.investorAccounts.map((_) => data.pause),
+    )
+    .addData('agent', account.address)
+    .setMeta({
+      senderAccount: account.address,
+      chainId: getNetwork().chainId,
+    })
+    .addSigner(getPubkeyFromAccount(account), (withCap) => [
+      withCap(`${getAsset()}.ONLY-AGENT`, 'freezer'),
+      withCap(`coin.GAS`),
+    ])
+
+    .setNetworkId(getNetwork().networkId)
+    .createTransaction();
+};
