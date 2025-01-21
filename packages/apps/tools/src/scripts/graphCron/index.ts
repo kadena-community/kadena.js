e49d5389ca2c988161401179808ddb86107cc687
Albert G <516972+alber70g@users.noreply.github.com>
2025-01-21 12:06:20 +0100

    chore(graphCron): use totalCutHeight from Graph by adding up all heights. Add lastBlockHeight to message. Adds types

--- packages/apps/tools/src/scripts/graphCron/index.ts
+++ packages/apps/tools/src/scripts/graphCron/index.ts
@@ -5,23 +5,51 @@ interface IEnvProps {
   env: 'testnet04' | 'mainnet01';
   chainweb: string;
   graphql: string;
+  graphqlRef: string;
 }
 
-const countHeightOnChainweb = async (props: IEnvProps): Promise<number> => {
+interface IChainwebCutResponse {
+  hashes: Record<string, { height: number; hash: string }>;
+  height: number;
+  instance: string;
+  id: string;
+}
+
+const countHeightOnChainweb = async (
+  props: IEnvProps,
+): Promise<{ totalCutHeight: number; lastBlockHeight: number }> => {
   try {
     const result = await fetch(props.chainweb);
-    const data: any = await result.json();
-    return Object.entries(data.hashes)
-      .map(([key, val]) => val)
-      .reduce((acc: number, val: any) => {
-        return acc + val.height;
-      }, 0);
+    const cwCut = (await result.json()) as IChainwebCutResponse;
+
+    return {
+      totalCutHeight: cwCut.height,
+      lastBlockHeight: propsToArray(cwCut.hashes).reduce(
+        (maxHeight, { height }) => Math.max(maxHeight, height),
+        0,
+      ),
+    };
   } catch (e) {
-    return parseInt('no Number');
+    return {
+      totalCutHeight: NaN,
+      lastBlockHeight: NaN,
+    };
   }
 };
 
-const countHeightOnGraph = async (props: IEnvProps): Promise<number> => {
+function propsToArray<T>(props: Record<string, T>) {
+  return Object.keys(props).map((key) => props[key]);
+}
+
+interface ICompletedBlockHeightsResponse {
+  data: {
+    completedBlockHeights: { edges: Array<{ node: { height: number } }> };
+  };
+}
+
+const countHeightOnGraph = async (
+  props: IEnvProps,
+): Promise<{ totalCutHeight: number; lastBlockHeight: number }> => {
   const result = await fetch(props.graphql, {
     method: 'POST',
     headers: {
@@ -35,21 +63,46 @@ const countHeightOnGraph = async (props: IEnvProps): Promise<number> => {
     },
     body: JSON.stringify({
       query: `query graphBlockHeight {
-            lastBlockHeight
+        completedBlockHeights(heightCount: 1) {
+          edges {
+            node {
+              height
+            }
+          }
+        }
       }`,
       variables: {},
       extensions: {},
     }),
   });
 
-  const data = (await result.json()) as any;
-  return data.data.lastBlockHeight * 20;
+  const completedBlockHeightsResult =
+    (await result.json()) as ICompletedBlockHeightsResponse;
+
+  return {
+    totalCutHeight:
+      completedBlockHeightsResult.data.completedBlockHeights.edges.reduce(
+        (acc, { node: { height } }) => acc + height,
+        0,
+      ),
+    lastBlockHeight:
+      completedBlockHeightsResult.data.completedBlockHeights.edges.reduce(
+        (maxHeight, { node: { height } }) => Math.max(maxHeight, height),
+        0,
+      ),
+  };
 };
 
 export const runJobPerEnvironment = async (props: IEnvProps) => {
   try {
-    const totalHeightOnChainWeb = await countHeightOnChainweb(props);
-    const totalHeightOnGraph = await countHeightOnGraph(props);
+    const {
+      totalCutHeight: totalHeightOnChainWeb,
+      lastBlockHeight: lastBlockHeightChainweb,
+    } = await countHeightOnChainweb(props);
+    const {
+      totalCutHeight: totalCutHeightGraph,
+      lastBlockHeight: lastBlockHeightGraph,
+    } = await countHeightOnGraph(props);
 
     if (Number.isNaN(totalHeightOnChainWeb)) {
       await sendErrorMessage({
@@ -58,7 +111,7 @@ export const runJobPerEnvironment = async (props: IEnvProps) => {
       });
       return;
     }
-    if (Number.isNaN(totalHeightOnGraph)) {
+    if (Number.isNaN(totalCutHeightGraph)) {
       await sendErrorMessage({
         title: `${props.env} graph fail`,
         msg: `We were unable to retrieve the blockheights from the test graph. \n There seems to be an issue with test graph (${props.graphql})`,
@@ -67,32 +120,52 @@ export const runJobPerEnvironment = async (props: IEnvProps) => {
     }
 
     if (
-      Math.abs(totalHeightOnChainWeb - totalHeightOnGraph) >
+      Math.abs(totalHeightOnChainWeb - totalCutHeightGraph) >
       MAXBLOCKHEIGHT_DIFFERENCE
     ) {
       await sendErrorMessage({
-        title: `${props.env} graph issue`,
-        msg: `There is a large difference in the last blockheight between ${props.env} chainweb and graph. \n difference: ${Math.abs(totalHeightOnChainWeb - totalHeightOnGraph)}`,
+        title: `${props.env} chainweb and graph blockheight difference`,
+        msg: `There is a large difference in the totalCutHeight between ${props.env} chainweb and graph.
+
+Total cut height difference: ${Math.abs(totalHeightOnChainWeb - totalCutHeightGraph)}
+
+[Chainweb lastBlockHeight](${props.chainweb}): ${lastBlockHeightChainweb}
+[GraphiQL lastBlockHeight](${props.graphqlRef}): ${lastBlockHeightGraph}
+Total height difference: ${lastBlockHeightChainweb - lastBlockHeightGraph}
+`,
       });
     }
   } catch (e) {
     await sendErrorMessage({
       title: `There was a general issue with the ${props.env} graph cron job`,
-      msg: ``,
+      msg: e,
     });
   }
 };
 
-export const runJob = async () => {
-  await runJobPerEnvironment({
-    env: 'testnet04',
-    chainweb: 'https://ap1.testnet.chainweb.com/chainweb/0.0/testnet04/cut',
-    graphql: 'https://graph.testnet.kadena.network/graphql',
-  });
+const testnet04Props = {
+  env: 'testnet04',
+  chainweb: 'https://ap1.testnet.chainweb.com/chainweb/0.0/testnet04/cut',
+  graphql: 'https://graph.testnet.kadena.network/graphql',
+  graphqlRef:
+    'https://graph.testnet.kadena.network/graphql?query=query+graphBlockHeight+%7B%0A++++++++completedBlockHeights%28heightCount%3A+1%29+%7B%0A++++++++++edges+%7B%0A++++++++++++node+%7B%0A++++++++++++++height%0A++++++++++++%7D%0A++++++++++%7D%0A++++++++%7D%0A++++++%7D',
+} as const;
 
-  await runJobPerEnvironment({
-    env: 'mainnet01',
-    chainweb: 'https://api.chainweb.com/chainweb/0.0/mainnet01/cut ',
-    graphql: 'https://graph.kadena.network/graphql',
-  });
+const mainnet01Props = {
+  env: 'mainnet01',
+  chainweb: 'https://api.chainweb.com/chainweb/0.0/mainnet01/cut',
+  graphql: 'https://graph.kadena.network/graphql',
+  graphqlRef:
+    'https://graph.kadena.network/graphql?query=query+graphBlockHeight+%7B%0A++++++++completedBlockHeights%28heightCount%3A+1%29+%7B%0A++++++++++edges+%7B%0A++++++++++++node+%7B%0A++++++++++++++height%0A++++++++++++%7D%0A++++++++++%7D%0A++++++++%7D%0A++++++%7D',
+} as const;
+
+const networksToTest = [testnet04Props, mainnet01Props];
+
+export const runJob = async () => {
+  await Promise.all(networksToTest.map(runJobPerEnvironment));
 };
+
+// To test locally run `bun ./index.tx` or equivalent
+// runJob()
+//   .then(() => console.log('done'))
+//   .catch(console.error);
