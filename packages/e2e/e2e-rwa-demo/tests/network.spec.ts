5f73ddf0aa43572bbefcc86cc4c28df0cc68bab2
Steven Straatemans <steven@straatemans.nl>
2025-01-22 16:51:41 +0100

    cleanup

--- packages/e2e/e2e-rwa-demo/tests/network.spec.ts
+++ packages/e2e/e2e-rwa-demo/tests/network.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@kadena-dev/e2e-base/src/fixtures/shared/test.fixture';
 import { expect } from '@playwright/test';
 
-test('Login', async ({ initiator, RWADemoApp }) => {
+test('Login', async ({ initiator }) => {
   await test.step('Login', async () => {
     await initiator.goto('/');
 
