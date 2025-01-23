45e45b79cbfc56a18ca3aff5c3aa903cbcbe73ee
Steven Straatemans <steven@straatemans.nl>
2025-01-23 17:26:19 +0100

    remove try

--- packages/e2e/e2e-base/src/smart-contracts/rwa/deploy.ts
+++ packages/e2e/e2e-base/src/smart-contracts/rwa/deploy.ts
@@ -4,13 +4,13 @@ import { promisify } from 'util';
 export const deployRWA = async () => {
   const promiseExec = await promisify(exec);
 
-  try {
-    await promiseExec(
-      `git clone git@github.com:kadena-io/RWA-token.git ./.tempimport/RWA-token`,
-    );
-  } catch (e) {
-    await Promise.resolve();
-  }
+  //try {
+  await promiseExec(
+    `git clone git@github.com:kadena-io/RWA-token.git ./.tempimport/RWA-token`,
+  );
+  // } catch (e) {
+  //   await Promise.resolve();
+  // }
 
   const result = await promiseExec(
     `cd ./../e2e-rwa-demo/.tempimport/RWA-token/deploy-mvp && npm i`,
