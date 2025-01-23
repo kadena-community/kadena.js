e7fb4321cc8044050624d12ab20c84e359001eab
Steven Straatemans <steven@straatemans.nl>
2025-01-23 16:49:38 +0100

    still issues

--- packages/e2e/e2e-base/src/smart-contracts/rwa/deploy.ts
+++ packages/e2e/e2e-base/src/smart-contracts/rwa/deploy.ts
@@ -1,22 +1,22 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
+// import { exec } from 'child_process';
+// import { promisify } from 'util';
 
 export const deployRWA = async () => {
-  const promiseExec = await promisify(exec);
-  const TEMP_DIR = './.tempimport';
+  // const promiseExec = await promisify(exec);
+  // const TEMP_DIR = './.tempimport';
 
-  try {
-    await promiseExec(
-      `git clone git@github.com:kadena-io/RWA-token.git ${TEMP_DIR}/RWA-token`,
-    );
-  } catch (e) {
-    await Promise.resolve();
-  }
+  // try {
+  //   await promiseExec(
+  //     `git clone git@github.com:kadena-io/RWA-token.git ${TEMP_DIR}/RWA-token`,
+  //   );
+  // } catch (e) {
+  //   await Promise.resolve();
+  // }
 
-  const result = await promiseExec(
-    `cd ${__dirname}/../../../../e2e-rwa-demo/${TEMP_DIR}/RWA-token/deploy-mvp && npm i`,
-  );
+  // const result = await promiseExec(
+  //   `cd ${__dirname}/../../../../e2e-rwa-demo/${TEMP_DIR}/RWA-token/deploy-mvp && npm i`,
+  // );
 
-  console.log({ result });
+  // console.log({ result });
   return true;
 };
