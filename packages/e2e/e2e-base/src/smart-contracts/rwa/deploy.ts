d00b4c3ada608d7b5c1295fd294e889a2ecbbb9c
Steven Straatemans <steven@straatemans.nl>
2025-01-23 17:18:30 +0100

    fix the install of test

--- packages/e2e/e2e-base/src/smart-contracts/rwa/deploy.ts
+++ packages/e2e/e2e-base/src/smart-contracts/rwa/deploy.ts
@@ -3,18 +3,17 @@ import { promisify } from 'util';
 
 export const deployRWA = async () => {
   const promiseExec = await promisify(exec);
-  const TEMP_DIR = './.tempimport';
 
   try {
     await promiseExec(
-      `git clone git@github.com:kadena-io/RWA-token.git ${TEMP_DIR}/RWA-token`,
+      `git clone git@github.com:kadena-io/RWA-token.git ./.tempimport/RWA-token`,
     );
   } catch (e) {
     await Promise.resolve();
   }
 
   const result = await promiseExec(
-    `cd ${__dirname}/../../../../e2e-rwa-demo/${TEMP_DIR}/RWA-token/deploy-mvp && npm i`,
+    `cd ./../e2e-rwa-demo/.tempimport/RWA-token/deploy-mvp && npm i`,
   );
 
   console.log({ result });
