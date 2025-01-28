744d2f6d5857a465dedb48ac54a7247cf40118b4
Steven Straatemans <steven@straatemans.nl>
2025-01-28 14:14:36 +0100

    start

--- packages/e2e/e2e-base/src/page-objects/rwa-demo/RWADemoApp.index.ts
+++ packages/e2e/e2e-base/src/page-objects/rwa-demo/RWADemoApp.index.ts
@@ -1,3 +1,17 @@
+import { WebAuthNHelper } from '../../helpers/chainweaver/webauthn.helper';
+import { expect  } from '@playwright/test';
+import type {Page} from '@playwright/test';
+
 export class RWADemoAppIndex {
+  private _webAuthNHelper: WebAuthNHelper = new WebAuthNHelper();
+  private _PROFILENAME: string = 'He-man';
   public constructor() {}
+
+  public async login(actor: Page): Promise<boolean> {
+    await expect(actor.getByRole('heading' {
+      name: 'Login',
+    })).toBeVisible();
+
+    return true;
+  }
 }
--- packages/e2e/e2e-rwa-demo/tests/network.spec.ts
+++ packages/e2e/e2e-rwa-demo/tests/network.spec.ts
@@ -1,9 +1,11 @@
 import { test } from '@kadena-dev/e2e-base/src/fixtures/shared/test.fixture';
 import { expect } from '@playwright/test';
 
-test('Login', async ({ initiator }) => {
+test('Login', async ({ initiator, RWADemoApp }) => {
   await test.step('Login', async () => {
     await initiator.goto('/');
+
+    RWADemoApp.login(initiator);
 
     await expect(true).toEqual(true);
   });
