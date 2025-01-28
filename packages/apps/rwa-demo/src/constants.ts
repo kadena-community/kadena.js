6e3d937e3d93d84300265d8c06c55b2a1b2ee335
Steven Straatemans <steven@straatemans.nl>
2025-01-28 11:34:56 +0100

    aliases are now in localstorage instead of database

--- packages/apps/rwa-demo/src/constants.ts
+++ packages/apps/rwa-demo/src/constants.ts
@@ -1,6 +1,7 @@
 export const ACCOUNT_COOKIE_NAME = 'rwa';
 export const LOCALSTORAGE_ASSETS_KEY = 'assets';
 export const LOCALSTORAGE_ASSETS_SELECTED_KEY = 'selected_asset';
+export const LOCALSTORAGE_ACCOUNTS = 'accounts';
 export const INFINITE_COMPLIANCE = -1;
 export const NETWORK_POLLING_RATE = 1000 * 30; /* 30 sec */
 
