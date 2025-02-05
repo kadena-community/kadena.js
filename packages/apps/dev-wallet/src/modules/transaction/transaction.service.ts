afb393f7524dd75e1584696337e9c3007bc49fc3
Javad Khalilian <khalilian.javad@gmail.com>
2025-02-05 19:50:09 +0100

    doc(dw): inline doc

--- packages/apps/dev-wallet/src/modules/transaction/transaction.service.ts
+++ packages/apps/dev-wallet/src/modules/transaction/transaction.service.ts
@@ -251,6 +251,7 @@ function isContinuationDone(
   if (result.result.status === 'failure' && result.result.error) {
     const error = result.result.error;
     if (error && 'message' in error && typeof error.message === 'string') {
+      // TODO: this code is very fragile and should be replaced with a more robust solution
       return (
         // pact4
         error.message.includes('pact completed') ||
