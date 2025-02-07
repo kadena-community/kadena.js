07bc3fb99f19e6a692e5319200c3c30e222a5bb9
Albert G <516972+alber70g@users.noreply.github.com>
2025-02-07 15:54:15 +0100

    chore(client-utils): reduce tests timeout to 15s instead of 150s

--- packages/libs/client-utils/src/integration-tests/marmalade-conventional-auction.int.test.ts
+++ packages/libs/client-utils/src/integration-tests/marmalade-conventional-auction.int.test.ts
@@ -1,5 +1,5 @@
-import type { ChainId } from '@kadena/client';
-import { createSignWithKeypair } from '@kadena/client';
+import type { ChainId, PactErrorCode } from '@kadena/client';
+import { createSignWithKeypair, getPactErrorCode } from '@kadena/client';
 import { PactNumber } from '@kadena/pactjs';
 import type { IPactInt } from '@kadena/types';
 import { describe, expect, it } from 'vitest';
@@ -325,9 +325,9 @@ describe('offerToken - with auction data', () => {
       saleConfig,
     );
 
-    await expect(() => task.execute()).rejects.toThrowError(
-      new Error('with-read: row not found: non-existing-token'),
-    );
+    const res = await task.execute().catch((err) => getPactErrorCode(err));
+
+    expect(res).toBe('RECORD_NOT_FOUND' as PactErrorCode);
   });
 });
 
@@ -868,7 +868,11 @@ describe('buyToken', () => {
           }
         }),
       )
-      .execute();
+      .execute()
+      .catch((err) => {
+        console.error(JSON.stringify(err, null, 2));
+        return false;
+      });
 
     expect(result).toBe(true);
   });
