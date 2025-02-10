76994afb4f3bd936006f0690be291b29a9e97fe2
Javad Khalilian <khalilian.javad@gmail.com>
2025-02-10 14:26:19 +0100

    chore(client): api

--- packages/libs/client/src/utils/type-guards.ts
+++ packages/libs/client/src/utils/type-guards.ts
@@ -1,15 +1,30 @@
-import { IPactDecimal, IPactInt } from "@kadena/types";
+import type { IPactDecimal, IPactInt } from '@kadena/types';
 
 export const isObject = (obj: unknown): obj is Record<string, unknown> => {
-  return typeof obj === "object" && obj !== null;
-}
+  return typeof obj === 'object' && obj !== null;
+};
 
-export const hasField = <T extends string>(obj: Record<string, unknown>, field: T): obj is Record<T, unknown> => {
+export const hasField = <T extends string>(
+  obj: Record<string, unknown>,
+  field: T,
+): obj is Record<T, unknown> => {
   return field in obj;
-}
+};
 
+/**
+ *
+ * @param arg
+ * @returns boolean
+ * @public
+ */
 export const isIPactDecimal = (arg: unknown): arg is IPactDecimal =>
-  isObject(arg) && hasField(arg, "decimal") && typeof arg.decimal === "string";
+  isObject(arg) && hasField(arg, 'decimal') && typeof arg.decimal === 'string';
 
+/**
+ *
+ * @param arg
+ * @returns boolean
+ * @public
+ */
 export const isIPactInt = (arg: unknown): arg is IPactInt =>
-  isObject(arg) && hasField(arg, "int") && typeof arg.int === "string";
+  isObject(arg) && hasField(arg, 'int') && typeof arg.int === 'string';
