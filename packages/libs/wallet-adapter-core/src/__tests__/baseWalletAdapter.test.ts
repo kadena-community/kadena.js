1c7e060a1dbf33e4f479a098d308093718ae883e
Bart Huijgen <barthuijgen@users.noreply.github.com>
2025-04-17 10:43:31 +0200

    test: add initial test for wallet adapter core

---
+++ packages/libs/wallet-adapter-core/src/__tests__/baseWalletAdapter.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it, vi } from 'vitest';
+import { BaseWalletAdapter } from '../BaseWalletAdapter';
+
+const mockProvider = {
+  request: vi.fn(),
+  on: vi.fn(),
+  off: vi.fn(),
+};
+
+class MockAdapter extends BaseWalletAdapter {
+  public name: string = 'MockAdapter';
+  public constructor() {
+    super({ provider: mockProvider });
+  }
+}
+
+const adapter = new MockAdapter();
+
+describe('BaseWalletAdapter', () => {
+  it('Request calls provider request method correctly', async () => {
+    mockProvider.request.mockResolvedValueOnce({
+      id: 1,
+      jsonrpc: '2.0',
+      result: { foo: 'bar' },
+    });
+    const response = await adapter.request({
+      method: 'test',
+      params: {},
+    });
+    expect(mockProvider.request).toHaveBeenCalledWith({
+      method: 'test',
+      params: {},
+    });
+    expect(response).toEqual({
+      id: 1,
+      jsonrpc: '2.0',
+      result: { foo: 'bar' },
+    });
+  });
+});
