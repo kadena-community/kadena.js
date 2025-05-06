9a403b8a34baca8178e5ed7d7731226c8ef5e290
Steven Straatemans <steven@straatemans.nl>
2025-05-06 11:44:17 +0200

    add console.log

--- packages/apps/tools/src/app/api/galxe/route.route.ts
+++ packages/apps/tools/src/app/api/galxe/route.route.ts
@@ -1,6 +1,7 @@
 import { runJob } from '@/scripts/galxe';
 
 export async function GET(request: Request) {
+  console.log(`galxe balance check start`);
   await runJob();
   return new Response(`galxe balance check`);
 }
