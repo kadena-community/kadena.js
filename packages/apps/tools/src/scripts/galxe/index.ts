0e86052dd286555d1c97bb69d9add3f1d5432c36
Steven Straatemans <steven@straatemans.nl>
2025-05-06 11:49:10 +0200

    testing steps

--- packages/apps/tools/src/scripts/galxe/index.ts
+++ packages/apps/tools/src/scripts/galxe/index.ts
@@ -8,6 +8,7 @@ import { sendErrorMessage, sendMessage } from './messages';
 const checkMessages = async (network: INETWORK) => {
   const accountResult = await fetchAccount(network, GalxeAccount);
 
+  console.log({ accountResult });
   if (accountResult?.errors?.length) {
     await sendErrorMessage(network);
     return;
@@ -17,12 +18,14 @@ const checkMessages = async (network: INETWORK) => {
     (chain: any) => chain.chainId === '6',
   );
 
+  console.log({ chain6 });
   if (!chain6) {
     return;
   }
 
   const lowBalanceChainsResult = lowBalanceChains([chain6], MINXGALXEBALANCE);
 
+  console.log({ lowBalanceChainsResult });
   if (!lowBalanceChainsResult.length) {
     return;
   }
@@ -34,6 +37,7 @@ export const runJob = async () => {
   console.log({ GalxeAccount, MINXGALXEBALANCE });
 
   const mainnet = getMainNet();
+  console.log({ mainnet });
   if (!mainnet) return;
 
   await checkMessages(mainnet);
