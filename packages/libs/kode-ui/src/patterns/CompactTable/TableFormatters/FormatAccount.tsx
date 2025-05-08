00059ed94d667f67383b4b3c1caa450294078aea
Steven Straatemans <steven@straatemans.nl>
2025-05-08 10:42:00 +0200

    fix edit contact form bug

--- packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatAccount.tsx
+++ packages/libs/kode-ui/src/patterns/CompactTable/TableFormatters/FormatAccount.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import type { MaskOptions } from './../../../components';
 import { maskValue, Text } from './../../../components';
 import type { ICompactTableFormatterProps } from './types';
 import { valueToString } from './utils';
 
-export const FormatAccount = () => {
+export const FormatAccount = (options?: Partial<MaskOptions>) => {
   const Component = ({ value }: ICompactTableFormatterProps) => {
     if (!value) return null;
-    return <Text variant="code">{maskValue(valueToString(value))}</Text>;
+    return (
+      <Text variant="code">{maskValue(valueToString(value), options)}</Text>
+    );
   };
 
   return Component;
