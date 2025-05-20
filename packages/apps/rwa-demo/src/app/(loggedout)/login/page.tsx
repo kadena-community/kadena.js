6be5f94f1a84cf70660309b8ff831279dfc773ab
Steven Straatemans <steven@straatemans.nl>
2025-05-20 09:44:18 +0200

    show user info

--- packages/apps/rwa-demo/src/app/(loggedout)/login/page.tsx
+++ packages/apps/rwa-demo/src/app/(loggedout)/login/page.tsx
@@ -5,12 +5,14 @@ import { CardContentBlock } from '@kadena/kode-ui/patterns';
 import { cardWrapperClass } from '../style.css';
 
 const Home = () => {
-  const { signIn } = useUser();
+  const { signIn, user } = useUser();
 
   return (
     <Card fullWidth className={cardWrapperClass}>
       <CardContentBlock title="Login">
         <Button onPress={signIn}>sign in with google account</Button>
+
+        <pre>{JSON.stringify(user, null, 2)}</pre>
       </CardContentBlock>
     </Card>
   );
