eda53d0d82e88d23498f177d54fd9bfe30c8dd19
Steven <steven@straatemans.nl>
2025-05-30 11:04:47 +0200

    chore(tools): elastic search documents for alerts - seperate document per chain balance (#2993)

--- packages/apps/tools/src/app/api/alerts/utils/alerts/slack/balanceChangeAlert.ts
+++ packages/apps/tools/src/app/api/alerts/utils/alerts/slack/balanceChangeAlert.ts
@@ -2,34 +2,29 @@ import { differenceInMinutes } from 'date-fns';
 import { getClient } from '../../elasticClient';
 import { sendBalanceChangeErrorMessages } from '../../messages/balanceChange/sendBalanceChangeErrorMessages';
 import { sendBalanceChangeMessages } from '../../messages/balanceChange/sendBalanceChangeMessages';
-import type { IAlert, IChainAccount } from './../../constants';
+import type { IAlert } from './../../constants';
 
 export const balanceChangeAlert = async (alert: IAlert): Promise<string[]> => {
   const promises = alert.networks.map(async (network) => {
     const client = getClient();
 
-    const [latest, previous] = await client.getLastRecord(alert);
+    const [latest, previous] = await client.getLastRecord(alert, network);
 
     const latestTimeDiff = differenceInMinutes(
       Date.now(),
-      latest._source.timestamp,
+      new Date(latest._source['@timestamp']),
     );
     if (latestTimeDiff > 20) {
       return sendBalanceChangeErrorMessages(alert, network);
     }
 
-    //check balance for chain 2.
-    const latestChain2 = latest._source.balances.find(
-      (balance: any) => balance.chainId === '2',
-    ) as IChainAccount;
-    const previousChain2 = previous._source.balances.find(
-      (balance: any) => balance.chainId === '2',
-    ) as IChainAccount;
+    const latestChain2Balance = latest._source.balance;
+    const previousChain2Balance = previous._source.balance;
 
-    if (latestChain2.balance !== previousChain2.balance) {
+    if (latestChain2Balance !== previousChain2Balance) {
       return sendBalanceChangeMessages(
         alert,
-        [latestChain2, previousChain2],
+        [latestChain2Balance, previousChain2Balance],
         network,
         [latest, previous],
       );
