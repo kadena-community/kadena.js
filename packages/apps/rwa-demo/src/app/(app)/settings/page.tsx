3cb79e83cc63d360209ed2d16385c51980153f5a
Steven Straatemans <steven@straatemans.nl>
2025-06-04 10:34:45 +0200

    change password option in settings

--- packages/apps/rwa-demo/src/app/(app)/settings/page.tsx
+++ packages/apps/rwa-demo/src/app/(app)/settings/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 import { AssetsList } from '@/components/AssetsList/AssetsList';
+import { ChangePasswordForm } from '@/components/Forms/ChangePasswordForm/ChangePasswordForm';
 import { Profile } from '@/components/Profile/Profile';
 import { SideBarBreadcrumbs } from '@/components/SideBarBreadcrumbs/SideBarBreadcrumbs';
 import { WalletsList } from '@/components/WalletsList/WalletsList';
 import { Link, Stack } from '@kadena/kode-ui';
-import { SideBarBreadcrumbsItem } from '@kadena/kode-ui/patterns';
+import {
+  SectionCard,
+  SectionCardBody,
+  SectionCardContentBlock,
+  SectionCardHeader,
+  SideBarBreadcrumbsItem,
+} from '@kadena/kode-ui/patterns';
 
 const Home = () => {
   return (
@@ -17,6 +24,14 @@ const Home = () => {
 
       <Stack flexDirection="column" width="100%" gap="md">
         <Profile />
+        <SectionCard stack="vertical">
+          <SectionCardContentBlock>
+            <SectionCardHeader title="Change password" />
+            <SectionCardBody>
+              <ChangePasswordForm />
+            </SectionCardBody>
+          </SectionCardContentBlock>
+        </SectionCard>
         <WalletsList />
 
         <AssetsList />
