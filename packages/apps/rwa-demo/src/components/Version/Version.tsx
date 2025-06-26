c3a72f0e21cdb39dccafa99bb29acd0fc28f8e90
Steven Straatemans <steven@straatemans.nl>
2025-06-26 09:56:50 +0200

    add sha to localstorage as well

--- packages/apps/rwa-demo/src/components/Version/Version.tsx
+++ packages/apps/rwa-demo/src/components/Version/Version.tsx
@@ -4,14 +4,16 @@ import { useEffect, useRef } from 'react';
 export const Version: FC = () => {
   const ref = useRef<HTMLSpanElement>(null);
   useEffect(() => {
+    const commitSha = process.env.NEXT_PUBLIC_COMMIT_SHA || 'unknown';
     const commentStr = `
-release sha: ${process.env.NEXT_PUBLIC_COMMIT_SHA || 'unknown'}                      
+release sha: ${commitSha}                      
     `;
 
     const comment = document.createComment(commentStr);
     if (ref.current && ref.current.parentNode) {
       ref.current.parentNode.insertBefore(comment, ref.current);
     }
+    localStorage.setItem('version', commitSha);
   }, []);
   return (
     <>
