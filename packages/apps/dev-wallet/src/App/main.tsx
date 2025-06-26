7c0cc5c5ef782eaba14c8e9bdea6f6cd63402ee3
Steven Straatemans <steven@straatemans.nl>
2025-06-26 13:21:21 +0200

    fix the sha variable in dev-wallet

--- packages/apps/dev-wallet/src/App/main.tsx
+++ packages/apps/dev-wallet/src/App/main.tsx
@@ -8,9 +8,9 @@ export function renderApp() {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <Version
-        sha={import.meta.env.VERCEL_GIT_COMMIT_SHA}
+        sha={import.meta.env.VITE_VERCEL_GIT_COMMIT_SHA}
         SSRTime={import.meta.env.VITE_BUILD_TIME}
-        repo={`https://github.com/kadena-community/kadena.js/tree/${import.meta.env.VERCEL_GIT_COMMIT_SHA}/packages/apps/dev-wallet`}
+        repo={`https://github.com/kadena-community/kadena.js/tree/${import.meta.env.VITE_VERCEL_GIT_COMMIT_SHA}/packages/apps/dev-wallet`}
       />
       <App />
     </React.StrictMode>,
