4945ee185c8b9e19ce734b49083df7e0178b58c6
Bart <45095973+barthuijgen@users.noreply.github.com>
2025-08-07 19:02:12 +0200

    fix(wallet-adapter): format wallet adapter packages (#3070)
    Co-authored-by: Bart Huijgen <barthuijgen@users.noreply.github.com>

--- packages/libs/wallet-adapter-core/vitest.config.ts
+++ packages/libs/wallet-adapter-core/vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: "node",
-    include: ["src/__tests__/**/*.{test,spec}.{ts,tsx}"],
+    environment: 'node',
+    include: ['src/__tests__/**/*.{test,spec}.{ts,tsx}'],
   },
 });
