22aef7a024c6a7fbd231892ebb54939c6c3f6ab6
Isa Ozler <isaozler@gmail.com>
2025-08-18 12:55:02 +0200

    docs specificity update for global styles on codeblocks

--- packages/libs/kode-ui/.storybook/global.css.ts
+++ packages/libs/kode-ui/.storybook/global.css.ts
@@ -1,10 +1,13 @@
 import { globalStyle, vars } from '../src/styles';
 
-globalStyle('pre.prismjs > div, pre.prismjs > div span, code', {
-  overflowWrap: 'break-word',
-  fontFamily: `${vars.fonts.$mono} !important`,
-  fontWeight: vars.fontWeights.$medium,
-});
+globalStyle(
+  '#storybook-docs pre.prismjs, #storybook-docs pre.prismjs > div, #storybook-docs pre.prismjs > div span, #storybook-docs code',
+  {
+    overflowWrap: 'break-word',
+    fontFamily: `${vars.fonts.$mono} !important`,
+    fontWeight: vars.fontWeights.$medium,
+  },
+);
 
 globalStyle('code', {
   fontWeight: vars.fontWeights.$semiBold,
