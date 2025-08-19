7f84d2ab922e07d9cc037a8a8e60d99e1fffbce9
Steven <steven@straatemans.nl>
2025-08-19 16:37:20 +0200

    fix(wallet): the migrate account page (#3081)

--- packages/libs/kode-ui/src/patterns/FocussedLayout/components/LayoutProvider/FocussedLayoutProvider.tsx
+++ packages/libs/kode-ui/src/patterns/FocussedLayout/components/LayoutProvider/FocussedLayoutProvider.tsx
@@ -17,7 +17,7 @@ export const useLayout = (): ILayoutContext => {
   const context = useContext(LayoutContext);
   if (!context) {
     throw new Error(
-      'useOrganisation must be used within a LayoutContextProvider',
+      'useLayout must be used within a FocussedLayoutContextProvider',
     );
   }
   return context;
