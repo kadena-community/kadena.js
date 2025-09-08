17ea2463918f1fa1e42a03285e811735861323b1
Steven Straatemans <steven@straatemans.nl>
2025-09-08 15:47:10 +0200

    cleanup

--- packages/apps/explorer/src/pages/[networkSlug]/transaction/[requestKey].tsx
+++ packages/apps/explorer/src/pages/[networkSlug]/transaction/[requestKey].tsx
@@ -70,10 +70,6 @@ const Transaction: React.FC = () => {
     }
   }, [loading, data, error, setIsLoading]);
 
-  useEffect(() => {
-    throw new Error('An unexpected error occurred in Transaction component');
-  }, []);
-
   return (
     <Layout>
       {innerData && innerData.transaction && !error ? (
