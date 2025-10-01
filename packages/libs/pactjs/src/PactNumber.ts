f74cb88803c4dc93c2af3ffaaeb1f32093e7be19
Javad Khalilian <khalilian.javad@gmail.com>
2025-10-01 12:42:21 +0200

    fix: pactjs types

--- packages/libs/pactjs/src/PactNumber.ts
+++ packages/libs/pactjs/src/PactNumber.ts
@@ -1,4 +1,4 @@
-/// <reference path="./extend-bignumber.ts" />
+import type { IPactDecimal, IPactInt } from '@kadena/types';
 import BigNumber from 'bignumber.js';
 
 // Configure BigNumber to prevent exponential notation (Scientific notation) for numbers.
@@ -6,24 +6,43 @@ import BigNumber from 'bignumber.js';
 // It is highly unlikely that someone would pass a number with more than 1,000,000,000 digits.
 BigNumber.config({ EXPONENTIAL_AT: [-1e9, 1e9] });
 
+interface IExtendedBigNumber {
+    toInteger(): string;
+    toStringifiedInteger(): string;
+    toPactInteger(): IPactInt;
+    toDecimal(): string;
+    toStringifiedDecimal(): string;
+    toPactDecimal(): IPactDecimal;
+  }
+
+
+type OverrideMethods<T> = {
+  [K in keyof T]: T[K] extends (...args: infer A) => BigNumber
+  ? (...args: A) => PactNumber
+  : T[K] extends BigNumber
+  ? PactNumber
+  : T[K];
+};
+
+const ExtendedBigNumber:  (new (...args: unknown[]) => OverrideMethods<typeof BigNumber & IExtendedBigNumber>) = BigNumber as any
+
 // In order to extend BigNumber methods correctly, I had to add the PactNumber methods to
 // the prototype of BigNumber. Then, something like this works:
 // const decimal = new PactNumber('0.9').plus("1").toPactDecimal()
 // The reason is that plus (and the other methods) return a new instance of BigNumber, not PactNumber.
 // By adding the PactNumber methods to the prototype of BigNumber, this will work as expected.
-
-BigNumber.prototype.toInteger = function toInteger() {
+ExtendedBigNumber.prototype.toInteger = function toInteger() {
   if (!this.isInteger()) {
     throw new Error('PactNumber is not an integer');
   }
   return this.toString();
 };
 
-BigNumber.prototype.toStringifiedInteger = function toStringifiedInteger() {
+ExtendedBigNumber.prototype.toStringifiedInteger = function toStringifiedInteger() {
   return JSON.stringify(this.toInteger());
 };
 
-BigNumber.prototype.toPactInteger = function toPactInteger() {
+ExtendedBigNumber.prototype.toPactInteger = function toPactInteger() {
   if (!this.isInteger()) {
     throw new Error('PactNumber is not an integer');
   }
@@ -32,29 +51,31 @@ BigNumber.prototype.toPactInteger = function toPactInteger() {
   };
 };
 
-BigNumber.prototype.toDecimal = function toDecimal() {
+ExtendedBigNumber.prototype.toDecimal = function toDecimal() {
   if (this.isInteger()) {
     return `${this.toString()}.0`;
   }
   return `${this.toString()}`;
 };
 
-BigNumber.prototype.toStringifiedDecimal = function toStringifiedDecimal() {
+ExtendedBigNumber.prototype.toStringifiedDecimal = function toStringifiedDecimal() {
   return JSON.stringify(this.toDecimal());
 };
 
-BigNumber.prototype.toPactDecimal = function toPactDecimal() {
+ExtendedBigNumber.prototype.toPactDecimal = function toPactDecimal() {
   return {
     decimal: this.toDecimal(),
   };
 };
+
+
 
 /**
  * Constructs a bignumber.js instance and formats into Pact number formats.
  *
  * @alpha
  */
-export class PactNumber extends BigNumber {
+export class PactNumber extends ExtendedBigNumber {
   public constructor(
     value: string | number | { int: string } | { decimal: string },
   ) {
@@ -73,4 +94,8 @@ export class PactNumber extends BigNumber {
     super(num);
     if (isNaN(Number(num))) throw new Error('Value is NaN');
   }
+
 }
+
+
+
