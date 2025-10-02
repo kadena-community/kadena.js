82ea881c48234a1c7cddcba6c59ed73c3a251be5
Steven Straatemans <steven@straatemans.nl>
2025-10-02 14:55:36 +0200

    remove not needed test

--- packages/apps/e2e-overview/src/dockerplaywright/tests/_example.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('example test', async ({ page }) => {
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'screenshots/example1111.png' });
-  await page.screenshot({ path: 'screenshots/example1.png' });
-  expect(await page.title()).toBe('Example Domain');
-});
