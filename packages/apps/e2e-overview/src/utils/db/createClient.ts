eba64442b668d750b4b234175ba51532c944df8d
Steven Straatemans <steven@straatemans.nl>
2025-10-02 14:08:42 +0200

    fix build

--- packages/apps/e2e-overview/src/utils/db/createClient.ts
+++ packages/apps/e2e-overview/src/utils/db/createClient.ts
@@ -1,8 +1,9 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { env } from '../env';
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    env.SUPABASE_PROJECT_URL,
+    env.SUPABASE_PUBLISHABLE_KEY,
   );
 }
 
--- packages/apps/e2e-overview/src/utils/db/createServerClient.ts
+++ packages/apps/e2e-overview/src/utils/db/createServerClient.ts
@@ -1,29 +1,26 @@
 import { createServerClient as createClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { env } from '../env';
 export const createServerClient = async () => {
   const cookieStore = await cookies();
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
+  return createClient(env.SUPABASE_PROJECT_URL, env.SUPABASE_PUBLISHABLE_KEY, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options),
+          );
+        } catch {
+          // The `setAll` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
       },
     },
-  );
+  });
 };
 
 export const supabase = createClient;
