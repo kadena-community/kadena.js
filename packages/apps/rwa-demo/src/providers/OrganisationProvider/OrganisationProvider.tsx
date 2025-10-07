3fdcbd7a4ab6b647637c04afba8b201b41aaf01b
Steven <steven@straatemans.nl>
2025-10-07 13:13:07 +0200

    fix(rwa): check on the origin (#3116)

--- packages/apps/rwa-demo/src/providers/OrganisationProvider/OrganisationProvider.tsx
+++ packages/apps/rwa-demo/src/providers/OrganisationProvider/OrganisationProvider.tsx
@@ -3,23 +3,25 @@
 import type { IOrganisation } from '@/contexts/OrganisationContext/OrganisationContext';
 import { OrganisationContext } from '@/contexts/OrganisationContext/OrganisationContext';
 import { OrganisationStore } from '@/utils/store/organisationStore';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import type { FC, PropsWithChildren } from 'react';
 import { useEffect, useState } from 'react';
 
 export const OrganisationProvider: FC<PropsWithChildren> = ({ children }) => {
   const [organisation, setOrganisation] = useState<IOrganisation | undefined>();
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     const init = async () => {
       const orgStore = await OrganisationStore();
-
       //if there is no organisation found,
       //it is not allowed to be on this domain
       if (!orgStore) {
         console.log('no organisation found for this domain');
-        router.push('/404');
+        if (pathname !== '/404') {
+          router.push('/404');
+        }
         return;
       }
       const data = await orgStore.getCurrentOrganisation();
